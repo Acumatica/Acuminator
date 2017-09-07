14f821cb7c0278b55d249cd49167d7004e4bad87
Sergey Nikomarov <snikomarov@acumatica.com>
2017-09-07 17:03:58 +0300

    master: dirty coloriser

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/Utils/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PX.Analyzers
+{
+	public static class Constants
+	{
+		public const string Priority = Microsoft.VisualStudio.Text.Classification.Priority.High;
+
+		public const String DacFormat = "DACName";
+		public const String DacFieldFormat = "DACFieldName";
+		//public const String ExtensionMethodFormat = "s.roslyn.extension-method";
+	}
+
+	public static class Labels
+	{
+		public const String DacFormatLabel  = "DAC Format";
+		public const String DacFieldFormatLabel = "DAC Field Format";
+	}
+}
---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/Utils/Extensions.cs
@@ -0,0 +1,24 @@
+﻿//using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
+using System;
+
+namespace PX.Analyzers.Coloriser
+{
+	//public static class Extensions
+	//{
+	//	public static ITagSpan<IClassificationTag> ToTagSpan(this TextSpan span, ITextSnapshot snapshot, IClassificationType classificationType)
+	//	{
+	//		return new TagSpan<IClassificationTag>(
+	//		  new SnapshotSpan(snapshot, span.Start, span.Length),
+	//		  new ClassificationTag(classificationType));
+	//	}
+
+
+	//	public static String GetText(this ITextSnapshot snapshot, TextSpan span)
+	//	{
+	//		return snapshot.GetText(new Span(span.Start, span.Length));
+	//	}
+	//}
+}
