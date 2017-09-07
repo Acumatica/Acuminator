a85451cfa5fefbb32e4efcf3e5ee4a6026df0df4
Ilyas Tazitdinov <itazitdinov@acumatica.com>
2017-09-07 15:03:37 +0300

    missing list attr tests

---
+++ PX.Analyzers/PX.Analyzers.Test/Sources/MissingTypeListAttributeBad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Analyzers.Test.Sources
+{
+    public class BadDac : IBqlTable
+    {
+        public abstract class someField { }
+        [PXStringList(new[] { "O", "N" }, new[] { "Open", "New" })]
+        public string SomeField { get; set; }
+    }
+}
