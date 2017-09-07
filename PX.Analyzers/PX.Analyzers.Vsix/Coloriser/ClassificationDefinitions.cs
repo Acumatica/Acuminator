dc2360a8029cb2157bb8abc3c061b2c09ab273a1
Sergey Nikomarov <snikomarov@acumatica.com>
2017-09-07 17:03:58 +0300

    master: dirty coloriser

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/ClassificationDefinitions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace PX.Analyzers.Coloriser
+{
+	internal static class ClassificationTypes
+	{
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name(Constants.DacFormat)]
+		internal static ClassificationTypeDefinition DACType = null;
+
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name(Constants.DacFieldFormat)]
+		internal static ClassificationTypeDefinition DACFieldType = null;
+
+		//[Export(typeof(ClassificationTypeDefinition))]
+		//[Name(Constants.ExtensionMethodFormat)]
+		//internal static ClassificationTypeDefinition ExtensionMethodType = null;
+	}
+}
---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/EditorFormats.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.Windows;
+
+namespace PX.Analyzers.Coloriser
+{
+	[Export(typeof(EditorFormatDefinition))]
+	[ClassificationType(ClassificationTypeNames = Constants.DacFormat)]
+	[Name(Constants.DacFormat)]
+	[UserVisible(true)]
+	[Order(After = Constants.Priority)]
+	internal sealed class DACFormat : ClassificationFormatDefinition
+	{
+		public DACFormat()
+		{
+			DisplayName = Labels.DacFormatLabel;
+			ForegroundColor = Colors.BurlyWood;
+			BackgroundColor = Colors.White;
+			DisplayName = Labels.DacFormatLabel; // Human readable version of the name		
+			ForegroundCustomizable = true;			
+		}
+	}
+
+	[Export(typeof(EditorFormatDefinition))]
+	[ClassificationType(ClassificationTypeNames = Constants.DacFieldFormat)]
+	[Name(Constants.DacFieldFormat)]
+	[UserVisible(true)]
+	[Order(After = Priority.Default)]
+	internal sealed class DACFieldFormat : ClassificationFormatDefinition
+	{
+		public DACFieldFormat()
+		{
+			DisplayName = Labels.DacFieldFormatLabel;
+			ForegroundColor = Colors.DarkGreen;
+			BackgroundColor = Colors.White;
+			DisplayName = Labels.DacFieldFormatLabel; // Human readable version of the name		
+			ForegroundCustomizable = true;
+		}
+	}
+
+	//[Export(typeof(EditorFormatDefinition))]
+	//[ClassificationType(ClassificationTypeNames = Constants.ExtensionMethodFormat)]
+	//[Name(Constants.ExtensionMethodFormat)]
+	//[UserVisible(true)]
+	//[Order(After = Priority.Default)]
+	//internal sealed class RoslynExtensionMethodFormat : ClassificationFormatDefinition
+	//{
+	//	public RoslynExtensionMethodFormat()
+	//	{
+	//		this.DisplayName = "Roslyn Extension Method";
+	//		this.IsItalic = true;
+	//	}
+	//}
+}
