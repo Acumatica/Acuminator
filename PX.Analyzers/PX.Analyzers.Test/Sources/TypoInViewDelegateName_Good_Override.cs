bb07459a7c4b95804948f093f3ff4cb15e6cf87a
Vladimir Panchenko <vpanchenko@acumatica.com>
2017-09-08 04:48:09 +0300

    Fix: don't apply diagnostic for overriden view delegate methods

---
+++ PX.Analyzers/PX.Analyzers.Test/Sources/TypoInViewDelegateName_Good_Override.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Analyzers.Test.Sources
+{
+	public class SomeBaseGraph : PXGraph<SomeBaseGraph>
+	{
+		public PXSelect<SomeDocument> Adjustments;
+
+		public virtual IEnumerable adjustments()
+		{
+			yield break;
+		}
+	}
+
+	public class SomeDerivedGraph : SomeBaseGraph
+	{
+		public PXSelect<SomeDocument> SOAdjustments;
+
+		public override IEnumerable adjustments()
+		{
+			yield break;
+		}
+	}
+
+	public class SomeDocument : IBqlTable
+	{
+	}
+}
