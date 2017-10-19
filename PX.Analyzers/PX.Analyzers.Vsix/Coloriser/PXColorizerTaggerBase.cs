4942a0f6c0e960a9be0b5cda166e4091774b0e52
Sergey Nikomarov <snikomarov@acumatica.com>
2017-10-19 04:10:10 +0300

    roslyn-coloriser: added base class

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/PXColorizerTaggerBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace PX.Analyzers.Coloriser
+{
+    /// <summary>
+    /// A colorizer tagger base class.
+    /// </summary>
+    internal abstract class PXColorizerTaggerBase : ITagger<IClassificationTag>
+    {
+#pragma warning disable CS0067
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+#pragma warning restore CS0067
+
+        protected ITextBuffer Buffer { get; }
+        private ITextSnapshot cache;
+        private readonly PXColorizerTaggerProvider provider;
+
+        public abstract IEnumerable<ITagSpan<IClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans);
+    }
+}
