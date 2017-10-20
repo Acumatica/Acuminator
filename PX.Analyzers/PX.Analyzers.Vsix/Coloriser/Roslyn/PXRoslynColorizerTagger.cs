b238df798cdd8d5899bc3f116d797c9ee83c2a32
Sergey Nikomarov <snikomarov@acumatica.com>
2017-10-20 04:34:50 +0300

    roslyn-coloriser: Some work on coloring

--- PX.Analyzers/PX.Analyzers.Vsix/Coloriser/Roslyn/PXRoslynColorizerTagger.cs
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/Roslyn/PXRoslynColorizerTagger.cs
@@ -12,60 +12,49 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.ComponentModel.Composition;
 using CSharp = Microsoft.CodeAnalysis.CSharp;
+using PX.Analyzers.Utilities;
 
 namespace PX.Analyzers.Coloriser
 {
-    internal class PXRoslynColorizerTagger : ITagger<IClassificationTag>
+    internal class PXRoslynColorizerTagger : PXColorizerTaggerBase
     {
-        private ITextBuffer theBuffer;
-        private ITextSnapshot cache;
-        private readonly PXColorizerTaggerProvider provider;
-
-#pragma warning disable CS0067
-        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
-#pragma warning restore CS0067
-
-        internal PXRoslynColorizerTagger(ITextBuffer buffer, PXColorizerTaggerProvider aProvider)
+        internal PXRoslynColorizerTagger(ITextBuffer buffer, PXColorizerTaggerProvider aProvider) : 
+                                    base(buffer, aProvider)
         {
-            theBuffer = buffer;
-            provider = aProvider;
         }
 
-        public IEnumerable<ITagSpan<IClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        public override IEnumerable<ITagSpan<IClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
         {
             if (spans == null || spans.Count == 0)
                 return Enumerable.Empty<ITagSpan<IClassificationTag>>();
 
-            var workspace = theBuffer.GetWorkspace();
-            Document document = spans[0].Snapshot.GetOpenDocumentInCurrentContextWithChanges();
+            if (Cache != null && Cache == spans[0].Snapshot)
+                return TagsList;
+         
+            TagsList.Clear();
+            Cache = spans[0].Snapshot;
+            GetTagsFromSnapshot(spans);
+            return TagsList;
+        }
 
-            if (document == null)
-            {
-                // Razor cshtml returns a null document for some reason.
-                return null;
-            }
+        private void GetTagsFromSnapshot(NormalizedSnapshotSpanCollection spans)
+        {
+            Task<ParsedDocument> getDocumentTask = ParsedDocument.Resolve(Buffer, Cache);
 
-            Task task = GetSemanticModel(document);
+            if (getDocumentTask == null)    // Razor cshtml returns a null document for some reason.        
+                return;
 
             try
             {
-                task.Wait();
+                getDocumentTask.Wait();
             }
             catch (Exception e)
-            {
-                // TODO: report this to someone.
-                return Enumerable.Empty<ITagSpan<IClassificationTag>>();
+            {         
+                return;     // TODO: report this to someone.
             }
 
-            return Enumerable.Empty<ITagSpan<IClassificationTag>>();
-        }
-
-        private async Task GetSemanticModel(Document document)
-        {
-            // the ConfigureAwait() calls are important,
-            // otherwise we'll deadlock VS
-            var semanticModel = await document.GetSemanticModelAsync().ConfigureAwait(false);
-            var syntaxRoot = await document.GetSyntaxRootAsync().ConfigureAwait(false);
+            ParsedDocument document = getDocumentTask.Result;
+            
         }
     }
 }
---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/Roslyn/ParsedDocument.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent; 
+using System.Linq;
+using System.Threading.Tasks;
+
+using Path = System.IO.Path;
+
+namespace PX.Analyzers.Coloriser
+{
+    internal class ParsedDocument
+    {
+        private static readonly HashSet<string> allowedExtensions = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            ".cs"
+        };
+
+        public Workspace Workspace { get; }
+
+        public Document Document { get; }
+
+        public SemanticModel SemanticModel { get; }
+
+        public SyntaxNode SyntaxRoot { get; }
+
+        public ITextSnapshot Snapshot { get; }
+
+        private ParsedDocument(Workspace workspace, Document document, SemanticModel semanticModel, SyntaxNode syntaxRoot,
+                               ITextSnapshot snapshot)
+        {
+            Workspace = workspace;
+            Document = document;
+            SemanticModel = semanticModel;
+            SyntaxRoot = syntaxRoot;
+            Snapshot = snapshot;
+        }
+
+        public static async Task<ParsedDocument> Resolve(ITextBuffer buffer, ITextSnapshot snapshot)
+        {
+            Workspace workspace = buffer.GetWorkspace();
+            Document document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
+
+            if (document == null || !IsSupportedFileType(document) || !document.SupportsSemanticModel || 
+                !document.SupportsSyntaxTree)
+            {     
+                return null;        // Razor cshtml returns a null document for some reason.
+            }
+
+            if (document.TryGetSemanticModel(out SemanticModel semanticModel) && 
+                document.TryGetSyntaxRoot(out SyntaxNode syntaxRoot))
+            {         
+                return new ParsedDocument(workspace, document, semanticModel, syntaxRoot, snapshot);
+            }
+
+            // the ConfigureAwait() calls are important, otherwise we'll deadlock VS
+            Task<SemanticModel> semanticModelTask = document.GetSemanticModelAsync();
+            Task<SyntaxNode> syntaxRootTask = document.GetSyntaxRootAsync();
+
+            await Task.WhenAll(semanticModelTask, syntaxRootTask)
+                      .ConfigureAwait(continueOnCapturedContext: false);
+
+            semanticModel = semanticModelTask.Result;
+            syntaxRoot = syntaxRootTask.Result;
+            
+            return new ParsedDocument(workspace, document, semanticModel, syntaxRoot, snapshot);
+        }
+
+        private static bool IsSupportedFileType(Document document) => allowedExtensions.Contains(Path.GetExtension(document.FilePath));       
+    }
+}
