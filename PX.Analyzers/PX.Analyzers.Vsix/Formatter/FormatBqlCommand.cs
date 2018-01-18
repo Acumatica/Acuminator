6c1c9ba4872dfdd2a6a5e17591496c95a55984f7
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-18 14:16:14 +0300

    AC-103229: 'Format BQL Statements' command

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Formatter/FormatBqlCommand.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace PX.Analyzers.Vsix.Formatter
+{
+	/// <summary>
+	/// Command handler
+	/// </summary>
+	internal sealed class FormatBqlCommand
+	{
+		/// <summary>
+		/// Command ID.
+		/// </summary>
+		public const int CommandId = 0x0100;
+
+		/// <summary>
+		/// Command menu group (command set GUID).
+		/// </summary>
+		public static readonly Guid CommandSet = new Guid("3cd59430-1e8d-40af-b48d-9007624b3d77");
+
+		/// <summary>
+		/// VS Package that provides this command, not null.
+		/// </summary>
+		private readonly Package package;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FormatBqlCommand"/> class.
+		/// Adds our command handlers for menu (commands must exist in the command table file)
+		/// </summary>
+		/// <param name="package">Owner package, not null.</param>
+		private FormatBqlCommand(Package package)
+		{
+			if (package == null)
+			{
+				throw new ArgumentNullException("package");
+			}
+
+			this.package = package;
+
+			OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+			if (commandService != null)
+			{
+				var menuCommandID = new CommandID(CommandSet, CommandId);
+				var menuItem = new OleMenuCommand(this.FormatButtonCallback, menuCommandID);
+				menuItem.BeforeQueryStatus += QueryFormatButtonStatus;
+				commandService.AddCommand(menuItem);
+			}
+		}
+
+		/// <summary>
+		/// Gets the instance of the command.
+		/// </summary>
+		public static FormatBqlCommand Instance
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the service provider from the owner package.
+		/// </summary>
+		private IServiceProvider ServiceProvider
+		{
+			get
+			{
+				return this.package;
+			}
+		}
+
+		/// <summary>
+		/// Initializes the singleton instance of the command.
+		/// </summary>
+		/// <param name="package">Owner package, not null.</param>
+		public static void Initialize(Package package)
+		{
+			Instance = new FormatBqlCommand(package);
+		}
+
+		/// <summary>
+		/// This function is the callback used to execute the command when the menu item is clicked.
+		/// See the constructor to see how the menu item is associated with this function using
+		/// OleMenuCommandService service and MenuCommand class.
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="e">Event args.</param>
+		private void FormatButtonCallback(object sender, EventArgs e)
+		{
+		}
+
+		private void QueryFormatButtonStatus(object sender, EventArgs e)
+		{
+			var menuCommand = sender as OleMenuCommand;
+			DTE2 dte = (DTE2) ServiceProvider.GetService(typeof (DTE));
+			if (menuCommand != null)
+			{
+				bool enabled = false;
+				if (dte.ActiveDocument != null && !dte.ActiveDocument.ReadOnly)
+				{
+					string fileExtension = System.IO.Path.GetExtension(dte.ActiveDocument.FullName);
+					enabled = !String.IsNullOrEmpty(fileExtension) && fileExtension.Equals(".cs", StringComparison.OrdinalIgnoreCase);
+				}
+
+				menuCommand.Enabled = enabled;
+			}
+		}
+	}
+}
