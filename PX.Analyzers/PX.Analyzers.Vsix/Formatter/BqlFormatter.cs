ab84cc133d7a74274c44b73b1842e322e1c6d263
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-18 17:03:05 +0300

    AC-103229: Formatter skeleton; replacement actions

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Formatter/BqlFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace PX.Analyzers.Vsix.Formatter
+{
+	internal static class BqlFormatter
+	{
+		public static SyntaxNode Format(SyntaxNode syntaxRoot, SemanticModel semanticModel)
+		{
+			return syntaxRoot;
+		}
+	}
+}
