00e4df2757d5273c7cd9fa306ceb1d5a09778b44
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-22 15:35:36 +0300

    AC-103229: Format BQL in two passes: collect changes on the first pass and apply them on the second pass because we lose semantic model otherwise

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Formatter/BqlRewritingPlanner.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using PX.Analyzers.Utilities;
+
+namespace PX.Analyzers.Vsix.Formatter
+{
+	/// <summary>
+	/// Collects all modifications without modifying original syntax tree
+	/// </summary>
+	class BqlRewritingPlanner : BqlRewritingPlannerBase
+	{
+		public BqlRewritingPlanner(BqlContext context, SemanticModel semanticModel,
+			SyntaxTriviaList endOfLineTrivia, SyntaxTriviaList indentationTrivia)
+			: base(context, semanticModel, endOfLineTrivia, indentationTrivia, SyntaxTriviaList.Empty)
+		{
+		}
+
+
+		public override void VisitGenericName(GenericNameSyntax node)
+		{
+			INamedTypeSymbol typeSymbol = GetTypeSymbol(node);
+			INamedTypeSymbol constructedFromSymbol = typeSymbol?.ConstructedFrom; // get generic type
+			
+			if (constructedFromSymbol != null)
+			{
+				if (constructedFromSymbol.ImplementsInterface(Context.IBqlWhere))
+				{
+					Set(node, OnNewLineAndIndentet(node));
+				}
+			}
+
+			base.VisitGenericName(node);
+		}
+
+		public override void VisitIdentifierName(IdentifierNameSyntax node)
+		{
+			SyntaxNode genericNameNode = node.Parent?.Parent; // IdentifierNameSyntax -> TypeArgumentSyntaxList -> GenericNameSyntax
+			if (genericNameNode != null)
+			{
+				INamedTypeSymbol parentType = GetTypeSymbol(genericNameNode);
+				INamedTypeSymbol constructedFromSymbol = parentType?.ConstructedFrom; // get generic type
+
+				if (constructedFromSymbol != null)
+				{
+					if (constructedFromSymbol.InheritsFromOrEquals(Context.PXSelectBase)
+					    || constructedFromSymbol.ImplementsInterface(Context.IBqlSelect)
+					    || constructedFromSymbol.ImplementsInterface(Context.IBqlSearch)) // TODO: could be Coalesce - handle this case in the future
+					{
+						Set(node, OnNewLineAndIndentet(node));
+					}
+				}
+			}
+
+			base.VisitIdentifierName(node);
+		}
+
+		private SyntaxNode OnNewLineAndIndentet(SyntaxNode node)
+		{
+			return node.WithLeadingTrivia(EndOfLineTrivia
+				.AddRange(GetDefaultLeadingTrivia(node))
+				.AddRange(IndentationTrivia));
+		}
+
+		private SyntaxTriviaList GetDefaultLeadingTrivia(SyntaxNode node)
+		{
+			if (node == null) return SyntaxTriviaList.Empty;
+			if (node.HasLeadingTrivia &&
+				(node.IsKind(SyntaxKind.FieldDeclaration) // View
+				|| node.IsKind(SyntaxKind.AttributeList) // BQL in attribute
+				|| node.IsKind(SyntaxKind.SimpleMemberAccessExpression))) // Static call
+			{
+				return node.GetLeadingTrivia();
+			}
+
+			return GetDefaultLeadingTrivia(node.Parent);
+		}
+	}
+}
