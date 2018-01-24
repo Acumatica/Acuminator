b8bcbdf709ab4829afe0bdd34fd5cf1cad912cdf
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-24 13:12:37 +0300

    AC-103229: New test case for group by

---
+++ PX.Analyzers/PX.Analyzers.Test/Sources/BQL/Formatted/StaticCall_GroupBy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects
+{
+	public class SOOrderEntry : PXGraph<SOOrderEntry>
+	{
+		public PXSelect<SupplierProduct> Products;
+
+		public IEnumerable products()
+		{
+			var filteredProducts = PXSelectGroupByOrderBy<
+				SupplierProduct,
+				InnerJoin<Supplier, On<Supplier.supplierID, Equal<SupplierProduct.supplierID>>>,
+				Aggregate<
+					GroupBy<SupplierProduct.productID,
+						Avg<SupplierProduct.supplierPrice,
+						Min<SupplierProduct.minOrderQty,
+						Max<SupplierProduct.lastPurchaseDate>>>>>,
+					OrderBy<
+						Asc<SupplierProduct.productID,
+						Asc<SupplierProduct.supplierID>>>>
+				.Select(this);
+			return filteredProducts;
+		}
+	}
+}
