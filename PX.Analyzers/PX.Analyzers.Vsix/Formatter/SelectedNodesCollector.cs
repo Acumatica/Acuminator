19b11150848e2723e459148b3f3f220478e4a90b
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-24 11:40:51 +0300

    AC-103229: Use Roslyn to update the document - fixes selection restore issue; format all selected nodes instead of parent one

---
+++ PX.Analyzers/PX.Analyzers.Vsix/Formatter/SelectedNodesCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+
+namespace PX.Analyzers.Vsix.Formatter
+{
+	class SelectedNodesCollector : CSharpSyntaxWalker
+	{
+		private readonly TextSpan _selectedSpan;
+		private readonly List<SyntaxNode> _nodesWithinSpan = new List<SyntaxNode>();
+
+		public IReadOnlyCollection<SyntaxNode> NodesWithinSpan => _nodesWithinSpan;
+
+		public SelectedNodesCollector(TextSpan selectedSpan)
+		{
+			_selectedSpan = selectedSpan;
+		}
+
+		public override void Visit(SyntaxNode node)
+		{
+			if (node != null && node.SpanStart >= _selectedSpan.Start && node.Span.End <= _selectedSpan.End)
+			{
+				_nodesWithinSpan.Add(node);
+			}
+			else
+			{
+				base.Visit(node);
+			}
+		}
+	}
+}
