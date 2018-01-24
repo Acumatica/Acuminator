f05dc4dd95a7faae3e3781f6109574cc8f9e4b66
Sergey Nikomarov <snikomarov@acumatica.com>
2018-01-24 18:13:30 +0300

    editor-utils-integration: Added tags cache

--- PX.Analyzers/PX.Analyzers.Vsix/Coloriser/AsyncTagging/AsyncUtils/TagsCache.cs
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/AsyncTagging/AsyncUtils/TagsCache.cs
@@ -1,32 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.Text;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 using Microsoft.VisualStudio.Text.Tagging;
-
+using System.Collections;
+using PX.Analyzers.Vsix.Utilities;
 
 
 namespace PX.Analyzers.Coloriser.AsyncTagging
 {
-    internal class TagsCache<TTag>
+    internal class TagsCache<TTag> : IReadOnlyCollection<ITagSpan<TTag>>
     where TTag : ITag
     {
-        public ITextSnapshot Snapshot { get; }
-
+        private const int defaultCapacity = 64;
+        private const int defaultChunkSize = 50;
+      
         public bool IsCompleted { get; private set; }
 
-        /// <summary>
-        /// Set of known tags
-        /// </summary>
-        public IReadOnlyCollection<ITagSpan<TTag>> TagList { get; } 
+        private readonly List<ITagSpan<TTag>> resultTagsList;
+        private readonly ConcurrentQueue<ITagSpan<TTag>> tagsQueue = new ConcurrentQueue<ITagSpan<TTag>>();
 
-        public TagsCache()
+        public IReadOnlyCollection<ITagSpan<TTag>> ProcessedTags { get; }
+
+        public int Count => ProcessedTags.Count;
+
+        public TagsCache(int? capacity = null)
         {
-            
-            
-        }       
+            resultTagsList = new List<ITagSpan<TTag>>(capacity ?? defaultCapacity);
+            ProcessedTags = resultTagsList.AsReadOnly();
+        }
+
+        public void PersistIntermediateResult()
+        {
+            int counter = 0;
+
+            while (tagsQueue.TryDequeue(out ITagSpan<TTag> tag) && counter < defaultChunkSize)
+            {
+                resultTagsList.Add(tag);
+                counter++;
+            }
+        }
+
+        public void CompleteProcessing()
+        {
+            while (tagsQueue.TryDequeue(out ITagSpan<TTag> tag))
+            {
+                resultTagsList.Add(tag);              
+            }
+
+            IsCompleted = true;
+        }
+
+        public void Reset()
+        {
+            resultTagsList.Clear();
+            tagsQueue.Clear();
+            IsCompleted = false;
+        }
+
+        public void AddTag(ITagSpan<TTag> tag)
+        {
+            if (tag == null)
+                return;
+
+            tagsQueue.Enqueue(tag);
+        }
+
+        public IEnumerator<ITagSpan<TTag>> GetEnumerator() => ProcessedTags.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
---
+++ PX.Analyzers/PX.Analyzers.Vsix/Coloriser/AsyncTagging/PXColorizerTaggerBaseAsyncTagging.cs
@@ -0,0 +1,34 @@
+﻿
+// <summary> The asynchronous tagging part of the PXColorizerTaggerBase class</summary>
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
+using PX.Analyzers.Vsix.Utilities;
+
+
+
+namespace PX.Analyzers.Coloriser
+{
+    /// <content>
+    /// A colorizer tagger base class.
+    /// </content>
+    public abstract partial class PXColorizerTaggerBase : ITagger<IClassificationTag>, IDisposable
+    {
+        /// <summary>
+        /// Gets the tags asynchronous in this collection.
+        /// </summary>
+        /// <param name="snapshot">The snapshot.</param>
+        /// <returns>
+        /// An enumerator that allows foreach to be used to process the tags asynchronous in this collection.
+        /// </returns>
+        protected virtual IEnumerable<ITagSpan<IClassificationTag>> GetTagsAsync(ITextSnapshot snapshot)
+        {
+
+        }     
+    }
+}
