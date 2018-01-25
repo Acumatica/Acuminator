9723989ea013d74de48ba7818bd3709907b918ee
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-25 16:25:26 +0300

    AC-103229: Rework conditions formatting to match expected test data

--- PX.Analyzers/PX.Analyzers.Vsix/Formatter/BqlConditionRewriter.cs
+++ PX.Analyzers/PX.Analyzers.Vsix/Formatter/BqlConditionRewriter.cs
@@ -1,31 +1,61 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using PX.Analyzers.Utilities;
 
 namespace PX.Analyzers.Vsix.Formatter
 {
 	class BqlConditionRewriter : BqlRewriterBase
 	{
+		private bool _firstWhereInParenthesis;
+		private bool _externalMode;
+
 		public BqlConditionRewriter(BqlRewriterBase parent, SyntaxTriviaList defaultLeadingTrivia) 
+			: base(parent, defaultLeadingTrivia)
+		{
+			_externalMode = true;
+		}
+
+		private BqlConditionRewriter(BqlConditionRewriter parent, SyntaxTriviaList defaultLeadingTrivia)
 			: base(parent, defaultLeadingTrivia)
 		{
 		}
 
 		public override SyntaxNode VisitGenericName(GenericNameSyntax node)
 		{
-			INamedTypeSymbol originalSymbol = GetOriginalTypeSymbol(node);
+			bool moveWhereToNextLine = _firstWhereInParenthesis || _externalMode;
+			_firstWhereInParenthesis = false;
 
-			if (originalSymbol != null && originalSymbol.ImplementsInterface(Context.IBqlPredicateChain))
+			INamedTypeSymbol originalSymbol = GetOriginalTypeSymbol(node);
+			
+			if (originalSymbol != null)
 			{
-				SyntaxTriviaList trivia = DefaultLeadingTrivia;
-				TypeSyntax firstArg = node.TypeArgumentList.Arguments.FirstOrDefault();
-				if (firstArg != null)
+				if (originalSymbol.InheritsFromOrEqualsGeneric(Context.Where2)
+					|| originalSymbol.InheritsFromOrEqualsGeneric(Context.And2)
+					|| originalSymbol.InheritsFromOrEqualsGeneric(Context.Or2))
 				{
-					INamedTypeSymbol firstArgOriginalSymbol = GetOriginalTypeSymbol(firstArg);
-					if (firstArgOriginalSymbol != null && firstArgOriginalSymbol.ImplementsInterface(Context.IBqlWhere))
-						trivia = IndentedDefaultTrivia;
+					var childRewriter = new BqlConditionRewriter(this, IndentedDefaultTrivia)
+					{
+						_firstWhereInParenthesis = true
+					};
+					return RewriteGenericNode(node, childRewriter);
+				}
+				
+				if (originalSymbol.ImplementsInterface(Context.IBqlWhere)) // "parenthesis" in BQL statement
+				{
+					if (moveWhereToNextLine)
+					{
+						return RewriteGenericNode(node, new BqlConditionRewriter(this, IndentedDefaultTrivia));
+					}
+
+					// Leave Where on the same line, but indent and move all logic operators within the content to a new line
+					var childRewriter = new BqlConditionRewriter(this, IndentedDefaultTrivia);
+					return node.WithTypeArgumentList((TypeArgumentListSyntax) childRewriter.Visit(node.TypeArgumentList));
 				}
 
-				return RewriteGenericNode(node, new BqlConditionRewriter(this, trivia));
+				if (originalSymbol.ImplementsInterface(Context.IBqlPredicateChain))
+				{
+					return RewriteGenericNode(node, new BqlConditionRewriter(this, DefaultLeadingTrivia));
+				}
 			}
 
 			return base.VisitGenericName(node);
