6fd20bb1a79913733779282837142c659ab72ac7
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-01-26 15:14:36 +0300

    AC-103229: New test case for Search with join

---
+++ PX.Analyzers/PX.Analyzers.Test/Sources/BQL/Formatted/Search_Join.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.IN
+{
+	public class InventoryItem : IBqlTable
+	{
+		#region ItemClassID
+		public abstract class itemClassID : PX.Data.IBqlField
+		{
+		}
+		protected int? _ItemClassID;
+
+		/// <summary>
+		/// The identifier of the <see cref="INItemClass">Item Class</see>, to which the Inventory Item belongs.
+		/// Item Classes provide default settings for items, which belong to them, and are used to group items.
+		/// </summary>
+		/// <value>
+		/// Corresponds to the <see cref="INItemClass.ItemClassID"/> field.
+		/// </value>
+		[PXDBInt]
+		[PXUIField(DisplayName = "Item Class", Visibility = PXUIVisibility.SelectorVisible)]
+		[PXDimensionSelector(INItemClass.Dimension, typeof(Search<INItemClass.itemClassID>), typeof(INItemClass.itemClassCD), DescriptionField = typeof(INItemClass.descr))]
+		[PXDefault(typeof(Search2<
+			INItemClass.itemClassID, 
+			InnerJoin<INSetup, 
+				On<Current<InventoryItem.stkItem>, Equal<boolFalse>, 
+					And<INSetup.dfltNonStkItemClassID, Equal<INItemClass.itemClassID>, 
+					Or<Current<InventoryItem.stkItem>, Equal<boolTrue>, 
+					And<INSetup.dfltStkItemClassID, Equal<INItemClass.itemClassID>>>>>>>))]
+		[PXUIRequired(typeof(INItemClass.stkItem))]
+		public virtual int? ItemClassID
+		{
+			get
+			{
+				return this._ItemClassID;
+			}
+			set
+			{
+				this._ItemClassID = value;
+			}
+		}
+		#endregion
+	}
+}
