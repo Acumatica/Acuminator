794d380c41e0b24b465ffae95df3b329f3217563
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-03-28 10:26:29 +0100

    Rename folders, solutions, projects

--- src/Acuminator/Acuminator.Tests/Properties/AssemblyInfo.cs
+++ src/Acuminator/Acuminator.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PX.Analyzers")]
+[assembly: AssemblyTitle("Acuminator.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PX.Analyzers")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Acuminator.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2017-2018 Acumatica Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,7 +27,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("PX.Analyzers.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b71e848be0108cd15a138bdd63275ea7b7e91218eee4b80836d6b9868e55a60bb3388943184fe221a31a073fc8e2944c94bf73d930eeaf1c239bca6896bc4ba370b22519fabc36a3dfc5d215b2d9070931f9eea963d88678bee14cb3922003f4eb7928c58533d0bc85759472c300fd2539dfa7ca19a292f7a3921d915ed934aa")]
