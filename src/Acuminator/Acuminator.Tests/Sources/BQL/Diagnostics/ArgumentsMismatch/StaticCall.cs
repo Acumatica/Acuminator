62695bb611a41f1e05610fdada91092783e874d3
Sergey Nikomarov <snikomarov@acumatica.com>
2018-04-24 03:43:46 +0300

    ATR-23: changed tests folder structure  to order tests. Added test for BQL parameter mismatch

---
+++ src/Acuminator/Acuminator.Tests/Sources/BQL/Diagnostics/ArgumentsMismatch/StaticCall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects
+{
+	public class SOOrderEntry : PXGraph<SOOrderEntry>
+	{
+		public object Foo()
+		{
+			var result =
+                PXSelect<SOOrder, 
+				Where<SOOrder.orderType, Equal<Required<SalesOrder.orderType>>, 
+					And<SOOrder.status, Equal<Required<SalesOrder.status>>>>, 
+				OrderBy<
+					Asc<SOOrder.orderNbr>>>
+				.Select(this);
+
+            return this;
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/Diagnostics/BQL/BqlParameterMismatchTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acuminator.Analyzers;
+using Acuminator.Tests.Helpers;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace Acuminator.Tests
+{
+	public class BqlParameterMismatchTests : CodeFixVerifier
+	{
+        private DiagnosticResult CreatePX1015DiagnosticResult(int line, int column)
+        {
+            var diagnostic = new DiagnosticResult
+            {
+                Id = Descriptors.PX1015_PXBqlParametersMismatch.Id,
+                Message = Descriptors.PX1001_PXGraphCreateInstance.Title.ToString(),
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                        new DiagnosticResultLocation("Test0.cs", line, column)
+                    }
+            };
+
+            return diagnostic;
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+            new BqlParameterMismatchAnalyzer();
+       
+        [Theory]
+        [EmbeddedFileData(@"BQL\Diagnostics\ArgumentsMismatch\StaticCall.cs")]
+        public virtual void FormatDocument(string actual) =>
+            VerifyCSharpDiagnostic(actual, CreatePX1015DiagnosticResult(line: 20, column: 18));     
+    }
+}
