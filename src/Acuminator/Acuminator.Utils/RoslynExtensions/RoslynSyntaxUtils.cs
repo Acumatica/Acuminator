fd20f308cf978b9a8f8573255113e8619b73700e
Sergey Nikomarov <snikomarov@acumatica.com>
2018-05-01 02:25:29 +0300

    ATR-23: added value tuples to utils package. Added syntax utils and modified CLA algorithm.

--- src/Acuminator/Acuminator.Utils/RoslynExtensions/RoslynSyntaxUtils.cs
+++ src/Acuminator/Acuminator.Utils/RoslynExtensions/RoslynSyntaxUtils.cs
@@ -118,26 +118,126 @@ namespace Acuminator.Utilities
 
 			return null;
 		}
+		
+		/// <summary>
+		/// Get a parent with a specified type <typeparamref name="TParent"/>
+		/// </summary>
+		/// <typeparam name="TParent">Parent node type.</typeparam>
+		/// <param name="node">The node to act on.</param>
+		/// <returns/>
+		public static TParent Parent<TParent>(this SyntaxNode node)
+		where TParent : SyntaxNode
+		{
+			if (node == null)
+				return null;
 
+			SyntaxNode curNode = node.Parent;
+
+			while (curNode != null)
+			{
+				if (curNode is TParent parent)
+					return parent;
+
+				curNode = curNode.Parent;
+			}
+
+			return null;
+		}
+		
+		/// <summary>
+		/// Get depths of the given syntax node measuring from root.
+		/// </summary>
+		/// <param name="node">The node to act on.</param>
+		/// <returns/>
 		public static int Depth(this SyntaxNode node)
 		{
 			node.ThrowOnNull(nameof(node));
-			return node.Ancestors().Count();
+
+			int depth = 0;
+			SyntaxNode curNode = node.Parent;
+
+			while (curNode != null)
+			{
+				depth++;
+				curNode = curNode.Parent;
+			}
+
+			return depth;
+		}
+		
+		/// <summary>
+		/// Get depths of the given syntax node measuring from the root of type <typeparamref name="TRoot"/>. 
+		/// If node doesn't have the root of this type then the normal depth is returned.
+		/// </summary>
+		/// <typeparam name="TRoot">Type of the root.</typeparam>
+		/// <param name="node">The node to act on.</param>
+		/// <returns/>
+		public static int Depth<TRoot>(this SyntaxNode node)
+		where TRoot : SyntaxNode
+		{
+			node.ThrowOnNull(nameof(node));
+
+			if (node is TRoot)
+				return 0;
+
+			int depth = 0;
+			SyntaxNode curNode = node.Parent;
+
+			while (curNode != null && !(curNode is TRoot))
+			{
+				depth++;
+				curNode = curNode.Parent;
+			}
+
+			return depth;
+		}
+		
+		/// <summary>
+		/// Get depths of the given syntax node measuring from root.
+		/// </summary>
+		/// <typeparam name="TRoot">Type of the root.</typeparam>
+		/// <typeparam name="TNode">Type of the node.</typeparam>
+		/// <param name="node">The node to act on.</param>
+		/// <returns>
+		/// An int.
+		/// </returns>
+		public static int Depth<TRoot, TNode>(this TNode node)
+		where TRoot : SyntaxNode
+		where TNode : SyntaxNode
+		{
+			node.ThrowOnNull(nameof(node));
+
+			if (node is TRoot)
+				return 0;
+
+			int depth = 0;
+			TNode curNode = node.Parent<TNode>();
+
+			while (curNode != null && !(curNode is TRoot))
+			{
+				depth++;
+				curNode = curNode.Parent<TNode>();
+			}
+
+			return depth;
 		}
 
 		public static SyntaxNode LowestCommonAncestor(SyntaxNode nodeX, SyntaxNode nodeY)
 		{
+			nodeX.ThrowOnNull(nameof(nodeX));
+			nodeY.ThrowOnNull(nameof(nodeY));
+
 			int depthX = nodeX.Depth();            //Depth is average O(log n) operation, worst case is O(n) but it isn't the case for the syntax tree which is wide but not very deep
 			int depthY = nodeY.Depth();
 
-			SyntaxNode curentX = nodeX;
+			SyntaxNode currentX = nodeX;
 			SyntaxNode currentY = nodeY;
-
+			
 			while (depthX != depthY)				//First get nodes on the equal levels of depth
 			{
 				if (depthX > depthY)
 				{
-					curentX = curentX.Parent;
+					currentX = currentX.Parent;
 					depthX--;
 				}
 				else
@@ -147,13 +247,53 @@ namespace Acuminator.Utilities
 				}
 			}
 
-			while (curentX != currentY)          //Then move up the branches until nodes coincide
+			while (currentX != currentY)          //Then move up the branches until nodes coincide
 			{
-				curentX = curentX.Parent;
+				currentX = currentX.Parent;
 				currentY = currentY.Parent;
 			}
 
-			return curentX;
+			return currentX;
+		}
+
+		public static (StatementSyntax Ancestor, StatementSyntax ScopedX, StatementSyntax ScopedY) LowestCommonAncestorSyntaxStatement(StatementSyntax x, StatementSyntax y)
+		{
+			x.ThrowOnNull(nameof(x));
+			y.ThrowOnNull(nameof(y));
+
+			//Depth is average O(log n) operation, worst case is O(n) but it isn't the case for the syntax tree which is wide but not very deep.
+			//For statements we could consider depth constrained by MethodDeclarationSyntax 
+			int depthX = x.Depth<MethodDeclarationSyntax, StatementSyntax>();           
+			int depthY = y.Depth<MethodDeclarationSyntax, StatementSyntax>();
+
+			StatementSyntax currentX = x, prevX = null;
+			StatementSyntax currentY = y, prevY = null;
+
+			while (depthX != depthY)                //First get nodes on the equal levels of depth
+			{
+				if (depthX > depthY)
+				{
+					prevX = currentX;
+					currentX = currentX.Parent<StatementSyntax>();
+					depthX--;
+				}
+				else
+				{
+					prevY = currentY;
+					currentY = currentY.Parent<StatementSyntax>();
+					depthY--;
+				}
+			}
+
+			while (currentX != currentY)          //Then move up the branches until nodes coincide
+			{
+				prevX = currentX;
+				prevY = currentY;
+				currentX = currentX.Parent<StatementSyntax>();
+				currentY = currentY.Parent<StatementSyntax>();
+			}
+
+			return (currentX, prevX, prevY);
 		}
 	}
 }
