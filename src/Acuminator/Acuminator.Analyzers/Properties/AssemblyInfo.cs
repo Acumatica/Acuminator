33f757f478b2fccd02761a4b585b3ccf9ad04a39
Andrey Popkov <apopkov@acumatica.com>
2018-05-21 17:38:33 +0300

    ATR-56: Configure build process

--- src/Acuminator/Acuminator.Analyzers/Properties/AssemblyInfo.cs
+++ src/Acuminator/Acuminator.Analyzers/Properties/AssemblyInfo.cs
@@ -19,16 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Acuminator.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b71e848be0108cd15a138bdd63275ea7b7e91218eee4b80836d6b9868e55a60bb3388943184fe221a31a073fc8e2944c94bf73d930eeaf1c239bca6896bc4ba370b22519fabc36a3dfc5d215b2d9070931f9eea963d88678bee14cb3922003f4eb7928c58533d0bc85759472c300fd2539dfa7ca19a292f7a3921d915ed934aa")]
