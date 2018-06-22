0d84bc383cb1ec568e59dc15b18c5bef73a4038f
Sergey Nikomarov <snikomarov@acumatica.com>
2018-06-23 02:57:48 +0300

    ATR-122: Enhanced unit test

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LongOperationDelegateClosuresTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LongOperationDelegateClosuresTests.cs
@@ -15,49 +15,33 @@ namespace Acuminator.Tests
 {
     public class LongOperationDelegateClosuresTests : CodeFixVerifier
     {
-	    private DiagnosticResult[] CreateDiagnosticResults()
-	    {
-		    return new DiagnosticResult[] 
-            {
-                new DiagnosticResult
-                {
-                    Id = Descriptors.PX1008_LongOperationDelegateClosures.Id,
-                    Message = Descriptors.PX1008_LongOperationDelegateClosures.Title.ToString(),
-                    Severity = DiagnosticSeverity.Warning,
-                    Locations =
-                        new[] { new DiagnosticResultLocation("Test0.cs", 15, 9) }
-                },
-
-                new DiagnosticResult
-                {
-                    Id = Descriptors.PX1008_LongOperationDelegateClosures.Id,
-                    Message = Descriptors.PX1008_LongOperationDelegateClosures.Title.ToString(),
-                    Severity = DiagnosticSeverity.Warning,
-                    Locations =
-                        new[] { new DiagnosticResultLocation("Test0.cs", 25, 9) }
-                },
-
-                new DiagnosticResult
-                {
-                    Id = Descriptors.PX1008_LongOperationDelegateClosures.Id,
-                    Message = Descriptors.PX1008_LongOperationDelegateClosures.Title.ToString(),
-                    Severity = DiagnosticSeverity.Warning,
-                    Locations =
-                        new[] { new DiagnosticResultLocation("Test0.cs", 28, 9) }
-                },
-            };
-	    }
-
         [Theory]
         [EmbeddedFileData("LongOperationDelegateClosuresAnalyzer.cs")]
         public void TestDiagnostic(string actual)
         {
-            VerifyCSharpDiagnostic(actual, CreateDiagnosticResults());
+            VerifyCSharpDiagnostic(actual,
+				new DiagnosticResult[]
+				{
+					CreatePX1008DiagnosticResult(line: 23, column: 3),
+					CreatePX1008DiagnosticResult(line: 34, column: 3),
+					CreatePX1008DiagnosticResult(line: 37, column: 3),
+					CreatePX1008DiagnosticResult(line: 44, column: 3),
+					CreatePX1008DiagnosticResult(line: 45, column: 3),
+				});
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new LongOperationDelegateClosuresAnalyzer();
         }
-    }
+
+		private DiagnosticResult CreatePX1008DiagnosticResult(int line, int column) =>
+			new DiagnosticResult
+			{
+				Id = Descriptors.PX1008_LongOperationDelegateClosures.Id,
+				Message = Descriptors.PX1008_LongOperationDelegateClosures.Title.ToString(),
+				Severity = DiagnosticSeverity.Warning,
+				Locations = new[] { new DiagnosticResultLocation("Test0.cs", line, column) }
+			};
+	}
 }
