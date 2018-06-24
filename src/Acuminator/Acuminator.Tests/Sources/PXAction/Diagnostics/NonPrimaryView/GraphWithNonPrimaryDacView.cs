4848682d7adc4db5a1233920620a79eb3b19ad27
Sergey Nikomarov <snikomarov@acumatica.com>
2018-06-24 05:50:10 +0300

    ATR-28: Added unit test for new diagnostic on non primary views

---
+++ src/Acuminator/Acuminator.Tests/Sources/PXAction/Diagnostics/NonPrimaryView/GraphWithNonPrimaryDacView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.HackathonDemo
+{
+	public class SOOrder : IBqlTable { }
+
+	public class SOTran : IBqlTable { }
+
+	public class TestDac : IBqlTable { }
+
+	public class SOOrderEntryWithNonPrimaryDacView1 : PXGraph<SOOrderEntryWithNonPrimaryDacView1>
+	{
+		public PXSelect<SOOrder> Orders;
+
+		public PXSelect<SOTran> OrderDetails;
+
+		public PXAction<SOTran> Release1;
+
+		public PXAction<SOTran> Release2 { get; }
+
+		public PXAction<SOOrder> Release3;
+	}
+
+	public class SOOrderEntryWithNonPrimaryDacViewExtension : PXGraphExtension<SOOrderEntryWithNonPrimaryDacView1>
+	{
+		public PXSelect<TestDac> TestView;
+
+		public PXAction<SOTran> Action1;
+
+		public PXAction<SOOrder> Action2;
+
+		public PXAction<SOTran> Action3 { get; }
+	}
+
+	public class SOOrderEntryWithNonPrimaryDacView2 : PXGraph<SOOrderEntryWithNonPrimaryDacView2, SOOrder>
+	{
+		public PXSelect<SOOrder> Orders;
+
+		public PXSelect<SOTran> OrderDetails;
+
+		public PXAction<SOTran> Release1;
+
+		public PXAction<SOTran> Release2 { get; }
+
+		public PXAction<SOOrder> Release3;
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXAction/PXActionOnNonPrimaryViewTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acuminator.Analyzers;
+using Acuminator.Tests.Helpers;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace Acuminator.Tests
+{
+	public class PXActionOnNonPrimaryViewTests : DiagnosticVerifier
+	{
+		[Theory]
+		[EmbeddedFileData(@"PXAction\Diagnostics\NonPrimaryView\GraphWithNonPrimaryDacView.cs")] 
+		public virtual void Test_Action_Declared_For_Not_Primary_View(string source) =>
+			VerifyCSharpDiagnostic(source,
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 23, column: 10, actionName: "Release1", mainDacName: "SOOrder"),
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 25, column: 10, actionName: "Release2", mainDacName: "SOOrder"),
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 34, column: 10, actionName: "Action1", mainDacName: "SOOrder"),
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 38, column: 10, actionName: "Action3", mainDacName: "SOOrder"),
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 47, column: 10, actionName: "Release1", mainDacName: "SOOrder"),
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 49, column: 10, actionName: "Release2", mainDacName: "SOOrder"));	
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new PXActionOnNonPrimaryViewAnalyzer();
+
+		
+		private DiagnosticResult CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(int line, int column, string actionName, 
+																					string mainDacName)
+		{
+			string format = Descriptors.PX1012_PXActionOnNonPrimaryView.Title.ToString();
+			string expectedMessage = string.Format(format, actionName, mainDacName);
+
+			return new DiagnosticResult
+			{
+				Id = Descriptors.PX1012_PXActionOnNonPrimaryView.Id,
+				Message = expectedMessage,
+				Severity = DiagnosticSeverity.Warning,
+				Locations =
+					new[]
+					{
+						new DiagnosticResultLocation("Test0.cs", line, column)
+					}
+			};
+		}
+	}
+}
