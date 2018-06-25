b3fa1610f414cc67d2c4b447d108caa09840fa0c
Sergey Nikomarov <snikomarov@acumatica.com>
2018-06-25 03:02:21 +0300

    ATR-28: Added unit test for PX1012 code fix

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXAction/PX1012/PXActionOnNonPrimaryViewTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXAction/PX1012/PXActionOnNonPrimaryViewTests.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Acuminator.Analyzers;
-using Acuminator.Tests.Helpers;
 using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using TestHelper;
 using Xunit;
+using Acuminator.Analyzers;
+using Acuminator.Analyzers.FixProviders;
+using Acuminator.Tests.Helpers;
 
 namespace Acuminator.Tests
 {
-	public class PXActionOnNonPrimaryViewTests : DiagnosticVerifier
+	public class PXActionOnNonPrimaryViewTests : CodeFixVerifier
 	{
 		[Theory]
 		[EmbeddedFileData(@"PXAction\Diagnostics\PX1012\GraphWithNonPrimaryDacView.cs")] 
@@ -25,11 +26,20 @@ namespace Acuminator.Tests
 				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 34, column: 10, actionName: "Action1", mainDacName: "SOOrder"),
 				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 38, column: 10, actionName: "Action3", mainDacName: "SOOrder"),
 				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 47, column: 10, actionName: "Release1", mainDacName: "SOOrder"),
-				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 49, column: 10, actionName: "Release2", mainDacName: "SOOrder"));	
+				CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(line: 49, column: 10, actionName: "Release2", mainDacName: "SOOrder"));
+
+		[Theory]
+		[EmbeddedFileData(@"PXAction\Diagnostics\PX1012\GraphWithNonPrimaryDacView.cs",
+						  @"PXAction\CodeFixes\PX1012\GraphWithNonPrimaryDacViewExpected.cs")]
+		public void TestCodeFix(string actual, string expected)
+		{
+			VerifyCSharpFix(actual, expected);
+		}
 
 		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new PXActionOnNonPrimaryViewAnalyzer();
 
-		
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new PXActionOnNonPrimaryViewCodeFix();
+
 		private DiagnosticResult CreatePX1012ActionOnNonPrimaryViewDiagnosticResult(int line, int column, string actionName, 
 																					string mainDacName)
 		{
