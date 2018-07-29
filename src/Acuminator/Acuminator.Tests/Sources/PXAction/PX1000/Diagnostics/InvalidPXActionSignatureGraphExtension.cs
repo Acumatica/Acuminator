5da911b9fcc1dc1643a04d221bc5e1008aff60f0
Sergey Nikomarov <snikomarov@acumatica.com>
2018-07-30 02:08:07 +0300

    ATR-147: Added unit tests for PX1000 for graph extensions

---
+++ src/Acuminator/Acuminator.Tests/Sources/PXAction/PX1000/Diagnostics/InvalidPXActionSignatureGraphExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Common;
+using PX.Data;
+
+namespace PX.Objects.SO
+{
+	public class SOOrder : IBqlTable
+	{
+		public abstract class orderType { }
+		[PXDBString(IsKey = true, InputMask = "")]
+		[PXDefault]
+		public string OrderType { get; set; }
+
+		public abstract class orderNbr { }
+		[PXDBString(IsKey = true, InputMask = "")]
+		[PXDefault]
+		public string OrderNbr { get; set; }
+	}
+
+	public class SOEntry : PXGraph<SOEntry>
+	{
+		public PXSelect<SOOrder> Documents;
+	}
+
+	public class SOEntryExt : PXGraphExtension<SOEntry>
+	{
+		public PXAction<SOOrder> NewOrder;
+
+		[PXUIField(DisplayName = "New Order", MapEnableRights = PXCacheRights.Select, MapViewRights = PXCacheRights.Select)]
+		[PXButton(ImageKey = PX.Web.UI.Sprite.Main.DataEntry)]
+		public void newOrder(PXAdapter adapter)
+		{
+
+		}
+	}
+}
