046983143538fbaaebb65a40a2d61a7e3d78c13d
Sergey Nikomarov <snikomarov@acumatica.com>
2018-07-29 22:12:39 +0300

    ATR-28: Filtered processing rule

---
+++ src/Acuminator/Acuminator.Utils/RoslynExtensions/PrimaryDacFinder/PrimaryDacRules/GraphRules/PXFilteredProcessingGraphRule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Composition;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Editing;
+using Acuminator.Analyzers;
+using Acuminator.Utilities;
+using PX.Data;
+
+
+namespace Acuminator.Utilities.PrimaryDAC
+{
+	/// <summary>
+	/// A rule to determine primary DAC from PXFilteredProcessingType view's second type parameter.
+	/// </summary>
+	public class PXFilteredProcessingGraphRule : GraphRuleBase
+	{
+		private readonly ITypeSymbol pxFilteredProcessingType;
+
+		public sealed override bool IsAbsolute => true;
+
+		public PXFilteredProcessingGraphRule(PXContext context, double? customWeight = null) : base(customWeight)
+		{
+			context.ThrowOnNull(nameof(context));
+
+			pxFilteredProcessingType = context.Compilation.GetTypeByMetadataName(typeof(PXFilteredProcessing<,>).FullName);
+		}
+
+		public override IEnumerable<ITypeSymbol> GetCandidatesFromGraphRule(PrimaryDacFinder dacFinder)
+		{
+			if (pxFilteredProcessingType == null ||
+				dacFinder?.Graph == null || dacFinder.CancellationToken.IsCancellationRequested || dacFinder.GraphViewSymbolsWithTypes.Length == 0)
+			{
+				return Enumerable.Empty<ITypeSymbol>();
+			}
+
+			List<ITypeSymbol> primaryDacCandidates = new List<ITypeSymbol>(1);
+
+			foreach (var (view, viewType) in dacFinder.GraphViewSymbolsWithTypes)
+			{
+				if (dacFinder.CancellationToken.IsCancellationRequested)
+					return Enumerable.Empty<ITypeSymbol>();
+
+				var fProcessingView = viewType.GetBaseTypesAndThis()
+											  .FirstOrDefault(t => pxFilteredProcessingType.Equals(t) || 
+																   pxFilteredProcessingType.Equals(t?.OriginalDefinition));
+
+				if (fProcessingView == null || !(fProcessingView is INamedTypeSymbol filteredProcessingView))
+					continue;
+
+				var typeParameters = filteredProcessingView.TypeArguments;
+
+				if (typeParameters.Length < 2 || !typeParameters[1].IsDAC())
+					continue;
+
+				primaryDacCandidates.Add(typeParameters[1]);
+			}
+
+			return primaryDacCandidates;
+		}
+	}
+}
