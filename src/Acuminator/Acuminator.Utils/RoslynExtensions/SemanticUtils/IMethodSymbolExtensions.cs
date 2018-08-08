b00b31acc3635b572a6848c0ccf578f14649f235
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-08-08 21:28:15 +0300

    ATR-136: Use ThrowOnNull instead of direct check

--- src/Acuminator/Acuminator.Utils/RoslynExtensions/SemanticUtils/IMethodSymbolExtensions.cs
+++ src/Acuminator/Acuminator.Utils/RoslynExtensions/SemanticUtils/IMethodSymbolExtensions.cs
@@ -11,7 +11,7 @@ namespace Acuminator.Utilities
 	{
 		public static bool IsInstanceConstructor(this IMethodSymbol methodSymbol)
 		{
-			if (methodSymbol == null) throw new ArgumentNullException(nameof (methodSymbol));
+			methodSymbol.ThrowOnNull(nameof (methodSymbol));
 
 			return !methodSymbol.IsStatic && methodSymbol.MethodKind == MethodKind.Constructor;
 		}
