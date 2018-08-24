fb10dc50289dfab0bb3e9919724b8351cc449ece
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-08-24 08:49:57 +0100

    ATR-98: sample for HackathonDemo

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/Database call inside RowSelecting/SOOrderEntryExt.cs
@@ -0,0 +1,22 @@
+﻿using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.HackathonDemo
+{
+	public class SOOrderEntryExt : PXGraphExtension<SOOrderEntry>
+	{
+		protected virtual void _(Events.RowSelecting<SOOrder> e)
+		{
+			var setup = SelectSetup();
+		}
+
+		private SOSetup SelectSetup()
+		{
+			return PXSelect<SOSetup>.SelectSingleBound(Base, null);
+		}
+	}
+}
