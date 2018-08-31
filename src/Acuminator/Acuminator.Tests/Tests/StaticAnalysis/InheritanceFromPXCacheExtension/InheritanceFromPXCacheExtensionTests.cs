efa59d8a4e779eb42d8a1a9ba86e618976038c90
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-08-31 16:06:46 +0300

    ATR-208: Refactor diagnostic result creation

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/InheritanceFromPXCacheExtension/InheritanceFromPXCacheExtensionTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/InheritanceFromPXCacheExtension/InheritanceFromPXCacheExtensionTests.cs
@@ -9,39 +9,11 @@ using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.InheritanceFromPXCacheExtension
 {
-    public class InheritanceFromPXCacheExtensionTests : Verification.CodeFixVerifier
+    public class InheritanceFromPXCacheExtensionTests : CodeFixVerifier
     {
-	    private DiagnosticResult CreatePX1009DiagnosticResult(int line, int column)
-	    {
-			var diagnostic = new DiagnosticResult
-			{
-				Id = Descriptors.PX1009_InheritanceFromPXCacheExtension.Id,
-				Message = Descriptors.PX1009_InheritanceFromPXCacheExtension.Title.ToString(),
-				Severity = DiagnosticSeverity.Error,
-				Locations =
-					new[] {
-						new DiagnosticResultLocation("Test0.cs", line, column)
-					}
-			};
-
-		    return diagnostic;
-	    }
-
-	    private DiagnosticResult CreatePX1011DiagnosticResult(int line, int column)
-	    {
-		    var diagnostic = new DiagnosticResult
-		    {
-			    Id = Descriptors.PX1011_InheritanceFromPXCacheExtension.Id,
-			    Message = Descriptors.PX1011_InheritanceFromPXCacheExtension.Title.ToString(),
-			    Severity = DiagnosticSeverity.Warning,
-			    Locations =
-				    new[] {
-					    new DiagnosticResultLocation("Test0.cs", line, column)
-				    }
-		    };
-
-		    return diagnostic;
-	    }
+	    protected override CodeFixProvider GetCSharpCodeFixProvider() => new InheritanceFromPXCacheExtensionFix();
+	    
+	    protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new InheritanceFromPXCacheExtensionAnalyzer();
 
 		[Theory]
         [EmbeddedFileData("InheritanceFromPXCacheExtension_Good.cs")]
@@ -55,9 +27,9 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.InheritanceFromPXCacheExtension
 		public void TestDiagnostic(string actual)
         {
             VerifyCSharpDiagnostic(actual,
-	            CreatePX1009DiagnosticResult(10, 15),
-				CreatePX1011DiagnosticResult(12, 15),
-				CreatePX1011DiagnosticResult(13, 15));
+	            Descriptors.PX1009_InheritanceFromPXCacheExtension.CreateFor(10, 15),
+	            Descriptors.PX1011_InheritanceFromPXCacheExtension.CreateFor(12, 15),
+	            Descriptors.PX1011_InheritanceFromPXCacheExtension.CreateFor(13, 15));
         }
 
 	    [Theory]
@@ -73,35 +45,19 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.InheritanceFromPXCacheExtension
 	    {
 		    VerifyCSharpFix(actual, expected);
 	    }
-
-		protected override CodeFixProvider GetCSharpCodeFixProvider()
-		{
-			return new InheritanceFromPXCacheExtensionFix();
-		}
-
-		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-        {
-            return new InheritanceFromPXCacheExtensionAnalyzer();
-        }
     }
 
-	public class InheritanceFromPXCacheExtensionMakeSealedTests : Verification.CodeFixVerifier
+	public class InheritanceFromPXCacheExtensionMakeSealedTests : CodeFixVerifier
 	{
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new InheritanceFromPXCacheExtensionMakeSealedFix();
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new InheritanceFromPXCacheExtensionAnalyzer();
+
 		[Theory]
 		[EmbeddedFileData("InheritanceFromPXCacheExtensionMakeSealed_Bad.cs", "InheritanceFromPXCacheExtensionMakeSealed_Bad_Expected.cs")]
 		public void TestCodeFix(string actual, string expected)
 		{
 			VerifyCSharpFix(actual, expected);
-		}
-
-		protected override CodeFixProvider GetCSharpCodeFixProvider()
-		{
-			return new InheritanceFromPXCacheExtensionMakeSealedFix();
-		}
-
-		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-		{
-			return new InheritanceFromPXCacheExtensionAnalyzer();
 		}
 	}
 }
--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/InvalidPXActionSignature/InvalidPXActionSignatureTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/InvalidPXActionSignature/InvalidPXActionSignatureTests.cs
@@ -8,17 +8,21 @@ using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.InvalidPXActionSignature
 {
-	public class InvalidPXActionSignatureTests : Verification.CodeFixVerifier
+	public class InvalidPXActionSignatureTests : CodeFixVerifier
 	{
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new InvalidPXActionSignatureFix();
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new InvalidPXActionSignatureAnalyzer();
+
 		[Theory]
 		[EmbeddedFileData("InvalidPXActionSignature.cs")]
 		public void Test_Invalid_Action_Signature_In_PXGraph(string actual) =>
-			VerifyCSharpDiagnostic(actual, CreatePX100DiagnosticResult(line: 17, column: 15));
+			VerifyCSharpDiagnostic(actual, Descriptors.PX1000_InvalidPXActionHandlerSignature.CreateFor(line: 17, column: 15));
 
 		[Theory]
 		[EmbeddedFileData("InvalidPXActionSignatureGraphExtension.cs")]
 		public void Test_Invalid_Action_Signature_In_PXGraph_Extension(string actual) =>
-			VerifyCSharpDiagnostic(actual, CreatePX100DiagnosticResult(line: 35, column: 15));
+			VerifyCSharpDiagnostic(actual, Descriptors.PX1000_InvalidPXActionHandlerSignature.CreateFor(line: 35, column: 15));
 
 		[Theory]
 		[EmbeddedFileData("InvalidPXActionSignature.cs",
@@ -42,21 +46,5 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.InvalidPXActionSignature
 		{
 			VerifyCSharpDiagnostic(actual);
 		}
-
-		protected override CodeFixProvider GetCSharpCodeFixProvider() => new InvalidPXActionSignatureFix();	
-
-		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new InvalidPXActionSignatureAnalyzer();
-
-		private DiagnosticResult CreatePX100DiagnosticResult(int line, int column) =>
-			new DiagnosticResult
-			{
-				Id = Descriptors.PX1000_InvalidPXActionHandlerSignature.Id,
-				Message = Descriptors.PX1000_InvalidPXActionHandlerSignature.Title.ToString(),
-				Severity = Descriptors.PX1000_InvalidPXActionHandlerSignature.DefaultSeverity,
-				Locations =
-					new[] {
-						new DiagnosticResultLocation("Test0.cs", line, column)
-					}
-			};
 	}
 }
