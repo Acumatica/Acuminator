d5c345d82764a23e3b6d93d500c05e3a5d1deadd
metallmonster <yakutenko.va@gmail.com>
2018-09-14 18:22:20 +0300

    ATR-81: test methods names fix

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXGraphCreationDuringInitialization/PXGraphCreationDuringInitializationTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXGraphCreationDuringInitialization/PXGraphCreationDuringInitializationTests.cs
@@ -15,28 +15,28 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.PXGraphCreationDuringInitializat
 
         [Theory]
         [EmbeddedFileData("PXGraphWithCreateInstanceInInstanceConstructor.cs")]
-        public void PXGraph_Instance_Constructor_Cannot_Invoke_CreateInstance_Method(string source)
+        public void PXGraphCreationDuringInitialization_GraphInstanceConstructor_CreateInstanceUsage(string source)
         {
             VerifyCSharpDiagnostic(source, Descriptors.PX1057_PXGraphCreationDuringInitialization.CreateFor(15, 41));
         }
 
         [Theory]
         [EmbeddedFileData("PXGraphExtensionWithCreateInstanceInInitMethod.cs")]
-        public void PXGraphExtension_Initialize_Method_Cannot_Invoke_CreateInstance_Method(string source)
+        public void PXGraphCreationDuringInitialization_GraphExtensionInitialize_CreateInstanceUsage(string source)
         {
             VerifyCSharpDiagnostic(source, Descriptors.PX1057_PXGraphCreationDuringInitialization.CreateFor(14, 41));
         }
 
         [Theory]
         [EmbeddedFileData("PXGrapWithCreateInstanceInInitDelegate.cs")]
-        public void PXGraph_Init_Delegate_Cannot_Invoke_CreateInstance_Method(string source)
+        public void PXGraphCreationDuringInitialization_GraphInitDelegate_CreateInstanceUsage(string source)
         {
             VerifyCSharpDiagnostic(source, Descriptors.PX1057_PXGraphCreationDuringInitialization.CreateFor(20, 14));
         }
 
         [Theory]
         [EmbeddedFileData("PXGraphWithCreateInstanceOutsideOfInitialization.cs")]
-        public void PXGraph_CreateInstance_Method_Can_Be_Invoked_Outside_Of_Initialization(string source)
+        public void PXGraph_OutsideOfInitialization_CreateInstanceUsage(string source)
         {
             VerifyCSharpDiagnostic(source);
         }
