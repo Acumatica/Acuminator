c430777d7484e3ad28df39206d69dcc738408d0b
metallmonster <yakutenko.va@gmail.com>
2018-09-17 15:45:22 +0300

    ATR-86: an example for the diagnostic

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/Graph is saving changes during initialization/SMUserMaint.cs
@@ -0,0 +1,21 @@
+﻿using PX.Data;
+using PX.SM;
+
+namespace PX.Objects.HackathonDemo
+{
+    public class SMUserMaint : PXGraph<SMUserMaint>
+    {
+        public PXSelect<Users> Users;
+
+        public SMUserMaint()
+        {
+            int icount = Users.Select().Count;
+
+            if (icount > 1)
+            {
+                Users.Delete(Users.Current);
+                Actions.PressSave();
+            }
+        }
+    }
+}
