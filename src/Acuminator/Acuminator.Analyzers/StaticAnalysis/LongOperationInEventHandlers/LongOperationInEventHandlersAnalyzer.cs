d3a7ec461ba2345921a2a5639c8d8158dc88e722
metallmonster <yakutenko.va@gmail.com>
2018-09-17 18:59:27 +0300

    ATR-78: StartLongOperationWalker

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationInEventHandlers/LongOperationInEventHandlersAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationInEventHandlers/LongOperationInEventHandlersAnalyzer.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using Acuminator.Analyzers.StaticAnalysis.EventHandlers;
-using Acuminator.Utilities.Common;
+﻿using Acuminator.Analyzers.StaticAnalysis.EventHandlers;
 using Acuminator.Utilities.Roslyn;
-using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Syntax;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
 
 namespace Acuminator.Analyzers.StaticAnalysis.LongOperationInEventHandlers
 {
-	public class LongOperationInEventHandlersAnalyzer : IEventHandlerAnalyzer
+    public class LongOperationInEventHandlersAnalyzer : IEventHandlerAnalyzer
 	{
 		public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => 
 			ImmutableArray.Create(Descriptors.PX1046_LongOperationInEventHandlers);
@@ -26,44 +19,9 @@ namespace Acuminator.Analyzers.StaticAnalysis.LongOperationInEventHandlers
 
 			var methodSymbol = (IMethodSymbol)context.Symbol;
 			var methodSyntax = methodSymbol.GetSyntax(context.CancellationToken) as CSharpSyntaxNode;
-			var walker = new Walker(context, pxContext);
+			var walker = new StartLongOperationWalker(context, pxContext, Descriptors.PX1046_LongOperationInEventHandlers);
 
 			methodSyntax?.Accept(walker);
-		}
-
-		private class Walker : NestedInvocationWalker
-		{
-			private const string StartOperationMethodName = "StartOperation";
-
-			private readonly SymbolAnalysisContext _context;
-			private readonly PXContext _pxContext;
-
-			public Walker(SymbolAnalysisContext context, PXContext pxContext)
-				: base(context.Compilation, context.CancellationToken)
-			{
-				pxContext.ThrowOnNull(nameof (pxContext));
-
-				_context = context;
-				_pxContext = pxContext;
-			}
-
-			public override void VisitInvocationExpression(InvocationExpressionSyntax node)
-			{
-				ThrowIfCancellationRequested();
-
-				var methodSymbol = GetSymbol<IMethodSymbol>(node);
-
-				if (methodSymbol?.ContainingType?.OriginalDefinition != null
-				    && methodSymbol.ContainingType.OriginalDefinition.Equals(_pxContext.PXLongOperation)
-				    && methodSymbol.Name == StartOperationMethodName)
-				{
-					ReportDiagnostic(_context.ReportDiagnostic, Descriptors.PX1046_LongOperationInEventHandlers, node);
-				}
-				else
-				{
-					base.VisitInvocationExpression(node);
-				}
-			}
 		}
 	}
 }
