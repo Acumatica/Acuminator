46739d7433033062858ad26ee2d9714b23b0dc8f
metallmonster <yakutenko.va@gmail.com>
2018-09-18 17:47:31 +0300

    ATR-222: ChangesInPXCacheInPXGraphInitializationAnalyzer template compiling state

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ChangesInPXCache/ChangesInPXCacheInPXGraphInitializationAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ChangesInPXCache/ChangesInPXCacheInPXGraphInitializationAnalyzer.cs
@@ -1,6 +1,11 @@
 ﻿using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Utilities.Roslyn;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +14,12 @@ namespace Acuminator.Analyzers.StaticAnalysis.ChangesInPXCache
 {
     public class ChangesInPXCacheInPXGraphInitializationAnalyzer : IPXGraphAnalyzer
     {
+        public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(Descriptors.PX1059_PXGraphChangesPXCacheDuringInitialization);
+
+        public void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphSemanticModel pxGraph)
+        {
+
+        }
     }
 }
