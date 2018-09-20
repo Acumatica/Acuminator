3d9612f9ebcb97945d261a7aca586d417a6ab74d
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-09-20 12:20:11 +0300

    ATR-97: Add tests

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/RowChangesInEventHandlers/Sources/RowInserting/DirectAssignment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects
+{
+	public class SOInvoiceEntry : PXGraph<SOInvoiceEntry, SOInvoice>
+	{
+		public PXSelect<SOInvoice> Documents;
+
+		protected virtual void _(Events.RowInserting<SOInvoice> e)
+		{
+			e.Row?.RefNbr = "<NEW>"; // OK
+			Documents.Current.RefNbr = "<NEW>"; // not OK
+		}
+	}
+
+	public class SOInvoice : IBqlTable
+	{
+		#region RefNbr
+		[PXDBString(8, IsKey = true, InputMask = "")]
+		public string RefNbr { get; set; }
+		public abstract class refNbr : IBqlField { }
+		#endregion	
+	}
+}
