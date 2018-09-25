f5ac7c0c8e644a1e2b833088334b51e1608132c0
Nikomarov Sergey <snikomarov@acumatica.com>
2018-09-25 19:37:14 +0300

    ATR-239: refactoring

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ViewDeclarationOrder/ViewDeclarationOrderAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ViewDeclarationOrder/ViewDeclarationOrderAnalyzer.cs
@@ -1,25 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using Acuminator.Utilities.Roslyn;
-using Acuminator.Utilities.Roslyn.Semantic;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Acuminator.Utilities.Roslyn;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 
 namespace Acuminator.Analyzers.StaticAnalysis.ViewDeclarationOrder
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class ViewDeclarationOrderAnalyzer : PXDiagnosticAnalyzer
-    {
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+	/// <summary>
+	/// An analyzer for the order of view declaration in graph/graph extension.
+	/// </summary>
+	public class ViewDeclarationOrderAnalyzer : IPXGraphAnalyzer
+	{
+        public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
             ImmutableArray.Create(Descriptors.PX1004_ViewDeclarationOrder, Descriptors.PX1006_ViewDeclarationOrder);
 
-		internal override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
-		{
-            compilationStartContext.RegisterSymbolAction(c => AnalyzeGraphViews(c, pxContext), SymbolKind.NamedType);
-		}
-
-		private static void AnalyzeGraphViews(SymbolAnalysisContext context, PXContext pxContext)
+		
+		public void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphSemanticModel pxGraph)
 		{
 			INamedTypeSymbol graph = (INamedTypeSymbol)context.Symbol;
 
@@ -28,7 +28,8 @@ namespace Acuminator.Analyzers.StaticAnalysis.ViewDeclarationOrder
 			{
 				return;
 			}
-        
+
+            graph.GetViewsWithSymbolsFromPXGraph(pxContext);
             var graphViews = graph.GetMembers()
 								  .OfType<IFieldSymbol>()
 								  .Select(field => field.Type as INamedTypeSymbol)
