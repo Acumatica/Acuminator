9f3f143f4709e50ce7c9854c5338daff7df3b8df
Ivan Saloponov <ivan.saloponov@acumatica.com>
2018-09-26 18:04:02 +0300

    ATR-177: Tests refactoring

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacKeyFieldDeclaration/KeyFieldDacTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacKeyFieldDeclaration/KeyFieldDacTests.cs
@@ -17,31 +17,31 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.DacKeyFieldDeclaration
 		protected override CodeFixProvider GetCSharpCodeFixProvider() => new KeyFieldDeclarationFix();
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_CompoundKey.cs")]
-		public virtual void TestDacKeyFields_CompoundKey(string source) =>
+		[EmbeddedFileData("CompoundKey.cs")]
+		public virtual void TestCompoundKey(string source) =>
 			VerifyCSharpDiagnostic(source);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_Key.cs")]
-		public virtual void TestDacKeyFields_Key(string source) =>
+		[EmbeddedFileData("Key.cs")]
+		public virtual void TestKey(string source) =>
 			VerifyCSharpDiagnostic(source);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey.cs")]
-		public virtual void TestDacKeyFields_IdentityKey(string source) =>
+		[EmbeddedFileData("IdentityKey.cs")]
+		public virtual void TestIdentityKey(string source) =>
 			VerifyCSharpDiagnostic(source);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey+Key.cs")]
-		public virtual void TestDacKeyFields_IdentityKey_Key(string source) =>
+		[EmbeddedFileData("IdentityKey+Key.cs")]
+		public virtual void TestIdentityKey_Key(string source) =>
 			VerifyCSharpDiagnostic(source,
 				Descriptors.PX1055_DacKeyFieldBound.CreateFor(locations: new (int line, int column)[] { (line: 10, column: 4), (line: 10, column: 4), (line: 16, column: 4) }),
 				Descriptors.PX1055_DacKeyFieldBound.CreateFor(locations: new (int line, int column)[] { (line: 16, column: 4), (line: 10, column: 4), (line: 16, column: 4) }));
 
 		
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey+CompoundKey.cs")]
-		public virtual void TestDacKeyFields_IdentityKey_CompoundKey(string source) =>
+		[EmbeddedFileData("IdentityKey+CompoundKey.cs")]
+		public virtual void TestIdentityKey_CompoundKey(string source) =>
 			VerifyCSharpDiagnostic(source,
 				Descriptors.PX1055_DacKeyFieldBound.CreateFor(locations: new (int line, int column)[] { (line: 9, column: 4) ,
 																										(line: 9, column: 4), (line: 17, column: 4), (line: 27, column: 4)
@@ -50,42 +50,60 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.DacKeyFieldDeclaration
 				Descriptors.PX1055_DacKeyFieldBound.CreateFor(locations: new (int line, int column)[] { (line: 27, column: 4), (line: 9, column: 4), (line: 17, column: 4), (line: 27, column: 4) }));
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityNoKey.cs")]
-		public virtual void TestDacKeyFields_IdentityNoKey(string source) =>
+		[EmbeddedFileData("IdentityNoKey.cs")]
+		public virtual void TestIdentityNoKey(string source) =>
 			VerifyCSharpDiagnostic(source);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityNoKey+CompoundKey.cs")]
-		public virtual void TestDacKeyFields_IdentityNoKey_CompoundKey(string source) =>
+		[EmbeddedFileData("IdentityNoKey+CompoundKey.cs")]
+		public virtual void TestIdentityNoKey_CompoundKey(string source) =>
 			VerifyCSharpDiagnostic(source);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityNoKey+Key.cs")]
-		public virtual void TestDacKeyFields_IdentityNoKey_Key(string source) =>
+		[EmbeddedFileData("IdentityNoKey+Key.cs")]
+		public virtual void TestIdentityNoKey_Key(string source) =>
 			VerifyCSharpDiagnostic(source);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey+Key.cs",
-						"DacKeyFields_IdentityKey+Key_EditFieldsAttr_Expected.cs")]
-		public virtual void TestFixForDacKeyFields_IdentityKey_Key(string actual, string expected) =>
+		[EmbeddedFileData("IdentityKey+Key.cs",
+						"IdentityKey+Key_EditFieldsAttr_Expected.cs")]
+		public virtual void TestFixForIdentityKey_Key(string actual, string expected) =>
 		  VerifyCSharpFix(actual, expected);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey+CompoundKey.cs",
-							"DacKeyFields_IdentityKey+CompoundKey_EditFiedlsAttr_Expected.cs")]
-		public virtual void TestFixForDacKeyFields_IdentityKey_CompoundKey(string actual, string expected) =>
+		[EmbeddedFileData("IdentityKey+CompoundKey.cs",
+							"IdentityKey+CompoundKey_EditFiedlsAttr_Expected.cs")]
+		public virtual void TestFixForIdentityKey_CompoundKey(string actual, string expected) =>
 			VerifyCSharpFix(actual, expected);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey+Key.cs",
-						"DacKeyFields_IdentityKey+Key_EditIdentityAttr_Expected.cs")]
-		public virtual void TestFixForDacKeyFields_IdentityKey_Key_EditIdentityAttribute(string actual, string expected) =>
+		[EmbeddedFileData("IdentityKey+Key.cs",
+						"IdentityKey+Key_EditIdentityAttr_Expected.cs")]
+		public virtual void TestFixForIdentityKey_Key_EditIdentityAttribute(string actual, string expected) =>
 		  VerifyCSharpFix(actual, expected,1);
 
 		[Theory]
-		[EmbeddedFileData("DacKeyFields_IdentityKey+CompoundKey.cs",
-							"DacKeyFields_IdentityKey+CompoundKey_EditIdentityAttr_Expected.cs")]
-		public virtual void TestFixForDacKeyFields_IdentityKey_CompoundKey_EditIdentityAttribute(string actual, string expected) =>
+		[EmbeddedFileData("IdentityKey+CompoundKey.cs",
+							"IdentityKey+CompoundKey_EditIdentityAttr_Expected.cs")]
+		public virtual void TestFixForIdentityKey_CompoundKey_EditIdentityAttribute(string actual, string expected) =>
 			VerifyCSharpFix(actual, expected,1);
+
+		[Theory]
+		[EmbeddedFileData("IdentityKey+Key.cs",
+						"IdentityKey+Key_RemoveIdentityAttr_Expected.cs")]
+		public virtual void TestFixForIdentityKey_Key_RemoveIdentityAttribute(string actual, string expected) =>
+		  VerifyCSharpFix(actual, expected, 2);
+
+		[Theory]
+		[EmbeddedFileData("IdentityKey+CompoundKey.cs",
+							"IdentityKey+CompoundKey_RemoveIdentityAttr_Expected.cs")]
+		public virtual void TestFixForIdentityKey_CompoundKey_RemoveIdentityAttribute(string actual, string expected) =>
+			VerifyCSharpFix(actual, expected, 2);
+
+		[Theory]
+		[EmbeddedFileData("IdentityKey+CompoundKey_AttributeListSyntax.cs",
+							"IdentityKey+CompoundKey_AttributeListSyntax_Expected.cs")]
+		public virtual void TestFixForIdentityKey_CompoundKey_AttributeListSyntax(string actual, string expected) =>
+			VerifyCSharpFix(actual, expected, 2);
 	}
 }
