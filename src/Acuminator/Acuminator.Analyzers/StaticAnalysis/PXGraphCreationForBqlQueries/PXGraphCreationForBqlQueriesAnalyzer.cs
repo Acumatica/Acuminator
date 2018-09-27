42734a1b5989fdf49cb7ffccf780711e64e00baf
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-09-27 10:31:46 +0300

    ATR-130: refactoring

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraphCreationForBqlQueries/PXGraphCreationForBqlQueriesAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraphCreationForBqlQueries/PXGraphCreationForBqlQueriesAnalyzer.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Acuminator.Utilities;
-using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Syntax;
@@ -19,7 +16,7 @@ using Microsoft.CodeAnalysis.Semantics;
 namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreationForBqlQueries
 {
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	public class PXGraphCreationForBqlQueriesAnalyzer : PXDiagnosticAnalyzer
+	public partial class PXGraphCreationForBqlQueriesAnalyzer : PXDiagnosticAnalyzer
 	{
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
 			Descriptors.PX1072_PXGraphCreationForBqlQueries);
@@ -36,7 +33,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreationForBqlQueries
 			CSharpSyntaxNode body = GetBody(context.CodeBlock);
 			if (body == null) return;
 
-			var walker = new Walker(context.SemanticModel, pxContext);
+			var walker = new BqlGraphArgWalker(context.SemanticModel, pxContext);
 			body.Accept(walker);
 
 			if (walker.GraphArguments.IsEmpty) return;
@@ -108,47 +105,6 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreationForBqlQueries
 			None,
 			Constructor,
 			CreateInstance
-		}
-
-		private class Walker : CSharpSyntaxWalker
-		{
-			private const string SelectMethodName = "Select";
-			private const string SearchMethodName = "Search";
-
-			private readonly SemanticModel _semanticModel;
-			private readonly PXContext _pxContext;
-
-			private readonly List<ExpressionSyntax> _graphArguments = new List<ExpressionSyntax>();
-			public ImmutableArray<ExpressionSyntax> GraphArguments => _graphArguments.ToImmutableArray();
-
-			public Walker(SemanticModel semanticModel, PXContext pxContext)
-			{
-				semanticModel.ThrowOnNull(nameof (semanticModel));
-				pxContext.ThrowOnNull(nameof (pxContext));
-
-				_semanticModel = semanticModel;
-				_pxContext = pxContext;
-			}
-
-			public override void VisitInvocationExpression(InvocationExpressionSyntax node)
-			{
-				var symbolInfo = _semanticModel.GetSymbolInfo(node);
-				var methodSymbol = (symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.FirstOrDefault()) as IMethodSymbol;
-
-				// Check BQL Select / Search methods by name because
-				// variations of these methods are declared in different PXSelectBase-derived classes
-				var declaringType = methodSymbol?.ContainingType?.OriginalDefinition;
-
-				if (declaringType != null && declaringType.IsBqlCommand(_pxContext) 
-					&& !methodSymbol.Parameters.IsEmpty && methodSymbol.Parameters[0].Type.IsPXGraph(_pxContext) 
-					&& node.ArgumentList.Arguments.Count > 0 &&
-				    (methodSymbol.Name.StartsWith(SelectMethodName, StringComparison.Ordinal) ||
-				     methodSymbol.Name.StartsWith(SearchMethodName, StringComparison.Ordinal)))
-				{
-					var graphArg = node.ArgumentList.Arguments[0].Expression;
-					_graphArguments.Add(graphArg);
-				}
-			}
 		}
 	}
 }
