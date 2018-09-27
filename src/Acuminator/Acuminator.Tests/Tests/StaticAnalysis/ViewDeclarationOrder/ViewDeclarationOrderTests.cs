329891b3e345b58d656f56b63dab38a672aed433
Nikomarov Sergey <snikomarov@acumatica.com>
2018-09-27 15:40:28 +0300

    ATR-239: Added tests for graph inheritance

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ViewDeclarationOrder/ViewDeclarationOrderTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ViewDeclarationOrder/ViewDeclarationOrderTests.cs
@@ -14,12 +14,21 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.ViewDeclarationOrder
 			new PXGraphAnalyzer(new ViewDeclarationOrderAnalyzer());
 
 		[Theory]
-        [EmbeddedFileData("ViewDeclarationOrder.cs")]
+        [EmbeddedFileData("ViewDeclarationOrderInOneGraph.cs")]
         public void SimpleGraph(string actual)
         {
             VerifyCSharpDiagnostic(actual, 
 				Descriptors.PX1006_ViewDeclarationOrder.CreateFor(9, 29, "Vendor", "BAccount"),
 	            Descriptors.PX1004_ViewDeclarationOrder.CreateFor(18, 28, "Customer", "BAccount"));
+        }
+
+		[Theory]
+        [EmbeddedFileData("ViewDeclarationOrderWithGraphInheritance.cs")]
+        public void GraphInheritance(string actual)
+        {
+            VerifyCSharpDiagnostic(actual, 
+				Descriptors.PX1004_ViewDeclarationOrder.CreateFor(25, 29, "SOInvoice", "ARInvoice"),
+	            Descriptors.PX1006_ViewDeclarationOrder.CreateFor(27, 26, "SOTran", "ARTran"));
         }
     }
 }
