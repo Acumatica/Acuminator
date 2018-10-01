1d9a591dc1f48f1e65564ae769c28e276f0035ed
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-10-01 15:22:05 +0300

    ATR-130: Refactoring: extract Create Instance logic

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraphCreationForBqlQueries/PXGraphCreationForBqlQueriesAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraphCreationForBqlQueries/PXGraphCreationForBqlQueriesAnalyzer.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Acuminator.Utilities;
 using Acuminator.Utilities.Roslyn;
 using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Acuminator.Utilities.Roslyn.Syntax;
+using Acuminator.Utilities.Roslyn.Syntax.PXGraph;
 using CommonServiceLocator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -63,7 +65,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreationForBqlQueries
 
 			foreach (var graphArgSyntax in walker.GraphArguments)
 			{
-				var instantiationType = GetGraphInstantiationType(graphArgSyntax, context.SemanticModel, pxContext);
+				var instantiationType = graphArgSyntax.GetGraphInstantiationType(context.SemanticModel, pxContext);
 
 				if (instantiationType != GraphInstantiationType.None && existingGraphs.Count > 0
 				    || availableGraphs.Length > 0 && context.SemanticModel.GetSymbolInfo(graphArgSyntax).Symbol is ILocalSymbol localVar 
@@ -90,35 +92,6 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreationForBqlQueries
 			}
 		}
 
-		private GraphInstantiationType GetGraphInstantiationType(SyntaxNode node, SemanticModel semanticModel, 
-			PXContext pxContext)
-		{
-			// new PXGraph()
-			if (node is ObjectCreationExpressionSyntax objCreationSyntax && objCreationSyntax.Type != null
-			                                                             && semanticModel
-				                                                             .GetSymbolInfo(objCreationSyntax.Type)
-				                                                             .Symbol is ITypeSymbol typeSymbol
-			                                                             && typeSymbol.IsPXGraph())
-			{
-				return GraphInstantiationType.Constructor;
-			}
-
-			// PXGraph.CreateInstance
-			if (node is InvocationExpressionSyntax invocationSyntax)
-			{
-				var symbolInfo = semanticModel.GetSymbolInfo(invocationSyntax);
-				var methodSymbol = (symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.FirstOrDefault()) as IMethodSymbol;
-				methodSymbol = methodSymbol?.OverriddenMethod?.OriginalDefinition ?? methodSymbol?.OriginalDefinition;
-
-				if (methodSymbol != null && pxContext.PXGraphRelatedMethods.CreateInstance.Contains(methodSymbol))
-				{
-					return GraphInstantiationType.CreateInstance;
-				}
-			}
-
-			return GraphInstantiationType.None;
-		}
-
 		private IEnumerable<ISymbol> GetSymbolUsages(CSharpSyntaxNode node, 
 			IEnumerable<ISymbol> symbols, SemanticModel semanticModel, IEnumerable<SyntaxNode> nodesToSkip)
 		{
@@ -133,14 +106,6 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreationForBqlQueries
 				if (symbol != null && symbolsSet.Contains(symbol))
 					yield return symbol;
 			}
-		}
-
-
-		private enum GraphInstantiationType
-		{
-			None,
-			Constructor,
-			CreateInstance
 		}
 	}
 }
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Syntax/PXGraph/GraphSyntaxUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Syntax.PXGraph
+{
+	public static class GraphSyntaxUtils
+	{
+		/// <summary>
+		/// Determines PXGraph instantiation type for the syntax node (e.g. new PXGraph(), PXGraph.CreateInstance, etc.)
+		/// </summary>
+		public static GraphInstantiationType GetGraphInstantiationType(this SyntaxNode node, SemanticModel semanticModel, 
+			PXContext pxContext)
+		{
+			node.ThrowOnNull(nameof (node));
+			semanticModel.ThrowOnNull(nameof (semanticModel));
+			pxContext.ThrowOnNull(nameof (pxContext));
+
+			// new PXGraph()
+			if (node is ObjectCreationExpressionSyntax objCreationSyntax && objCreationSyntax.Type != null
+			                                                             && semanticModel
+				                                                             .GetSymbolInfo(objCreationSyntax.Type)
+				                                                             .Symbol is ITypeSymbol typeSymbol
+			                                                             && typeSymbol.IsPXGraph(pxContext))
+			{
+				return typeSymbol.Equals(pxContext.PXGraphType)
+					? GraphInstantiationType.ConstructorOfBaseType
+					: GraphInstantiationType.ConstructorOfSpecificType;
+			}
+
+			// PXGraph.CreateInstance
+			if (node is InvocationExpressionSyntax invocationSyntax)
+			{
+				var symbolInfo = semanticModel.GetSymbolInfo(invocationSyntax);
+				var methodSymbol = (symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.FirstOrDefault()) as IMethodSymbol;
+				methodSymbol = methodSymbol?.OverriddenMethod?.OriginalDefinition ?? methodSymbol?.OriginalDefinition;
+
+				if (methodSymbol != null && pxContext.PXGraphRelatedMethods.CreateInstance.Contains(methodSymbol))
+				{
+					return GraphInstantiationType.CreateInstance;
+				}
+			}
+
+			return GraphInstantiationType.None;
+		}
+	}
+}
