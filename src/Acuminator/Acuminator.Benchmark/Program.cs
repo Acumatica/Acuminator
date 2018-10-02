ec79e63e5663bba952c259bb11c69d500137b00a
Vladimir Panchenko <vpanchenko@acumatica.com>
2018-10-02 17:59:03 +0300

    ATR-256: add benchmark that performs static code analysis of the demo project using BenchmarkDotNet

---
+++ src/Acuminator/Acuminator.Benchmark/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Parameters;
+using BenchmarkDotNet.Running;
+
+namespace Acuminator.Benchmark
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<RoslynCodeAnalysis>();
+		}
+	}
+}
