8a07595cd5fc4c53effb8d72e7a3db61705c80c5
Nikomarov Sergey <snikomarov@acumatica.com>
2018-10-25 15:34:24 +0300

    decepticode: navigation utils

---
+++ src/Acuminator/Acuminator.Vsix/Utils/Navigation/VSDocumentNavigation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acuminator.Vsix.Utilities;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+
+using TextSpan = Microsoft.CodeAnalysis.Text.TextSpan;
+using Document = Microsoft.CodeAnalysis.Document;
+
+namespace Acuminator.Vsix.Utils.Navigation
+{
+	public static class VSDocumentNavigation
+	{
+		public static 
+	}
+}
