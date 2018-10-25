922c0ac5915a25445bdb16fd31239c22bab5ed09
Ilyas Tazitdinov <itazitdinov@acumatica.com>
2018-10-25 19:46:06 +0300

    initial forcegraph

---
+++ src/ForceGraph/ForceGraph/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using GlmNet;
+using SharpGL;
+using SharpGL.Shaders;
+using SharpGL.VertexBuffers;
+using System;
+using System.Windows;
+
+namespace ForceGraph
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        mat4 _projectionMatrix;
+        mat4 _viewMatrix;
+        mat4 _modelMatrix;
+
+        ForceGraphScene _forceGraph;
+
+        private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            var gl = args.OpenGL;
+            gl.ClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+
+            const float rads = (60.0f / 360.0f) * (float)Math.PI * 2.0f;
+            _projectionMatrix = glm.perspective(rads, 800 / 600, 0.1f, 100.0f);
+            _viewMatrix = glm.translate(new mat4(1.0f), new vec3(0.0f, 0.0f, -5.0f));
+            _modelMatrix = glm.scale(new mat4(1.0f), new vec3(2.5f));
+
+            _forceGraph = new ForceGraphScene();
+            _forceGraph.Init(gl);
+        }
+
+        private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            var gl = args.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
+
+            _forceGraph.Render(gl, _projectionMatrix, _viewMatrix, _modelMatrix);
+        }
+    }
+}
