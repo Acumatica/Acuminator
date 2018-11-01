f7048e85aaff839b372b5c55f912dc601aec0afb
metallmonster <yakutenko.va@gmail.com>
2018-11-01 15:47:00 +0300

    ATR-254:ThrowingExceptionsInLongRunningOperationAnalyzer

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/Descriptors.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/Descriptors.cs
@@ -221,7 +221,7 @@ namespace Acuminator.Analyzers.StaticAnalysis
         public static DiagnosticDescriptor PX1085_DatabaseQueriesInPXGraphInitialization { get; } =
             Rule("PX1085", nameof(Resources.PX1085Title).GetLocalized(), Category.Default, DiagnosticSeverity.Error);
 
-        public static DiagnosticDescriptor PX1086_ThrowingSetupNotEnteredExceptionInPXGraphInitialization { get; } =
+        public static DiagnosticDescriptor PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation { get; } =
             Rule("PX1086", nameof(Resources.PX1086Title).GetLocalized(), Category.Default, DiagnosticSeverity.Error);
 
         public static DiagnosticDescriptor PX1087_CausingStackOverflowExceptionInBaseViewDelegateInvocation { get; } =
--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ThrowingExceptions/ThrowingExceptionsInDataViewDelegateAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ThrowingExceptions/ThrowingExceptionsInDataViewDelegateAnalyzer.cs
@@ -1,18 +1,22 @@
-﻿using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+﻿using Acuminator.Analyzers.StaticAnalysis.LongOperationStart;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Utilities;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
 
 namespace Acuminator.Analyzers.StaticAnalysis.ThrowingExceptions
 {
-    public class ThrowingExceptionsInDataViewDelegateAnalyzer : IPXGraphAnalyzer
+    public class ThrowingExceptionsInLongRunningOperationAnalyzer : IPXGraphAnalyzer
     {
         public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
-            ImmutableArray.Create(Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInPXGraphInitialization);
+            ImmutableArray.Create(Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation);
 
         public void Analyze(SymbolAnalysisContext context, PXContext pxContext, CodeAnalysisSettings settings, PXGraphSemanticModel pxGraph)
         {
@@ -20,10 +24,83 @@ namespace Acuminator.Analyzers.StaticAnalysis.ThrowingExceptions
 
             var walker = new Walker(context, pxContext);
 
-            foreach (var viewDel in pxGraph.ViewDelegates)
+            CheckProcessingDelegates(pxGraph, walker, context.CancellationToken);
+            CheckLongOperationStartDelegates(pxGraph.Symbol, walker, pxContext, context.Compilation, context.CancellationToken);
+        }
+
+        private void CheckProcessingDelegates(PXGraphSemanticModel pxGraph, Walker walker, CancellationToken cancellation)
+        {
+            cancellation.ThrowIfCancellationRequested();
+
+            if (!pxGraph.IsProcessing)
             {
-                walker.Visit(viewDel.Node);
+                return;
             }
+
+            var processingViews = pxGraph.Views.Where(v => v.IsProcessing);
+
+            foreach (var viewDel in processingViews)
+            {
+                var finallyDelegates = viewDel.FinallyProcessDelegates.Where(d => d.Node != null);
+
+                foreach (var finDel in finallyDelegates)
+                {
+                    cancellation.ThrowIfCancellationRequested();
+                    walker.Visit(finDel.Node);
+                }
+
+                var parametersDelegates = viewDel.ParametersDelegates.Where(d => d.Node != null);
+
+                foreach (var parDel in parametersDelegates)
+                {
+                    cancellation.ThrowIfCancellationRequested();
+                    walker.Visit(parDel.Node);
+                }
+
+                var processDelegates = viewDel.ProcessDelegates.Where(d => d.Node != null);
+
+                foreach (var processDel in processDelegates)
+                {
+                    cancellation.ThrowIfCancellationRequested();
+                    walker.Visit(processDel.Node);
+                }
+            }
+        }
+
+        private void CheckLongOperationStartDelegates(ISymbol symbol, Walker walker, PXContext pxContext,
+                                                      Compilation compilation, CancellationToken cancellation)
+        {
+            cancellation.ThrowIfCancellationRequested();
+
+            var longOperationDelegates = GetLongOperationStartDelegates(symbol, pxContext, compilation, cancellation);
+
+            foreach (var loDel in longOperationDelegates)
+            {
+                cancellation.ThrowIfCancellationRequested();
+                walker.Visit(loDel);
+            }
+        }
+
+        private IEnumerable<SyntaxNode> GetLongOperationStartDelegates(ISymbol symbol, PXContext pxContext, Compilation compilation,
+                                                                       CancellationToken cancellation)
+        {
+            cancellation.ThrowIfCancellationRequested();
+
+            var loDelegateNodeList = new List<SyntaxNode>();
+            var declaringNodes = symbol.DeclaringSyntaxReferences
+                                 .Select(r => r.GetSyntax(cancellation));
+
+            foreach (var node in declaringNodes)
+            {
+                cancellation.ThrowIfCancellationRequested();
+
+                var loStartWalker = new StartLongOperationDelegateWalker(pxContext, compilation, cancellation);
+
+                loStartWalker.Visit(node);
+                loDelegateNodeList.AddRange(loStartWalker.Delegates);
+            }
+
+            return loDelegateNodeList;
         }
 
         private class Walker : WalkerBase
@@ -39,7 +116,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.ThrowingExceptions
 
                 if (IsPXSetupNotEnteredException(node))
                 {
-                    ReportDiagnostic(_context.ReportDiagnostic, Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInPXGraphInitialization, node);
+                    ReportDiagnostic(_context.ReportDiagnostic, Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation, node);
                 }
                 else
                 {
