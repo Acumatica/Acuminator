1206411d659682e6085aa1686d1c08dbb302a3cc
Nikomarov Sergey <snikomarov@acumatica.com>
2018-11-13 02:45:21 +0300

    ATR-282: added ShouldAnalyze method to aggregated analyzer's ISymbolAnalyzer interface + added logic that use ShouldAnalyze check to analyzer aggregators

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/AnalyzersAggregator/ISymbolAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/AnalyzersAggregator/ISymbolAnalyzer.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.CodeAnalysis;
 using System.Collections.Immutable;
+using Acuminator.Utilities.Roslyn.Semantic;
 
 namespace Acuminator.Analyzers.StaticAnalysis.AnalyzersAggregator
 {
     public interface ISymbolAnalyzer
     {
         ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
-    }
+
+		bool ShouldAnalyze(PXContext pxContext);
+	}
 }
