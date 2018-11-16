0538f59387d563a1468ac76c424ef2f2de0da8fc
metallmonster <yakutenko.va@gmail.com>
2018-11-16 16:49:13 +0300

    ATR-107: boilerplate

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacUiAttributes/Sources/Dac_Bad.cs
@@ -0,0 +1,30 @@
+﻿using PX.Data;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.DacUiAttributes.Sources
+{
+	
+	public class SOOrder : IBqlTable
+	{
+		[PXDBString(IsKey = true, InputMask = "")]
+		[PXDefault]
+		[PXUIField(DisplayName = "Order Type")]
+		public string OrderType { get; set; }
+		public abstract class orderType : IBqlField { }
+
+		[PXDBString(IsKey = true, InputMask = "")]
+		[PXDefault]
+		[PXUIField(DisplayName = "Order Nbr.")]
+		public string OrderNbr { get; set; }
+		public abstract class orderNbr : IBqlField { }
+
+		[PXStringList(new[] { "N", "O" }, new[] { "New", "Open" })]
+		[PXDBString]
+		[PXUIField(DisplayName = "Status")]
+		public string Status { get; set; }
+		public abstract class status : IBqlField { }
+
+		[PXDBTimestamp]
+		public virtual byte[] tstamp { get; set; }
+		public abstract class Tstamp : IBqlField { }
+	}
+}
