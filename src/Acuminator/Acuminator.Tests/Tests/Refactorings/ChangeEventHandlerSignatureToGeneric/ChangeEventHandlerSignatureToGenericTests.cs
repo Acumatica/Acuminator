b5daf2de24f5b982de7b0fd7ab6786be620b7ee9
Nikomarov Sergey <snikomarov@acumatica.com>
2018-12-03 16:42:21 +0300

    decepticode: uncommented tests and added ValueTuples nuget to fix unit tests and dependencies

--- src/Acuminator/Acuminator.Tests/Tests/Refactorings/ChangeEventHandlerSignatureToGeneric/ChangeEventHandlerSignatureToGenericTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/Refactorings/ChangeEventHandlerSignatureToGeneric/ChangeEventHandlerSignatureToGenericTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using Acuminator.Analyzers.Refactorings.ChangeEventHandlerSignatureToGeneric;
+using Acuminator.Analyzers.Refactorings.ChangeEventHandlerSignatureToGeneric;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
 using Microsoft.CodeAnalysis.CodeRefactorings;
@@ -13,64 +13,64 @@ using Xunit;
 
 namespace Acuminator.Tests.Tests.Refactorings.ChangeEventHandlerSignatureToGeneric
 {
-	//public class ChangeEventHandlerSignatureToGenericTests : CodeRefactoringVerifier
-	//{
-	//	protected override CodeRefactoringProvider GetCSharpCodeRefactoringProvider() => new ChangeEventHandlerSignatureToGenericRefactoring();
+	public class ChangeEventHandlerSignatureToGenericTests : CodeRefactoringVerifier
+	{
+		protected override CodeRefactoringProvider GetCSharpCodeRefactoringProvider() => new ChangeEventHandlerSignatureToGenericRefactoring();
 
-	//	[Theory]
-	//	[EmbeddedFileData("CacheAttached.cs", "CacheAttached_Expected.cs")]
-	//	public Task CacheAttached(string actual, string expected)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("CacheAttached.cs", "CacheAttached_Expected.cs")]
+		public Task CacheAttached(string actual, string expected)
+		{
+			return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("RowEventHandler.cs", "RowEventHandler_Expected.cs")]
-	//	public Task RowEventHandler(string actual, string expected)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("RowEventHandler.cs", "RowEventHandler_Expected.cs")]
+		public Task RowEventHandler(string actual, string expected)
+		{
+			return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("FieldEventHandler.cs", "FieldEventHandler_Expected.cs")]
-	//	public Task FieldEventHandler(string actual, string expected)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("FieldEventHandler.cs", "FieldEventHandler_Expected.cs")]
+		public Task FieldEventHandler(string actual, string expected)
+		{
+			return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("CacheAttachedWithArgUsages.cs", "CacheAttachedWithArgUsages_Expected.cs")]
-	//	public Task CacheAttachedWithArgUsages(string actual, string expected)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("CacheAttachedWithArgUsages.cs", "CacheAttachedWithArgUsages_Expected.cs")]
+		public Task CacheAttachedWithArgUsages(string actual, string expected)
+		{
+			return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("EventHandlerWithArgsUsages.cs", "EventHandlerWithArgsUsages_Expected.cs")]
-	//	public Task EventHandlerWithArgsUsages(string actual, string expected)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("EventHandlerWithArgsUsages.cs", "EventHandlerWithArgsUsages_Expected.cs")]
+		public Task EventHandlerWithArgsUsages(string actual, string expected)
+		{
+			return VerifyCSharpRefactoringAsync(actual, expected, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("AdditionalParameters.cs")]
-	//	public Task AdditionalParameters_ShouldNotSuggestRefactoring(string actual)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, actual, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("AdditionalParameters.cs")]
+		public Task AdditionalParameters_ShouldNotSuggestRefactoring(string actual)
+		{
+			return VerifyCSharpRefactoringAsync(actual, actual, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("Override.cs")]
-	//	public Task Override_ShouldNotSuggestRefactoring(string actual)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, actual, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
+		[Theory]
+		[EmbeddedFileData("Override.cs")]
+		public Task Override_ShouldNotSuggestRefactoring(string actual)
+		{
+			return VerifyCSharpRefactoringAsync(actual, actual, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
 
-	//	[Theory]
-	//	[EmbeddedFileData("PXOverride.cs")]
-	//	public Task PXOverride_ShouldNotSuggestRefactoring(string actual)
-	//	{
-	//		return VerifyCSharpRefactoringAsync(actual, actual, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
-	//	}
-	//}
+		[Theory]
+		[EmbeddedFileData("PXOverride.cs")]
+		public Task PXOverride_ShouldNotSuggestRefactoring(string actual)
+		{
+			return VerifyCSharpRefactoringAsync(actual, actual, root => root.DescendantNodes().OfType<MethodDeclarationSyntax>().First());
+		}
+	}
 }
