aa69557c83109a38bd15069e68feed0b104d8a4a
Nikomarov Sergey <snikomarov@acumatica.com>
2018-12-19 14:26:38 +0300

    ATR-301: added workspace changed event utils

---
+++ src/Acuminator/Acuminator.Vsix/Utils/WorkspaceChangedUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Acuminator.Utilities.Common;
+
+
+
+namespace Acuminator.Vsix.Utilities
+{
+    /// <summary>
+    /// A helper class with utility methods related to the workspace changed event.
+    /// </summary>
+    public static class WorkspaceChangedUtils
+	{       
+        public static bool IsActiveDocumentChanged(this WorkspaceChangeEventArgs changeEventArgs, Document oldDocument)
+		{
+			oldDocument.ThrowOnNull(nameof(oldDocument));
+			changeEventArgs.ThrowOnNull(nameof(changeEventArgs));
+
+			if (changeEventArgs.Kind != WorkspaceChangeKind.DocumentChanged)
+				return false;
+			
+			return HaveDocumentIdOrProjectIdChanged(changeEventArgs, oldDocument);
+		}
+
+		public static bool IsDocumentTextChanged(this WorkspaceChangeEventArgs changeEventArgs, Document oldDocument)
+		{
+			oldDocument.ThrowOnNull(nameof(oldDocument));
+			changeEventArgs.ThrowOnNull(nameof(changeEventArgs));
+
+			if (changeEventArgs.Kind != WorkspaceChangeKind.DocumentChanged)
+				return false;
+
+			return !HaveDocumentIdOrProjectIdChanged(changeEventArgs, oldDocument);
+		}
+
+		private static bool HaveDocumentIdOrProjectIdChanged(WorkspaceChangeEventArgs changeEventArgs, Document oldDocument) =>
+			oldDocument.Id != changeEventArgs.DocumentId || oldDocument.Project.Id != changeEventArgs.ProjectId;
+	}
+}
