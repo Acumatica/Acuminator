fef1af45c5a04b0288bb86b0672911920c2739ff
metallmonster <yakutenko.va@gmail.com>
2019-01-18 17:42:00 +0300

    ATR-107: remove unnecessary usings

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DacUiAttributes/DacUiAttributesFix.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DacUiAttributes/DacUiAttributesFix.cs
@@ -5,12 +5,8 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
