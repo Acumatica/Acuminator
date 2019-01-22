a9054f591e865d535ab43941d84834a36a8f9ca1
Nikomarov Sergey <snikomarov@acumatica.com>
2019-01-22 14:49:11 +0300

    ATR-309: Added support for generic events to CodeMap

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Base/GraphEventCategoryNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Base/GraphEventCategoryNodeViewModel.cs
@@ -33,7 +33,7 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 			(dacGroupVM, eventInfo) => new GraphMemberNodeViewModel(dacGroupVM.GraphMemberCategoryVM, eventInfo);
 
 		protected GraphEventCategoryNodeViewModel(GraphNodeViewModel graphViewModel, GraphMemberType graphMemberType,
-												  bool isExpanded) : 
+												  bool isExpanded) :
 										   base(graphViewModel, graphMemberType, isExpanded)
 		{
 			_name = CategoryDescription;
@@ -42,8 +42,8 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 		protected override void AddCategoryMembers()
 		{
 			var graphSemanticModel = GraphViewModel.GraphSemanticModel;
-			var graphCategoryEvents = GetCategoryGraphNodeSymbols()?.OfType<GraphEventInfo>();
-
+			var graphCategoryEvents = GetCategoryGraphNodeSymbols()?.OfType<GraphEventInfo>()
+																	.Where(eventInfo => eventInfo.SignatureType != EventHandlerSignatureType.None);
 			if (graphCategoryEvents.IsNullOrEmpty())
 				return;
 
--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/GraphMembers/FieldEventNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/GraphMembers/FieldEventNodeViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Acuminator.Vsix.Utilities;
 using Acuminator.Vsix.Utilities.Navigation;
@@ -27,11 +28,8 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 								base(dacViewModel?.GraphMemberCategoryVM, memberInfo, isExpanded)
 		{
 			DacViewModel = dacViewModel;
-
-			int startPos = dacViewModel.DacName.Length + 1;
-			int lastUnderscoreIndex = MemberSymbol.Name.LastIndexOf('_');
-			Name = lastUnderscoreIndex > 0
-				? MemberSymbol.Name.Substring(startPos)
+			Name = MemberInfo is GraphEventInfo eventInfo 
+				? eventInfo.EventType.ToString()
 				: MemberSymbol.Name;
 		}	
 	}
