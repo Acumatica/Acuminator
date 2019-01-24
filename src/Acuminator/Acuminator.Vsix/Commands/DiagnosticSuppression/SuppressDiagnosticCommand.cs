477766dec0d9e298df8bdc6e7e1a4f83b0b5d428
Nikomarov Sergey <snikomarov@acumatica.com>
2019-01-24 21:21:12 +0300

    ATR-304: added suppress diagnostic command

---
+++ src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/SuppressDiagnosticCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Acuminator.Vsix.Utilities;
+using TextSpan = Microsoft.CodeAnalysis.Text.TextSpan;
+using Document = Microsoft.CodeAnalysis.Document;
+
+
+namespace Acuminator.Vsix.GoToDeclaration
+{
+	/// <summary>
+	/// Suppress Diagnostic Command.
+	/// </summary>
+	internal sealed class SuppressDiagnosticCommand : VSCommandBase
+	{
+		private static int _isCommandInitialized = NOT_INITIALIZED;
+
+		/// <summary>
+		/// Suppress Diagnostic command ID.
+		/// </summary>
+		public const int SuppressDiagnosticCommandId = 0x0104;
+
+		protected override bool CanModifyDocument => true;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SuppressDiagnosticCommand"/> class.
+		/// Adds our command handlers for menu (commands must exist in the command table file)
+		/// </summary>
+		/// <param name="package">Owner package, not null.</param>
+		private SuppressDiagnosticCommand(Package package) : base(package, SuppressDiagnosticCommandId)
+		{
+		}
+
+		/// <summary>
+		/// Gets the instance of the command.
+		/// </summary>
+		public static SuppressDiagnosticCommand Instance
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Initializes the singleton instance of the command.
+		/// </summary>
+		/// <param name="package">Owner package, not null.</param>
+		public static void Initialize(Package package)
+		{
+			if (Interlocked.CompareExchange(ref _isCommandInitialized, value: INITIALIZED, comparand: NOT_INITIALIZED) == NOT_INITIALIZED)
+			{
+				Instance = new SuppressDiagnosticCommand(package);
+			}
+		}
+
+		protected override void CommandCallback(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
