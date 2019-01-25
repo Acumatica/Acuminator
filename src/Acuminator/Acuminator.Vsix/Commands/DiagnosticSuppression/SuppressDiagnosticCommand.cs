d9ecfc64c1db3e2b8e13150e915aa1aa4fcb6f81
Nikomarov Sergey <snikomarov@acumatica.com>
2019-01-25 12:38:57 +0300

    ATR-304: work on implementation

--- src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/SuppressDiagnosticCommand.cs
+++ src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/SuppressDiagnosticCommand.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.ComponentModel.Design;
-using System.Globalization;
 using System.Linq;
 using System.Threading;
-using EnvDTE;
-using EnvDTE80;
+using System.Windows;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.Text.Outlining;
-using Microsoft.VisualStudio.TextManager.Interop;
 using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Acuminator.Vsix.Utilities;
+using Acuminator.Utilities.DiagnosticSuppression;
+
 using TextSpan = Microsoft.CodeAnalysis.Text.TextSpan;
 using Document = Microsoft.CodeAnalysis.Document;
 
@@ -76,7 +72,73 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 
 		protected override void CommandCallback(object sender, EventArgs e)
 		{
-			
+			IWpfTextView textView = ServiceProvider.GetWpfTextView();
+
+			if (textView == null)
+				return;
+
+			SnapshotPoint caretPosition = textView.Caret.Position.BufferPosition;
+			ITextSnapshotLine caretLine = caretPosition.GetContainingLine();
+
+			if (caretLine == null)
+				return;
+
+			Document document = caretPosition.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
+			if (document == null)
+				return;
+
+			(SyntaxNode syntaxRoot, SemanticModel semanticModel) = ThreadHelper.JoinableTaskFactory.Run(
+				async () => (await document.GetSyntaxRootAsync(), await document.GetSemanticModelAsync()));
+
+			if (syntaxRoot == null || semanticModel == null || !IsPlatformReferenced(semanticModel))
+				return;
+
+			TextSpan caretSpan = GetTextSpanFromCaret(caretPosition, caretLine);
+			SyntaxNode syntaxNode = syntaxRoot.FindNode(caretSpan);
+			SyntaxNode targetNode = SuppressionManager.FindTargetNode(syntaxNode);
+
+			SuppressDiagnosticsOnTargetNode(targetNode, semanticModel);	
+		}
+
+		private TextSpan GetTextSpanFromCaret(SnapshotPoint caretPosition, ITextSnapshotLine caretLine)
+		{
+			if (caretLine.Length == 0)
+			{
+				return TextSpan.FromBounds(caretLine.Start.Position, caretLine.End.Position);
+			}
+			else if (caretPosition.Position < caretLine.End.Position)
+			{
+				var nextPoint = caretPosition.Add(1);
+				return TextSpan.FromBounds(caretPosition.Position, nextPoint.Position);
+			}
+			else
+			{
+				var prevPoint = caretPosition.Add(-1);
+				return TextSpan.FromBounds(prevPoint.Position, caretPosition.Position);
+			}
+		}
+
+		private bool IsPlatformReferenced(SemanticModel semanticModel)
+		{
+			PXContext context = new PXContext(semanticModel.Compilation);
+			return context.IsPlatformReferenced;
+		}
+
+		private void SuppressDiagnosticsOnTargetNode(SyntaxNode targetNode, SemanticModel semanticModel)
+		{
+			if (targetNode == null)
+				return;
+
+
+
+
+			//if (!targetNode.ContainsDiagnostics)
+			//{
+			//	MessageBox.Show(VSIXResource.DiagnosticSuppression_NoDiagnosticFound, AcuminatorVSPackage.PackageName);
+			//	return false;
+			//}
+
+			//return true;
 		}
 	}
 }
