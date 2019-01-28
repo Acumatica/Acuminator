3d8ba2111ba831f49dfca3a9913ab17afbab7421
Nikomarov Sergey <snikomarov@acumatica.com>
2019-01-28 18:20:04 +0300

    ATR-304: reflection hell to access IDiagnosticAnalyzerService

--- src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/SuppressDiagnosticCommand.cs
+++ src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/SuppressDiagnosticCommand.cs
@@ -4,10 +4,13 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
@@ -71,7 +74,7 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 		}
 
 		protected override void CommandCallback(object sender, EventArgs e)
-		{
+		{		
 			IWpfTextView textView = ServiceProvider.GetWpfTextView();
 
 			if (textView == null)
@@ -84,7 +87,7 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 				return;
 
 			Document document = caretPosition.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
-			if (document == null)
+			if (document == null || !document.SupportsSyntaxTree /*|| document.SourceCodeKind ==*/)
 				return;
 
 			(SyntaxNode syntaxRoot, SemanticModel semanticModel) = ThreadHelper.JoinableTaskFactory.Run(
@@ -93,11 +96,86 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 			if (syntaxRoot == null || semanticModel == null || !IsPlatformReferenced(semanticModel))
 				return;
 
+
 			TextSpan caretSpan = GetTextSpanFromCaret(caretPosition, caretLine);
 			SyntaxNode syntaxNode = syntaxRoot.FindNode(caretSpan);
 			SyntaxNode targetNode = SuppressionManager.FindTargetNode(syntaxNode);
 
-			SuppressDiagnosticsOnTargetNode(targetNode, semanticModel);	
+			var componentModel = ServiceProvider.GetService(typeof(Microsoft.VisualStudio.ComponentModelHost.SComponentModel)) as Microsoft.VisualStudio.ComponentModelHost.IComponentModel;
+
+			if (componentModel == null)
+				return;
+
+			var refAssemblyTypes = AppDomain.CurrentDomain.GetAssemblies()
+														  .Where(a => a.FullName.StartsWith("Microsoft.CodeAnalysis"))
+														  .SelectMany(a => a.GetTypes())
+														  .Where(type => type.IsInterface && type.Name == "IDiagnosticAnalyzerService")
+														  .ToArray();
+
+			var diagnosticAnalyzerServiceType = refAssemblyTypes.FirstOrDefault();
+			var diagnosticDataType = typeof(DocumentId).Assembly.GetTypes()																												
+																.Where(type => type.Name == "DiagnosticData")
+																.FirstOrDefault();	
+			
+			if (diagnosticAnalyzerServiceType == null || diagnosticDataType == null)
+				return;
+
+			var componentModelType = componentModel.GetType();
+			var methodInfo = componentModelType.GetMethod(nameof(componentModel.GetService))
+											  ?.MakeGenericMethod(diagnosticAnalyzerServiceType);
+			
+			if (methodInfo == null)
+				return;
+
+			try
+			{
+				var service = methodInfo.Invoke(componentModel, null);
+
+				if (service == null)
+					return;
+
+				MethodInfo getDiagnosticMethod = diagnosticAnalyzerServiceType.GetMethod("GetDiagnosticsForSpanAsync");
+
+				if (getDiagnosticMethod == null)
+					return;
+
+				dynamic dataTask = getDiagnosticMethod.Invoke(service, new object[] { document, caretSpan, null, false, CancellationToken.None });
+
+				if (!(dataTask is System.Threading.Tasks.Task task))
+					return;
+
+				task.Wait();
+
+
+				Type genericIEnumerableType = typeof(IEnumerable<>).MakeGenericType(diagnosticDataType);
+
+				if (genericIEnumerableType == null)
+					return;
+
+				Type genericTask = typeof(System.Threading.Tasks.Task<>).MakeGenericType(genericIEnumerableType);
+				var resultPropertyInfo = genericTask?.GetProperty(nameof(System.Threading.Tasks.Task<object>.Result));
+
+				if (resultPropertyInfo == null)
+					return;
+
+				object diagnosticsCollectionRaw = resultPropertyInfo.GetValue(dataTask);
+
+				if (!(diagnosticsCollectionRaw is IEnumerable<object> diagnostics) || diagnostics.IsNullOrEmpty())
+					return;
+
+				foreach (dynamic diagnostic in diagnostics)
+				{
+					var id = diagnostic.Id;
+				}
+			}
+			catch (Exception exc)
+			{
+
+			
+			}
+			
+
+			//SuppressDiagnosticsOnTargetNode(targetNode, semanticModelFromVS);	
 		}
 
 		private TextSpan GetTextSpanFromCaret(SnapshotPoint caretPosition, ITextSnapshotLine caretLine)
@@ -124,21 +202,78 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 			return context.IsPlatformReferenced;
 		}
 
-		private void SuppressDiagnosticsOnTargetNode(SyntaxNode targetNode, SemanticModel semanticModel)
+		
+	
+		private List<IVsTaskItem> GetErrorList()
 		{
-			if (targetNode == null)
-				return;
+			ThreadHelper.ThrowIfNotOnUIThread();
 
+			var errorService = ServiceProvider.GetService<SVsErrorList, IVsTaskList>();
 
+			if (errorService == null)
+				return null;
 
+			int result = VSConstants.S_OK;
+			List<IVsTaskItem> taskItemsList = new List<IVsTaskItem>(8);
 
-			//if (!targetNode.ContainsDiagnostics)
-			//{
-			//	MessageBox.Show(VSIXResource.DiagnosticSuppression_NoDiagnosticFound, AcuminatorVSPackage.PackageName);
-			//	return false;
-			//}
+			try
+			{
+				ErrorHandler.ThrowOnFailure(errorService.EnumTaskItems(out IVsEnumTaskItems errorItems));
 
-			//return true;
+				if (errorItems == null)
+				{
+					return null;
+				}
+
+				// Retrieve the task item text and check whether it is equal with one that supposed to be thrown.
+
+				uint[] fetched = new uint[1];
+
+				do
+				{
+					IVsTaskItem[] taskItems = new IVsTaskItem[1];
+
+					result = errorItems.Next(1, taskItems, fetched);
+
+					if (fetched[0] == 1 && taskItems[0] is IVsTaskItem2 taskItem)
+					{
+						taskItemsList.Add(taskItem);
+					}
+
+				}
+				while (result == VSConstants.S_OK && fetched[0] == 1);
+
+			}
+			catch (System.Runtime.InteropServices.COMException e)
+			{		
+				result = e.ErrorCode;
+			}
+
+			if (result != VSConstants.S_OK)
+			{
+				return null;
+			}
+
+			return taskItemsList;
 		}
+
+		//private void SuppressDiagnosticsOnTargetNode(SyntaxNode targetNode, SemanticModel semanticModel)
+		//{
+		//	if (targetNode == null)
+		//		return;
+
+		//	bool x = targetNode.DescendantNodesAndTokens().Any(n => n.ContainsDiagnostics);
+
+		//	var diagnostics = semanticModel.GetDiagnostics(targetNode.Span)
+		//								   .Where(diagnostic => diagnostic.IsAcuminatorDiagnostic());
+
+		//	if (!diagnostics.IsNullOrEmpty())
+		//	{
+		//		MessageBox.Show(VSIXResource.DiagnosticSuppression_NoDiagnosticFound, AcuminatorVSPackage.PackageName);
+		//		return;
+		//	}
+
+		//	//return true;
+		//}
 	}
 }
