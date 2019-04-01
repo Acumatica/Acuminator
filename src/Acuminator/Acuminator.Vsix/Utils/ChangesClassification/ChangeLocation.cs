673ff7f71c9050f0cebf8dc9e46cc26f91b6756b
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-01 03:05:33 +0300

    ATR-303: started work on code changes location classification

---
+++ src/Acuminator/Acuminator.Vsix/Utils/ChangesClassification/ChangeLocation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Acuminator.Vsix.ChangesClassification
+{
+	/// <summary>
+	/// Values that represent the location of changes in the document.
+	/// </summary>
+	[Flags]
+	public enum ChangeLocation
+	{
+		None      = 0b0000,
+		Method    = 0b0001,
+		Property  = 0b0010,
+		Class     = 0b0100,
+		Namespace = 0b1000
+	}
+}
---
+++ src/Acuminator/Acuminator.Vsix/Utils/ChangesClassification/DocumentChangesClassifier.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Syntax;
+using Acuminator.Vsix.Utilities;
+
+
+
+namespace Acuminator.Vsix.ChangesClassification
+{
+	/// <summary>
+	/// A base class for document changes classification.
+	/// </summary>
+	public class DocumentChangesClassifier
+	{
+		public async Task<ChangeLocation> GetChangesLocationAsync(Document oldDocument, SyntaxNode oldRoot, Document newDocument, 
+																  CancellationToken cancellationToken = default)
+		{
+			oldDocument.ThrowOnNull(nameof(oldDocument));
+			oldRoot.ThrowOnNull(nameof(oldRoot));
+			newDocument.ThrowOnNull(nameof(newDocument));
+
+			IEnumerable<TextChange> textChanges = await newDocument.GetTextChangesAsync(oldDocument, cancellationToken)
+																   .ConfigureAwait(false);
+			if (textChanges.IsNullOrEmpty())
+				return ChangeLocation.None;
+
+			return GetChangesLocationImplAsync(oldDocument, oldRoot, newDocument, textChanges, cancellationToken);
+		}
+
+		protected virtual ChangeLocation GetChangesLocationImplAsync(Document oldDocument, SyntaxNode oldRoot, Document newDocument,
+																	 IEnumerable<TextChange> textChanges, 
+																	 CancellationToken cancellationToken = default)
+		{
+			ChangeLocation accumulatedChangeLocation = ChangeLocation.None;
+
+			foreach (TextChange change in textChanges)
+			{
+				ChangeLocation changeLocation = GetTextChangeLocation(change, oldRoot);
+				accumulatedChangeLocation = accumulatedChangeLocation | changeLocation;
+
+				cancellationToken.ThrowIfCancellationRequested();
+			}
+
+			return accumulatedChangeLocation;
+		}
+
+		protected ChangeLocation GetTextChangeLocation(TextChange textChange, SyntaxNode oldRoot)
+		{
+			var containingNode = oldRoot.FindNode(textChange.Span);
+
+			if (containingNode == null)
+				return ChangeLocation.None;
+
+			while (!containingNode.IsKind(SyntaxKind.CompilationUnit))
+			{
+				
+
+				switch (containingNode)
+				{
+					case MethodDeclarationSyntax methodNode:
+
+				}
+			}
+
+			return ChangeLocation.Namespace;
+		}
+
+		protected ChangeLocation? AnalyzeMethodNode(MethodDeclarationSyntax methodNode, TextChange textChange)
+		{
+
+		}
+	}
+}
