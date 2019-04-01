86679dd7bebaa655be43add8a1f65888555681c8
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-01 05:11:29 +0300

    ATR-303: added code map doc changes classifier

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Utils/CodeMapDocChangesClassifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Syntax;
+using Acuminator.Vsix.Utilities;
+using Acuminator.Vsix.ChangesClassification;
+
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// A class for Code Map specific document changes classification. Contains optimizations for code map.
+	/// </summary>
+	internal class CodeMapDocChangesClassifier : DocumentChangesClassifier
+	{
+		protected override ChangeLocation GetChangesLocationImplAsync(Document oldDocument, SyntaxNode oldRoot, Document newDocument,
+																	  IEnumerable<TextChange> textChanges, CancellationToken cancellationToken = default)
+		{
+			ChangeLocation accumulatedChangeLocation = ChangeLocation.None;
+
+			foreach (TextChange change in textChanges)
+			{
+				ChangeLocation changeLocation = GetTextChangeLocation(change, oldRoot);
+
+				if (changeLocation.ContainsLocation(ChangeLocation.Class) || changeLocation.ContainsLocation(ChangeLocation.Namespace))
+					return changeLocation;
+
+				accumulatedChangeLocation = accumulatedChangeLocation | changeLocation;
+				cancellationToken.ThrowIfCancellationRequested();
+			}
+
+			return accumulatedChangeLocation;
+		}
+	}
+}
