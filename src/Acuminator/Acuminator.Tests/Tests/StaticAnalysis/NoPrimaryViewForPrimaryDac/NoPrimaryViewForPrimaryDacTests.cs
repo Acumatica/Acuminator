b387a9b9aaab6bf9998f0456fa02b6af87802398
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-23 14:14:19 +0300

    ATR-362: Added more fluent BQL tests for diagnostic

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoPrimaryViewForPrimaryDac/NoPrimaryViewForPrimaryDacTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoPrimaryViewForPrimaryDac/NoPrimaryViewForPrimaryDacTests.cs
@@ -18,13 +18,23 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.NoPrimaryViewForPrimaryDac
 			
 		[Theory]
 		[EmbeddedFileData("NoPrimaryViewForPrimaryDac.cs")]
-		public virtual void GraphWithPrimaryDac_WithoutPrimaryView(string source) =>
+		public virtual void WithoutPrimaryView(string source) =>
 			VerifyCSharpDiagnostic(source,
 				Descriptors.PX1018_NoPrimaryViewForPrimaryDac.CreateFor(line: 17, column: 56),
 				Descriptors.PX1018_NoPrimaryViewForPrimaryDac.CreateFor(line: 23, column: 15));
 
 		[Theory]
 		[EmbeddedFileData("HasPrimaryViewForPrimaryDac.cs")]
-		public virtual void GraphWithPrimaryDac_WithPrimaryView_NoDiagnostics(string source) => VerifyCSharpDiagnostic(source);
+		public virtual void WithPrimaryView_NoDiagnostics(string source) => VerifyCSharpDiagnostic(source);
+
+		[Theory]
+		[EmbeddedFileData("NoPrimaryViewForPrimaryDacFluentBQL.cs")]
+		public virtual void WithoutPrimaryView_FluentBql(string source) =>
+			VerifyCSharpDiagnostic(source,
+				Descriptors.PX1018_NoPrimaryViewForPrimaryDac.CreateFor(line: 18, column: 68));
+
+		[Theory]
+		[EmbeddedFileData("HasPrimaryViewForPrimaryDacFluentBQL.cs")]
+		public virtual void WithPrimaryView_FluentBql_NoDiagnostics(string source) => VerifyCSharpDiagnostic(source);
 	}
 }
