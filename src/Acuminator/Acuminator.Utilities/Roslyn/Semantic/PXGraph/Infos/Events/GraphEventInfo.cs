d8eadf150e64b440ed36d74a9a8c0c5ff167b40d
Nikomarov Sergey <snikomarov@acumatica.com>
2019-05-20 15:28:48 +0300

    ATR-377: refactored event infos architecture

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/PXGraph/Infos/Events/GraphEventInfo.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/PXGraph/Infos/Events/GraphEventInfo.cs
@@ -6,184 +6,21 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 {
 	/// <summary>
-	/// Information about the cache attached event in graph.
+	/// Information about the event in graph. Used for all events except events related to the DAC fields.
 	/// </summary>
-	public class GraphEventInfo : GraphNodeSymbolItem<MethodDeclarationSyntax, IMethodSymbol>
+	public class GraphEventInfo : GraphEventInfoBase<GraphEventInfo>
 	{
-		public GraphEventInfo Base { get; }
-
-		public EventHandlerSignatureType SignatureType { get; }
-
-		public EventType EventType { get; }
-
-		public string DacName { get; }
-
+		
 		public GraphEventInfo(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
 							  EventHandlerSignatureType signatureType, EventType eventType) :
-						 base(node, symbol, declarationOrder)
-		{
-			SignatureType = signatureType;
-			EventType = eventType;
-			DacName = GetDacName();
+						 base(node, symbol, declarationOrder, signatureType, eventType)
+		{			
 		}
 
 		public GraphEventInfo(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
 							  EventHandlerSignatureType signatureType, EventType eventType, GraphEventInfo baseInfo)
-			: this(node, symbol, declarationOrder, signatureType, eventType)
-		{
-			baseInfo.ThrowOnNull(nameof(baseInfo));
-
-			Base = baseInfo;
-		}
-
-		public bool IsDacFieldEvent()
-		{
-			switch (EventType)
-			{
-				case EventType.FieldSelecting:
-				case EventType.FieldDefaulting:
-				case EventType.FieldVerifying:
-				case EventType.FieldUpdating:
-				case EventType.FieldUpdated:
-					return true;
-				default:
-					return false;
-			}
-		}
-
-		public bool IsDacRowEvent()
-		{
-			switch (EventType)
-			{
-				case EventType.RowSelecting:
-				case EventType.RowSelected:
-				case EventType.RowInserting:
-				case EventType.RowInserted:
-				case EventType.RowUpdating:
-				case EventType.RowUpdated:
-				case EventType.RowDeleting:
-				case EventType.RowDeleted:
-				case EventType.RowPersisting:
-				case EventType.RowPersisted:
-					return true;
-				default:
-					return false;
-			}
-		}
-
-		public static string GetDacFieldNameForFieldEvent(GraphEventInfo eventInfo)
-		{
-			if (eventInfo == null || !eventInfo.IsDacFieldEvent())
-				return string.Empty;
-
-			switch (eventInfo.SignatureType)
-			{
-				case EventHandlerSignatureType.Default:
-					return GetDacFieldNameForDefaultFieldEvent(eventInfo);
-				case EventHandlerSignatureType.Generic:
-					return GetDacFieldNameForGenericFieldEvent(eventInfo);
-				default:
-					return string.Empty;
-			}
-		}
-
-
-		private static string GetDacFieldNameForDefaultFieldEvent(GraphEventInfo defaultFieldEventInfo)
-		{
-			if (defaultFieldEventInfo?.SignatureType != EventHandlerSignatureType.Default || !defaultFieldEventInfo.IsDacFieldEvent())
-			{
-				return string.Empty;
-			}
-
-			string eventName = defaultFieldEventInfo.Symbol.Name;
-
-			if (eventName.Length < 5 || eventName[0] == '_' || eventName[eventName.Length - 1] == '_')
-			{
-				return string.Empty;
-			}
-
-			int underscoreCount = 0;
-			int firstUnderscoreIndex = -1, lastUnderscoreIndex = -1;
-
-			for (int i = 0; i < eventName.Length; i++)
-			{
-				if (eventName[i] != '_')
-					continue;
-
-				if (underscoreCount == 0)
-				{
-					firstUnderscoreIndex = i;
-				}
-				else
-				{
-					lastUnderscoreIndex = i;
-				}
-
-				underscoreCount++;
-			}
-
-			if (underscoreCount != 2 || firstUnderscoreIndex < 0 || lastUnderscoreIndex < 0)
-				return string.Empty;
-
-			int length = lastUnderscoreIndex - firstUnderscoreIndex - 1;
-
-			if (length == 0)
-				return string.Empty;
-
-			return eventName.Substring(firstUnderscoreIndex + 1, length);
-		}
-
-		private static string GetDacFieldNameForGenericFieldEvent(GraphEventInfo genericFieldEventInfo)
-		{
-			if (genericFieldEventInfo?.SignatureType != EventHandlerSignatureType.Generic || !genericFieldEventInfo.IsDacFieldEvent() ||
-				genericFieldEventInfo.Symbol.Parameters.IsDefaultOrEmpty)
-			{
-				return string.Empty;
-			}
-
-			if (!(genericFieldEventInfo.Symbol.Parameters[0]?.Type is INamedTypeSymbol firstParameter) ||
-				 firstParameter.TypeArguments.Length < 2)
-			{
-				return string.Empty;
-			}
-
-			ITypeSymbol dacField = firstParameter.TypeArguments[1];
-			return dacField.IsDacField()
-				? dacField.Name.ToPascalCase()
-				: string.Empty;
-		}
-
-		private string GetDacName()
-		{
-			switch (SignatureType)
-			{
-				case EventHandlerSignatureType.Default:
-					var underscoreIndex = Symbol.Name.IndexOf('_');
-					return underscoreIndex > 0
-						? Symbol.Name.Substring(0, underscoreIndex)
-						: string.Empty;
-
-				case EventHandlerSignatureType.Generic:
-					return GetDacNameFromGenericEvent();
-
-				case EventHandlerSignatureType.None:
-				default:
-					return string.Empty;
-			}
-		}
-
-		private string GetDacNameFromGenericEvent()
-		{
-			if (Symbol.Parameters.IsDefaultOrEmpty ||
-					   !(Symbol.Parameters[0]?.Type is INamedTypeSymbol firstParameter) ||
-					   firstParameter.TypeArguments.IsDefaultOrEmpty)
-			{
-				return string.Empty;
-			}
-
-			return firstParameter.TypeArguments[0].IsDAC()
-				? firstParameter.TypeArguments[0].Name
-				: string.Empty;
+					   : base(node, symbol, declarationOrder, signatureType, eventType, baseInfo)
+		{		
 		}
 	}
 }
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/PXGraph/Infos/Events/GraphFieldEventInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using Acuminator.Utilities.Common;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
+{
+	/// <summary>
+	/// Information about the graph events related to the DAC fields.
+	/// </summary>
+	public class GraphFieldEventInfo : GraphEventInfoBase<GraphFieldEventInfo>
+	{
+		public GraphFieldEventInfo(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
+								   EventHandlerSignatureType signatureType, EventType eventType) :
+							  base(node, symbol, declarationOrder, signatureType, eventType)
+		{
+			ValidateEventType(eventType);
+		}
+
+		public GraphFieldEventInfo(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
+								   EventHandlerSignatureType signatureType, EventType eventType, GraphFieldEventInfo baseInfo)
+							: base(node, symbol, declarationOrder, signatureType, eventType, baseInfo)
+		{
+			ValidateEventType(eventType);
+		}
+
+		private void ValidateEventType(EventType eventType)
+		{
+			if (!IsDacFieldEvent())
+				throw new ArgumentOutOfRangeException(nameof(eventType), $"The {eventType.ToString()} is not a field event type.");
+		}
+
+
+	}
+}
