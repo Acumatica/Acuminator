4a73a717504ac7c37d29e67b8f1bcb6a22cbfe4b
Nikomarov Sergey <snikomarov@acumatica.com>
2019-05-20 16:16:52 +0300

    ATR-377: added non-generic foundation for event infos

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/PXGraph/Infos/Events/GraphEventInfoBase.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/PXGraph/Infos/Events/GraphEventInfoBase.cs
@@ -6,13 +6,10 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 {
 	/// <summary>
-	/// Base class for graph event information.
+	/// A common non-generic graph event info DTO base class.
 	/// </summary>
-	public abstract class GraphEventInfoBase<TDerived> : GraphNodeSymbolItem<MethodDeclarationSyntax, IMethodSymbol>
-	where TDerived : GraphEventInfoBase<TDerived>
+	public abstract class GraphEventInfoBase : GraphNodeSymbolItem<MethodDeclarationSyntax, IMethodSymbol>
 	{
-		public TDerived Base { get; }
-
 		public EventHandlerSignatureType SignatureType { get; }
 
 		public EventType EventType { get; }
@@ -26,50 +23,6 @@ namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 			SignatureType = signatureType;
 			EventType = eventType;
 			DacName = GetDacName();
-		}
-
-		protected GraphEventInfoBase(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
-									 EventHandlerSignatureType signatureType, EventType eventType, TDerived baseInfo)
-							  : this(node, symbol, declarationOrder, signatureType, eventType)
-		{
-			baseInfo.ThrowOnNull(nameof(baseInfo));
-
-			Base = baseInfo;
-		}
-
-		public bool IsDacFieldEvent()
-		{
-			switch (EventType)
-			{
-				case EventType.FieldSelecting:
-				case EventType.FieldDefaulting:
-				case EventType.FieldVerifying:
-				case EventType.FieldUpdating:
-				case EventType.FieldUpdated:
-					return true;
-				default:
-					return false;
-			}
-		}
-
-		public bool IsDacRowEvent()
-		{
-			switch (EventType)
-			{
-				case EventType.RowSelecting:
-				case EventType.RowSelected:
-				case EventType.RowInserting:
-				case EventType.RowInserted:
-				case EventType.RowUpdating:
-				case EventType.RowUpdated:
-				case EventType.RowDeleting:
-				case EventType.RowDeleted:
-				case EventType.RowPersisting:
-				case EventType.RowPersisted:
-					return true;
-				default:
-					return false;
-			}
 		}
 
 		private string GetDacName()
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/PXGraph/Infos/Events/GraphEventInfoBaseGeneric.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Acuminator.Utilities.Common;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
+{
+	/// <summary>
+	/// A common generic graph event info DTO base class.
+	/// </summary>
+	public abstract class GraphEventInfoBase<TDerived> : GraphEventInfoBase
+	where TDerived : GraphEventInfoBase<TDerived>
+	{
+		public TDerived Base { get; }
+
+		protected GraphEventInfoBase(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
+									 EventHandlerSignatureType signatureType, EventType eventType) :
+								base(node, symbol, declarationOrder, signatureType, eventType)
+		{		
+		}
+
+		protected GraphEventInfoBase(MethodDeclarationSyntax node, IMethodSymbol symbol, int declarationOrder,
+									 EventHandlerSignatureType signatureType, EventType eventType, TDerived baseInfo)
+							  : base(node, symbol, declarationOrder, signatureType, eventType)
+		{
+			baseInfo.ThrowOnNull(nameof(baseInfo));
+
+			Base = baseInfo;
+		}	
+	}
+}
