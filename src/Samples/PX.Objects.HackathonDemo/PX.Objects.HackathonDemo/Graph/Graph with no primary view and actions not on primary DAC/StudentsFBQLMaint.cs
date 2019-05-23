1f153dcbb1e0b4fddbdad47f9fc4a4504c355523
Nikomarov Sergey <snikomarov@acumatica.com>
2019-05-23 10:34:49 +0300

    ATR-381: added reference to FBQL dll to the demo solution. Added example with FBQL to the demo solution

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/Graph with no primary view and actions not on primary DAC/StudentsFBQLMaint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Data.BQL.Fluent;
+
+namespace PX.Objects.HackathonDemo
+{
+    [PXHidden]
+    public class Student : IBqlTable
+    {
+
+    }
+
+    public class StudentsFBQLMaint : PXGraph<StudentsFBQLMaint, Student>
+    {
+		SelectFrom<Student> Students;
+    }
+}
