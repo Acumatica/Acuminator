5074f21233ec818cbf8602c1ac534dd26bde0f7f
Nikomarov Sergey <snikomarov@acumatica.com>
2019-06-27 18:21:13 +0300

    ATR-331: start working on DAC semantic model

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/Dac/Infos/DacPropertyInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Acuminator.Utilities.Common;
+
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Dac
+{
+	public class DacPropertyInfo : NodeSymbolItem<PropertyDeclarationSyntax, IPropertySymbol>
+	{
+		/// <summary>
+		/// The overriden property if any
+		/// </summary>
+		public DacPropertyInfo Base { get; }
+
+		public DacPropertyInfo(PropertyDeclarationSyntax node, IPropertySymbol symbol, int declarationOrder) :
+						  base(node, symbol, declarationOrder)
+		{
+		}
+
+		public DacPropertyInfo(PropertyDeclarationSyntax node, IPropertySymbol symbol, int declarationOrder, DacPropertyInfo baseInfo) :
+						  this(node, symbol, declarationOrder)
+		{
+			baseInfo.ThrowOnNull(nameof(baseInfo));
+			Base = baseInfo;
+		}
+	}
+}
