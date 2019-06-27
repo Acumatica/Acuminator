a0d74ad6799475ee6e204962e1c6f2995ae3b23f
Nikomarov Sergey <snikomarov@acumatica.com>
2019-06-27 19:36:30 +0300

    ATR-331: added info DTO for DAC fields

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/Dac/Infos/DacFieldInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Acuminator.Utilities.Common;
+
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Dac
+{
+	public class DacFieldInfo : NodeSymbolItem<ClassDeclarationSyntax, INamedTypeSymbol>
+	{
+		/// <summary>
+		/// The overriden dac field if any
+		/// </summary>
+		public DacFieldInfo Base { get; }
+
+		public DacFieldInfo(ClassDeclarationSyntax node, INamedTypeSymbol symbol, int declarationOrder) :
+					   base(node, symbol, declarationOrder)
+		{
+		}
+
+		public DacFieldInfo(ClassDeclarationSyntax node, INamedTypeSymbol symbol, int declarationOrder, DacFieldInfo baseInfo) :
+					   this(node, symbol, declarationOrder)
+		{
+			baseInfo.ThrowOnNull(nameof(baseInfo));
+			Base = baseInfo;
+		}
+	}
+}
