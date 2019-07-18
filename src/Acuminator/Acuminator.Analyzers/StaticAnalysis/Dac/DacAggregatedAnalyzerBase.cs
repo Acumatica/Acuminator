f970f45c920a5cf441a61d26b6ffb530b75ec03c
Nikomarov Sergey <snikomarov@acumatica.com>
2019-07-18 20:08:27 +0300

    ATR-414: fixed compilation errors

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/Dac/DacAggregatedAnalyzerBase.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/Dac/DacAggregatedAnalyzerBase.cs
@@ -17,6 +17,6 @@ namespace Acuminator.Analyzers.StaticAnalysis.Dac
 
 		public abstract void Analyze(SymbolAnalysisContext context, PXContext pxContext, DacSemanticModel dac);
 
-		public virtual bool ShouldAnalyze(PXContext pxContext, DacSemanticModel dac) => dac != null && dac.DacType != DacType.None;
+		public virtual bool ShouldAnalyze(PXContext pxContext, DacSemanticModel dac) => dac != null;
 	}
 }
