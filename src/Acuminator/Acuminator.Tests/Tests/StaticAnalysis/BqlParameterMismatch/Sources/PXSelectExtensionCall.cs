9df59a30daf08b8ff3f0f76ed94852ec1b9af4b6
Nikomarov Sergey <snikomarov@acumatica.com>
2019-07-19 21:34:38 +0300

    ATR-407: added unit test for the PXSelectExtension call

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BqlParameterMismatch/Sources/PXSelectExtensionCall.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.HackathonDemo
+{
+	[PXHidden]
+	public class Document : PXMappedCacheExtension
+	{
+		#region BranchID
+		/// <exclude />
+		public abstract class branchID : IBqlField
+		{
+		}
+
+		public virtual string BranchID { get; set; }
+		#endregion
+		#region HeaderDocDate
+		/// <exclude />
+		public abstract class headerDocDate : IBqlField
+		{
+		}
+
+		public virtual DateTime? HeaderDocDate { get; set; }
+		#endregion
+		#region HeaderTranPeriodID
+		/// <exclude />
+		public abstract class headerTranPeriodID : IBqlField
+		{
+		}
+
+		public virtual string HeaderTranPeriodID { get; set; }
+		#endregion
+	}
+
+	public abstract class DocumentWithLinesGraphExtension<TGraph, TDocument, TDocumentMapping> : PXGraphExtension<TGraph>
+	where TGraph : PXGraph
+	where TDocument : Document, new()
+	where TDocumentMapping : IBqlMapping
+	{
+		public PXSelectExtension<TDocument> Documents;
+
+		public TDocument GetDocument(int? docID)
+		{
+			return Documents.Select(docID)
+							.RowCast<TDocument>()
+							.FirstOrDefault();
+		}
+	}
+}
