6895494653136e80e25f38771c745d7fc9245bbb
Nikomarov Sergey <snikomarov@acumatica.com>
2019-07-19 20:45:35 +0300

    ATR-407: added base classes for symbol sets

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/Symbols/Base/SymbolsSetForTypeBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Acuminator.Utilities.Common;
+using Microsoft.CodeAnalysis;
+using Acuminator.Utilities.Roslyn.Constants;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Symbols
+{
+	/// <summary>
+	/// The symbols set for some type base class.
+	/// </summary>
+	public abstract class SymbolsSetForTypeBase : SymbolsSetBase
+	{
+		public INamedTypeSymbol Type { get; }
+
+		public bool IsDefined => Type != null;
+
+		internal SymbolsSetForTypeBase(Compilation compilation, string typeName) : base(compilation)
+		{
+			Type = compilation.GetTypeByMetadataName(typeName);
+		}
+	}
+}
