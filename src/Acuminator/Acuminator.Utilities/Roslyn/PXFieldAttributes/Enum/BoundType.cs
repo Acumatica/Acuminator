9429c80950566f04645d313caacfc3eb36f22187
Nikomarov Sergey <snikomarov@acumatica.com>
2019-07-24 12:28:28 +0300

    ATR-419: added combine operation for bound type enum

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Enum/BoundType.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Enum/BoundType.cs
@@ -4,17 +4,37 @@
 namespace Acuminator.Utilities.Roslyn.PXFieldAttributes
 {
 	/// <summary>
-	/// Helper used to classify Acumatica attributes.
+	/// Enumeration used to classify Acumatica attributes.
 	/// </summary>
 	/// <remarks>
 	/// By Acumatica atribute we mean an attribute derived from PXEventSubscriberAttribute.
 	/// </remarks>
-	
 	public enum BoundType
 	{
-		Unknown = 0,
+		/// <summary>
+		/// The attribute is not related to the database binding.
+		/// </summary>	
+		NotDefined = 0,
+
+		/// <summary>
+		/// The attribute is database unbound.
+		/// </summary>
 		Unbound = 1,
+
+		/// <summary>
+		/// The attribute is database bound.
+		/// </summary>
 		DbBound = 2,
-		NotDefined = 3
+
+		/// <summary>
+		/// The attribute classification is unknown and cannot be deduced by means of static analysis.
+		/// </summary>
+		Unknown = 4
+	}
+
+
+	public static class BoundTypeCombiner
+	{
+		public static BoundType Combine(this BoundType x, BoundType y) => x >= y ? x : y;
 	}
 }
