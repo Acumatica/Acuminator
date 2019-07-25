6d94b8123607ed968499e5b8fa693909dc6b5b96
Nikomarov Sergey <snikomarov@acumatica.com>
2019-07-25 21:34:10 +0300

    ATR-390: added unit test for fully unbound DAC

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacExtensionDefaultAttribute/Sources/FullyUnboundDac.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.HackathonDemo
+{
+	[Serializable]
+	public partial class CreateAccountsFilter : IBqlTable
+	{
+		#region BAccountID
+		public abstract class bAccountID : PX.Data.BQL.BqlString.Field<bAccountID> { }
+
+		[PXDefault]
+		[PXString(30, IsUnicode = true, InputMask = ">CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC")]
+		[PXUIField(DisplayName = "Business Account ID", Required = true)]
+		public virtual string BAccountID { get; set; }
+		#endregion
+
+		#region AccountName
+		public abstract class accountName : PX.Data.BQL.BqlString.Field<accountName> { }
+		protected string _AccountName;
+		[PXDefault]
+		[PXString(60, IsUnicode = true)]
+		[PXUIField(DisplayName = "Company Name", Required = true)]
+		public virtual string AccountName
+		{
+			get { return _AccountName; }
+			set { _AccountName = value; }
+		}
+		#endregion
+
+		#region LinkContactToAccount
+		public abstract class linkContactToAccount : PX.Data.BQL.BqlBool.Field<linkContactToAccount> { }
+		[PXBool]
+		[PXUIField(DisplayName = "Link Contact to Account", Visible = false, Enabled = false)]
+		public virtual bool? LinkContactToAccount { get; set; }
+		#endregion
+	}
+}
