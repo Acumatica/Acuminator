bbafb9ad9c678d2751c73777a2301e493769e11e
acuminator-sync <acuminator@acumatica.com>
2019-07-28 22:08:08 -1200

    Auto-commit: Updating program version

--- src/Acuminator/Acuminator.Vsix/Properties/AssemblyInfo.cs
+++ src/Acuminator/Acuminator.Vsix/Properties/AssemblyInfo.cs
@@ -11,3 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
