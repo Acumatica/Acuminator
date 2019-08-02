0fe9b26b0328b79cd0c0a66e63636ee17dc47f6d
Nikomarov Sergey <snikomarov@acumatica.com>
2019-08-03 01:00:19 +0300

    ATR-429: added builders

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Builders/Base/TreeBuilderBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Acuminator.Utilities.Common;
+using Acuminator.Vsix.Utilities;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Base class for code map tree builder.
+	/// </summary>
+	public abstract class TreeBuilderBase
+	{
+		public virtual TreeViewModel CreateEmptyCodeMapTree(CodeMapWindowViewModel windowViewModel) => new TreeViewModel(windowViewModel);
+
+		public TreeViewModel BuildCodeMapTree(CodeMapWindowViewModel windowViewModel)
+		{
+			TreeViewModel codeMapTree = CreateEmptyCodeMapTree(windowViewModel);
+
+			if (codeMapTree == null)
+				return null;
+
+			var roots = CreateRoots()?.Where(root => root != null);
+
+			if (roots.IsNullOrEmpty())
+				return codeMapTree;
+
+			codeMapTree.RootItems.AddRange(roots);
+
+			foreach (TreeNodeViewModel root in codeMapTree.RootItems)
+			{
+				var rootNodes = CreateChildrenNodes(root)?.Where(node => node != null);
+
+				if (!rootNodes.IsNullOrEmpty())
+				{
+					root.Children.AddRange(rootNodes);
+				}
+			}
+
+			return codeMapTree;
+		}
+
+		protected abstract IEnumerable<TreeNodeViewModel> CreateRoots();
+
+		public abstract IEnumerable<TreeNodeViewModel> CreateChildrenNodes(TreeNodeViewModel parentNode);
+	}
+}
