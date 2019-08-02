9cb5bb14faec33d5fa6de3361aec2491dbaa8962
Nikomarov Sergey <snikomarov@acumatica.com>
2019-08-03 02:59:09 +0300

    ATR-429: refatored graph node

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphNodeViewModel.cs
@@ -22,32 +22,11 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 			get => GraphSemanticModel.Symbol.Name;
 			protected set { }
 		}
-		protected GraphNodeViewModel(GraphSemanticModelForCodeMap codeMapGraphModel, TreeViewModel tree, bool isExpanded) : 
+
+		public GraphNodeViewModel(GraphSemanticModelForCodeMap codeMapGraphModel, TreeViewModel tree, bool isExpanded) : 
 							 base(tree, isExpanded)
 		{
 			CodeMapGraphModel = codeMapGraphModel;
-		}
-
-		public static GraphNodeViewModel Create(GraphSemanticModelForCodeMap codeMapGraphModel, TreeViewModel tree, 
-												bool isExpanded, bool expandChildren)
-		{
-			if (codeMapGraphModel == null || tree == null)
-				return null;
-
-			GraphNodeViewModel graphNodeVM = new GraphNodeViewModel(codeMapGraphModel, tree, isExpanded);
-			graphNodeVM.AddGraphMemberCategories(expandChildren);
-			return graphNodeVM;
-		}		
-
-		private void AddGraphMemberCategories(bool expandChildren)
-		{
-			var memberCategories =
-				GraphMemberTypeUtils.GetGraphMemberTypes()
-									.Select(graphMemberType =>
-										 GraphMemberCategoryNodeViewModel.Create(this, graphMemberType, isExpanded: expandChildren))
-									.Where(graphMemberCategory => graphMemberCategory != null);
-
-			Children.AddRange(memberCategories);
 		}
 
 		public override Task NavigateToItemAsync()
