936ade3b6da43f8e5445512afa780c42e18d6e32
Nikomarov Sergey <snikomarov@acumatica.com>
2019-08-09 20:27:24 +0300

    ATR-413: added DAC member category node

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacCategories/DacMemberCategoryNodeViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using System.Threading.Tasks;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public abstract class DacMemberCategoryNodeViewModel : TreeNodeViewModel, IGroupNodeWithCyclingNavigation
+	{
+		public DacNodeViewModel DacViewModel { get; }
+
+		public DacSemanticModel DacModel => DacViewModel.DacModel;
+
+		public override bool DisplayNodeWithoutChildren => false;
+
+		public DacMemberCategory CategoryType { get; }
+
+		protected string CategoryDescription { get; }
+
+		public override string Name
+		{
+			get => $"{CategoryDescription}({Children.Count})";
+			protected set { }
+		}
+
+		protected abstract bool AllowNavigation { get; }
+
+		bool IGroupNodeWithCyclingNavigation.AllowNavigation => AllowNavigation;
+
+		int IGroupNodeWithCyclingNavigation.CurrentNavigationIndex
+		{
+			get;
+			set;
+		}
+
+		IList<TreeNodeViewModel> IGroupNodeWithCyclingNavigation.Children => Children;
+
+		protected DacMemberCategoryNodeViewModel(DacNodeViewModel dacViewModel, DacMemberCategory dacCategoryType, bool isExpanded) : 
+										    base(dacViewModel?.Tree, isExpanded)
+		{
+			DacViewModel = dacViewModel;
+			CategoryType = dacCategoryType;
+			CategoryDescription = CategoryType.Description();
+		}
+
+		public abstract IEnumerable<SymbolItem> GetCategoryGraphNodeSymbols();
+
+		public async override Task NavigateToItemAsync()
+		{
+			var childToNavigateTo = this.GetChildToNavigateTo();
+
+			if (childToNavigateTo != null)
+			{
+				await childToNavigateTo.NavigateToItemAsync();
+				IsExpanded = true;
+				Tree.SelectedItem = childToNavigateTo;
+			}
+		}
+
+		bool IGroupNodeWithCyclingNavigation.CanNavigateToChild(TreeNodeViewModel child) => CanNavigateToChild(child);
+
+		protected virtual bool CanNavigateToChild(TreeNodeViewModel child) => true;
+	}
+}
