9a708f63469c028afeb0e0b050f5c5b6302cc42b
Ivan Saloponov <ivan.saloponov@acumatica.com>
2019-09-02 09:22:44 +0300

    ATR-394: Edit suppress comment in Sample project

--- src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/GoTo Test/APInvoiceEntry.cs
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/GoTo Test/APInvoiceEntry.cs
@@ -111,7 +111,7 @@ namespace PX.Objects.HackathonDemo
 
 			startRow = PXView.StartRow;
 
-			// Acuminator disable once PX1010 Description [Justification]
+			// Acuminator disable once PX1010 StartRowResetForPaging [Justification]
 			IEnumerable<ListEntryPoint> rows = new PXView(this, false, new Select<ListEntryPoint>())
 					.Select(PXView.Currents, PXView.Parameters, PXView.Searches, PXView.SortColumns, PXView.Descendings, PXView.Filters,
 					ref startRow, PXView.MaximumRows, ref totalRows).Cast<ListEntryPoint>();
