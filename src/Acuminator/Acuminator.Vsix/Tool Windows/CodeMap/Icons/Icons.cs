9304d1d15487e6b7433e2214044bb3ccbf1f0b99
Nikomarov Sergey <snikomarov@acumatica.com>
2019-09-04 22:39:50 +0300

    ATR-413: added icons enum

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Icons/Icons.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Data;
+using Acuminator.Vsix.Utilities;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Values that represent icons for tree nodes in code map tree.
+	/// </summary>
+	public enum Icons
+	{
+		GraphIcon,
+		EventIcon,
+		DacIcon,
+		DacFieldIcon,
+
+		GroupNodeIcon,
+		GroupingDacIcon,
+		GroupingDacFieldIcon,
+
+		ViewIcon,
+		ViewDelegateIcon,
+
+		ActionIcon,
+		ActionHandlerIcon,
+
+		PXOverrideIcon,
+
+		RowEventIcon,
+		FieldEventIcon,
+		CacheAttachedIcon	
+	}
+}
