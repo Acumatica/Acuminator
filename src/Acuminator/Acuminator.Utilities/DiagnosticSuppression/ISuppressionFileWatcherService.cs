b8e9a91087f03abb31d74c069333263272d55703
Nikomarov Sergey <snikomarov@acumatica.com>
2019-10-04 15:05:31 +0300

    ATR-400: reworked file watcher service

--- src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/ISuppressionFileWatcherService.cs
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/ISuppressionFileWatcherService.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.IO;
 
 namespace Acuminator.Utilities.DiagnosticSuppression
 {
 	public interface ISuppressionFileWatcherService : IDisposable
 	{
-		event Action<object, SuppressionFileEventArgs> Changed;
+		event FileSystemEventHandler Changed;
 	}
 }
