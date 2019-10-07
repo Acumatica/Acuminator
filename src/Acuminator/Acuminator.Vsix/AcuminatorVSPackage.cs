ae6ae2ffe6f5c0ffabea40916ef54324423178d1
Nikomarov Sergey <snikomarov@acumatica.com>
2019-10-07 12:43:49 +0300

    ATR-400: removed old suppression command from package initialization and UI

--- src/Acuminator/Acuminator.Vsix/AcuminatorVSPackage.cs
+++ src/Acuminator/Acuminator.Vsix/AcuminatorVSPackage.cs
@@ -205,7 +205,6 @@ namespace Acuminator.Vsix
 			FormatBqlCommand.Initialize(this, oleCommandService);
 			GoToDeclarationOrHandlerCommand.Initialize(this, oleCommandService);
 			BqlFixer.FixBqlCommand.Initialize(this, oleCommandService);
-			SuppressDiagnosticInSuppressionFileCommand.Initialize(this, oleCommandService);
 
 			OpenCodeMapWindowCommand.Initialize(this, oleCommandService);
 		}
