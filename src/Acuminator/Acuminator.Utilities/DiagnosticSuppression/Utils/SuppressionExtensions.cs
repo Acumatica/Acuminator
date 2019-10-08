5f553c222a460d909c5a38e03a3a2b2614915665
Nikomarov Sergey <snikomarov@acumatica.com>
2019-10-08 18:06:31 +0300

    ATR-458: added xdocument xml getter to utils

--- src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/Utils/SuppressionExtensions.cs
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/Utils/SuppressionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using Acuminator.Utilities.Common;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Acuminator.Utilities.DiagnosticSuppression
@@ -26,6 +30,20 @@ namespace Acuminator.Utilities.DiagnosticSuppression
 		{
 			SuppressionManager.ReportDiagnosticWithSuppressionCheck(
 				context.SemanticModel, context.ReportDiagnostic, diagnostic, settings, context.CancellationToken);
+		}
+
+		public static string GetXDocumentStringWithDeclaration(this XDocument xDocument)
+		{
+			xDocument.ThrowOnNull(nameof(xDocument));
+
+			var builder = new StringBuilder(capacity: 65);
+
+			using (TextWriter writer = new Utf8StringWriter(builder))
+			{
+				xDocument.Save(writer);
+			}
+
+			return builder.ToString();
 		}
 	}
 }
