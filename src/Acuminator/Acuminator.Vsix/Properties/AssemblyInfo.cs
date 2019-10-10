bdc03f8e3daacbb6b6b12d7d989be235f88b1df0
acuminator-sync <acuminator@acumatica.com>
2019-10-09 22:21:42 -1200

    Auto-commit: Updating program version

--- src/Acuminator/Acuminator.Vsix/Properties/AssemblyInfo.cs
+++ src/Acuminator/Acuminator.Vsix/Properties/AssemblyInfo.cs
@@ -1,16 +1,13 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
 [assembly: AssemblyTitle("Acuminator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Acuminator")]
-[assembly: AssemblyCopyright("Copyright © 2017-2019 Acumatica Ltd.")]
+[assembly: AssemblyCopyright("Copyright � 2017-2019 Acumatica Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.9")]
+[assembly: AssemblyFileVersion("2.0.9")]
