b55e2ee78336494005a790a34499b9c7ebbab161
Nikomarov Sergey <snikomarov@acumatica.com>
2019-10-11 18:03:53 +0300

    ATR-461: added suppress in suppression file code action operation

---
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/CodeActions/SuppressInSuppressionFileOperation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Acuminator.Utilities.Common;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+
+namespace Acuminator.Utilities.DiagnosticSuppression.CodeActions
+{
+	/// <summary>
+	/// A code action operation to suppress diagnostic in suppression file.
+	/// </summary>
+	internal class SuppressInSuppressionFileOperation : CodeActionOperation
+	{
+		private readonly string _filePath;
+		private readonly Project _project;
+		private readonly Diagnostic _diagnosticToSuppress;
+		private readonly SemanticModel _semanticModel;
+
+		public override string Title => "Load new suppression file and suppress diagnostic code action operation";
+
+		public LoadNewSuppressionFileOperation(string filePath, Diagnostic diagnosticToSuppress,
+																			  Project project, SemanticModel semanticModel)
+		{
+			_filePath = filePath;
+			_project = project.CheckIfNull(nameof(project));
+			_diagnosticToSuppress = diagnosticToSuppress.CheckIfNull(nameof(diagnosticToSuppress));
+			_semanticModel = semanticModel.CheckIfNull(nameof(semanticModel));
+		}
+
+		public override void Apply(Workspace workspace, CancellationToken cancellationToken)
+		{
+			if (cancellationToken.IsCancellationRequested)
+				return;
+
+			SuppressionFile suppressionFile = _filePath.IsNullOrWhiteSpace()
+				? null
+				: SuppressionManager.Instance?.LoadSuppressionFileFrom(_filePath);
+
+			if (suppressionFile == null)
+			{
+				ShowErrorForFileNotFoundMessage();
+				return;
+			}
+
+			if (!SuppressionManager.SuppressDiagnostic(_semanticModel, _diagnosticToSuppress.Id, _diagnosticToSuppress.Location.SourceSpan,
+														_diagnosticToSuppress.DefaultSeverity, cancellationToken))
+			{
+				ShowErrorForSuppressionNotAddedMessage();
+			}
+		}
+
+		private void ShowErrorForFileNotFoundMessage()
+		{
+			var errorMessage = new LocalizableResourceString(nameof(Resources.DiagnosticSuppression_FailedToFindSuppressionFile),
+																    Resources.ResourceManager, typeof(Resources), _project.Name);
+			Debug.WriteLine($"{SharedConstants.PackageName.ToUpperInvariant()}: {errorMessage.ToString()}");
+		}
+
+		private void ShowErrorForSuppressionNotAddedMessage()
+		{
+			var errorMessage = new LocalizableResourceString(nameof(Resources.DiagnosticSuppression_FailedToAddToSuppressionFile),
+															 Resources.ResourceManager, typeof(Resources), _filePath);
+			Debug.WriteLine($"{SharedConstants.PackageName.ToUpperInvariant()}: {errorMessage.ToString()}");
+		}
+	}
+}
