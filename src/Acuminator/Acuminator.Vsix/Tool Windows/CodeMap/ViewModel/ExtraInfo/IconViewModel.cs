cf7014f973ef28b3e1f694305433d742018ceeac
Nikomarov Sergey <snikomarov@acumatica.com>
2019-11-06 02:05:51 +0300

    ATR-444: added extra info view models

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/ExtraInfo/IconViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Acuminator.Utilities.Common;
+using Acuminator.Vsix.Utilities;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class IconViewModel : ExtraInfoViewModel
+	{
+		public Icon IconType { get; }
+
+		public IconViewModel(Icon icon) : base()
+		{
+			IconType = icon;
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/ExtraInfo/TextViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Acuminator.Utilities.Common;
+using Acuminator.Vsix.Utilities;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class TextViewModel : ExtraInfoViewModel
+	{
+		public string Text { get; }
+
+		public TextViewModel(string text) : base()
+		{
+			Text = text.CheckIfNull(nameof(text));
+		}
+	}
+}
