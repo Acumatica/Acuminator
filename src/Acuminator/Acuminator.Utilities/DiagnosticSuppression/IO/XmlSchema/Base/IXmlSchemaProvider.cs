9e96f5610b1bc97d2ffa098b70a28a4f27de4662
Nikomarov Sergey <snikomarov@acumatica.com>
2019-11-30 23:55:57 +0300

    ATR-503: added xml schema provider mechanism

---
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/IO/XmlSchema/Base/IXmlSchemaProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Acuminator.Utilities.DiagnosticSuppression.IO
+{
+	/// <summary>
+	/// Interface for the helper used to get xml schema.
+	/// </summary>
+	public interface IXmlSchemaProvider
+	{
+		/// <summary>
+		/// Gets XML schema.
+		/// </summary>
+		XmlSchema GetXmlSchema();
+	}
+}
---
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/IO/XmlSchema/EmbeddedResourceXmlSchemaProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Utilities.DiagnosticSuppression.IO
+{
+	/// <summary>
+	/// An embedded resource XML schema provider.
+	/// </summary>
+	public class EmbeddedResourceXmlSchemaProvider : IXmlSchemaProvider
+	{
+		/// <summary>
+		/// Gets XML schema from the given assembly embedded resources.
+		/// </summary>
+		public  XmlSchema GetXmlSchema()
+		{
+			try
+			{
+				Assembly utilsAssembly = typeof(SuppressionFileSchemaValidator).GetTypeInfo().Assembly;
+				string schemaResourceFullName = GetSchemaResourceFullName(utilsAssembly);
+
+				if (schemaResourceFullName.IsNullOrWhiteSpace())
+					return null;
+
+				using (Stream stream = utilsAssembly.GetManifestResourceStream(schemaResourceFullName))
+				{
+					if (stream == null)
+						return null;
+
+					using (XmlReader schemaReader = XmlReader.Create(stream))
+					{
+
+						return XmlSchema.Read(schemaReader, null);
+					}
+
+					return null;
+				}
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+		protected virtual string GetSchemaResourceFullName(Assembly curentAssembly) =>
+			curentAssembly.GetManifestResourceNames()
+						 ?.FirstOrDefault(rName => rName.EndsWith(SharedConstants.SuppressionFileXmlSchemaFileName));
+	}
+}
