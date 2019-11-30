871126494eb883aae1746180d66e38bae03c4741
Nikomarov Sergey <snikomarov@acumatica.com>
2019-12-01 01:04:54 +0300

    ATR-503: added validation log

---
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/IO/Validation/ValidationLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Utilities.DiagnosticSuppression.IO
+{
+	public class ValidationLog
+	{
+		private const string LogSeparator = "*******************************************************************"
+
+		public int ErrorsCount
+		{
+			get;
+			private set;
+		}
+
+		protected StringBuilder Log { get; } = new StringBuilder();
+
+		public string GetValidationLog() => Log.ToString();
+
+		public void LogError(string errorMessage)
+		{
+			if (errorMessage.IsNullOrWhiteSpace())
+				return;
+
+			try
+			{
+				if (Log.Length > 0)
+				{
+					Log.AppendLine(LogSeparator);
+				}
+
+				Log.AppendLine(errorMessage);
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+			}
+			finally
+			{
+				if (ErrorsCount < Int32.MaxValue)
+					ErrorsCount++;
+			}
+		}
+	}
+}
