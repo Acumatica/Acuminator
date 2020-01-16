6ae2a276e60d5c4df5b4695e981d6c1566c98afd
Nikomarov Sergey <snikomarov@acumatica.com>
2020-01-16 20:01:26 +0300

    ATR-498: Added file service for tests

---
+++ src/Acuminator/Acuminator.Tests/Tests/DiagnosticSuppression/SuppressionFileTestService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Acuminator.Utilities.DiagnosticSuppression.IO;
+
+namespace Acuminator.Tests.Tests.DiagnosticSuppression.SuppressionFileOrdering
+{
+	/// <summary>
+	/// A test implementation of suppression file service.
+	/// </summary>
+	internal class SuppressionFileTestService : SuppressionFileSystemServiceBase
+	{
+		public SuppressionFileTestService() :
+									 base(null, null)
+		{
+		}
+
+		public SuppressionFileTestService(IIOErrorProcessor errorProcessor) :
+									 base(errorProcessor, null)
+		{
+		}
+
+		public SuppressionFileTestService(IIOErrorProcessor errorProcessor, SuppressionFileValidation customValidation) :
+									 base(errorProcessor, customValidation)
+		{
+		}
+
+		public override ISuppressionFileWatcherService CreateWatcher(string path) => null;
+	}
+}
