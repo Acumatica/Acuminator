005408d8db9d8073230014ae4912d8c5c689eb0b
Nikomarov Sergey <snikomarov@acumatica.com>
2020-01-16 19:38:16 +0300

    ATR-498: refactor suppression message comparer to be more readable

--- src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/Utils/SuppressionMessageComparer.cs
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/Utils/SuppressionMessageComparer.cs
@@ -5,11 +5,19 @@ namespace Acuminator.Utilities.DiagnosticSuppression
 {
 	public class SuppressionMessageComparer : IComparer<SuppressMessage>
 	{
-		public int Compare(SuppressMessage msgX, SuppressMessage msgY) =>
-			string.CompareOrdinal(msgX.Id, msgY.Id) is var idComparison && idComparison != 0
-				? idComparison
-				: string.CompareOrdinal(msgX.Target, msgY.Target) is var targetComparison && targetComparison != 0
-					? targetComparison
-					: string.CompareOrdinal(msgX.SyntaxNode, msgY.SyntaxNode);
+		public int Compare(SuppressMessage msgX, SuppressMessage msgY)
+		{
+			int idComparison = string.CompareOrdinal(msgX.Id, msgY.Id);
+
+			if (idComparison != 0)
+				return idComparison;
+
+			int targetComparison = string.CompareOrdinal(msgX.Target, msgY.Target);
+
+			if (targetComparison != 0)
+				return targetComparison;
+
+			return string.CompareOrdinal(msgX.SyntaxNode, msgY.SyntaxNode);
+		}
 	}
 }
