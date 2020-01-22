8d6192d3edf50b486267d1decf31491c34fbad1a
Nikomarov Sergey <snikomarov@acumatica.com>
2020-01-22 20:30:40 +0300

    ATR-500: added viusal tree traversal utility helper

---
+++ src/Acuminator/Acuminator.Vsix/Utils/WPF/VisualTreeUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using Acuminator.Utilities.Common;
+
+
+namespace Acuminator.Vsix.Utilities
+{
+	/// <summary>
+	/// A static helper for the WPF Visual Tree.
+	/// </summary>
+	internal static class VisualTreeUtils
+	{
+		/// <summary>
+		/// Gets the visual descendants of the <paramref name="uiElement"/> from the visual tree.
+		/// </summary>
+		/// <param name="uiElement">The UI element to act on.</param>
+		/// <returns/>
+		public static IEnumerable<FrameworkElement> GetVisualDescendants(this FrameworkElement uiElement)
+		{
+			uiElement.ThrowOnNull(nameof(uiElement));
+
+			int childrenCount = VisualTreeHelper.GetChildrenCount(uiElement);
+
+			if (childrenCount == 0)
+				return Enumerable.Empty<FrameworkElement>();
+
+			return GetVisualDescendantsImpl(uiElement, childrenCount);
+
+			//-------------------------------------------Local function-----------------------------------
+			static IEnumerable<FrameworkElement> GetVisualDescendantsImpl(FrameworkElement parent, int childrenCount)
+			{
+				for (int i = 0; i < childrenCount; i++)
+				{
+					var child = VisualTreeHelper.GetChild(parent, i) as FrameworkElement;
+
+					if (child == null)
+						continue;
+
+					yield return child;
+
+					int grandchildrenCount = VisualTreeHelper.GetChildrenCount(child);
+
+					foreach (var descendant in GetVisualDescendantsImpl(child, grandchildrenCount).OfType<FrameworkElement>())
+					{
+						yield return descendant;
+					}
+				}
+			}
+		}
+	}
+}
