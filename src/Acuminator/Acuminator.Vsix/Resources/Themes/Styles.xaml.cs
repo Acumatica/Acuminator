eccc255635cc37f30d53e5edcbda0e0eb73b1b3d
Nikomarov Sergey <snikomarov@acumatica.com>
2020-01-23 13:25:46 +0300

    ATR-500: made code-behind file nested for styles, added comment

--- src/Acuminator/Acuminator.Vsix/Resources/Themes/Styles.xaml.cs
+++ src/Acuminator/Acuminator.Vsix/Resources/Themes/Styles.xaml.cs
@@ -13,6 +13,11 @@ namespace Acuminator.Vsix.Themes
 {
 	public partial class Styles : ResourceDictionary
 	{
+		/// <summary>
+		/// A "hack" that removes a strange small white rectangle on load of context menu by searching for responsible rectangles in the visual tree and hidhing them.
+		/// </summary>
+		/// <param name="sender">Source of the event.</param>
+		/// <param name="e">Event information to send to registered event handlers.</param>
 		private void OnContextMenuLoaded(object sender, RoutedEventArgs e)
 		{
 			if (!(sender is ContextMenu contextMenu))
