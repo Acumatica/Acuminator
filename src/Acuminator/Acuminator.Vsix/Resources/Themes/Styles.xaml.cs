7114f44c664810bef71ff31327d60fd55911a723
Nikomarov Sergey <snikomarov@acumatica.com>
2020-01-23 03:40:01 +0300

    ATR-500: added styles for contextmenu and menu item

---
+++ src/Acuminator/Acuminator.Vsix/Resources/Themes/Styles.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Acuminator.Utilities.Common;
+using Acuminator.Vsix.Utilities;
+
+
+namespace Acuminator.Vsix.Themes
+{
+	public partial class Styles : ResourceDictionary
+	{
+		private void OnContextMenuLoaded(object sender, RoutedEventArgs e)
+		{
+			if (!(sender is ContextMenu contextMenu))
+				return;
+
+			contextMenu.GetVisualDescendants()
+					   .OfType<Rectangle>()
+					   .ForEach(rectangle => rectangle.Visibility = Visibility.Collapsed);
+		}
+	}
+}
