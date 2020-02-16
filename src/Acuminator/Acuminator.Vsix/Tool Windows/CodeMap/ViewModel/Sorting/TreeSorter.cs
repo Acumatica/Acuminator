cbd1fc93cd4a1d8196a4e064ae2a675fb81d9698
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-16 22:50:48 +0300

    ATR-431: added tree sorter

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/TreeSorter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Utilities.Common;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class TreeSorter
+	{
+		private readonly IComparer<TreeNodeViewModel> _comparer;
+
+		public TreeSorter(IComparer<TreeNodeViewModel> comparer)
+		{
+			_comparer = comparer.CheckIfNull(nameof(comparer));
+		}
+
+		public virtual void SortChildren(TreeNodeViewModel treeNode)
+		{
+			switch (treeNode)
+			{
+
+				case null:
+					return;
+				default:
+					return;
+			}
+		}
+	}
+}
