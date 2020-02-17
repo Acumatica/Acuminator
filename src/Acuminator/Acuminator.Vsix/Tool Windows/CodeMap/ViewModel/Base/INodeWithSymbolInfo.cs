aa4e0f171885c1c56f296b970551221090ddbbed
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-17 20:30:34 +0300

    ATR-431: added interface for nodes with symbolitem

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Base/INodeWithSymbolInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Interface for code map node with symbol item.
+	/// </summary>
+	public interface INodeWithSymbolItem
+	{
+		/// <summary>
+		/// The symbol item reperesented by node.
+		/// </summary>
+		SymbolItem SymbolItem { get; }
+	}
+}
