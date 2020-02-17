c80db3586d273b9585114ef0de394069031ac295
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-17 20:30:04 +0300

    ATR-431: renaming

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/SortType.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/SortType.cs
@@ -3,9 +3,9 @@
 namespace Acuminator.Vsix.ToolWindows.CodeMap
 {
 	/// <summary>
-	/// Values that represent sorting order for tree nodes in code map tree.
+	/// Values that represent sorting type for tree nodes in code map tree.
 	/// </summary>
-	public enum SortOrder : byte
+	public enum SortType : byte
 	{
 		Declaration,
 		Alphabet
