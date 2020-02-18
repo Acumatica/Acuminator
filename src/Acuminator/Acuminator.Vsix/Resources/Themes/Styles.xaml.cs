1177c4747aa6e447c3e293646f15c461dff88d1e
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-18 19:12:33 +0300

    ATR-431: added style for submenu item with a small hack - setting correct background for border containing submenu item

--- src/Acuminator/Acuminator.Vsix/Resources/Themes/Styles.xaml.cs
+++ src/Acuminator/Acuminator.Vsix/Resources/Themes/Styles.xaml.cs
@@ -27,5 +27,26 @@ namespace Acuminator.Vsix.Themes
 					   .OfType<Rectangle>()
 					   .ForEach(rectangle => rectangle.Visibility = Visibility.Collapsed);
 		}
+
+		/// <summary>
+		/// A "hack" that sets correct background to submenu item on load by searching for appropriate <see cref="Border"/> in the visual tree and setting its background.
+		/// </summary>
+		/// <param name="sender">Source of the event.</param>
+		/// <param name="e">Event information to send to registered event handlers.</param>
+		private void OnSubmenuItemLoaded(object sender, RoutedEventArgs e)
+		{
+			if (!(sender is MenuItem submenu))
+				return;
+
+			const string subMenuBorderName = "SubMenuBorder";
+			var subMenuBorder = submenu.GetVisualAncestors()
+									   .OfType<Border>()
+									   .FirstOrDefault(border => border.Name == subMenuBorderName);
+
+			if (subMenuBorder != null)
+			{
+				subMenuBorder.Background = submenu.Background;
+			}			
+		}
 	}
 }
