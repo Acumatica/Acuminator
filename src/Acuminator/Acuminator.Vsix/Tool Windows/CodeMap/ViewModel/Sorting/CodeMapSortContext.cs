5a5c5dbc3574baec441e4739415ce9d82f418570
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-20 00:37:06 +0300

    ATR-431: added code map sort context struct

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/CodeMapSortContext.cs
@@ -0,0 +1,24 @@
+﻿using Acuminator.Utilities.Common;
+using System;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Information for sort of code map tree
+	/// </summary>
+	public struct CodeMapSortContext
+	{
+		public SortType SortType { get; }
+
+		public SortDirection SortDirection { get; }
+
+		public bool SortDescendants { get; }
+
+		public CodeMapSortContext(SortType sortType, SortDirection sortDirection, bool sortDescendants)
+		{
+			SortType = sortType;
+			SortDirection = sortDirection;
+			SortDescendants = sortDescendants;
+		}
+	}
+}
