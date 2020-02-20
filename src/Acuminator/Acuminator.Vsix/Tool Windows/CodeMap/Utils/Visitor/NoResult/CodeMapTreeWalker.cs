6243f98eae9b49ebe67c0179164406633e9bf269
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-20 04:06:03 +0300

    ATR-431: added code map syntax walker

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/NoResult/CodeMapTreeWalker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Base class for code map tree walker traversing the code map tree.
+	/// </summary>
+	public abstract class CodeMapTreeWalker : CodeMapTreeVisitor
+	{
+		private int _recursionDepth;
+
+		public override void VisitNode(TreeNodeViewModel node)
+		{
+			if (node == null)
+				return;
+
+			_recursionDepth++;
+
+			try
+			{
+				StackGuard.EnsureSufficientExecutionStack(_recursionDepth);
+				node.AcceptVisitor(this);
+			}
+			finally
+			{
+				_recursionDepth--;
+			}	
+		}
+
+		public override void DefaultVisit(TreeNodeViewModel nodeViewModel) { }
+	}
+}
