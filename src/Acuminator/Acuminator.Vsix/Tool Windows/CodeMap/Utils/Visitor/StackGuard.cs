d52e4dd0d038b070b0eaacdec88c0f97a1f8b2b0
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-20 03:48:33 +0300

    ATR-431: refactored stack guard into separate helper

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/StackGuard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+    internal static class StackGuard
+    {
+        public const int MaxUncheckedRecursionDepth = 20;
+
+		/// <summary>
+		/// Ensures that the remaining stack space is large enough to execute the average function.
+		/// </summary>
+		/// <param name="recursionDepth">How many times the calling function has recursed</param>
+		/// <exception cref="InsufficientExecutionStackException">
+		/// The available stack space is insufficient to execute the average function.
+		/// </exception>
+		[DebuggerStepThrough]
+		public static void EnsureSufficientExecutionStack(int recursionDepth)
+		{
+			if (recursionDepth > MaxUncheckedRecursionDepth)
+			{
+				RuntimeHelpers.EnsureSufficientExecutionStack();
+			}
+		}
+	}
+}
