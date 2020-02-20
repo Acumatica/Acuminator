954928b8ab6779672594ae911cd49f4cae5eed35
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-20 03:42:26 +0300

    ATR-431: added visitors producing no result

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/WithResult/CodeMapTreeVisitor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Base class for code map tree visitor.
+	/// </summary>
+	public abstract partial class CodeMapTreeVisitor<TResult>
+	{
+		protected const int MaxUncheckedRecursionDepth = 20;
+		private int _recursionDepth;
+
+		protected TResult DefaultValue { get; }
+
+		protected CodeMapTreeVisitor(TResult defaultValue)
+		{
+			DefaultValue = defaultValue;
+		}
+
+		public virtual TResult Visit(TreeNodeViewModel node)
+		{
+			if (node == null)
+				return DefaultValue;
+
+			_recursionDepth++;
+
+			try
+			{
+				EnsureSufficientExecutionStack();
+				return node.AcceptVisitor(this);
+			}
+			finally
+			{
+				_recursionDepth--;
+			}	
+		}
+
+		public virtual TResult DefaultVisit(TreeNodeViewModel nodeViewModel) => DefaultValue;
+
+		public virtual TResult VisitNode(AttributeNodeViewModel attributeNode) => DefaultVisit(attributeNode);
+
+		/// <summary>
+		/// Ensures that the remaining stack space is large enough to execute the average function.
+		/// </summary>
+		/// <param name="recursionDepth">how many times the calling function has recursed</param>
+		/// <exception cref="InsufficientExecutionStackException">
+		///  The available stack space is insufficient to execute the average function.
+		/// </exception>
+		[DebuggerStepThrough]
+		protected void EnsureSufficientExecutionStack()
+		{
+			if (_recursionDepth > MaxUncheckedRecursionDepth)
+			{
+				RuntimeHelpers.EnsureSufficientExecutionStack();
+			}
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/WithResult/CodeMapTreeVisitor_DacVisitMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Base class for code map tree visitor.
+	/// </summary>
+	public abstract partial class CodeMapTreeVisitor<TResult>
+	{
+		#region Roots
+		public virtual TResult VisitNode(DacNodeViewModel dac) => DefaultVisit(dac);
+		#endregion
+
+		#region Categories
+		public virtual TResult VisitNode(DacPropertiesCategoryNodeViewModel dacPropertiesCategory) => DefaultVisit(dacPropertiesCategory);
+
+		public virtual TResult VisitNode(DacKeysCategoryNodeViewModel dacKeysCategory) => DefaultVisit(dacKeysCategory);
+		#endregion
+
+		#region Leaf Nodes
+		public virtual TResult VisitNode(PropertyNodeViewModel property) => DefaultVisit(property);
+		#endregion
+	}
+}
