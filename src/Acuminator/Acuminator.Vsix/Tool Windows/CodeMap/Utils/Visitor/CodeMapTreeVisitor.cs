29bb09f0d53cdbf9a4d8fcf28800c82e71d6d09c
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-20 03:32:17 +0300

    ATR-431: enhanced Visit method with checks for enough execution stack (taken from Roslyn)

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/CodeMapTreeVisitor.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/CodeMapTreeVisitor.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading;
+using System.Runtime.CompilerServices;
 using Acuminator.Utilities.Common;
-using Acuminator.Utilities.Roslyn.Semantic;
 
 namespace Acuminator.Vsix.ToolWindows.CodeMap
 {
@@ -12,6 +12,9 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 	/// </summary>
 	public abstract partial class CodeMapTreeVisitor<TResult>
 	{
+		protected const int MaxUncheckedRecursionDepth = 20;
+		private int _recursionDepth;
+
 		protected TResult DefaultValue { get; }
 
 		protected CodeMapTreeVisitor(TResult defaultValue)
@@ -19,13 +22,42 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 			DefaultValue = defaultValue;
 		}
 
-		public virtual TResult Visit(TreeNodeViewModel nodeViewModel) =>
-			nodeViewModel != null
-				? nodeViewModel.AcceptVisitor(this)
-				: DefaultVisit(nodeViewModel);
+		public virtual TResult Visit(TreeNodeViewModel node)
+		{
+			if (node == null)
+				return DefaultValue;
+
+			_recursionDepth++;
+
+			try
+			{
+				EnsureSufficientExecutionStack();
+				return node.AcceptVisitor(this);
+			}
+			finally
+			{
+				_recursionDepth--;
+			}	
+		}
 
 		public virtual TResult DefaultVisit(TreeNodeViewModel nodeViewModel) => DefaultValue;
 
-		public virtual TResult VisitNode(AttributeNodeViewModel attributeNode) => DefaultVisit(attributeNode);	
+		public virtual TResult VisitNode(AttributeNodeViewModel attributeNode) => DefaultVisit(attributeNode);
+
+		/// <summary>
+		/// Ensures that the remaining stack space is large enough to execute the average function.
+		/// </summary>
+		/// <param name="recursionDepth">how many times the calling function has recursed</param>
+		/// <exception cref="InsufficientExecutionStackException">
+		///  The available stack space is insufficient to execute the average function.
+		/// </exception>
+		[DebuggerStepThrough]
+		protected void EnsureSufficientExecutionStack()
+		{
+			if (_recursionDepth > MaxUncheckedRecursionDepth)
+			{
+				RuntimeHelpers.EnsureSufficientExecutionStack();
+			}
+		}
 	}
 }
