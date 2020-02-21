bdd99713012d84a9f751662ea0c4e26b0d6bda93
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-21 19:45:27 +0300

    ATR-431: added a new type of visitor - a visitor that accepts an input

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Utils/Visitor/WithResult/WithInput/CodeMapVisitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Base class for code map tree visitor which accepts an input and produces a result.
+	/// </summary>
+	public abstract partial class CodeMapVisitor<TInput, TResult>
+	{
+		protected TResult DefaultValue { get; }
+
+		protected CodeMapVisitor(TResult defaultValue)
+		{
+			DefaultValue = defaultValue;
+		}
+
+		public virtual TResult VisitNode(TreeNodeViewModel node, TInput input) =>
+			node != null
+				? node.AcceptVisitor(this, input)
+				: DefaultValue;
+
+		public virtual TResult DefaultVisit(TreeNodeViewModel node, TInput input) => DefaultValue;
+
+		public virtual TResult VisitNode(AttributeNodeViewModel attributeNode, TInput input) => DefaultVisit(attributeNode, input);
+	}
+}
