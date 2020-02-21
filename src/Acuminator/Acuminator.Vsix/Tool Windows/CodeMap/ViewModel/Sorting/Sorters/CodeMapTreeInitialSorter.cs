4e190c79d5390201418a67040cf9f12e010ab077
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-21 15:55:56 +0300

    ATR-431: added initial sorter for code map tree

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/Sorters/CodeMapTreeInitialSorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Acuminator.Utilities.Common;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// A Code Map tree nodes default sorter implementation.
+	/// </summary>
+	public class CodeMapTreeSorterDefault : CodeMapTreeSorterBase
+	{
+		protected override bool IsSortTypeSupported(TreeNodeViewModel node, SortType sortType)
+		{
+			switch (node)
+			{
+				case DacGroupingNodeBaseViewModel _:
+				case DacFieldGroupingNodeBaseViewModel _:
+					return sortType == SortType.Alphabet;
+				case DacMemberNodeViewModel _:
+					return sortType == SortType.Alphabet || sortType == SortType.Declaration;
+				default:
+					return base.IsSortTypeSupported(node, sortType);
+			}
+		}
+
+		public override void VisitNode(AttributeNodeViewModel attributeNode)
+		{
+			//Stop visit for better performance
+		}
+	}
+}
