a5699d2c681716d600a94d91e3f3c3ff8f271adb
Nikomarov Sergey <snikomarov@acumatica.com>
2020-02-21 17:56:34 +0300

    ATR-431: changed default sorter implementation to use nodes sorter

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/Visitors/CodeMapTreeSorterDefault.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/Visitors/CodeMapTreeSorterDefault.cs
@@ -12,10 +12,17 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 	/// </summary>
 	public class CodeMapTreeSorterDefault : CodeMapTreeWalker
 	{
+		private readonly NodesSorter _nodesSorter;
+
 		protected CodeMapSortContext SortContext
 		{
 			get;
 			set;
+		}
+
+		public CodeMapTreeSorterDefault(NodesSorter nodesSorter = null)
+		{
+			_nodesSorter = nodesSorter ?? new NodesSorter();
 		}
 
 		public void SortChildren(TreeNodeViewModel node, SortType sortType, SortDirection sortDirection) =>
@@ -51,7 +58,7 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 			if (node.Children.Count == 0)
 				return;
 
-			var sorted = SortNodes(node.Children, SortContext.SortType, SortContext.SortDirection)
+			var sorted = _nodesSorter.SortNodes(node.Children, SortContext.SortType, SortContext.SortDirection)
 							?.ToList(capacity: node.Children.Count) 
 							?? Enumerable.Empty<TreeNodeViewModel>();
 
@@ -67,92 +74,5 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 		{
 			//Stop visit for better performance
 		}
-
-		/// <summary>
-		/// This method checks if the node can be sorted with the specified <paramref name="sortType"/> and reordered by sorting of code map nodes.
-		/// </summary>
-		/// <param name="node">The node view model.</param>
-		/// <param name="sortType">Type of the sort.</param>
-		/// <returns/>
-		protected virtual bool IsSortTypeSupported(TreeNodeViewModel node, SortType sortType)
-		{
-			switch (node)
-			{
-				case DacGroupingNodeBaseViewModel _:
-				case DacFieldGroupingNodeBaseViewModel _:
-					return sortType == SortType.Alphabet;
-				default:
-					return sortType == SortType.Alphabet || sortType == SortType.Declaration;
-			}
-		}
-
-		protected List<TreeNodeViewModel> SortNodes(IEnumerable<TreeNodeViewModel> nodes, SortType sortType, SortDirection sortDirection)
-		{
-			if (nodes.IsNullOrEmpty())
-				return new List<TreeNodeViewModel>();
-
-			var sortedNodes = new List<TreeNodeViewModel>(capacity: 8);
-			var sortableNodes = new List<TreeNodeViewModel>(capacity: 8);
-
-			foreach (TreeNodeViewModel node in nodes)
-			{
-				if (!IsSortTypeSupported(node, sortType))
-					sortedNodes.Add(node);
-				else
-					sortableNodes.Add(node);
-			}
-
-			var sortResult = SortNodesBySortTypeAndDirection(sortableNodes, sortType, sortDirection);
-			sortedNodes.AddRange(sortResult);
-			return sortedNodes;
-		}
-
-		protected IEnumerable<TreeNodeViewModel> SortNodes(IReadOnlyCollection<TreeNodeViewModel> nodes, SortType sortType, SortDirection sortDirection)
-		{
-			if (nodes.IsNullOrEmpty())
-				yield break;
-
-			int sortableCount = nodes.Count;
-
-			foreach (TreeNodeViewModel node in nodes)
-			{
-				if (IsSortTypeSupported(node, sortType))
-				{
-					sortableCount--;
-					yield return node;
-				}
-			}
-
-			if (sortableCount == 0)
-				yield break;
-
-			var sortableNodes = nodes.Where(child => IsSortTypeSupported(child, sortType));
-			var sortedNodes = SortNodesBySortTypeAndDirection(sortableNodes, sortType, sortDirection);
-
-			foreach (TreeNodeViewModel node in sortedNodes)
-			{
-				yield return node;
-			}		
-		}
-
-		protected virtual IEnumerable<TreeNodeViewModel> SortNodesBySortTypeAndDirection(IEnumerable<TreeNodeViewModel> nodesToSort,
-																						 SortType sortType, SortDirection sortDirection)
-		{
-			switch (sortType)
-			{
-				case SortType.Declaration:
-					return sortDirection == SortDirection.Ascending
-						? nodesToSort.OrderBy(NodeDeclarationOrderComparer.Instance)
-						: nodesToSort.OrderByDescending(NodeDeclarationOrderComparer.Instance);
-
-				case SortType.Alphabet:
-					return sortDirection == SortDirection.Ascending
-						? nodesToSort.OrderBy(node => node.Name)
-						: nodesToSort.OrderByDescending(node => node.Name);
-
-				default:
-					return nodesToSort;
-			}
-		}	
 	}
 }
