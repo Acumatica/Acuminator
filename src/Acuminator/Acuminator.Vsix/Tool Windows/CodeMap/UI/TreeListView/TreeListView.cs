a6dd5638395133725e4866a35cdefe8dd5c289f7
Sergey Nikomarov <snikomarov@acumatica.com>
2020-02-25 10:05:30 +0300

    ATR-444: added TreeListView subclass of TreeView

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/UI/TreeListView/TreeListView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class TreeListView : TreeView
+	{
+		private GridViewColumnCollection _columns;
+
+		public GridViewColumnCollection Columns
+		{
+			get
+			{
+				if (_columns == null)
+				{
+					_columns = new GridViewColumnCollection();
+				}
+
+				return _columns;
+			}
+		}
+
+		protected override DependencyObject GetContainerForItemOverride() => new TreeListViewItem();
+
+		protected override bool IsItemItsOwnContainerOverride(object item) => item is TreeListViewItem;
+	}
+}
---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/UI/TreeListView/TreeListViewItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class TreeListViewItem : TreeViewItem
+	{
+		protected override DependencyObject GetContainerForItemOverride() => new TreeListViewItem();
+		
+		protected override bool IsItemItsOwnContainerOverride(object item) => item is TreeListViewItem;
+	}
+}
