85138046c0e116a2a7ce049774cc6fcfdb565772
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-09 21:54:56 +0300

    ATR-538: replaced value tuples from concurrent extensions with task result

---
+++ src/Acuminator/Acuminator.Utilities/Common/Concurrent/TaskResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Acuminator.Utilities.Common
+{
+	[StructLayout(LayoutKind.Auto)]
+	public readonly struct TaskResult<TResult>
+	{
+		public bool IsSuccess { get; }
+					
+		public TResult Result { get; }
+
+		public TaskResult(bool isSuccess, TResult result)
+		{
+			IsSuccess = isSuccess;
+			Result = result;
+		}
+	}
+}
