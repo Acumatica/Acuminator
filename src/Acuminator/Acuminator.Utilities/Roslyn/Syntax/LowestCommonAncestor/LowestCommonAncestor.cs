10e8da2248b7526047692f5ebf48dd75bca95d9b
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-09 23:18:53 +0300

    ATR-538: refactored LCA utils

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Syntax/LowestCommonAncestor/LowestCommonAncestor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Acuminator.Utilities.Common;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Syntax
+{
+	public static class LowestCommonAncestor
+	{
+		internal static (StatementSyntax Ancestor, StatementSyntax ScopedX, StatementSyntax ScopedY) GetCommonAncestorForSyntaxStatements(StatementSyntax x, StatementSyntax y)
+		{
+			x.ThrowOnNull(nameof(x));
+			y.ThrowOnNull(nameof(y));
+
+			//Depth is average O(log n) operation, worst case is O(n) but it isn't the case for the syntax tree which is wide but not very deep.
+			//For statements we could consider depth constrained by MethodDeclarationSyntax 
+			int depthX = x.Depth<MethodDeclarationSyntax, StatementSyntax>();
+			int depthY = y.Depth<MethodDeclarationSyntax, StatementSyntax>();
+
+			StatementSyntax currentX = x, prevX = null;
+			StatementSyntax currentY = y, prevY = null;
+
+			while (depthX != depthY)                //First get nodes on the equal levels of depth
+			{
+				if (depthX > depthY)
+				{
+					prevX = currentX;
+					currentX = currentX.Parent<StatementSyntax>();
+					depthX--;
+				}
+				else
+				{
+					prevY = currentY;
+					currentY = currentY.Parent<StatementSyntax>();
+					depthY--;
+				}
+			}
+
+			while (currentX != currentY)          //Then move up the branches until nodes coincide
+			{
+				prevX = currentX;
+				prevY = currentY;
+				currentX = currentX.Parent<StatementSyntax>();
+				currentY = currentY.Parent<StatementSyntax>();
+			}
+
+			return (currentX, prevX, prevY);
+		}
+	}
+}
--- src/Acuminator/Acuminator.Utilities/Roslyn/Syntax/MethodDeclarationUtils.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Syntax/MethodDeclarationUtils.cs
@@ -152,46 +152,6 @@ namespace Acuminator.Utilities.Roslyn.Syntax
 			return null;
 		}
 
-		public static (StatementSyntax Ancestor, StatementSyntax ScopedX, StatementSyntax ScopedY) LowestCommonAncestorSyntaxStatement(StatementSyntax x, StatementSyntax y)
-		{
-			x.ThrowOnNull(nameof(x));
-			y.ThrowOnNull(nameof(y));
-
-			//Depth is average O(log n) operation, worst case is O(n) but it isn't the case for the syntax tree which is wide but not very deep.
-			//For statements we could consider depth constrained by MethodDeclarationSyntax 
-			int depthX = x.Depth<MethodDeclarationSyntax, StatementSyntax>();
-			int depthY = y.Depth<MethodDeclarationSyntax, StatementSyntax>();
-
-			StatementSyntax currentX = x, prevX = null;
-			StatementSyntax currentY = y, prevY = null;
-
-			while (depthX != depthY)                //First get nodes on the equal levels of depth
-			{
-				if (depthX > depthY)
-				{
-					prevX = currentX;
-					currentX = currentX.Parent<StatementSyntax>();
-					depthX--;
-				}
-				else
-				{
-					prevY = currentY;
-					currentY = currentY.Parent<StatementSyntax>();
-					depthY--;
-				}
-			}
-
-			while (currentX != currentY)          //Then move up the branches until nodes coincide
-			{
-				prevX = currentX;
-				prevY = currentY;
-				currentX = currentX.Parent<StatementSyntax>();
-				currentY = currentY.Parent<StatementSyntax>();
-			}
-
-			return (currentX, prevX, prevY);
-		}
-
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsStatic(this BaseMethodDeclarationSyntax node)
         {
