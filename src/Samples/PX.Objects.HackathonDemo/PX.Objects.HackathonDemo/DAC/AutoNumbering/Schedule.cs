d40b7ddcdac81783a4cce0fd579dd219998ef84e
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-10 20:44:43 +0300

    ATR-536: added autonumbering example to demo solution

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/DAC/AutoNumbering/Schedule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Objects.CS;
+using PX.Objects.GL;
+
+namespace PX.Objects.HackathonDemo
+{
+	[PXCacheName("Schedule")]
+	public partial class Schedule : IBqlTable
+	{
+		#region Selected
+		public abstract class selected : PX.Data.BQL.BqlBool.Field<selected> { }
+
+		[PXBool]
+		[PXDefault(false, PersistingCheck = PXPersistingCheck.Nothing)]
+		[PXUIField(DisplayName = "Selected")]
+		public bool? Selected
+		{
+			get;
+			set;
+		}
+		#endregion
+
+		#region ScheduleID
+		public abstract class scheduleID : PX.Data.BQL.BqlString.Field<scheduleID> { }
+
+		[PXDBString(10, IsUnicode = true, IsKey = true, InputMask = ">CCCCCCCCCCCCCCC")]
+		[PXUIField(DisplayName = "Schedule ID", Visibility = PXUIVisibility.SelectorVisible)]
+		[AutoNumber(typeof(GLSetup.scheduleNumberingID), typeof(AccessInfo.businessDate))]
+		[PXSelector(typeof(Search<Schedule.scheduleID, Where<Schedule.module, Equal<Current<Schedule.module>>>>))]
+		[PXDefault]
+		public virtual string ScheduleID
+		{
+			get;
+			set;
+		}
+		#endregion
+
+		#region ScheduleName
+		public abstract class scheduleName : PX.Data.BQL.BqlString.Field<scheduleName> { }
+
+		[PXDBString(60, IsUnicode = true)]
+		[PXUIField(DisplayName = "Description", Visibility = PXUIVisibility.SelectorVisible)]
+		public virtual string ScheduleName
+		{
+			get;
+			set;
+		}
+		#endregion
+
+		#region Module
+		public abstract class module : PX.Data.BQL.BqlString.Field<module> { }
+
+		[PXDBString(2, IsFixed = true)]
+		[PXDefault(BatchModule.GL)]
+		public virtual string Module
+		{
+			get;
+			set;
+		}
+		#endregion
+	}
+}
