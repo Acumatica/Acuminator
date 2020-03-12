0574ee4d3e42da88b4df1548bc99775518252a2c
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-12 18:40:41 +0300

    ATR-536: added tests for PX1020

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/AutoNumberAttribute/InsufficientStringLengthForAutoNumberingTests.cs
@@ -0,0 +1,37 @@
+﻿using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.AutoNumberAttribute;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.AutoNumberAttribute
+{
+    public class InsufficientStringLengthForAutoNumberingTests : CodeFixVerifier
+    {
+        private const int NumberingSequenceStartNbrStringLength = 15;
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+            new DacAnalyzersAggregator(
+                CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+                                            .WithSuppressionMechanismDisabled(),
+                new DacAutoNumberAttributeAnalyzer());
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new InsufficientStringLengthForAutoNumberingFix();
+
+        [Theory]
+        [EmbeddedFileData("InsufficientStringLengthDac.cs")]
+        public async Task NonStringDAC_WithAutoNumbering(string source) =>
+             await VerifyCSharpDiagnosticAsync(source,
+                Descriptors.PX1020_InsufficientStringLengthForDacPropertyWithAutoNumbering.CreateFor(18, 15, NumberingSequenceStartNbrStringLength));
+
+        [Theory]
+        [EmbeddedFileData("InsufficientStringLengthDac.cs", "InsufficientStringLengthDac_Expected.cs")]
+        public async Task NonStringDAC_WithAutoNumbering_CodeFix(string actual, string expected) =>
+            await VerifyCSharpFixAsync(actual, expected);
+    }
+}
