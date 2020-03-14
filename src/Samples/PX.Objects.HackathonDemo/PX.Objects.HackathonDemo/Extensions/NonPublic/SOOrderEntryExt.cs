43e11662c30fc58fad8483bbc79f2c0c7de69d48
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-15 01:04:32 +0300

    ATR-441: added example to demo solution

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Extensions/NonPublic/SOOrderEntryExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.HackathonDemo
+{
+	public class SOOrderBalanced : PXGraphExtension<SOOrderEntry>
+	{
+		public SOOrderBalanced()
+		{
+			Base.Orders.AllowInsert = false;
+			Base.Orders.AllowUpdate = false;
+			Base.Orders.AllowDelete = false;
+			Base.Orders.WhereAnd<Where<SOOrderWithHold.hold, Equal<False>>>();
+		}
+	}
+}
---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Extensions/NonPublic/SOOrderExt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.HackathonDemo.Extensions.NonPublic
+{ 
+	internal sealed class SOOrderExt : PXCacheExtension<SOOrder>	//Non public DAC extensions are not supported
+	{
+        #region Total
+        public abstract class total : IBqlField { }
+		[PXDBDecimal]
+		[PXUIField(DisplayName = "Total")]
+		public decimal? Total { get; set; }
+		#endregion
+	}
+
+	sealed class SOOrderExtDiscount : PXCacheExtension<SOOrder>    //Non public DAC extensions are not supported
+	{
+		#region TotalDiscount
+		public abstract class totalDiscount : IBqlField { }
+
+		[PXDBDecimal]
+		[PXUIField(DisplayName = "Total Discount")]
+		public decimal? TotalDiscount { get; set; }
+		#endregion
+	}
+
+	static class SalesGlobalState
+	{
+		private class SalesInfo
+		{
+			public sealed class SOOrderExtSales : PXCacheExtension<SOOrder>    //Non public DAC extensions are not supported
+			{
+				#region TotalSales
+				public abstract class totalSales : IBqlField { }
+
+				[PXDBDecimal]
+				[PXUIField(DisplayName = "Total Sales")]
+
+				public decimal? TotalSales { get; set; }
+				#endregion
+			}
+		}
+	}
+}
