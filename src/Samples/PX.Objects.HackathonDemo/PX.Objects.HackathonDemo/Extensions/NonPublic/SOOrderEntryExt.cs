82d1131ce7b11bd03b0f39a2678d961a09fe31e7
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-15 01:40:17 +0300

    ATR-441: added example with non public graph extension to demo solution

--- src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Extensions/NonPublic/SOOrderEntryExt.cs
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Extensions/NonPublic/SOOrderEntryExt.cs
@@ -5,16 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using PX.Data;
 
-namespace PX.Objects.HackathonDemo
+namespace PX.Objects.HackathonDemo.Extensions.NonPublic
 {
-	public class SOOrderBalanced : PXGraphExtension<SOOrderEntry>
-	{
-		public SOOrderBalanced()
+	internal class SOOrderExt1 : PXGraphExtension<SOOrderEntry>  //Non public graph extensions are not supported
+	{ 
+		public virtual void _(Events.RowUpdating<SOOrder> e)
 		{
-			Base.Orders.AllowInsert = false;
-			Base.Orders.AllowUpdate = false;
-			Base.Orders.AllowDelete = false;
-			Base.Orders.WhereAnd<Where<SOOrderWithHold.hold, Equal<False>>>();
+
+		}
+	}
+
+	class SOOrderExt2 : PXGraphExtension<SOOrderEntry>          //Non public graph extensions are not supported
+	{
+		public virtual void _(Events.RowUpdated<SOOrder> e)
+		{
+
+		}
+	}
+
+
+	static class PurchaseGlobalState
+	{
+		private class PurchaseEngine
+		{
+			public sealed class SOOrderEntryExtPurchase : PXGraphExtension<SOOrderEntry>    //Non public graph extensions are not supported
+			{
+				#region TotalSales
+				public abstract class totalSales : IBqlField { }
+
+				[PXDBDecimal]
+				[PXUIField(DisplayName = "Total Sales")]
+
+				public decimal? TotalSales { get; set; }
+				#endregion
+			}
 		}
 	}
 }
