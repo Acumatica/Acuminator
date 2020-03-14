5f4b6d7de9284c41f9f481c4fabb1fb872b5d3e0
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-15 00:30:32 +0300

    ATR-441: added Where helper for SyntaxTokenList

--- src/Acuminator/Acuminator.Utilities/Common/EnumerableExtensions.cs
+++ src/Acuminator/Acuminator.Utilities/Common/EnumerableExtensions.cs
@@ -139,6 +139,33 @@ namespace Acuminator.Utilities.Common
 		}
 
 		/// <summary>
+		/// Where method for <see cref="SyntaxTokenList"/>. This is an optimization method which allows to avoid boxing.
+		/// </summary>
+		/// <param name="source">The <see cref="SyntaxTokenList"/> to act on.</param>
+		/// <param name="predicate">The predicate.</param>
+		/// <returns/>
+		[DebuggerStepThrough]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static IEnumerable<SyntaxToken> Where(this SyntaxTokenList source, Func<SyntaxToken, bool> predicate)
+		{
+			predicate.ThrowOnNull(nameof(predicate));
+			return WhereForSyntaxTokenListImplementation();
+
+			IEnumerable<SyntaxToken> WhereForSyntaxTokenListImplementation()
+			{
+				for (int i = 0; i < source.Count; i++)
+				{
+					SyntaxToken token = source[i];
+
+					if (predicate(token))
+					{
+						yield return token;
+					}
+				}
+			}
+		}
+
+		/// <summary>
 		/// Where method for <see cref="SyntaxList{TNode}"/>. This is an optimization method which allows to avoid boxing.
 		/// </summary>
 		/// <typeparam name="TNode">Type of the syntax node.</typeparam>
