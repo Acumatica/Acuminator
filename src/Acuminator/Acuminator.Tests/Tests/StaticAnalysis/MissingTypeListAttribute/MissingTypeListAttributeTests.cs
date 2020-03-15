490251e6c5cfd8451442abd2bc4a3d0ba61b1dad
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-15 13:28:14 +0300

    ATR-441: rewrote old tests to be async

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/MissingTypeListAttribute/MissingTypeListAttributeTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/MissingTypeListAttribute/MissingTypeListAttributeTests.cs
@@ -1,4 +1,5 @@
-﻿using Acuminator.Analyzers.StaticAnalysis;
+﻿using System.Threading.Tasks;
+using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.Dac;
 using Acuminator.Analyzers.StaticAnalysis.MissingTypeListAttribute;
 using Acuminator.Tests.Helpers;
@@ -14,33 +15,26 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.MissingTypeListAttribute
     {
         [Theory]
         [EmbeddedFileData("MissingTypeListAttributeGood.cs")]
-        public void TestDiagnostic_Good(string actual)
-        {
-            VerifyCSharpDiagnostic(actual);
-        }
+        public async Task TestDiagnostic_Good(string actual) =>
+            await VerifyCSharpDiagnosticAsync(actual);
 
         [Theory]
         [EmbeddedFileData("MissingTypeListAttributeInheritedList.cs")]
-        public void TestDiagnostic_InheritedList_Good(string actual)
-        {
-            VerifyCSharpDiagnostic(actual);
-        }
+        public async Task TestDiagnostic_InheritedList_Good(string actual) =>
+            await VerifyCSharpDiagnosticAsync(actual);
 
-        [Theory]
-        [EmbeddedFileData("MissingTypeListAttributeBad.cs")]
-        public void TestDiagnostic_Bad(string actual)
-        {
-            VerifyCSharpDiagnostic(actual, Descriptors.PX1002_MissingTypeListAttributeAnalyzer.CreateFor(14, 17));
-        }
+        [Theory] 
+        [EmbeddedFileData("MissingTypeListAttributeBad.cs")] 
+        public async Task TestDiagnostic_Bad(string actual) => 
+            await VerifyCSharpDiagnosticAsync(actual, 
+                                              Descriptors.PX1002_MissingTypeListAttributeAnalyzer.CreateFor(14, 17));
 
         [Theory]
         [EmbeddedFileData("MissingTypeListAttributeBad.cs", "MissingTypeListAttributeBad_Expected.cs")]
-        public void TestCodeFix(string actual, string expected)
-        {
-            VerifyCSharpFix(actual, expected);
-        }
+        public async Task TestCodeFix(string actual, string expected) =>
+            await VerifyCSharpFixAsync(actual, expected);
 
-		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
 			new DacAnalyzersAggregator(
 				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
 											.WithSuppressionMechanismDisabled(),
