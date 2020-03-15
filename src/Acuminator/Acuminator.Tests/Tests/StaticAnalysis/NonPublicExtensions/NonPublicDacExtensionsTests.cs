e6b9b961764bee1afb1867abf8563d342f2c168d
Nikomarov Sergey <snikomarov@acumatica.com>
2020-03-15 13:07:35 +0300

    ATR-441: Added tests and sources for non public extensions analyzer

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicExtensions/NonPublicDacExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.NonPublicExtensions;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.NonPublicExtensions
+{
+    public class NonPublicDacExtensionsTests : CodeFixVerifier
+    {
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new NonPublicExtensionFix();
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new DacAnalyzersAggregator(
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled(),
+				new NonPublicGraphAndDacExtensionAnalyzer());
+
+		[Theory]
+	    [EmbeddedFileData("NonNullableTypeForBqlField.cs")] 
+		public void TestDiagnostic(string actual)
+	    {
+		    VerifyCSharpDiagnostic(actual, Descriptors.PX1014_NonNullableTypeForBqlField.CreateFor(16, 14));
+	    }
+
+		[Theory]
+        [EmbeddedFileData("NonNullableTypeForBqlField_Expected.cs")]
+        public void TestDiagnostic_ShouldNotShowDiagnostic(string actual)
+        {
+            VerifyCSharpDiagnostic(actual);
+        }
+
+	    [Theory]
+	    [EmbeddedFileData("NonNullableTypeForBqlField_Valid.cs")]
+	    public void TestDiagnostic_ShouldNotShowDiagnostic2(string actual)
+	    {
+		    VerifyCSharpDiagnostic(actual);
+	    }
+
+		[Theory]
+	    [EmbeddedFileData("NonNullableTypeForBqlField.cs",
+						  "NonNullableTypeForBqlField_Expected.cs")]
+	    public void TestCodeFix(string actual, string expected)
+	    {
+		    VerifyCSharpFix(actual, expected);
+	    }   
+    }
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicExtensions/Sources/NonPublicGraphExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace PX.Objects.HackathonDemo.Extensions.NonPublic
+{
+	internal class SOOrderExt1 : PXGraphExtension<SOOrderEntry>  //Non public graph extensions are not supported
+	{ 
+		public virtual void _(Events.RowUpdating<SOOrder> e)
+		{
+
+		}
+	}
+
+	class SOOrderExt2 : PXGraphExtension<SOOrderEntry>          //Non public graph extensions are not supported
+	{
+		public virtual void _(Events.RowUpdated<SOOrder> e)
+		{
+
+		}
+	}
+
+
+	static class PurchaseGlobalState
+	{
+		private class PurchaseEngine
+		{
+			public sealed class SOOrderEntryExtPurchase : PXGraphExtension<SOOrderEntry>    //Non public graph extensions are not supported
+			{
+				#region TotalSales
+				public abstract class totalSales : IBqlField { }
+
+				[PXDBDecimal]
+				[PXUIField(DisplayName = "Total Sales")]
+
+				public decimal? TotalSales { get; set; }
+				#endregion
+			}
+		}
+	}
+}
