13ada3c4b4a07c0d40cac838ec019a7e2930b2e2
Nikomarov Sergey <snikomarov@acumatica.com>
2020-04-09 04:42:26 +0300

    ATR-440: added graph semantic model crreation options enum

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Enums/GraphSemanticModelCreationOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
+{
+	/// <summary>
+	/// Values that represent graph semantic model creation options.
+	/// </summary>
+	public enum GraphSemanticModelCreationOptions
+	{
+		/// <summary>
+		/// Collect all possible semantic data during construction of the graph semantic model.
+		/// </summary>
+		CollectAll,
+
+		/// <summary>
+		/// Do not collect graph init delegates.
+		/// </summary>
+		DoNotCollectInitDelegates
+	}
+}
