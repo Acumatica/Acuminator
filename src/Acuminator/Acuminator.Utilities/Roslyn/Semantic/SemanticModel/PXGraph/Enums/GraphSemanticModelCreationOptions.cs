a1eafd616890f51a9c03f01641429730860250c4
Nikomarov Sergey <snikomarov@acumatica.com>
2020-04-10 17:16:07 +0300

    ATR-440: added graph semantic model creation options

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Enums/GraphSemanticModelCreationOptions.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Enums/GraphSemanticModelCreationOptions.cs
@@ -5,16 +5,34 @@ namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 	/// <summary>
 	/// Values that represent graph semantic model creation options.
 	/// </summary>
+	[Flags]
 	public enum GraphSemanticModelCreationOptions
 	{
 		/// <summary>
-		/// Collect all possible semantic data during construction of the graph semantic model.
+		/// A binary constant representing the collect general graph Information flag.
 		/// </summary>
-		CollectAll,
+		CollectGeneralGraphInfo = 0,
+	
+		/// <summary>
+		/// A binary constant representing the collect processing delegates flag.
+		/// </summary>
+		CollectProcessingDelegates = 0b_00001,
+		
+		/// <summary>
+		/// A binary constant representing the infer implicit models flag.
+		/// </summary>
+		InferImplicitModels = 0b_00010,
 
 		/// <summary>
-		/// Do not collect graph init delegates.
+		/// Collect all possible semantic data during construction of the graph semantic model.
 		/// </summary>
-		DoNotCollectInitDelegates
+		CollectAll = CollectGeneralGraphInfo | CollectProcessingDelegates | InferImplicitModels
+	}
+
+
+	public static class GraphSemanticModelCreationOptionsUtils
+	{
+		public static bool HasOption(this GraphSemanticModelCreationOptions creationOptions, GraphSemanticModelCreationOptions flagToCheck) =>
+			(creationOptions & flagToCheck) == flagToCheck;
 	}
 }
