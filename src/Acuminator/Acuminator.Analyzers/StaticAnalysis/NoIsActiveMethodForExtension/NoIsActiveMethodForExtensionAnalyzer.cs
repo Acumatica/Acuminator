8b09a8005d0ffe01a5f6f93c017aa51715235f25
Nikomarov Sergey <snikomarov@acumatica.com>
2020-06-07 17:01:01 +0300

    ATR-560: finished analysis for graph extensions

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NoIsActiveMethodForExtension/NoIsActiveMethodForExtensionAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NoIsActiveMethodForExtension/NoIsActiveMethodForExtensionAnalyzer.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 
-using Acuminator.Analyzers.StaticAnalysis.AnalyzersAggregator;
 using Acuminator.Analyzers.StaticAnalysis.Dac;
 using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Utilities.DiagnosticSuppression;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.Dac;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Acuminator.Utilities.Roslyn.Syntax;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Acuminator.Analyzers.StaticAnalysis.NoIsActiveMethodForExtension
@@ -30,16 +31,12 @@ namespace Acuminator.Analyzers.StaticAnalysis.NoIsActiveMethodForExtension
 			dacExtension.IsActiveMethod == null &&
 			!dacExtension.IsMappedCacheExtension && !dacExtension.Symbol.IsAbstract && !dacExtension.Symbol.IsStatic;
 
-		public bool ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel graphExtension) =>
-			graphExtension?.Type == GraphType.PXGraphExtension && 
-			graphExtension.IsActiveMethod == null &&
-			!graphExtension.Symbol.IsAbstract && !graphExtension.Symbol.IsStatic && !graphExtension.Symbol.IsGenericType;
-
 		public void Analyze(SymbolAnalysisContext symbolContext, PXContext pxContext, DacSemanticModel dacExtension)
 		{
 			symbolContext.CancellationToken.ThrowIfCancellationRequested();
 
-			//ShouldAnalyze already filtered everything and left only DAC extensions without IsActive
+			// ShouldAnalyze already filtered everything and left only DAC extensions without IsActive
+			// We just need to report them
 			Location location = dacExtension.Node.Identifier.GetLocation();
 
 			if (location == null)
@@ -50,18 +47,25 @@ namespace Acuminator.Analyzers.StaticAnalysis.NoIsActiveMethodForExtension
 				pxContext.CodeAnalysisSettings);
 		}
 
+		public bool ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel graphExtension) =>
+			graphExtension?.Type == GraphType.PXGraphExtension &&
+			graphExtension.IsActiveMethod == null &&
+			!graphExtension.Symbol.IsAbstract && !graphExtension.Symbol.IsStatic && !graphExtension.Symbol.IsGenericType;
+
 		public void Analyze(SymbolAnalysisContext symbolContext, PXContext pxContext, PXGraphSemanticModel graphExtension)
 		{
 			symbolContext.CancellationToken.ThrowIfCancellationRequested();
 
-			//ShouldAnalyze already filtered everything and left only graph extensions without IsActive
-			Location location = graphExtension..Identifier.GetLocation();
+			// ShouldAnalyze already filtered everything and left only graph extensions without IsActive
+			// We just need to report them
+			var syntaxNode = graphExtension.Symbol.GetSyntax(symbolContext.CancellationToken);
+			Location location = (syntaxNode as ClassDeclarationSyntax)?.Identifier.GetLocation() ?? syntaxNode?.GetLocation();
 
 			if (location == null)
 				return;
 
 			symbolContext.ReportDiagnosticWithSuppressionCheck(
-				Diagnostic.Create(Descriptors.PX1016_NoIsActiveMethodForDacExtension, location),
+				Diagnostic.Create(Descriptors.PX1016_NoIsActiveMethodForGraphExtension, location),
 				pxContext.CodeAnalysisSettings);
 		}
 	}
