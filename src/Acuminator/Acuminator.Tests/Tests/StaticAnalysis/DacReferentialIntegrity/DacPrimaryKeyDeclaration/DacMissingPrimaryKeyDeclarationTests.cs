3910818f5581e5694d2a8ac9c8b05b5cfa204702
Nikomarov Sergey <snikomarov@acumatica.com>
2020-06-17 02:58:28 +0300

    ATR-564: added tests for adding primary key declaration code fix

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacReferentialIntegrity/DacPrimaryKeyDeclaration/DacMissingPrimaryKeyDeclarationTests.cs
@@ -0,0 +1,48 @@
+﻿using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.DacReferentialIntegrity
+{
+	public class DacMissingPrimaryKeyDeclarationTests : CodeFixVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new DacAnalyzersAggregator(
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled(),
+				new DacPrimaryKeyDeclarationAnalyzer());
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new DacMissingPrimaryKeyFix();
+
+		[Theory]
+		[EmbeddedFileData(@"MissingPrimaryKey\SOOrder_Without_PrimaryKey.cs")]
+		public async Task SOOrder_WithoutPrimaryKey(string source) =>
+			await VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1033_MissingDacPrimaryKeyDeclaration.CreateFor(6, 15));
+
+		[Theory]
+		[EmbeddedFileData(@"MissingPrimaryKey\SOLine_Without_PrimaryKey.cs")]
+		public async Task SOLine_WithoutPrimaryKey(string source) =>
+			await VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1033_MissingDacPrimaryKeyDeclaration.CreateFor(6, 15));
+
+		[Theory]
+		[EmbeddedFileData(@"MissingPrimaryKey\SOOrder_Without_PrimaryKey.cs", 
+						  @"MissingPrimaryKey\SOOrder_Without_PrimaryKey_Expected.cs")]
+		public async Task AddPrimaryKeyDeclaration_ToSOOrder_VerifyCodeFix(string actual, string expected) =>
+			await VerifyCSharpFixAsync(actual, expected);
+
+		[Theory]
+		[EmbeddedFileData(@"MissingPrimaryKey\SOLine_Without_PrimaryKey.cs", 
+						  @"MissingPrimaryKey\SOLine_Without_PrimaryKey_Expected.cs")]
+		public async Task AddPrimaryKeyDeclaration_ToSOLine_VerifyCodeFix(string actual, string expected) =>
+			await VerifyCSharpFixAsync(actual, expected);
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacReferentialIntegrity/DacPrimaryKeyDeclaration/Sources/MissingPrimaryKey/SOLine_Without_PrimaryKey.cs
@@ -0,0 +1,38 @@
+﻿using PX.Data;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.DacReferentialIntegrity.Sources
+{
+	[PXCacheName("SO Line")]
+	public class SOLine : IBqlTable
+	{
+		public abstract class orderType : PX.Data.BQL.BqlString.Field<orderType> { }
+
+		[PXDBString(2, IsKey = true, IsFixed = true)]
+		[PXUIField(DisplayName = "Order Type", Visible = false, Enabled = false)]
+		public virtual string OrderType
+		{
+			get;
+			set;
+		}
+
+		public abstract class orderNbr : PX.Data.BQL.BqlString.Field<orderNbr> { }
+
+		[PXDBString(15, IsUnicode = true, IsKey = true, InputMask = "")]
+		[PXUIField(DisplayName = "Order Nbr.", Visible = false, Enabled = false)]
+		public virtual string OrderNbr
+		{
+			get;
+			set;
+		}
+
+		public abstract class lineNbr : PX.Data.BQL.BqlInt.Field<lineNbr> { }
+
+		[PXDBInt(IsKey = true)]
+		[PXUIField(DisplayName = "Line Nbr.", Visible = false)]
+		public virtual int? LineNbr
+		{
+			get;
+			set;
+		}
+	}
+}
