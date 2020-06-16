ce1ad4f62f5163c70765f3c12b20c54b60500609
Nikomarov Sergey <snikomarov@acumatica.com>
2020-06-16 05:30:22 +0300

    ATR-564: added generation of missing primary key with Find method

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DacReferentialIntegrity/DacPrimaryKeyDeclaration/DacMissingPrimaryKeyFix.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DacReferentialIntegrity/DacPrimaryKeyDeclaration/DacMissingPrimaryKeyFix.cs
@@ -16,9 +16,13 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Text;
 
+using PXReferentialIntegritySymbols = Acuminator.Utilities.Roslyn.Semantic.Symbols.PXReferentialIntegritySymbols;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
 
 namespace Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity
 {
@@ -64,103 +68,115 @@ namespace Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity
 				return document;
 		
 			var pxContext = new PXContext(semanticModel.Compilation, codeAnalysisSettings: null);
+
+            if (pxContext.ReferentialIntegritySymbols.PrimaryKeyOf == null)
+                return document;
+
 			var dacSemanticModel = DacSemanticModel.InferModel(pxContext, dacTypeSymbol, cancellation);
 			List<DacPropertyInfo> dacKeys = dacSemanticModel?.DacProperties.Where(property => property.IsKey).ToList(capacity: 4);
 
-			if (dacKeys.IsNullOrEmpty())
+			if (dacKeys.IsNullOrEmpty() || dacKeys.Count > PXReferentialIntegritySymbols.MaxPrimaryKeySize)
 				return document;
 
-			var primaryKeyNode = CreatePrimaryKeyNode(dacKeys);
+			var primaryKeyNode = CreatePrimaryKeyNode(document, pxContext, dacSemanticModel, dacKeys);
 			var newDacNode = dacNode.WithMembers(
 										dacNode.Members.Insert(0, primaryKeyNode));
 			var newRoot = root.ReplaceNode(dacNode, newDacNode);
-			var newDocument = document.WithSyntaxRoot(newRoot);
+            newRoot = AddUsingsForReferentialIntegrityNamespace(newRoot);
 
-			return newDocument;			
+			var newDocument = document.WithSyntaxRoot(newRoot);
+            var formattedDocument = await Formatter.FormatAsync(newDocument, cancellationToken: cancellation);
+			return formattedDocument;			
 		}
 
-        private ClassDeclarationSyntax CreatePrimaryKeyNode(List<DacPropertyInfo> dacKeys)
+        private ClassDeclarationSyntax CreatePrimaryKeyNode(Document document, PXContext pxContext, DacSemanticModel dacSemanticModel,
+                                                            List<DacPropertyInfo> dacKeys)
         {
-            ClassDeclaration(TypeNames.PrimaryKeyClassName)
-                .WithModifiers(
-                    TokenList(
-                        Token(SyntaxKind.PublicKeyword)))
-                .WithBaseList(
-                    )
-                .WithMembers(
-                    SingletonList<MemberDeclarationSyntax>(
-                        MethodDeclaration(
-                            IdentifierName("SOOrder"),
-                            Identifier("Find"))
-                        .WithModifiers(
-                            TokenList(
-                                new[]{
-                                    Token(SyntaxKind.PublicKeyword),
-                                    Token(SyntaxKind.StaticKeyword)}))
-                        .WithParameterList(
-                            ParameterList(
-                                SeparatedList<ParameterSyntax>(
-                                    new SyntaxNodeOrToken[]{
-                                        Parameter(
-                                            Identifier("graph"))
-                                        .WithType(
-                                            IdentifierName("PXGraph")),
-                                        Token(SyntaxKind.CommaToken),
-                                        Parameter(
-                                            Identifier("orderType"))
-                                        .WithType(
-                                            PredefinedType(
-                                                Token(SyntaxKind.StringKeyword))),
-                                        Token(SyntaxKind.CommaToken),
-                                        Parameter(
-                                            Identifier("orderNbr"))
-                                        .WithType(
-                                            PredefinedType(
-                                                Token(SyntaxKind.StringKeyword)))})))
-                        .WithExpressionBody(
-                            ArrowExpressionClause(
-                                InvocationExpression(
-                                    IdentifierName("FindBy"))
-                                .WithArgumentList(
-                                    ArgumentList(
-                                        SeparatedList<ArgumentSyntax>(
-                                            new SyntaxNodeOrToken[]{
-                                                Argument(
-                                                    IdentifierName("graph")),
-                                                Token(SyntaxKind.CommaToken),
-                                                Argument(
-                                                    IdentifierName("orderType")),
-                                                Token(SyntaxKind.CommaToken),
-                                                Argument(
-                                                    IdentifierName("orderNbr"))})))))
-                        .WithSemicolonToken(
-                            Token(SyntaxKind.SemicolonToken))))
-                .NormalizeWhitespace();
-
+            var generator = SyntaxGenerator.GetGenerator(document);
+            SyntaxNode baseClassNode = MakeBaseClassNode(generator, pxContext, dacSemanticModel, dacKeys);
+            var findMethod = MakeFindMethodNode(generator, pxContext, dacSemanticModel, dacKeys);
+            var keyDeclaration = generator.ClassDeclaration(TypeNames.PrimaryKeyClassName, accessibility: Accessibility.Public,
+                                                            baseType: baseClassNode, members: findMethod.ToEnumerable());
+            return keyDeclaration as ClassDeclarationSyntax;
         }
 
-		private BaseListSyntax MakeBaseListNode()
+        private SyntaxNode MakeBaseClassNode(SyntaxGenerator generator, PXContext pxContext, DacSemanticModel dacSemanticModel, 
+                                             List<DacPropertyInfo> dacKeys)
+        {
+            const string ByTypeName = "By";
+            var primaryKeyOfTypeNode = generator.GenericName(pxContext.ReferentialIntegritySymbols.PrimaryKeyOf.Name,
+                                                             generator.TypeExpression(dacSemanticModel.Symbol));
+            var dacFieldTypeArgNodes = dacKeys.Select(keyProperty => dacSemanticModel.FieldsByNames[keyProperty.Name])
+                                              .Select(keyField => generator.TypeExpression(keyField.Symbol));
+
+            return generator.QualifiedName(primaryKeyOfTypeNode, generator.GenericName(ByTypeName, dacFieldTypeArgNodes));
+        }
+
+        private SyntaxNode MakeFindMethodNode(SyntaxGenerator generator, PXContext pxContext, DacSemanticModel dacSemanticModel,
+                                              List<DacPropertyInfo> dacKeys)
+        {
+            var returnType = generator.TypeExpression(dacSemanticModel.Symbol);
+            var parameters = MakeParameterNodesForFindMethod(generator, pxContext, dacSemanticModel, dacKeys);
+            var findMethodNode = generator.MethodDeclaration(DelegateNames.PrimaryKeyFindMethod, parameters, 
+                                                             typeParameters: null, returnType,
+                                                             Accessibility.Public, DeclarationModifiers.Static) as MethodDeclarationSyntax;
+
+            var findByCallArguments = parameters.OfType<ParameterSyntax>()
+                                                .Select(parameter => Argument(
+                                                                            IdentifierName(parameter.Identifier)));
+            var findByInvocation = 
+                generator.InvocationExpression(
+                            IdentifierName(DelegateNames.PrimaryKeyFindByMethod), findByCallArguments) as InvocationExpressionSyntax;
+
+            if (findMethodNode.Body != null)
+            {
+                findMethodNode = findMethodNode.RemoveNode(findMethodNode.Body, SyntaxRemoveOptions.KeepNoTrivia);
+            }
+
+            return findMethodNode.WithExpressionBody(
+                                        ArrowExpressionClause(findByInvocation))
+                                 .WithSemicolonToken(
+                                        Token(SyntaxKind.SemicolonToken));      
+        }
+
+        private List<SyntaxNode> MakeParameterNodesForFindMethod(SyntaxGenerator generator, PXContext pxContext,
+                                                                 DacSemanticModel dacSemanticModel, List<DacPropertyInfo> dacKeys)
 		{
-            BaseList(
-                        SingletonSeparatedList<BaseTypeSyntax>(
-                            SimpleBaseType(
-                                QualifiedName(
-                                    GenericName(
-                                        Identifier("PrimaryKeyOf"))
-                                    .WithTypeArgumentList(
-                                        TypeArgumentList(
-                                            SingletonSeparatedList<TypeSyntax>(
-                                                IdentifierName("SOOrder")))),
-                                    GenericName(
-                                        Identifier("By"))
-                                    .WithTypeArgumentList(
-                                        TypeArgumentList(
-                                            SeparatedList<TypeSyntax>(
-                                                new SyntaxNodeOrToken[]{
-                                                    IdentifierName("orderType"),
-                                                    Token(SyntaxKind.CommaToken),
-                                                    IdentifierName("orderNbr")})))))))
+            const string graphParameterName = "graph";
+            var graphParameter = generator.ParameterDeclaration(graphParameterName,
+                                                                generator.TypeExpression(pxContext.PXGraph.Type));
+            var parameters = new List<SyntaxNode>(capacity: dacKeys.Count + 1)
+            {
+                graphParameter
+            };
 
+			foreach (DacPropertyInfo keyProperty in dacKeys)
+			{
+				DacFieldInfo keyField = dacSemanticModel.FieldsByNames[keyProperty.Name];
+                var parameterType = generator.TypeExpression(keyProperty.PropertyType);
+                var parameterNode = generator.ParameterDeclaration(keyField.Name, parameterType);
+
+                parameters.Add(parameterNode);
+			}
+
+            return parameters;
+		}
+
+        private SyntaxNode AddUsingsForReferentialIntegrityNamespace(SyntaxNode root)
+		{
+            if (!(root is CompilationUnitSyntax compilationUnit))
+                return root;
+
+            bool alreadyHasUsing =
+                 compilationUnit.Usings
+                                .Any(usingDirective => NamespaceNames.ReferentialIntegrityAttributes == usingDirective.Name?.ToString());
+
+            if (alreadyHasUsing)
+                return root;
+
+            return compilationUnit.AddUsings(
+                        UsingDirective(
+                            ParseName(NamespaceNames.ReferentialIntegrityAttributes)));  
         }
-	}
+    }
 }
