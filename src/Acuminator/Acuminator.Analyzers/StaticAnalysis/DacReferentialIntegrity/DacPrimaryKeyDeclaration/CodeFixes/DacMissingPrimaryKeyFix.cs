69426fc3e4f89cdf1c42b4b3992fd48928b51a29
Nikomarov Sergey <snikomarov@acumatica.com>
2020-07-10 13:01:39 +0300

    ATR-564: refactored missing primary key code fix

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DacReferentialIntegrity/DacPrimaryKeyDeclaration/CodeFixes/DacMissingPrimaryKeyFix.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DacReferentialIntegrity/DacPrimaryKeyDeclaration/CodeFixes/DacMissingPrimaryKeyFix.cs
@@ -35,43 +35,44 @@ namespace Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity
 
 		public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
-		public override Task RegisterCodeFixesAsync(CodeFixContext context)
+		public async override Task RegisterCodeFixesAsync(CodeFixContext context)
 		{
 			context.CancellationToken.ThrowIfCancellationRequested();
 
-			var codeActionTitle = nameof(Resources.PX1033Fix).GetLocalized().ToString();
-			var codeAction = CodeAction.Create(codeActionTitle,
-											   cancellation => AddPrimaryKeyDeclarationToDacAsync(context.Document, context.Span, cancellation),
-											   equivalenceKey: codeActionTitle);
-
-			context.RegisterCodeFix(codeAction, context.Diagnostics);
-			return Task.CompletedTask;
-		}
-
-		private async Task<Document> AddPrimaryKeyDeclarationToDacAsync(Document document, TextSpan span, CancellationToken cancellation)
-		{
-			cancellation.ThrowIfCancellationRequested();
-
-			var rootTask = document.GetSyntaxRootAsync(cancellation);
-			var semanticModelTask = document.GetSemanticModelAsync(cancellation);
+			var rootTask = context.Document.GetSyntaxRootAsync(context.CancellationToken);
+			var semanticModelTask = context.Document.GetSemanticModelAsync(context.CancellationToken);
 
 			await Task.WhenAll(rootTask, semanticModelTask).ConfigureAwait(false);
 
 			var root = rootTask.Result;
 			SemanticModel semanticModel = semanticModelTask.Result;
 
-			if (!(root?.FindNode(span) is ClassDeclarationSyntax dacNode))
-				return document;
+			if (!(root?.FindNode(context.Span) is ClassDeclarationSyntax dacNode))
+				return;
 
-			var dacTypeSymbol = semanticModel?.GetDeclaredSymbol(dacNode, cancellation);
+			INamedTypeSymbol dacTypeSymbol = semanticModel?.GetDeclaredSymbol(dacNode, context.CancellationToken);
 
 			if (dacTypeSymbol == null)
-				return document;
+				return;
 
 			var pxContext = new PXContext(semanticModel.Compilation, codeAnalysisSettings: null);
 
 			if (pxContext.ReferentialIntegritySymbols.PrimaryKeyOf == null)
-				return document;
+				return;
+
+			var codeActionTitle = nameof(Resources.PX1033Fix).GetLocalized().ToString();
+			var codeAction = CodeAction.Create(codeActionTitle,
+											   cancellation => AddPrimaryKeyDeclarationToDacAsync(context.Document, root, dacNode, pxContext, 
+																								  dacTypeSymbol, cancellation),
+											   equivalenceKey: codeActionTitle);
+
+			context.RegisterCodeFix(codeAction, context.Diagnostics);
+		}
+
+		private Task<Document> AddPrimaryKeyDeclarationToDacAsync(Document document, SyntaxNode root, ClassDeclarationSyntax dacNode, PXContext pxContext,
+																  INamedTypeSymbol dacTypeSymbol, CancellationToken cancellation)
+		{
+			cancellation.ThrowIfCancellationRequested();
 
 			var dacSemanticModel = DacSemanticModel.InferModel(pxContext, dacTypeSymbol, cancellation);
 			List<DacPropertyInfo> dacKeys = dacSemanticModel?.DacProperties
@@ -80,7 +81,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity
 															 .ToList(capacity: 4);
 
 			if (dacKeys.IsNullOrEmpty() || dacKeys.Count > PXReferentialIntegritySymbols.MaxPrimaryKeySize)
-				return document;
+				return Task.FromResult(document);
 
 			var primaryKeyNode = CreatePrimaryKeyNode(document, pxContext, dacSemanticModel, dacKeys);
 			var newDacNode = dacNode.WithMembers(
@@ -88,8 +89,9 @@ namespace Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity
 
 			var changedRoot = root.ReplaceNode(dacNode, newDacNode);
 			changedRoot = AddUsingsForReferentialIntegrityNamespace(changedRoot);
+			var modifiedDocument = document.WithSyntaxRoot(changedRoot);
 
-			return document.WithSyntaxRoot(changedRoot);
+			return Task.FromResult(modifiedDocument);
 		}
 
 		private ClassDeclarationSyntax CreatePrimaryKeyNode(Document document, PXContext pxContext, DacSemanticModel dacSemanticModel,
