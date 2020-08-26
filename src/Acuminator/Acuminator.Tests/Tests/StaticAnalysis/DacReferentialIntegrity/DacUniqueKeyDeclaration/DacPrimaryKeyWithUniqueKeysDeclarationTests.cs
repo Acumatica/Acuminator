be856938700ae5fd89ea6dc054b3cc7053ac5a3d
Nikomarov Sergey <snikomarov@acumatica.com>
2020-08-26 04:36:48 +0300

    ATR-576: added tests for correct unique keys declarations

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacReferentialIntegrity/DacUniqueKeyDeclaration/DacPrimaryKeyWithUniqueKeysDeclarationTests.cs
@@ -0,0 +1,30 @@
+﻿using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.DacReferentialIntegrity;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.DacReferentialIntegrity
+{
+	public class DacPrimaryKeyWithUniqueKeysDeclarationTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new DacAnalyzersAggregator(
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled(),
+				new DacPrimaryKeyDeclarationAnalyzer());
+
+		[Theory]
+		[EmbeddedFileData("Dac_SingleUniqueKey_Good.cs")]
+		public async Task Dac_WithCorrectPrimaryAndSingleUniqueKeys_DoesntReportDiagnostic(string source) =>
+			await VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData("Dac_MultipleUniqueKey_Good.cs")]
+		public async Task Dac_WithCorrectPrimaryAndMultipleUniqueKeys_DoesntReportDiagnostic(string source) =>
+			await VerifyCSharpDiagnosticAsync(source);
+	}
+}
