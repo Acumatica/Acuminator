3f35803691d4c51b00c708686d32c1fb54b78683
Nikomarov Sergey <snikomarov@acumatica.com>
2020-09-26 22:20:28 +0300

    ATR-579: fixing duplicate keys in DAC test

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacReferentialIntegrity/DacForeignKeyDeclaration/Sources/DuplicateKeys/SOLineWithDuplicateInventoryFK.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacReferentialIntegrity/DacForeignKeyDeclaration/Sources/DuplicateKeys/SOLineWithDuplicateInventoryFK.cs
@@ -56,4 +56,38 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.DacReferentialIntegrity.DacForei
 		public virtual int? InventoryID { get; set; }
 		#endregion
 	}
+
+	/// <summary>
+	/// A DAC used to provide PK to compile SOLine sources with foreign keys which refer to SOOrder PK
+	/// </summary>
+	[PXHidden]
+	public class SOOrder : IBqlTable
+	{
+		public class PK : PrimaryKeyOf<SOOrder>.By<orderType, orderNbr>
+		{
+			public static SOOrder Find(PXGraph graph, string orderType, string orderNbr) => FindBy(graph, orderType, orderNbr);
+		}
+
+		[PXDBString(IsKey = true, InputMask = "")]
+		[PXDefault]
+		[PXUIField(DisplayName = "Order Type")]
+		public string OrderType { get; set; }
+		public abstract class orderType : IBqlField { }
+
+		[PXDBString(IsKey = true, InputMask = "")]
+		[PXDefault]
+		[PXUIField(DisplayName = "Order Nbr.")]
+		public string OrderNbr { get; set; }
+		public abstract class orderNbr : IBqlField { }
+
+		[PXStringList(new[] { "N", "O" }, new[] { "New", "Open" })]
+		[PXDBString]
+		[PXUIField(DisplayName = "Status")]
+		public string Status { get; set; }
+		public abstract class status : IBqlField { }
+
+		[PXDBTimestamp]
+		public virtual byte[] tstamp { get; set; }
+		public abstract class Tstamp : IBqlField { }
+	}
 }
