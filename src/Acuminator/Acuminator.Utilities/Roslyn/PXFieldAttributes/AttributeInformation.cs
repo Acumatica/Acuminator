c4b47d7371b065b9f860ca14fba8df8905d4e6ae
Nikomarov Sergey <snikomarov@acumatica.com>
2020-09-29 13:33:35 +0300

    ATR-535: fixed attribute's bound type calculation to check for IsDBField presense first

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/AttributeInformation.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/AttributeInformation.cs
@@ -178,52 +178,57 @@ namespace Acuminator.Utilities.Roslyn.PXFieldAttributes
 			attribute.ThrowOnNull(nameof(attribute));
 
 			if (!attribute.AttributeClass.InheritsFromOrEquals(_eventSubscriberAttribute) ||
-			    attribute.AttributeClass.InheritsFromOrEquals(_defaultAttribute))
+				attribute.AttributeClass.InheritsFromOrEquals(_defaultAttribute))
 			{
-			    return BoundType.NotDefined;
+				return BoundType.NotDefined;
 			}
-		
+
+			//First check attribute for IsDBField property, it takes highest priority in attribute's boundability and can appear in all kinds of attributes like Account/Sub attributes
+			bool containsIsDbFieldproperty = attribute.AttributeClass.GetBaseTypesAndThis()
+																	 .TakeWhile(attributeType => attributeType != _eventSubscriberAttribute)
+																	 .SelectMany(attributeType => attributeType.GetMembers())
+																	 .OfType<IPropertySymbol>()                                                                     //only properties are considered
+																	 .Any(property => String.Equals(property.Name, IsDBField, StringComparison.OrdinalIgnoreCase));
+			if (!containsIsDbFieldproperty)
+				return GetBoundTypeFromStandardBoundTypeAttributes(attribute);
+
+			var isDbPropertyAttributeArgs = attribute.NamedArguments.Where(arg => String.Equals(arg.Key, IsDBField, StringComparison.OrdinalIgnoreCase))
+																	.ToList(capacity: 1);
+			switch (isDbPropertyAttributeArgs.Count)
+			{
+				case 0:     //Case when there is IsDBField property but it isn't set explicitly in attribute's declaration
+					ITypeSymbol typeFromRegister = TypesContainingIsDBField.Keys.FirstOrDefault(t => IsAttributeDerivedFromClass(attribute.AttributeClass, t));
+
+					// Query hard-coded register with attributes which has IsDBField property with some initial assigned value
+					bool? dbFieldPreInitializedValue = typeFromRegister != null
+						? TypesContainingIsDBField[typeFromRegister]
+						: (bool?)null;
+
+					return dbFieldPreInitializedValue == null
+						? GetBoundTypeFromStandardBoundTypeAttributes(attribute)
+						: dbFieldPreInitializedValue.Value 
+							? BoundType.DbBound 
+							: BoundType.Unbound;
+						
+				case 1 when isDbPropertyAttributeArgs[0].Value.Value is bool isDbPropertyAttributeArgument:     //Case when IsDBField property is set explicitly with correct bool value
+					return isDbPropertyAttributeArgument
+						? BoundType.DbBound
+						: BoundType.Unbound;
+
+				case 1:                        //Strange rare case when IsDBField property is set explicitly with value of type other than bool. In this case we don't know if attribute is bound
+				default:                       //Strange case when there are multiple different "IsDBField" properties set in attribute constructor (with different letters register case)
+					return BoundType.Unknown;
+			}
+		}
+
+		private BoundType GetBoundTypeFromStandardBoundTypeAttributes(AttributeData attribute)
+		{
 			if (BoundBaseTypes.Any(boundBaseType => IsAttributeDerivedFromClassInternal(attribute.AttributeClass, boundBaseType)))
 			{
 				if (_pxDBLocalizableStringAttribute != null && IsAttributeDerivedFromClassInternal(attribute.AttributeClass, _pxDBLocalizableStringAttribute))
 					return GetBoundTypeFromLocalizableStringDerivedAttribute(attribute);
 
 				return BoundType.DbBound;
-			}
-
-			bool containsIsDbFieldproperty = attribute.AttributeClass.GetBaseTypesAndThis()
-																	 .TakeWhile(attributeType => attributeType != _eventSubscriberAttribute)
-																	 .SelectMany(attributeType => attributeType.GetMembers())
-																	 .OfType<IPropertySymbol>()																		//only properties are considered
-																	 .Any(property => String.Equals(property.Name, IsDBField, StringComparison.OrdinalIgnoreCase));	
-			if (containsIsDbFieldproperty)
-			{
-				var isDbPropertyAttributeArgs = attribute.NamedArguments.Where(arg => String.Equals(arg.Key, IsDBField, StringComparison.OrdinalIgnoreCase))
-																		.ToList(capacity: 1);
-				switch (isDbPropertyAttributeArgs.Count)
-				{
-					case 0:		//Case when there is IsDBField property but it isn't set explicitly in attribute's declaration
-						ITypeSymbol typeFromRegister = TypesContainingIsDBField.Keys.FirstOrDefault(t => IsAttributeDerivedFromClass(attribute.AttributeClass, t));
-
-						// Query hard-coded register with attributes which has IsDBField property with some initial assigned value
-						bool? isDbFieldFromBase = typeFromRegister != null
-							? TypesContainingIsDBField[typeFromRegister]
-							: (bool?)null;
-
-						bool isDbBoundAttribute = isDbFieldFromBase ?? false;
-						return isDbBoundAttribute
-							? BoundType.DbBound
-							: BoundType.Unbound;
-
-					case 1 when isDbPropertyAttributeArgs[0].Value.Value is bool isDbPropertyAttributeArgument:     //Case when IsDBField property is set explicitly with correct bool value
-						return isDbPropertyAttributeArgument
-							? BoundType.DbBound
-							: BoundType.Unbound;
-
-					case 1:                        //Strange rare case when IsDBField property is set explicitly with value of type other than bool. In this case we don't know if attribute is bound
-					default:                       //Strange case when there are multiple different "IsDBField" properties set in attribute constructor (with different letters register case)
-						return BoundType.Unknown;  
-				}
 			}
 
 			return BoundType.Unbound;
