100ba697ae6ac3773f353d80145eadcf0eb15736
Nikomarov Sergey <snikomarov@acumatica.com>
2020-10-05 22:54:57 +0300

    ATR-578: added internal API example to demo

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/Internal API usage/SOShipmentExt.cs
@@ -0,0 +1,19 @@
+﻿using PX.Data;
+using PX.Common;
+using System;
+
+namespace PX.Objects.HackathonDemo.Graph.InternalApiUsage
+{
+    public class SOOrderMaintSync : PXGraph<SOOrderMaintSync>
+    {
+        public int CountSyncReadyFiles(int filesNbr = 0)
+        {
+            if (WebConfig.IsClusterEnabled)
+			{
+
+			}
+
+            return filesNbr;
+        }
+    }
+}
