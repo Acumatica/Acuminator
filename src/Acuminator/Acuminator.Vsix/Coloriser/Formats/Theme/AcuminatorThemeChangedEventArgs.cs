45576b32bd60c9641dc46fcb07fe762f66393edb
Nikomarov Sergey <snikomarov@acumatica.com>
2020-11-28 18:47:37 +0300

    ATR-593: reworked theme update mechanism to use batch update for all format definitions.

---
+++ src/Acuminator/Acuminator.Vsix/Coloriser/Formats/Theme/AcuminatorThemeChangedEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.Coloriser
+{
+    internal class AcuminatorThemeChangedEventArgs : EventArgs
+    {
+        public IVsFontAndColorStorage FontAndColorStorage { get; }
+
+        public IClassificationTypeRegistryService ClassificationTypeRegistry { get; }
+
+        public IClassificationFormatMap FormatMap { get; }
+
+        public AcuminatorThemeChangedEventArgs(IVsFontAndColorStorage fontAndColorStorage, IClassificationTypeRegistryService classificationTypeRegistry,
+                                               IClassificationFormatMap formatMap)
+		{
+            FontAndColorStorage = fontAndColorStorage.CheckIfNull(nameof(fontAndColorStorage));
+            ClassificationTypeRegistry = classificationTypeRegistry.CheckIfNull(nameof(classificationTypeRegistry));
+            FormatMap = formatMap;
+        }
+    }
+}
