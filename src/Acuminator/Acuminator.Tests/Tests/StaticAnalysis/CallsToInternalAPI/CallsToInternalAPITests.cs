49c0bbe10ad2793c1395602aa36bb3bcce82a51c
Nikomarov Sergey <snikomarov@acumatica.com>
2021-01-05 00:55:33 +0300

    ATR-578: added test drafts for PX1076

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/CallsToInternalAPI/CallsToInternalAPITests.cs
@@ -0,0 +1,36 @@
+﻿using Acuminator.Analyzers;
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.CallsToInternalAPI;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+using Resources = Acuminator.Analyzers.Resources;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.CallsToInternalAPI
+{
+	public class CallsToInternalAPITests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new CallsToInternalAPIAnalyzer(
+					CodeAnalysisSettings.Default
+										.WithStaticAnalysisEnabled()
+										.WithSuppressionMechanismDisabled()
+										.WithIsvSpecificAnalyzersEnabled());
+
+		[Theory]
+		[EmbeddedFileData("WithoutDescription.cs")]
+		public async Task PublicClass_WithoutDescription(string source) =>
+			await VerifyCSharpDiagnosticAsync(
+				source,
+				Descriptors.PX1007_PublicClassXmlComment.CreateFor(9, 15, messageArgs: nameof(Resources.PX1007Class).GetLocalized()),
+				Descriptors.PX1007_PublicClassXmlComment.CreateFor(13, 23, messageArgs: nameof(Resources.PX1007Delegate).GetLocalized()),
+				Descriptors.PX1007_PublicClassXmlComment.CreateFor(15, 16, messageArgs: nameof(Resources.PX1007Struct).GetLocalized()),
+				Descriptors.PX1007_PublicClassXmlComment.CreateFor(17, 19, messageArgs: nameof(Resources.PX1007Interface).GetLocalized()),
+				Descriptors.PX1007_PublicClassXmlComment.CreateFor(19, 14, messageArgs: nameof(Resources.PX1007Enum).GetLocalized()));		
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/CallsToInternalAPI/Sources/InternalAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Objects.CS;
+using PX.Objects.GL;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.CallsToInternalAPI.Sources
+{
+	public class InternalHelper 
+	{
+		
+	}
+}
