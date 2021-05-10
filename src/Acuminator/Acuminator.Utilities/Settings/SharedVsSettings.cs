1c859cf180889c978ca67d82ffab5e8f71234d83
Nikomarov Sergey <snikomarov@acumatica.com>
2021-05-11 02:59:18 +0300

    ATR-601: reworked shared VS setings

---
+++ src/Acuminator/Acuminator.Utilities/Settings/SharedVsSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acuminator.Utilities
+{
+	public static class SharedVsSettings
+	{
+		public static VSVersion VSVersion { get; set; }
+
+		public static bool IsOutOfProcessAnalysis => VSVersion == null; 
+	}
+}
--- src/Acuminator/Acuminator.Utilities/Settings/VSVersion.cs
+++ src/Acuminator/Acuminator.Utilities/Settings/VSVersion.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Acuminator.Utilities.Common;
 
@@ -13,24 +12,28 @@ namespace Acuminator.Utilities
     /// </summary>
     public class VSVersion
 	{
-        private const int VS2005 = 8;
-        private const int VS2008 = 9;
-        private const int VS2010 = 10;
-        private const int VS2012 = 11;
-        private const int VS2013 = 12;
-        private const int VS2015 = 14;
-        private const int VS2017 = 15;
-        private const int VS2019 = 16;
+        public const int UnknownVersion = 0;
+
+        public const int VS2005 = 8;
+        public const int VS2008 = 9;
+        public const int VS2010 = 10;
+        public const int VS2012 = 11;
+        public const int VS2013 = 12;
+        public const int VS2015 = 14;
+        public const int VS2017 = 15;
+        public const int VS2019 = 16;
 
 		public Version FullVersion { get; }
 
-        public bool VS2013OrOlder => FullVersion.Major <= VS2013; 
+        public bool VS2013OrOlder => FullVersion.Major <= VS2013 && FullVersion.Major != UnknownVersion; 
 
-        public bool VS2015OrOlder => FullVersion.Major <= VS2015;
+        public bool VS2015OrOlder => FullVersion.Major <= VS2015 && FullVersion.Major != UnknownVersion;
 
         public bool VS2017OrNewer => FullVersion.Major >= VS2017;
 
         public bool VS2019OrNewer => FullVersion.Major >= VS2019;
+
+        public bool IsUnknownVersion => FullVersion.Major == UnknownVersion;
 
         public bool IsVs2005 => FullVersion.Major == VS2005; 
         
