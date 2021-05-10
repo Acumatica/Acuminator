fad866edc558dae55ca5433e8d6f9794bf8c96cf
Nikomarov Sergey <snikomarov@acumatica.com>
2021-05-10 20:51:37 +0300

    ATR-601: Added storage class for out of process settings

---
+++ src/Acuminator/Acuminator.Utilities/Settings/OutOfProcessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acuminator.Utilities
+{
+	public static class OutOfProcessAnalysisSettings
+	{
+		public static bool isOutOfProcessAnalysis { get; set; } = true;
+	}
+}
