4503b751418eb26166a4d7fc74a7fc42db5f9df3
Nikomarov Sergey <snikomarov@acumatica.com>
2021-05-12 18:58:37 +0300

    ATR-601: work on out of process

---
+++ src/Acuminator/Acuminator.Utilities/Settings/OutOfProcess/Base/ISettingsWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+
+namespace Acuminator.Utilities.Settings.OutOfProcess
+{
+	public interface ISettingsWriter
+	{
+		void WriteSettingsToOtherProcesses(CodeAnalysisSettings codeAnalysisSettings);
+	}
+}
---
+++ src/Acuminator/Acuminator.Vsix/Settings/OutOfProcessSettingsUpdater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Acuminator.Utilities;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.Settings
+{
+	internal class OutOfProcessSettingsUpdater : IDisposable
+	{
+		private const int NotDisposed = 0, Disposed = 1;
+		private int _isDisposed = NotDisposed;
+		
+		private readonly ISettingsEvents _settingsEvents;
+
+		public OutOfProcessSettingsUpdater(ISettingsEvents settingsEvents)
+		{
+			_settingsEvents = settingsEvents.CheckIfNull(nameof(settingsEvents));
+
+			_settingsEvents.CodeAnalysisSettingChanged += SettingsEvents_CodeAnalysisSettingChanged;
+		}
+
+		private void SettingsEvents_CodeAnalysisSettingChanged(object sender, SettingChangedEventArgs e)
+		{
+			var currentSettings = GlobalCodeAnalysisSettings.Instance;
+		}
+
+		public void Dispose()
+		{
+			if (Interlocked.CompareExchange(ref _isDisposed, value: Disposed, comparand: NotDisposed) == NotDisposed)
+			{
+				if (_settingsEvents != null)
+					_settingsEvents.CodeAnalysisSettingChanged -= SettingsEvents_CodeAnalysisSettingChanged;
+			}
+		}
+	}
+}
