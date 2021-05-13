aaaa645f6dcb83f9f31b46134d408f39d4545beb
Nikomarov Sergey <snikomarov@acumatica.com>
2021-05-13 05:28:02 +0300

    ATR-601: added serialization unit tests

---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/CodeAnalysisSettingsSerialization/CodeAnalysisSettingsSerializationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Acuminator.Tests.Helpers;
+using Acuminator.Utilities;
+using Acuminator.Utilities.Settings.OutOfProcess;
+using Acuminator.Utilities.Common;
+using Xunit;
+using FluentAssertions;
+
+
+namespace Acuminator.Tests.Tests.Utilities.CodeAnalysisSettingsSerialization
+{
+
+	public class CodeAnalysisSettingsSerializationTests
+	{
+		[Theory]
+		[InlineData(true, true, true, true, true)]
+		[InlineData(false, false, false, false, false)]
+		[InlineData(false, true, false, false, true)]
+		[InlineData(true, false, false, true, true)]
+		public void CheckCodeAnalysisSettingsSerialization(bool recursiveAnalysisEnabled, bool isvSpecificAnalyzersEnabled, bool staticAnalysisEnabled,
+															 bool suppressionMechanismEnabled, bool px1007DocumentationDiagnosticEnabled)
+		{
+			var expectedSettings = new CodeAnalysisSettings(recursiveAnalysisEnabled, isvSpecificAnalyzersEnabled,
+															staticAnalysisEnabled, suppressionMechanismEnabled, 
+															px1007DocumentationDiagnosticEnabled);
+
+			using var stream = new MemoryStream(capacity: sizeof(bool) * 5 + 20);
+			using var writer = new CodeAnalysisSettingsBinaryWriter(stream);
+
+			writer.WriteCodeAnalysisSettings(expectedSettings);
+
+			stream.Position = 0;
+
+			using var reader = new CodeAnalysisSettingsBinaryReader(stream);
+			var deserializedSettings = reader.ReadCodeAnalysisSettings();
+
+			Assert.Equal(deserializedSettings, expectedSettings);
+		}
+	}
+}
