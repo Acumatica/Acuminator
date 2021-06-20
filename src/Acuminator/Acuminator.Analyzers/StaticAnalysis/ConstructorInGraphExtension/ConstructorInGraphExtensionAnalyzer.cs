34dcc525cab3bb36cd605322576c0ac517a20d0b
Nikomarov Sergey <snikomarov@acumatica.com>
2021-06-20 20:57:03 +0300

    ATR-616: turned ConstructorInGraphExtensionAnalyzer into aggregated graph analyzer

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ConstructorInGraphExtension/ConstructorInGraphExtensionAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ConstructorInGraphExtension/ConstructorInGraphExtensionAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using Acuminator.Utilities.DiagnosticSuppression;
+﻿using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Utilities.DiagnosticSuppression;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Microsoft.CodeAnalysis;
@@ -8,32 +9,25 @@ using System.Linq;
 
 namespace Acuminator.Analyzers.StaticAnalysis.ConstructorInGraphExtension
 {
-	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	public class ConstructorInGraphExtensionAnalyzer : PXDiagnosticAnalyzer
+	public class ConstructorInGraphExtensionAnalyzer : PXGraphAggregatedAnalyzerBase
 	{
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
 			ImmutableArray.Create(Descriptors.PX1040_ConstructorInGraphExtension);
 
-		internal override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
-		{
-			compilationStartContext.RegisterSymbolAction(c => AnalyzeNamedType(c, pxContext), SymbolKind.NamedType);
-		}
+		public override bool ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel graph) =>
+			base.ShouldAnalyze(pxContext, graph) && graph.Type == GraphType.PXGraphExtension &&
+			!graph.Symbol.InstanceConstructors.IsDefaultOrEmpty && !graph.Symbol.IsGraphExtensionBaseType();
 
-		private void AnalyzeNamedType(SymbolAnalysisContext context, PXContext pxContext)
+		public override void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphSemanticModel pxGraph)
 		{
 			context.CancellationToken.ThrowIfCancellationRequested();
-
-			var typeSymbol = (INamedTypeSymbol) context.Symbol;
-			
-			if (typeSymbol != null && typeSymbol.InheritsFrom(pxContext.PXGraphExtensionType)
-				&& !typeSymbol.IsGraphExtensionBaseType())
+			var constructorLocations = pxGraph.Symbol.InstanceConstructors.Where(constructor => !constructor.IsImplicitlyDeclared)
+																		  .SelectMany(constructor => constructor.Locations);
+			foreach (Location location in constructorLocations)
 			{
-				foreach (var constructor in typeSymbol.InstanceConstructors
-					.Where(c => !c.IsImplicitlyDeclared))
-				{
-					context.ReportDiagnosticWithSuppressionCheck(Diagnostic.Create(
-						Descriptors.PX1040_ConstructorInGraphExtension, constructor.Locations.First()), pxContext.CodeAnalysisSettings);
-				}
+				context.ReportDiagnosticWithSuppressionCheck(
+					Diagnostic.Create(Descriptors.PX1040_ConstructorInGraphExtension, location), 
+					pxContext.CodeAnalysisSettings);
 			}
 		}
 	}
