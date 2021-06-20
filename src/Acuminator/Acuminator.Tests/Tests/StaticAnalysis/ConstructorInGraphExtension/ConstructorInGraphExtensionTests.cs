b48c952c94dd54d9404c9b3bfb0a68af7d02845d
Nikomarov Sergey <snikomarov@acumatica.com>
2021-06-20 21:12:15 +0300

    ATR-616: reworked tests for ConstructorInGraphExtensionAnalyzer to integrate analyzer change to an aggregated graph analyzer

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ConstructorInGraphExtension/ConstructorInGraphExtensionTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ConstructorInGraphExtension/ConstructorInGraphExtensionTests.cs
@@ -1,8 +1,12 @@
-﻿using Acuminator.Analyzers.StaticAnalysis;
+﻿using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.ConstructorInGraphExtension;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
-using Microsoft.CodeAnalysis;
+using Acuminator.Utilities;
+
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Xunit;
@@ -11,59 +15,44 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.ConstructorInGraphExtension
 {
 	public class ConstructorInGraphExtensionTests : CodeFixVerifier
 	{
-		protected override CodeFixProvider GetCSharpCodeFixProvider()
-		{
-			return new ConstructorInGraphExtensionCodeFix();
-		}
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new ConstructorInGraphExtensionCodeFix();
 
-		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-		{
-			return new ConstructorInGraphExtensionAnalyzer();
-		}
-
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new PXGraphAnalyzer(CodeAnalysisSettings.Default
+													.WithStaticAnalysisEnabled()
+													.WithSuppressionMechanismDisabled(),
+								new ConstructorInGraphExtensionAnalyzer());
 
 		[Theory]
 		[EmbeddedFileData("ConstructorInGraphExtension.cs")]
-		public void TestDiagnostic(string actual)
-		{
-			VerifyCSharpDiagnostic(actual, Descriptors.PX1040_ConstructorInGraphExtension.CreateFor(18, 10));
-		}
+		public Task TestDiagnostic(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual, Descriptors.PX1040_ConstructorInGraphExtension.CreateFor(18, 10));
 
 		[Theory]
 		[EmbeddedFileData("ConstructorInGraphExtensionWithInitialize.cs")]
-		public void TestDiagnostic_WithInitialize(string actual)
-		{
-			VerifyCSharpDiagnostic(actual, Descriptors.PX1040_ConstructorInGraphExtension.CreateFor(18, 10));
-		}
+		public Task TestDiagnostic_WithInitialize(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual, Descriptors.PX1040_ConstructorInGraphExtension.CreateFor(18, 10));
 
 		[Theory]
 		[EmbeddedFileData("ConstructorInGraphExtension_Expected.cs")]
-		public void TestDiagnostic_ShouldNotShowDiagnostic(string actual)
-		{
-			VerifyCSharpDiagnostic(actual);
-		}
+		public Task TestDiagnostic_ShouldNotShowDiagnostic(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
 		[EmbeddedFileData("ConstructorInGraphExtensionWithInitialize_Expected.cs")]
-		public void TestDiagnostic_WithInitialize_ShouldNotShowDiagnostic(string actual)
-		{
-			VerifyCSharpDiagnostic(actual);
-		}
+		public Task TestDiagnostic_WithInitialize_ShouldNotShowDiagnostic(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
 		[EmbeddedFileData("ConstructorInGraphExtension.cs",
 			"ConstructorInGraphExtension_Expected.cs")]
-		public void TestCodeFix(string actual, string expected)
-		{
-			VerifyCSharpFix(actual, expected);
-		}
+		public Task TestCodeFix(string actual, string expected) => 
+			VerifyCSharpFixAsync(actual, expected);
 
 		[Theory]
 		[EmbeddedFileData("ConstructorInGraphExtensionWithInitialize.cs",
 			"ConstructorInGraphExtensionWithInitialize_Expected.cs")]
-		public void TestCodeFix_WithInitialize(string actual, string expected)
-		{
-			VerifyCSharpFix(actual, expected);
-		}
+		public Task TestCodeFix_WithInitialize(string actual, string expected) => 
+			VerifyCSharpFixAsync(actual, expected);
 	}
 }
