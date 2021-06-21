7813c65a8df812ba8ddb72fb43bb21c394aed14b
Nikomarov Sergey <snikomarov@acumatica.com>
2021-06-21 07:54:55 +0300

    ATR-619: refactored out logic to determine if compilation represents a test assembly

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXDiagnosticAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXDiagnosticAnalyzer.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
 
 using Acuminator.Analyzers.Settings.OutOfProcess;
 using Acuminator.Analyzers.Utils;
 using Acuminator.Utilities;
-using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn.Semantic;
 
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 
@@ -20,8 +14,6 @@ namespace Acuminator.Analyzers.StaticAnalysis
 {
 	public abstract class PXDiagnosticAnalyzer : DiagnosticAnalyzer
 	{
-		private const string AcuminatorTestsName = "Acuminator.Tests";
-
 		private bool _settingsProvidedExternally;
 
 		protected CodeAnalysisSettings CodeAnalysisSettings 
@@ -29,14 +21,6 @@ namespace Acuminator.Analyzers.StaticAnalysis
 			get;
 			private set;
 		}
-
-		protected static ImmutableArray<string> UnitTestAssemblyMarkers { get; } = 
-			new[] 
-			{
-				"TEST",
-				"BENCHMARK"
-			}
-			.ToImmutableArray();
 
 		/// <summary>
 		/// Constructor.
@@ -74,32 +58,7 @@ namespace Acuminator.Analyzers.StaticAnalysis
 
 		protected virtual bool ShouldAnalyze(PXContext pxContext) =>  pxContext.IsPlatformReferenced && 
 																	  pxContext.CodeAnalysisSettings.StaticAnalysisEnabled &&
-																	  !IsUnitTestAssembly(pxContext.Compilation);
-
-		/// <summary>
-		/// Check that compillation is a unit test assembly. The check is implemented by searching for <c>Test</c> word in the assembly name. 
-		/// It is a common pattern which on one hand is used almost everywhere and on the other hand allows us to distance from the concrete unit test frameworks
-		/// and support not only xUnit but also others like NUnit.
-		/// </summary>
-		/// <param name="compilation">The compilation.</param>
-		/// <returns/>
-		protected virtual bool IsUnitTestAssembly(Compilation compilation)
-		{
-			string assemblyName = compilation?.AssemblyName;
-
-			if (assemblyName.IsNullOrEmpty())
-				return false;
-
-			string assemblyNameUpperCase = assemblyName.ToUpperInvariant();
-
-			for (int i = 0; i < UnitTestAssemblyMarkers.Length; i++)
-			{
-				if (assemblyNameUpperCase.Contains(UnitTestAssemblyMarkers[i]))
-					return assemblyName != AcuminatorTestsName;
-			}
-
-			return false;
-		}
+																	  !pxContext.Compilation.IsUnitTestAssembly();
 
 		internal abstract void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext);
 	}
---
+++ src/Acuminator/Acuminator.Analyzers/Utils/Utils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Analyzers.Utils
+{
+	internal static class Utils
+	{
+		private const string AcuminatorTestsName = "Acuminator.Tests";
+
+		private static ImmutableArray<string> UnitTestAssemblyMarkers { get; } =
+			new[]
+			{
+				"TEST",
+				"BENCHMARK"
+			}
+			.ToImmutableArray();
+
+		/// <summary>
+		/// Check that compillation is a unit test assembly. The check is implemented by searching for <c>Test</c> word in the assembly name. 
+		/// It is a common pattern which on one hand is used almost everywhere and on the other hand allows us to distance from the concrete unit test frameworks
+		/// and support not only xUnit but also others like NUnit.
+		/// </summary>
+		/// <param name="compilation">The compilation.</param>
+		/// <returns/>
+		public static bool IsUnitTestAssembly(this Compilation compilation)
+		{
+			string assemblyName = compilation?.AssemblyName;
+
+			if (assemblyName.IsNullOrEmpty())
+				return false;
+
+			string assemblyNameUpperCase = assemblyName.ToUpperInvariant();
+
+			for (int i = 0; i < UnitTestAssemblyMarkers.Length; i++)
+			{
+				if (assemblyNameUpperCase.Contains(UnitTestAssemblyMarkers[i]))
+					return assemblyName != AcuminatorTestsName;
+			}
+
+			return false;
+		}
+	}
+}
