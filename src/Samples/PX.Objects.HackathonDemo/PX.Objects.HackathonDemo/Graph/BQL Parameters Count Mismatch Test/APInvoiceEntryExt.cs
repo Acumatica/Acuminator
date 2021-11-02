6115017b5858917cfc4c7c0cd45540fe23e2a4a0
Nikomarov Sergey <snikomarov@acumatica.com>
2021-11-03 00:21:36 +0300

    ATR-634: added example from github issue https://github.com/Acumatica/Acuminator/issues/409

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/BQL Parameters Count Mismatch Test/APInvoiceEntryExt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PX.Data;
+using PX.Objects.AP;
+
+namespace PX.Objects.HackathonDemo.BqlParamsMismatch
+{
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class APInvoiceEntryExt : PXGraphExtension<APInvoiceEntry>
+	{
+		protected virtual void __(Events.RowSelecting<APInvoice> e)
+		{
+			if (e.Row != null)
+			{
+				PXSelectBase<APTran> cmd = 
+					new PXSelectReadonly<APTran,
+								   Where<APTran.tranType, Equal<Required<APTran.tranType>>,
+									 And<APTran.refNbr, Equal<Required<APTran.refNbr>>,
+									//Accidentally left this And here when WhereAnd was written.
+									And<APTran.lineNbr, Equal<Required<APTran.lineNbr>>>>>>(Base);
+
+
+				if (cmd is PXSelectReadonly<APTran,
+								   Where<APTran.tranType, Equal<Required<APTran.tranType>>,
+									 And<APTran.refNbr, Equal<Required<APTran.refNbr>>,
+									//Accidentally left this And here when WhereAnd was written.
+									And<APTran.lineNbr, Equal<Required<APTran.lineNbr>>>>>> s)
+				{
+
+				}
+
+				
+
+				using (new PXConnectionScope())
+				{
+					bool b = false;
+
+					//Warning doesn't show in either case.
+					if (b)
+					{
+						cmd.WhereAnd<Where<APTran.lineNbr, Equal<Required<APTran.lineNbr>>>>();
+
+						cmd.Select(e.Row, e.Row.DocType);
+					}
+					else if (false)
+						cmd.Select(e.Row.DocType, e.Row.RefNbr);
+				}
+			}
+		}
+	}
+}
