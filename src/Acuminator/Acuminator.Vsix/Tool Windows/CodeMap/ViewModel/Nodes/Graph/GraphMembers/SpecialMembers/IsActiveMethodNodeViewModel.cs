d653d1a9406200dab78e14d56abb5c09599b8523
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-03 03:02:43 +0300

    ATR-432: added IsActiveMethodNodeViewModel code map tree node + added calculation of a tooltip

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphMembers/SpecialMembers/IsActiveMethodNodeViewModel.cs
@@ -0,0 +1,56 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.ProjectSystem;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Acuminator.Utilities.Roslyn.Semantic.SharedInfo;
+using Acuminator.Utilities.Roslyn.Syntax;
+using Acuminator.Vsix.Utilities;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class IsActiveMethodNodeViewModel : GraphMemberNodeViewModel, INodeWithCacheableTooltip
+	{
+		public override Icon NodeIcon => Icon.IsActiveMethod;
+
+		public IsActiveMethodInfo IsActiveMethodInfo => (IsActiveMethodInfo)MemberInfo;
+
+		public IsActiveMethodNodeViewModel(SpecialGraphMembersCategoryNodeViewModel specialGraphMembersCategoryVM,
+										   IsActiveMethodInfo isActiveMethodInfo, bool isExpanded = false) :
+									  base(specialGraphMembersCategoryVM, specialGraphMembersCategoryVM, isActiveMethodInfo, isExpanded)
+		{
+
+		}
+
+		string? INodeWithCacheableTooltip.CalculateTooltip()
+		{
+			IMethodSymbol isActiveMethod = IsActiveMethodInfo.Symbol;
+
+			if (isActiveMethod.Locations.Length != 1 || isActiveMethod.Locations[0].IsInMetadata || Tree.CodeMapViewModel.Workspace == null)
+				return null;
+
+			int tabSize = Tree.CodeMapViewModel.Workspace.GetWorkspaceIndentationSize();
+			var isActiveMethodSyntaxNode = isActiveMethod.GetSyntax(Tree.CodeMapViewModel.CancellationToken ?? default) as MethodDeclarationSyntax;
+
+			if (isActiveMethodSyntaxNode == null) 
+				return null;
+
+			int prependLength = IndentUtils.GetPrependLength(isActiveMethodSyntaxNode.Modifiers);
+			return isActiveMethodSyntaxNode.GetSyntaxNodeStringWithRemovedIndent(tabSize, prependLength);
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
