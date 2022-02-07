3ba402217406b58bef6da273f012409829f10395
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-07 23:59:51 +0300

    ATR-416: Added graph constructor support to tree builder

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Builders/Graph/DefaultCodeMapTreeBuilder_Graph.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Builders/Graph/DefaultCodeMapTreeBuilder_Graph.cs
@@ -64,9 +64,17 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 
 		public override IEnumerable<TreeNodeViewModel> VisitNode(GraphInitializationAndActivationCategoryNodeViewModel graphInitializationAndActivationCategory)
 		{
-			return CreateGraphCategoryChildren<IsActiveMethodInfo>(graphInitializationAndActivationCategory,
-						constructor: isActiveMethodInfo => new IsActiveGraphMethodNodeViewModel(graphInitializationAndActivationCategory, 
-																								isActiveMethodInfo, ExpandCreatedNodes));
+			return CreateGraphCategoryChildren<SymbolItem>(graphInitializationAndActivationCategory, InitializationAndActivationGraphMemberConstructor);
+
+			//----------------------------------Local Function-----------------------------------------------------
+			TreeNodeViewModel InitializationAndActivationGraphMemberConstructor(SymbolItem symbolInfo) => symbolInfo switch
+			{
+				IsActiveMethodInfo isActiveMethodInfo => new IsActiveGraphMethodNodeViewModel(graphInitializationAndActivationCategory,
+																							  isActiveMethodInfo, ExpandCreatedNodes),
+				InstanceConstructorInfoForCodeMap instanceConstructorInfo => new GraphInstanceConstructorNodeViewModel(graphInitializationAndActivationCategory,
+																							  instanceConstructorInfo, ExpandCreatedNodes),
+				_ => null
+			};
 		}
 
 		public override IEnumerable<TreeNodeViewModel> VisitNode(ActionCategoryNodeViewModel actionCategory)
