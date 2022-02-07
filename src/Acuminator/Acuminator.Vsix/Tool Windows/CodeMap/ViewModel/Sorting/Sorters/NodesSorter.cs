ff2b2f6d9ac0559444e8d2b87974102a032af4a5
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-08 00:47:06 +0300

    ATR-416: declared that Initialization and Activation category members are not sortable

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/Sorters/NodesSorter.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Sorting/Sorters/NodesSorter.cs
@@ -91,6 +91,9 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 				var n when n is DacMemberCategoryNodeViewModel ||
 						   n is GraphMemberCategoryNodeViewModel _ => false,
 
+				IsActiveGraphMethodNodeViewModel _ => false,
+				GraphInstanceConstructorNodeViewModel _ => false,
+
 				var n when n is DacGroupingNodeBaseViewModel ||
 						   n is DacFieldGroupingNodeBaseViewModel => sortType == SortType.Alphabet,
 
