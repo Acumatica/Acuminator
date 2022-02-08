d0037ffdd0b1dd11acb8437360e57a9877255a41
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-08 14:52:36 +0300

    ATR-649: added base graph member overrides category node

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphMembersCategories/GraphBaseMemberOverridesCategoryNodeViewModel.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Acuminator.Vsix.Utilities;
+
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class GraphBaseMemberOverridesCategoryNodeViewModel : GraphMemberCategoryNodeViewModel
+	{
+		protected override bool AllowNavigation => true;
+
+		public override Icon NodeIcon => Icon.BaseMemberOverrideGraphCategory;
+
+		public GraphBaseMemberOverridesCategoryNodeViewModel(GraphNodeViewModel graphViewModel, bool isExpanded) : 
+														base(graphViewModel, GraphMemberType.BaseMemberOverride, isExpanded)
+		{		
+		}
+
+		public override IEnumerable<SymbolItem> GetCategoryGraphNodeSymbols() => CodeMapGraphModel.BaseMemberOverrides;
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
