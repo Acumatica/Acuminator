d85b662400de926dd216cbe9c8065dac9849a35f
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-08 14:53:40 +0300

    ATR-649: Added support of persist method override to base memebr override node

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphMembers/GraphBaseMembeOverrideNodeViewModel.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Acuminator.Vsix.Utilities;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class GraphBaseMembeOverrideNodeViewModel : GraphMemberNodeViewModel
+	{
+		public bool IsPersistMethodOverride => BaseMemberOverrideInfo.IsPersistMethodOverride;
+
+		public override Icon NodeIcon => IsPersistMethodOverride
+			? Icon.PersistMethodOverride
+			: Icon.BaseMemberOverrideGraph;
+
+		public BaseMemberOverrideInfo BaseMemberOverrideInfo => (BaseMemberOverrideInfo)MemberInfo;
+
+		public GraphBaseMembeOverrideNodeViewModel(GraphBaseMemberOverridesCategoryNodeViewModel graphBaseMemberOverridesCategoryVM, 
+												   BaseMemberOverrideInfo baseMemberOverrideInfo, bool isExpanded = false) :
+											  base(graphBaseMemberOverridesCategoryVM, graphBaseMemberOverridesCategoryVM, baseMemberOverrideInfo, isExpanded)
+		{		
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
