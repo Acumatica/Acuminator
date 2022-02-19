aeb5ed205063ae5c44f0a52362789a7753b9e897
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-19 21:39:24 +0300

    ATR-148: changed all static readonly fields to constants

--- src/Acuminator/Acuminator.Utilities/Roslyn/Constants/DelegateNames.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Constants/DelegateNames.cs
@@ -4,81 +4,81 @@ namespace Acuminator.Utilities.Roslyn.Constants
 {
 	public static class DelegateNames
 	{
-		public static readonly string IsKey = "IsKey";
-		public static readonly string IsActive = "IsActive";
+		public const string IsKey = "IsKey";
+		public const string IsActive = "IsActive";
 
-		public static readonly string SetVisible = "SetVisible";
-		public static readonly string SetVisibility = "SetVisibility";
-		public static readonly string SetEnabled = "SetEnabled";
-		public static readonly string SetRequired = "SetRequired";
-		public static readonly string SetReadOnly = "SetReadOnly";
-		public static readonly string SetDisplayName = "SetDisplayName";
-		public static readonly string SetNeutralDisplayName = "SetNeutralDisplayName";
+		public const string SetVisible = "SetVisible";
+		public const string SetVisibility = "SetVisibility";
+		public const string SetEnabled = "SetEnabled";
+		public const string SetRequired = "SetRequired";
+		public const string SetReadOnly = "SetReadOnly";
+		public const string SetDisplayName = "SetDisplayName";
+		public const string SetNeutralDisplayName = "SetNeutralDisplayName";
 
-		public static readonly string Initialize = "Initialize";
-		public static readonly string StartOperation = "StartOperation";
+		public const string Initialize = "Initialize";
+		public const string StartOperation = "StartOperation";
 		
-		public static readonly string SetCaption = "SetCaption";
-		public static readonly string SetTooltip = "SetTooltip";
-		public static readonly string Press = "Press";
+		public const string SetCaption = "SetCaption";
+		public const string SetTooltip = "SetTooltip";
+		public const string Press = "Press";
 
-		public static readonly string Insert = "Insert";
-		public static readonly string Update = "Update";
-		public static readonly string Delete = "Delete";
-		public static readonly string RaiseExceptionHandling = "RaiseExceptionHandling";
+		public const string Insert = "Insert";
+		public const string Update = "Update";
+		public const string Delete = "Delete";
+		public const string RaiseExceptionHandling = "RaiseExceptionHandling";
 
-		public static readonly string Select = "Select";
-		public static readonly string ForceDelete = "ForceDelete";
-		public static readonly string Ensure = "Ensure";
+		public const string Select = "Select";
+		public const string ForceDelete = "ForceDelete";
+		public const string Ensure = "Ensure";
 		
-		public static readonly string InstanceCreatedEvents = "PX.Data.PXGraph+InstanceCreatedEvents";
-		public static readonly string InstanceCreatedEventsAddHandler = "AddHandler";
-		public static readonly string InitCacheMapping = "InitCacheMapping";
-		public static readonly string CreateInstance = "CreateInstance";
+		public const string InstanceCreatedEvents = "PX.Data.PXGraph+InstanceCreatedEvents";
+		public const string InstanceCreatedEventsAddHandler = "AddHandler";
+		public const string InitCacheMapping = "InitCacheMapping";
+		public const string CreateInstance = "CreateInstance";
 
-		public static readonly string SetList = "SetList";
+		public const string SetList = "SetList";
 
-		public static readonly string SetParameters = "SetParametersDelegate";
-		public static readonly string SetProcess = "SetProcessDelegate";
+		public const string SetParameters = "SetParametersDelegate";
+		public const string SetProcess = "SetProcessDelegate";
 
-		public static readonly string View = "View";
+		public const string View = "View";
 
-		public static readonly string WhereAnd = "WhereAnd";
-		public static readonly string WhereNew = "WhereNew";
-		public static readonly string WhereOr = "WhereOr";
-		public static readonly string Join = "Join";
+		public const string WhereAnd = "WhereAnd";
+		public const string WhereNew = "WhereNew";
+		public const string WhereOr = "WhereOr";
+		public const string Join = "Join";
 
-		public static readonly string GetItem = "GetItem";
+		public const string GetItem = "GetItem";
 
-		public static readonly string AppendList = "AppendList";
-		public static readonly string SetLocalizable = "SetLocalizable";
+		public const string AppendList = "AppendList";
+		public const string SetLocalizable = "SetLocalizable";
 
-		public static readonly string PXSave = "PX.Data.PXSave`1";
-		public static readonly string PXCancel = "PX.Data.PXCancel`1";
-		public static readonly string PXInsert = "PX.Data.PXInsert`1";
-		public static readonly string PXDelete = "PX.Data.PXDelete`1";
-		public static readonly string PXCopyPasteAction = "PX.Data.PXCopyPasteAction`1";
-		public static readonly string PXFirst = "PX.Data.PXFirst`1";
-		public static readonly string PXPrevious = "PX.Data.PXPrevious`1";
-		public static readonly string PXNext = "PX.Data.PXNext`1";
-		public static readonly string PXLast = "PX.Data.PXLast`1";
-		public static readonly string PXChangeID = "PX.Data.PXChangeID`1";
+		public const string PXSave = "PX.Data.PXSave`1";
+		public const string PXCancel = "PX.Data.PXCancel`1";
+		public const string PXInsert = "PX.Data.PXInsert`1";
+		public const string PXDelete = "PX.Data.PXDelete`1";
+		public const string PXCopyPasteAction = "PX.Data.PXCopyPasteAction`1";
+		public const string PXFirst = "PX.Data.PXFirst`1";
+		public const string PXPrevious = "PX.Data.PXPrevious`1";
+		public const string PXNext = "PX.Data.PXNext`1";
+		public const string PXLast = "PX.Data.PXLast`1";
+		public const string PXChangeID = "PX.Data.PXChangeID`1";
 
-		public static readonly string JoinNew = "JoinNew";
-		public static readonly string StartRow = "StartRow";
+		public const string JoinNew = "JoinNew";
+		public const string StartRow = "StartRow";
 
-		public static readonly string AggregateNew = "AggregateNew";
-		public static readonly string OrderByNew = "OrderByNew";
+		public const string AggregateNew = "AggregateNew";
+		public const string OrderByNew = "OrderByNew";
 
 
-		public static readonly string Compose = "Compose";
-		public static readonly string AddJoinConditions = "AddJoinConditions";
-		public static readonly string AppendJoin = "AppendJoin";
-		public static readonly string NewJoin = "NewJoin";
+		public const string Compose = "Compose";
+		public const string AddJoinConditions = "AddJoinConditions";
+		public const string AppendJoin = "AppendJoin";
+		public const string NewJoin = "NewJoin";
 
-		public static readonly string PrimaryKeyFindMethod = "Find";
-		public static readonly string PrimaryKeyFindByMethod = "FindBy";
+		public const string PrimaryKeyFindMethod = "Find";
+		public const string PrimaryKeyFindByMethod = "FindBy";
 
-		public static readonly string Persist = "Persist";
+		public const string Persist = "Persist";
 	}
 }
