a94043f22121be23ee69607c6da2154ff93f581f
Sergey Nikomarov <snikomarov@acumatica.com>
2022-02-21 22:21:28 +0300

    ATR-148: moved long delegates classification to a separate type and added classification for long run delegates

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationDelegateClosures/LongOperationDelegateClosuresAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationDelegateClosures/LongOperationDelegateClosuresAnalyzer.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Immutable;
+﻿#nullable enable
+
+using System.Collections.Immutable;
 using System.Linq;
 
 using Acuminator.Utilities;
+using Acuminator.Utilities.Common;
 using Acuminator.Utilities.DiagnosticSuppression;
+using Acuminator.Utilities.Roslyn.Constants;
 using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Syntax;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -15,7 +20,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.LongOperationDelegateClosures
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class LongOperationDelegateClosuresAnalyzer : PXDiagnosticAnalyzer
     {
-		private const string SetProcessDelegateMethodName = "SetProcessDelegate";
+		private readonly LongOperationDelegateTypeClassifier _longOperationDelegateTypeClassifier = new LongOperationDelegateTypeClassifier();
 
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
             ImmutableArray.Create(Descriptors.PX1008_LongOperationDelegateClosures);
@@ -23,86 +28,81 @@ namespace Acuminator.Analyzers.StaticAnalysis.LongOperationDelegateClosures
 		public LongOperationDelegateClosuresAnalyzer() : this(null)
 		{ }
 
-		public LongOperationDelegateClosuresAnalyzer(CodeAnalysisSettings codeAnalysisSettings) : base(codeAnalysisSettings)
+		public LongOperationDelegateClosuresAnalyzer(CodeAnalysisSettings? codeAnalysisSettings) : base(codeAnalysisSettings)
 		{ }
 
 		internal override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
 		{
-            compilationStartContext.RegisterSyntaxNodeAction(c => AnalyzeSetProcessDelegateMethod(c, pxContext), SyntaxKind.InvocationExpression);
+            compilationStartContext.RegisterSyntaxNodeAction(c => AnalyzeLongOperationDelegate(c, pxContext), SyntaxKind.InvocationExpression);
 		}
 
-		private static void AnalyzeSetProcessDelegateMethod(SyntaxNodeAnalysisContext syntaxContext, PXContext pxContext)
+		private void AnalyzeLongOperationDelegate(SyntaxNodeAnalysisContext syntaxContext, PXContext pxContext)
 		{
-			var setDelegateInvocation = syntaxContext.Node as InvocationExpressionSyntax;
+			var longOperationSetupMethodInvocationNode = syntaxContext.Node as InvocationExpressionSyntax;
+			var longOperationDelegateType = _longOperationDelegateTypeClassifier.GetLongOperationDelegateType(longOperationSetupMethodInvocationNode, 
+																											  syntaxContext.SemanticModel, pxContext,
+																											  syntaxContext.CancellationToken);
+			switch (longOperationDelegateType)
+			{
+				case LongOperationDelegateType.ProcessingDelegate:
+					AnalyzeSetProcessDelegateMethod(syntaxContext, pxContext, longOperationSetupMethodInvocationNode!);
+					break;
 
-			if (!CheckIfDiagnosticIsValid(setDelegateInvocation, syntaxContext, pxContext))
-				return;
-            
-            DataFlowAnalysis dfa = null;
+				case LongOperationDelegateType.LongRunDelegate:
+					break;
+			}
+		}
 
-            switch (setDelegateInvocation.ArgumentList.Arguments[0].Expression)
-            {
-                case IdentifierNameSyntax ins:
-					ISymbol identifierSymbol = syntaxContext.SemanticModel.GetSymbolInfo(ins, syntaxContext.CancellationToken).Symbol;
+		private static void AnalyzeSetProcessDelegateMethod(SyntaxNodeAnalysisContext syntaxContext, PXContext pxContext, 
+															InvocationExpressionSyntax setDelegateInvocation)
+		{
+			DataFlowAnalysis? dfa = null;
+
+			switch (setDelegateInvocation.ArgumentList.Arguments[0].Expression)
+			{
+				case IdentifierNameSyntax ins:
+					ISymbol identifierSymbol = syntaxContext.SemanticModel?.GetSymbolInfo(ins, syntaxContext.CancellationToken).Symbol;
 
 					if (identifierSymbol != null && !identifierSymbol.IsStatic)
-                    {
-                        syntaxContext.ReportDiagnosticWithSuppressionCheck(
-                            Diagnostic.Create(
-                                Descriptors.PX1008_LongOperationDelegateClosures, setDelegateInvocation.GetLocation()),
+					{
+						syntaxContext.ReportDiagnosticWithSuppressionCheck(
+							Diagnostic.Create(
+								Descriptors.PX1008_LongOperationDelegateClosures, setDelegateInvocation.GetLocation()),
 							pxContext.CodeAnalysisSettings);
-                    }
+					}
 
-                    return;
-				case MemberAccessExpressionSyntax memberAccess 
+					return;
+				case MemberAccessExpressionSyntax memberAccess
 				when memberAccess.Expression is ElementAccessExpressionSyntax arrayIndexAccess:
 					AnalyzeMemberAccessExpressions(arrayIndexAccess.Expression, syntaxContext, pxContext);
 					return;
 				case MemberAccessExpressionSyntax memberAccess:
 					AnalyzeMemberAccessExpressions(memberAccess.Expression, syntaxContext, pxContext);
 					return;
-				case ConditionalAccessExpressionSyntax conditionalAccess 
+				case ConditionalAccessExpressionSyntax conditionalAccess
 				when conditionalAccess.Expression is ElementAccessExpressionSyntax arrayIndexAccess:
 					AnalyzeMemberAccessExpressions(arrayIndexAccess.Expression, syntaxContext, pxContext);
 					return;
 				case ConditionalAccessExpressionSyntax conditionalAccess:
 					AnalyzeMemberAccessExpressions(conditionalAccess.Expression, syntaxContext, pxContext);
 					return;
-                case AnonymousMethodExpressionSyntax anonMethodNode:
-                    dfa = syntaxContext.SemanticModel.AnalyzeDataFlow(anonMethodNode);
-                    break;
-                case LambdaExpressionSyntax lambdaNode:
-                    dfa = syntaxContext.SemanticModel.AnalyzeDataFlow(lambdaNode);
-                    break;
-            }
-
-            if (dfa != null && dfa.DataFlowsIn.OfType<IParameterSymbol>().Any(p => p.IsThis))
-            {
-                syntaxContext.ReportDiagnosticWithSuppressionCheck(
-                    Diagnostic.Create(
-                        Descriptors.PX1008_LongOperationDelegateClosures, setDelegateInvocation.GetLocation()),
-					pxContext.CodeAnalysisSettings);
-            }
-        }
-
-		private static bool CheckIfDiagnosticIsValid(InvocationExpressionSyntax setDelegateInvocation, SyntaxNodeAnalysisContext syntaxContext,
-													 PXContext pxContext)
-		{
-			var setDelegatememberAccessExpr = setDelegateInvocation?.Expression as MemberAccessExpressionSyntax;
-
-			if (setDelegatememberAccessExpr?.Name.ToString() != SetProcessDelegateMethodName ||
-				syntaxContext.CancellationToken.IsCancellationRequested)
-			{
-				return false;
+				case AnonymousMethodExpressionSyntax anonMethodNode:
+					dfa = syntaxContext.SemanticModel.AnalyzeDataFlow(anonMethodNode);
+					break;
+				case LambdaExpressionSyntax lambdaNode:
+					dfa = syntaxContext.SemanticModel.AnalyzeDataFlow(lambdaNode);
+					break;
 			}
 
-			var setDelegateSymbol = syntaxContext.SemanticModel.GetSymbolInfo(setDelegatememberAccessExpr).Symbol as IMethodSymbol;
-
-			if (setDelegateSymbol == null || !setDelegateSymbol.ContainingType.ConstructedFrom.InheritsFromOrEquals(pxContext.PXProcessingBase.Type))
-				return false;
-
-			return true;
+			if (dfa != null && dfa.DataFlowsIn.OfType<IParameterSymbol>().Any(p => p.IsThis))
+			{
+				syntaxContext.ReportDiagnosticWithSuppressionCheck(
+					Diagnostic.Create(
+						Descriptors.PX1008_LongOperationDelegateClosures, setDelegateInvocation.GetLocation()),
+					pxContext.CodeAnalysisSettings);
+			}
 		}
+
 
 		private static void AnalyzeMemberAccessExpressions(ExpressionSyntax expression, SyntaxNodeAnalysisContext syntaxContext, 
 														   PXContext pxContext)
