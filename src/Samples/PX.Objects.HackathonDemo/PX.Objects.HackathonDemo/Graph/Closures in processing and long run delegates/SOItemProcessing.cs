72bb7741da5a6cb03e6e826be1db7fc19773581b
Sergey Nikomarov <snikomarov@acumatica.com>
2022-03-12 00:45:32 +0300

    ATR-148: added example of captured graph in long run delegate closures

--- src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/Closures in processing and long run delegates/SOItemProcessing.cs
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/Graph/Closures in processing and long run delegates/SOItemProcessing.cs
@@ -1,76 +1,137 @@
-﻿using PX.Data;
+﻿using System;
+using PX.Data;
 
+using System.Collections;
 using System.Collections.Generic;
 
-public class SOItemProcessing : PXGraph<SOItemProcessing>
+namespace PX.Objects.HackathonDemo
 {
-	public class SomeDAC : IBqlTable
+	public class SOItemProcessing : PXGraph<SOItemProcessing>
 	{
-	}
+		private static readonly Guid ID = Guid.NewGuid();
 
-	private readonly Processor processor = new Processor();
+		public class SomeDAC : IBqlTable
+		{
+		}
 
-	private Processor ProcessorProperty => processor;
+		private readonly Processor processor = new Processor();
 
-	private static Processor processorStatic = new Processor();
+		private Processor ProcessorProperty => processor;
 
-	public PXProcessing<SomeDAC> Processing;
+		private static Processor processorStatic = new Processor();
 
-	public SOItemProcessing()
-	{
-		object filter = null;
+		public PXProcessing<SomeDAC> Processing;
 
-		Processing.SetProcessDelegate(delegate (SOItemProcessing graph, SomeDAC applicationProjection) {
-			this.Clear();
-		});
+		public PXAction<SomeDAC> SomeAction;
 
-		Processing.SetProcessDelegate(delegate (SOItemProcessing graph, SomeDAC applicationProjection) {
-			List<SomeDAC> list = new List<SomeDAC>();
-			StaticFunc(list);
-		});
+		[PXUIField(DisplayName = "Some Action", MapEnableRights = PXCacheRights.Select, MapViewRights = PXCacheRights.Select)]
+		[PXButton]
+		public virtual IEnumerable someAction(PXAdapter adapter)
+		{
+			object filter = null;
+			object[] args = new object[] { 1, 2 };
+			List<SomeDAC> inputList = new List<SomeDAC>
+			{
+				new SomeDAC()
+			};
 
-		Processing.SetProcessDelegate(MemberFunc);
-		Processing.SetProcessDelegate(StaticFunc);
+			PXLongOperation.StartOperation(ID, arguments => MemberFunc(), args);                            //Should be diagnostic
+			PXLongOperation.StartOperation(ID, arguments => StaticFunc(filter, inputList, false), args);    //No diagnostic
 
-		Processing.SetProcessDelegate(list => MemberFunc(list));
-		Processing.SetProcessDelegate(list => StaticFunc(filter, list, false));
+			PXLongOperation.StartOperation(ID, () => MemberFunc());                 //Should be diagnostic
+			PXLongOperation.StartOperation(ID, delegate                             //Should be diagnostic
+			{
+				Clear();
+			});
 
-		//Test helper static function
-		Processing.SetProcessDelegate(Processor.StaticFunc);      //No diagnostic
+			PXLongOperation.StartOperation(ID, () => StaticFunc());					//No diagnostic
+			PXLongOperation.StartOperation(ID, delegate                             //No diagnostic
+			{
+				StaticFunc();
+			});
 
-		//test fields and properties
-		Processing.SetProcessDelegate(processor.MemberFunc);          //Should be diagnostic
-		Processing.SetProcessDelegate(ProcessorProperty.MemberFunc);  //Should be diagnostic
-		Processing.SetProcessDelegate(processorStatic.MemberFunc);    //No diagnostic
+			PXLongOperation.StartOperation(this, MemberFunc);                       //Should be diagnostic
+			PXLongOperation.StartOperation(this, StaticFunc);						//No diagnostic
 
-		Processing.SetProcessDelegate<SOItemProcessing>((graph, dac) => graph.MainProcessingHandler(dac), graph => graph.FinallyHandler());    //No diagnostic
-		Processing.SetProcessDelegate<SOItemProcessing>((graph, dac) => MainProcessingHandler(dac), graph => FinallyHandler());                //Should be diagnostic
-		Processing.SetProcessDelegate<SOItemProcessing>((graph, dac) => graph.MainProcessingHandler(dac), graph => FinallyHandler());    //Should be diagnostic
-	}
+			//Test helper static function
+			PXLongOperation.StartOperation(this, Processor.StaticFunc);             //No diagnostic
 
-	public static void StaticFunc(object filter, List<SomeDAC> list, bool markOnly)
-	{ }
+			//test fields and properties
+			PXLongOperation.StartOperation(this, processor.MemberFunc);             //Should be diagnostic
+			PXLongOperation.StartOperation(this, ProcessorProperty.MemberFunc);     //Should be diagnostic
+			PXLongOperation.StartOperation(this, processorStatic.MemberFunc);       //No diagnostic
 
-	public static void StaticFunc(List<SomeDAC> list)
-	{ }
+			return adapter.Get();
+		}
 
-	public void MemberFunc(List<SomeDAC> list)
-	{ }
+		public SOItemProcessing()
+		{
+			object filter = null;
 
-	public void MainProcessingHandler(SomeDAC dac)
-	{
-	}
+			Processing.SetProcessDelegate(delegate (SOItemProcessing graph, SomeDAC applicationProjection) {
+				this.Clear();
+			});
 
-	public void FinallyHandler()
-	{
-	}
+			Processing.SetProcessDelegate(delegate (SOItemProcessing graph, SomeDAC applicationProjection) {
+				List<SomeDAC> list = new List<SomeDAC>();
+				StaticFunc(list);
+			});
 
-	private class Processor
-	{
-		public void MemberFunc(List<SomeDAC> list)
+			Processing.SetProcessDelegate(MemberFunc);
+			Processing.SetProcessDelegate(StaticFunc);
+
+			Processing.SetProcessDelegate(list => MemberFunc(list));
+			Processing.SetProcessDelegate(list => StaticFunc(filter, list, false));
+
+			//Test helper static function
+			Processing.SetProcessDelegate(Processor.StaticFunc);      //No diagnostic
+
+			//test fields and properties
+			Processing.SetProcessDelegate(processor.MemberFunc);          //Should be diagnostic
+			Processing.SetProcessDelegate(ProcessorProperty.MemberFunc);  //Should be diagnostic
+			Processing.SetProcessDelegate(processorStatic.MemberFunc);    //No diagnostic
+
+			Processing.SetProcessDelegate<SOItemProcessing>((graph, dac) => graph.MainProcessingHandler(dac), graph => graph.FinallyHandler());    //No diagnostic
+			Processing.SetProcessDelegate<SOItemProcessing>((graph, dac) => MainProcessingHandler(dac), graph => FinallyHandler());                //Should be diagnostic
+			Processing.SetProcessDelegate<SOItemProcessing>((graph, dac) => graph.MainProcessingHandler(dac), graph => FinallyHandler());    //Should be diagnostic
+		}
+
+		public static void StaticFunc(object filter, List<SomeDAC> list, bool markOnly)
 		{ }
 
 		public static void StaticFunc(List<SomeDAC> list)
 		{ }
+
+		public static void StaticFunc()
+		{ }
+
+		public void MemberFunc(List<SomeDAC> list)
+		{ }
+
+		public void MemberFunc()
+		{ }
+
+		public void MainProcessingHandler(SomeDAC dac)
+		{
+		}
+
+		public void FinallyHandler()
+		{
+		}
+
+		private class Processor
+		{
+			public void MemberFunc()
+			{ }
+
+			public void MemberFunc(List<SomeDAC> list)
+			{ }
+
+			public static void StaticFunc(List<SomeDAC> list)
+			{ }
+
+			public static void StaticFunc()
+			{ }
+		}
 	}
 }
