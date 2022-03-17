67e51ab1b0a4a79aacd9bb3776b8c72cd6d3a41f
Sergey Nikomarov <snikomarov@acumatica.com>
2022-03-17 14:14:54 +0300

    ATR-449: implemented analysis for graph events throw analyzer

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ThrowingExceptions/ThrowingExceptionsInEventHandlersAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ThrowingExceptions/ThrowingExceptionsInEventHandlersAnalyzer.cs
@@ -1,78 +1,97 @@
-﻿using Acuminator.Analyzers.StaticAnalysis.EventHandlers;
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Acuminator.Analyzers.StaticAnalysis.EventHandlers;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Utilities;
 using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Acuminator.Utilities.Roslyn.Syntax;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System.Collections.Immutable;
 
 namespace Acuminator.Analyzers.StaticAnalysis.ThrowingExceptions
 {
-	public class ThrowingExceptionsInEventHandlersAnalyzer : EventHandlerAggregatedAnalyzerBase
+	public partial class ThrowingExceptionsInEventHandlersAnalyzer : IEventHandlerAnalyzer, IPXGraphAnalyzer
 	{
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+		public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
 			Descriptors.PX1073_ThrowingExceptionsInRowPersisted,
 			Descriptors.PX1073_ThrowingExceptionsInRowPersisted_NonISV,
 			Descriptors.PX1074_ThrowingSetupNotEnteredExceptionInEventHandlers);
 
-		public override bool ShouldAnalyze(PXContext pxContext, EventType eventType) => eventType != EventType.None;
+		bool IEventHandlerAnalyzer.ShouldAnalyze(PXContext pxContext, EventType eventType) => 
+			eventType != EventType.None;
 
-		public override void Analyze(SymbolAnalysisContext context, PXContext pxContext, EventType eventType)
+		bool IPXGraphAnalyzer.ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel graphOrGraphExtension) =>
+			!graphOrGraphExtension.Symbol.IsStatic;
+
+		/// <summary>
+		/// Analyze events outside graphs and graph extensions.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="pxContext">The Acumatica context.</param>
+		/// <param name="eventType">Type of the event.</param>
+		void IEventHandlerAnalyzer.Analyze(SymbolAnalysisContext context, PXContext pxContext, EventType eventType)
 		{
 			context.CancellationToken.ThrowIfCancellationRequested();
 
 			var methodSymbol = (IMethodSymbol) context.Symbol;
+
+			// For events analyzer filter out all analysis inside graph or graph extension
+			// It will be done by the graph analyzer
+			if (methodSymbol.ContainingType != null && methodSymbol.ContainingType.IsPXGraphOrExtension(pxContext))	
+				return;
+
 			var methodSyntax = methodSymbol.GetSyntax(context.CancellationToken) as CSharpSyntaxNode;
 
 			if (methodSyntax != null)
 			{
-				var walker = new Walker(context, pxContext, eventType);
+				var walker = new ThrowInEventsWalker(context, pxContext, eventType);
 
 				methodSyntax.Accept(walker);
 			}
 		}
 
-		private class Walker : WalkerBase
+		/// <summary>
+		/// Analyzes events in graph or graph extension.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="pxContext">The Acumatica context.</param>
+		/// <param name="graphOrGraphExtension">The graph or graph extension semantic model.</param>
+		void IPXGraphAnalyzer.Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphSemanticModel graphOrGraphExtension)
 		{
-			private readonly EventType _eventType;
+			context.CancellationToken.ThrowIfCancellationRequested();
 
-			public Walker(SymbolAnalysisContext context, PXContext pxContext, EventType eventType)
-				: base(context, pxContext)
+			PXGraphEventSemanticModel graphOrGraphExtensionWithEvents = 
+				PXGraphEventSemanticModel.EnrichGraphModelWithEvents(graphOrGraphExtension, context.CancellationToken);
+
+			foreach (EventType eventType in Enum.GetValues(typeof(EventType)))
 			{
-				_eventType = eventType;
+				if (eventType == EventType.None)
+					continue;
+
+				AnalyzeGraphEventsForEventType(eventType, context, pxContext, graphOrGraphExtensionWithEvents);
 			}
+		}
 
-			public override void VisitThrowStatement(ThrowStatementSyntax node)
+		private void AnalyzeGraphEventsForEventType(EventType eventType, SymbolAnalysisContext context, PXContext pxContext,
+													PXGraphEventSemanticModel graphOrGraphExtensionWithEvents)
+		{
+			var graphEventsOfEventType = graphOrGraphExtensionWithEvents.GetEventsByEventType(eventType);
+			ThrowInGraphEventsWalker? walker = null;
+
+			foreach (GraphEventInfoBase graphEvent in graphEventsOfEventType)
 			{
-				ThrowIfCancellationRequested();
+				walker ??= new ThrowInGraphEventsWalker(context, pxContext, eventType, graphOrGraphExtensionWithEvents);
 
-				var isReported = false;
-
-				if (_eventType == EventType.RowPersisted)
-				{
-					ReportDiagnostic(
-						_context.ReportDiagnostic,
-						_pxContext.CodeAnalysisSettings.IsvSpecificAnalyzersEnabled
-							? Descriptors.PX1073_ThrowingExceptionsInRowPersisted
-							: Descriptors.PX1073_ThrowingExceptionsInRowPersisted_NonISV,
-						node);
-					isReported = true;
-				}
-
-				if (_eventType != EventType.RowSelected && IsPXSetupNotEnteredException(node))
-				{
-					ReportDiagnostic(_context.ReportDiagnostic,
-						Descriptors.PX1074_ThrowingSetupNotEnteredExceptionInEventHandlers,
-						node, _eventType);
-					isReported = true;
-				}
-
-				if (!isReported)
-				{
-					base.VisitThrowStatement(node);
-				}
+				graphEvent.Node.Accept(walker);
 			}
 		}
 	}
