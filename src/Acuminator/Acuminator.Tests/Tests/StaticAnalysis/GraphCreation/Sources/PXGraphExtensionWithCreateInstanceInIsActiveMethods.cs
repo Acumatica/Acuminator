2e4b614b7e3750282e5d1e80e49b1987bfe4bf52
Sergey Nikomarov <snikomarov@acumatica.com>
2022-04-27 18:26:03 +0300

    ATR-588: edited test source for graph creation in IsActive methods

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/GraphCreation/Sources/PXGraphExtensionWithCreateInstanceInIsActiveMethods.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/GraphCreation/Sources/PXGraphExtensionWithCreateInstanceInIsActiveMethods.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PX.Data;
+using PX.Data.BQL;
+using PX.Data.BQL.Fluent;
+using PX.Objects.AR;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.PXGraphCreationInGraphInWrongPlaces.Sources
 {
@@ -28,7 +31,7 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.PXGraphCreationInGraphInWrongPla
 		where TGraph : PXGraph
 		{
 			bool isActive = PXGraph.CreateInstance<SWKMapadocConnMaint>().GetType() == typeof(SWKMapadocConnMaint) &&
-							SWKMapadocConnMaint.CheckThatFeatureIsActive1();
+							SWKMapadocConnMaint.IsActiveSwitch;
 			return isActive;
 		}
 	}
@@ -39,14 +42,28 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.PXGraphCreationInGraphInWrongPla
 
 	public class SWKMapadocConnMaint : PXGraph<SWKMapadocConnMaint>
     {
+		public static bool IsActiveSwitch => CheckThatFeatureIsActive1();
+
 		public bool IsActiveFeature() => true; 
 
 		public static bool CheckThatFeatureIsActive1() => PXGraph.CreateInstance<SWKMapadocConnMaint>().IsActiveFeature();
 
 		public static bool CheckThatFeatureIsActive2()
 		{
-			var graph = PXGraph.CreateInstance<SWKMapadocConnMaint>();
-			return graph.IsActiveFeature();
+			return SelectFrom<SWKCustomer>
+					   .Where<SWKCustomer.processingEnabled.IsEqual<True>>
+					   .View.ReadOnly
+					   .Select(new PXGraph())
+					   .Any();
 		}
+	}
+
+	[PXCacheName("SWK Customer")]
+	public class SWKCustomer : Customer
+	{
+		public abstract class processingEnabled : BqlBool.Field<processingEnabled> { }
+
+		[PXDBBool]
+		public bool? ProcessingEnabled { get; set; }
 	}
 }
