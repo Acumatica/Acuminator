2fe68fbfba4b81ac1186b81bf00b63ac7fbef3c8
Sergey Nikomarov <snikomarov@acumatica.com>
2022-06-23 19:50:23 +0300

    ATR-668: added test that package version is parsable

---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/Version/PackageVersionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Vsix;
+using Acuminator.Tests.Verification;
+
+using Xunit;
+
+using FluentAssertions;
+
+namespace Acuminator.Tests.Tests.Utilities.Version
+{
+	public class PackageVersionTests
+	{
+		[Fact]
+		public void VersionIsParsable()
+		{
+			string packageVersion = AcuminatorVSPackage.PackageVersion;
+			bool isParsable = System.Version.TryParse(packageVersion, out var version);
+			
+			isParsable.Should().BeTrue();
+			version.ToString().Should().Be(packageVersion);
+		}
+	}
+}
