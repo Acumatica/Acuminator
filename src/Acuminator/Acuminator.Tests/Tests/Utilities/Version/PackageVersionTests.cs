b8ec717379a98617826a8c02b91ff76833b35fe8
Sergey Nikomarov <snikomarov@acumatica.com>
2022-06-24 01:51:02 +0300

    ATR-668: added test that will check equality of manifest version and package version constant

--- src/Acuminator/Acuminator.Tests/Tests/Utilities/Version/PackageVersionTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/Version/PackageVersionTests.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
-using Acuminator.Vsix;
 using Acuminator.Tests.Verification;
-
-using Xunit;
+using Acuminator.Vsix;
 
 using FluentAssertions;
+
+using Xunit;
 
 namespace Acuminator.Tests.Tests.Utilities.Version
 {
@@ -22,5 +27,26 @@ namespace Acuminator.Tests.Tests.Utilities.Version
 			isParsable.Should().BeTrue();
 			version.ToString().Should().Be(packageVersion);
 		}
+
+		[Fact]
+		public void Check_ManifestVersion_Equals_PackageVersion()
+		{
+			string thisTestFilePath = GetFilePath();
+			DirectoryInfo thisTestFolder = new DirectoryInfo(Path.GetDirectoryName(thisTestFilePath));
+			string acuminatorFolder = thisTestFolder.Parent.Parent.Parent.Parent.FullName;
+			string manifestFilePath = Path.Combine(acuminatorFolder, "Acuminator.Vsix", "source.extension.vsixmanifest");
+			
+			var manifest = XDocument.Load(manifestFilePath);
+			var identityNode = manifest?.Root?.DescendantNodes()
+											  .OfType<XElement>()
+											  .FirstOrDefault(node => node.Name.LocalName == "Identity");
+			string? manifestVersion = identityNode?.Attribute("Version")?.Value;
+
+			identityNode.Should().NotBeNull();
+			manifestVersion.Should().NotBeNullOrWhiteSpace();
+			manifestVersion.Should().Be(AcuminatorVSPackage.PackageVersion);
+		}
+
+		private string GetFilePath([CallerFilePath] string? filePath = null) => filePath!;
 	}
 }
