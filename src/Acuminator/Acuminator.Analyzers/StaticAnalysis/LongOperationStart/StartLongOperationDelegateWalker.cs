6d14fe8a4cb2ec033e80a25d634fb75b8139d9c6
Sergey Nikomarov <snikomarov@acumatica.com>
2022-07-10 18:45:46 +0300

    ATR-635: refactoring - refactored start long operation walker

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationStart/StartLongOperationDelegateWalker.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationStart/StartLongOperationDelegateWalker.cs
@@ -1,78 +1,80 @@
-﻿using Acuminator.Utilities.Common;
-using Acuminator.Utilities.Roslyn;
-using Acuminator.Utilities.Roslyn.Semantic;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 namespace Acuminator.Analyzers.StaticAnalysis.LongOperationStart
 {
-    public class StartLongOperationDelegateWalker : NestedInvocationWalker
-    {
-        private readonly HashSet<SyntaxNode> _delegates = new HashSet<SyntaxNode>();
-        private readonly PXContext _pxContext;
+	public class StartLongOperationDelegateWalker : NestedInvocationWalker
+	{
+		private readonly HashSet<SyntaxNode> _delegates = new HashSet<SyntaxNode>();
+		private readonly PXContext _pxContext;
 
-        public ImmutableArray<SyntaxNode> Delegates => _delegates.ToImmutableArray();
+		public ImmutableArray<SyntaxNode> Delegates => _delegates.ToImmutableArray();
 
-        public StartLongOperationDelegateWalker(PXContext pxContext, Compilation compilation, CancellationToken cancellation)
-            : base(compilation, cancellation, pxContext.CodeAnalysisSettings)
-        {
-            pxContext.ThrowOnNull(nameof(pxContext));
+		public StartLongOperationDelegateWalker(PXContext pxContext, Compilation compilation, CancellationToken cancellation)
+			: base(compilation, cancellation, pxContext.CodeAnalysisSettings)
+		{
+			_pxContext = pxContext.CheckIfNull(nameof(pxContext));
+		}
 
-            _pxContext = pxContext;
-        }
+		public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+		{
+			ThrowIfCancellationRequested();
 
-        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
-        {
-            ThrowIfCancellationRequested();
+			IMethodSymbol? methodSymbol = GetSymbol<IMethodSymbol>(node);
 
-            IMethodSymbol methodSymbol = GetSymbol<IMethodSymbol>(node);
+			if (methodSymbol == null || !_pxContext.StartOperation.Contains(methodSymbol))
+			{
+				base.VisitInvocationExpression(node);
+			}
 
-            if (_pxContext.StartOperation.Contains(methodSymbol))
-            {
-                var delegateExists = node.ArgumentList?.Arguments.Count > 1;
-                if (!delegateExists)
-                {
-                    return;
-                }
+			var delegateExists = node.ArgumentList?.Arguments.Count > 1;
 
-                var firstArgument = node.ArgumentList.Arguments[1].Expression;
-                if (firstArgument == null)
-                {
-                    return;
-                }
+			if (!delegateExists)
+			{
+				return;
+			}
 
-                var delegateBody = GetDelegateBody(firstArgument);
-                if (delegateBody == null)
-                {
-                    return;
-                }
+			var firstArgument = node.ArgumentList!.Arguments[1].Expression;
+			if (firstArgument == null)
+			{
+				return;
+			}
 
-                _delegates.Add(delegateBody);
-            }
-            else
-            {
-                base.VisitInvocationExpression(node);
-            }
-        }
+			var delegateBody = GetLongRunDelegateBody(firstArgument);
+			if (delegateBody == null)
+			{
+				return;
+			}
 
-        private SyntaxNode GetDelegateBody(ExpressionSyntax expression)
-        {
-            ThrowIfCancellationRequested();
+			_delegates.Add(delegateBody);
+		}
 
-            if (expression is AnonymousFunctionExpressionSyntax anonymousFunction)
-            {
-                return anonymousFunction.Body;
-            }
-            else
-            {
-                var symbol = GetSymbol<ISymbol>(expression);
+		private SyntaxNode? GetLongRunDelegateBody(ExpressionSyntax longRunDelegateExpression)
+		{
+			ThrowIfCancellationRequested();
 
-                return symbol?.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax(CancellationToken);
-            }
-        }
-    }
+			if (longRunDelegateExpression is AnonymousFunctionExpressionSyntax anonymousFunction)
+			{
+				return anonymousFunction.Body;
+			}
+			else
+			{
+				var symbol = GetSymbol<ISymbol>(longRunDelegateExpression);
+
+				return symbol?.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax(CancellationToken);
+			}
+		}
+	}
 }
