2de8c1f6b7ac0660a8a6f7062c86f090ab484208
Sergey Nikomarov <snikomarov@acumatica.com>
2022-07-11 15:01:56 +0300

    ATR-635: fixed comment

--- src/Acuminator/Acuminator.Utilities/Roslyn/Walkers/DelegatesWalkerBase.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Walkers/DelegatesWalkerBase.cs
@@ -69,7 +69,7 @@ namespace Acuminator.Utilities.Roslyn.Walkers
 														 ?.GetSyntax(CancellationToken);
 
 						// Method is the most simple and frequent case for identifiers passed as expressions for delegates.
-						// It is very difficult to analyze local variables, properties and fields in a general case and they are rarely used
+						// It is very difficult to analyze local variables, properties, fields and general expressions and they are rarely used
 						// for identifiers passed as expressions for delegates. 
 						// Therefore, they are deemed as non recognized.
 						return delegateNode != null && delegateSymbol?.Kind == SymbolKind.Method
