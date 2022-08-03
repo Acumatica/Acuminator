2eb5b43cbe025c5e9244e477645961be8d275cc6
Sergey Nikomarov <snikomarov@acumatica.com>
2022-08-04 02:32:48 +0300

    ATR-670: added helper to check method symbol for a declared attribute

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/IMethodSymbolExtensions.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/IMethodSymbolExtensions.cs
@@ -19,9 +19,64 @@ namespace Acuminator.Utilities.Roslyn.Semantic
 			return !methodSymbol.IsStatic && methodSymbol.MethodKind == MethodKind.Constructor;
 		}
 
+		/// <summary>
+		/// Check if the <paramref name="methodSymbol"/> object has an attribute of a given <paramref name="attributeType"/>.
+		/// </summary>
+		/// <param name="methodSymbol">The method to act on.</param>
+		/// <param name="attributeType">Type of the attribute.</param>
+		/// <param name="checkOverrides">True to check method overrides.</param>
+		/// <param name="checkForDerivedAttributes">(Optional) True to check for attributes derived from <paramref name="attributeType"/>.</param>
+		/// <returns>
+		/// True if method has attribute of <paramref name="attributeType"/>, false if not.
+		/// </returns>
+		public static bool HasAttribute(this IMethodSymbol methodSymbol, INamedTypeSymbol attributeType, bool checkOverrides, 
+										bool checkForDerivedAttributes = true)
+		{
+			methodSymbol.ThrowOnNull(nameof(methodSymbol));
+			attributeType.ThrowOnNull(nameof(attributeType));
+
+			Func<IMethodSymbol, bool> attributeCheck = checkForDerivedAttributes
+				? (Func<IMethodSymbol, bool>)HasDerivedAttribute
+				: HasAttribute;
+
+			if (attributeCheck(methodSymbol))
+				return true;
+			
+			if (checkOverrides && methodSymbol.IsOverride)
+			{
+				var overrides = methodSymbol.GetOverrides();
+				return overrides.Any(attributeCheck);
+			}
+
+			return false;
+
+			//-----------------------------------------------------------
+			bool HasAttribute(IMethodSymbol methodToCheck) =>
+				methodToCheck.GetAttributes()
+							 .Any(a => a.AttributeClass.Equals(attributeType));
+
+			bool HasDerivedAttribute(IMethodSymbol methodToCheck) =>
+				methodToCheck.GetAttributes()
+							 .Any(a => a.AttributeClass.InheritsFrom(attributeType));
+		}
+
+		/// <summary>
+		/// Gets the <paramref name="methodSymbol"/> and its overrides.
+		/// </summary>
+		/// <param name="methodSymbol">The method to act on.</param>
+		/// <returns>
+		/// The <paramref name="methodSymbol"/> and its overrides.
+		/// </returns>
 		public static IEnumerable<IMethodSymbol> GetOverridesAndThis(this IMethodSymbol methodSymbol) =>
 			GetOverridesImpl(methodSymbol.CheckIfNull(nameof(methodSymbol)), includeThis: true);
 
+		/// <summary>
+		/// Gets the overrides of <paramref name="methodSymbol"/>.
+		/// </summary>
+		/// <param name="methodSymbol">The method to act on.</param>
+		/// <returns>
+		/// The overrides of <paramref name="methodSymbol"/>.
+		/// </returns>
 		public static IEnumerable<IMethodSymbol> GetOverrides(this IMethodSymbol methodSymbol) =>
 			GetOverridesImpl(methodSymbol.CheckIfNull(nameof(methodSymbol)), includeThis: false);
 
