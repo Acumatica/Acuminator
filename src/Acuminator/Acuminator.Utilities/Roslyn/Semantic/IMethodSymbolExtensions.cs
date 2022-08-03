b62318cec1df08f0166f62f6af30fdfb32a96451
Sergey Nikomarov <snikomarov@acumatica.com>
2022-08-03 22:21:00 +0300

    ATR-670: added utils to get method overrides

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/IMethodSymbolExtensions.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/IMethodSymbolExtensions.cs
@@ -1,5 +1,12 @@
-﻿using Acuminator.Utilities.Common;
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.CodeAnalysis;
+
+using Acuminator.Utilities.Common;
 
 namespace Acuminator.Utilities.Roslyn.Semantic
 {
@@ -10,6 +17,33 @@ namespace Acuminator.Utilities.Roslyn.Semantic
 			methodSymbol.ThrowOnNull(nameof (methodSymbol));
 
 			return !methodSymbol.IsStatic && methodSymbol.MethodKind == MethodKind.Constructor;
+		}
+
+		public static IEnumerable<IMethodSymbol> GetOverridesAndThis(this IMethodSymbol methodSymbol) =>
+			GetOverridesImpl(methodSymbol.CheckIfNull(nameof(methodSymbol)), includeThis: true);
+
+		public static IEnumerable<IMethodSymbol> GetOverrides(this IMethodSymbol methodSymbol) =>
+			GetOverridesImpl(methodSymbol.CheckIfNull(nameof(methodSymbol)), includeThis: false);
+
+		private static IEnumerable<IMethodSymbol> GetOverridesImpl(IMethodSymbol methodSymbol, bool includeThis)
+		{
+			if (!methodSymbol.IsOverride)
+			{
+				if (includeThis)
+					yield return methodSymbol;
+
+				yield break;
+			}
+			else
+			{
+				IMethodSymbol current = includeThis ? methodSymbol : methodSymbol.OverriddenMethod;
+
+				while (current?.IsOverride == true)
+				{
+					yield return current;
+					current = current.OverriddenMethod;
+				}
+			}
 		}
 	}
 }
