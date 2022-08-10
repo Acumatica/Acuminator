8a5198026101fd209681b1cd394d6e3d5e2485cb
Sergey Nikomarov <snikomarov@acumatica.com>
2022-08-11 01:19:33 +0300

    ATR-670: added test for complex mapping of arguments to parameters in method calls

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LongOperationDelegateClosures/LongOperationDelegateClosuresTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LongOperationDelegateClosures/LongOperationDelegateClosuresTests.cs
@@ -40,17 +40,59 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.LongOperationDelegateClosures
 		}
 
 		[Theory]
-		[EmbeddedFileData("LongRunDelegateClosures.cs")]
-		public Task LongRunDelegates_GraphCapturedInClosures(string actual)
+		[EmbeddedFileData("LongRunDelegateClosures_NormalCases.cs")]
+		public Task LongRunDelegates_GraphAndAdapterCaptured_NormalCases(string actual)
+		{
+			string[] formatArgsGraph = new[] { AnalyzerResources.PX1008Title_CapturedGraphFormatArg, AnalyzerResources.PX1008Title_LongRunDelegateFormatArg };
+			string[] formatArgsAdapter = new[] { AnalyzerResources.PX1008Title_CapturedPXAdapterFormatArg, AnalyzerResources.PX1008Title_LongRunDelegateFormatArg };
+			return VerifyCSharpDiagnosticAsync(actual,
+
+				// graph capture
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 37, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 40, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 41, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 52, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 59, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 60, column: 4, formatArgsGraph),
+
+				//recursive analysis graph capture
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 65, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 66, column: 4, formatArgsGraph),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 67, column: 4, formatArgsGraph),
+
+				// adapter capture
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 103, column: 4, formatArgsAdapter),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 106, column: 4, formatArgsAdapter),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 107, column: 4, formatArgsAdapter),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 118, column: 4, formatArgsAdapter),
+
+				//recursive analysis adapter capture
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 121, column: 4, formatArgsAdapter),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 122, column: 4, formatArgsAdapter),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 123, column: 4, formatArgsAdapter));
+		}
+
+		[Theory]
+		[EmbeddedFileData("LongRunDelegateClosures_ComplexMapping.cs")]
+		public Task LongRunDelegates_GraphCaptured_ComplexMappingOfArgumentsToParameters(string actual)
 		{
 			string[] formatArgs = new[] { AnalyzerResources.PX1008Title_CapturedGraphFormatArg, AnalyzerResources.PX1008Title_LongRunDelegateFormatArg };
 			return VerifyCSharpDiagnosticAsync(actual,
+				//Params check
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 27, column: 4, formatArgs),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 34, column: 4, formatArgs),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 35, column: 4, formatArgs),
 				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 36, column: 4, formatArgs),
-				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 39, column: 4, formatArgs),
+
+				// Named parameters check - names in position
 				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 40, column: 4, formatArgs),
-				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 51, column: 4, formatArgs),
-				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 58, column: 4, formatArgs),
-				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 59, column: 4, formatArgs));
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 42, column: 4, formatArgs),
+
+
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 47, column: 4, formatArgs),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 48, column: 4, formatArgs),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 49, column: 4, formatArgs),
+				Descriptors.PX1008_LongOperationDelegateClosures.CreateFor(line: 51, column: 4, formatArgs));
 		}
 	}
 }
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LongOperationDelegateClosures/Sources/LongRunDelegateClosures_ComplexMapping.cs
@@ -0,0 +1,100 @@
+﻿using PX.Data;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Acuminator.Tests.Sources
+{
+	public class ProcessingGraph_ComplexMapping : PXGraph<ProcessingGraph_ComplexMapping>
+	{
+		[PXHidden]
+		public class SomeDAC : IBqlTable
+		{
+		}
+
+		private readonly Processor processor = new Processor();
+
+		public PXAction<SomeDAC> ComplexMappingTest;
+
+		[PXUIField(DisplayName = "Complex Mapping Test", MapEnableRights = PXCacheRights.Select, MapViewRights = PXCacheRights.Select)]
+		[PXButton]
+		public virtual IEnumerable complexMappingTest(PXAdapter adapter)
+		{
+			var localGraph = PXGraph.CreateInstance<ProcessingGraph_ComplexMapping>();
+
+			// Params check
+			processor.TestGraphCaptured_ParamsNotCaptured(true, 1, this, adapter, null);									//Show diagnostic
+
+			processor.TestGraphCaptured_ParamsNotCaptured(true, 1, localGraph, adapter, "arg1", "arg2");					//No diagnostic
+			processor.TestGraphCaptured_ParamsNotCaptured(true, 1, localGraph, adapter, this);								//No diagnostic
+			processor.TestGraphCaptured_ParamsNotCaptured(true, 1, localGraph, adapter, new object[] { "arg", this });		//No diagnostic
+
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, localGraph, adapter, "arg1", "arg2");						//No diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, localGraph, adapter, "arg1", "arg2", this);					//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, localGraph, adapter, this);									//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, localGraph, adapter, new object[] { "arg", this });			//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, localGraph, adapter, new object[] { "arg", localGraph });	//No diagnostic
+
+			// Named parameters check - names in position
+			processor.TestGraphCaptured_ParamsCaptured(someFlag: true, count: 1, graph: this, adapter: adapter, args: null);	//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(someFlag: true, count: 1, graph: localGraph, adapter, "arg1", "arg2");	//No diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(someFlag: true, count: 1, graph: localGraph, adapter, localGraph, this); //Show diagnostic
+
+			// Named parameters check - names out of position
+			processor.TestGraphCaptured_ParamsCaptured(someFlag: true, count: 1, adapter: adapter, graph: localGraph, args: localGraph);	//No diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, adapter: adapter, graph: localGraph, args: localGraph);						//No diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(someFlag: true, count: 1, adapter: adapter, graph: localGraph, args: this);			//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(someFlag: true, count: 1, adapter: adapter, graph: this, args: localGraph);			//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, adapter: adapter, graph: this, args: new[] { localGraph });					//Show diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, adapter: adapter, graph: localGraph, args: new[] { localGraph });			//No diagnostic
+			processor.TestGraphCaptured_ParamsCaptured(true, 1, adapter: adapter, graph: localGraph, args: new[] { localGraph, this });		//Show diagnostic
+
+			return adapter.Get();
+		}
+
+		public void MemberFunc()
+		{
+
+		}
+		
+		private class Processor
+		{
+			public IEnumerable TestGraphCaptured_ParamsCaptured_OptionalArgs(PXAdapter adapter, ProcessingGraph_ComplexMapping graph, int count = 1,
+																			 bool someFlag = true, params object[] args)
+			{
+				return TestGraphCaptured_ParamsCaptured(someFlag, count, graph, adapter, args);
+			}
+
+			public IEnumerable TestGraphCaptured_ParamsNotCaptured(bool someFlag, int count, ProcessingGraph_ComplexMapping graph, 
+																	PXAdapter adapter, params object[] args)
+			{
+				PXLongOperation.StartOperation(graph, args =>
+				{
+					if (args.Length == count)
+						return;
+
+					if (someFlag)
+						graph.MemberFunc();
+				}, args);
+
+				return adapter.Get();
+			}
+
+			public IEnumerable TestGraphCaptured_ParamsCaptured(bool someFlag, int count, ProcessingGraph_ComplexMapping graph,
+																PXAdapter adapter, params object[] args)
+			{
+				PXLongOperation.StartOperation(graph, () =>
+				{
+					if (args.Length == count)
+						return;
+
+					if (someFlag)
+						graph.MemberFunc();
+				});
+
+				return adapter.Get();
+			}
+		}
+	}
+}
