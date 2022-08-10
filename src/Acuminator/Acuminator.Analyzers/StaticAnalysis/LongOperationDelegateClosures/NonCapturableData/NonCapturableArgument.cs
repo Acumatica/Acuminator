abb5b4531c2770f71cbedfaee5551a9c7a20ca5d
Sergey Nikomarov <snikomarov@acumatica.com>
2022-08-10 16:00:27 +0300

    ATR-670: added data structures for the non capturable arguments info

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationDelegateClosures/NonCapturableData/NonCapturableArgument.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Acuminator.Analyzers.StaticAnalysis.LongOperationDelegateClosures
+{
+	/// <summary>
+	/// Information about the non capturable argument 
+	/// </summary>
+	internal class NonCapturableArgument
+	{
+		public int Index { get; }
+
+		public bool UseThisReference { get; }
+
+		public List<string>? UsedParameters { get; }
+
+		[MemberNotNullWhen(returnValue: true, nameof(UsedParameters))]
+		public bool HasNonCapturableParameters => UsedParameters?.Count > 0;
+
+		public bool CapturesNonCapturableElement => UseThisReference || HasNonCapturableParameters;
+
+		public NonCapturableArgument(int argumentIndex, bool useThisReference, List<string>? usedParameters)
+		{
+			if (argumentIndex < 0)
+				throw new ArgumentOutOfRangeException(nameof(argumentIndex));
+
+			Index = argumentIndex;
+			UseThisReference = useThisReference;
+			UsedParameters = usedParameters;
+		}
+	}
+}
