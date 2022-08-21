0a3da993ec35a4b5965878b3594ec5741622b9d5
Sergey Nikomarov <snikomarov@acumatica.com>
2022-08-22 00:48:24 +0300

    ATR-670: added semantic model utils

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SematicModelUtils.cs
@@ -0,0 +1,45 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Utilities.Roslyn.Semantic
+{
+	public static class SematicModelUtils
+	{
+		/// <summary>
+		/// Analyse data flow for a <paramref name="node"/> and return <see cref="DataFlowAnalysis"/> if analysis succeeded.
+		/// </summary>
+		/// <param name="semanticModel">The semanticModel to act on.</param>
+		/// <param name="node">The node to analyse.</param>
+		/// <returns>
+		/// A <see cref="DataFlowAnalysis"/> if the data flow analysis succeeded, <see langword="null"/> if not.
+		/// </returns>
+		public static DataFlowAnalysis? AnalyseDataFlow(this SemanticModel semanticModel, CSharpSyntaxNode node)
+		{
+			semanticModel.ThrowOnNull(nameof(semanticModel));
+			node.ThrowOnNull(nameof(node));
+
+			DataFlowAnalysis? dataFlowAnalysis;
+
+			try
+			{
+				dataFlowAnalysis = semanticModel.AnalyzeDataFlow(node);
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+
+			return dataFlowAnalysis?.Succeeded == true
+				? dataFlowAnalysis
+				: null;
+		}
+	}
+}
