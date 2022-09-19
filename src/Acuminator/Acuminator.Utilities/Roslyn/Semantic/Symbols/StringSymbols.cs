b3425eed43b177dbfeb83454b99d65add3dd12df
Sergey Nikomarov <snikomarov@acumatica.com>
2022-09-19 22:01:32 +0300

    ATR-347: created new symbol set for string and string methods and integrated it into analyzers

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/Symbols/StringSymbols.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Symbols
+{
+	public class StringSymbols : SymbolsSetForTypeBase
+	{
+		public ImmutableArray<ISymbol> StringFormat { get; }
+
+		public ImmutableArray<ISymbol> StringConcat { get; }
+
+		internal StringSymbols(Compilation compilation) :
+						  base(compilation, compilation.GetSpecialType(SpecialType.System_String))
+		{
+			StringFormat = Type?.GetMembers(nameof(string.Format)) ?? ImmutableArray<ISymbol>.Empty;
+			StringConcat = Type?.GetMembers(nameof(string.Concat)) ?? ImmutableArray<ISymbol>.Empty;
+		}
+	}
+}
