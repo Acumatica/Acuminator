de7e7f78a60b5cdff152ff587bccec351603ebfd
Sergey Nikomarov <snikomarov@acumatica.com>
2022-11-01 18:05:10 +0300

    ATR-557: added cancellation into the fix and refactored the logic

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraphCreateInstance/PXGraphCreateInstanceFix.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraphCreateInstance/PXGraphCreateInstanceFix.cs
@@ -2,15 +2,21 @@
 
 using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+
 using Acuminator.Utilities.Roslyn.Constants;
 using Acuminator.Utilities.Roslyn.Semantic;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreateInstance
 {
@@ -22,18 +28,23 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreateInstance
 			private readonly PXContext _pxContext;
 			private readonly Document _document;
 			private readonly SemanticModel _semanticModel;
+			private readonly CancellationToken _cancellation;
 
-			public Rewriter(PXContext pxContext, Document document, SemanticModel semanticModel)
+			public Rewriter(PXContext pxContext, Document document, SemanticModel semanticModel, CancellationToken cancellation)
 			{
 				_pxContext = pxContext;
 				_document = document;
 				_semanticModel = semanticModel;
+				_cancellation = cancellation;
 			}
 
 			public override SyntaxNode VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
 			{
+				_cancellation.ThrowIfCancellationRequested();
+
 				var generator = SyntaxGenerator.GetGenerator(_document);
-				var typeSymbol = _semanticModel.GetSymbolInfo(node.Type).Symbol as ITypeSymbol;
+				var typeSymbol = _semanticModel.GetSymbolInfo(node.Type, _cancellation).Symbol as ITypeSymbol;
+
 				if (typeSymbol != null)
 				{
 					return generator.InvocationExpression(
@@ -51,24 +62,38 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraphCreateInstance
 
 		public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
-		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		public override Task RegisterCodeFixesAsync(CodeFixContext context)
 		{
-			var root = await context.Document.GetSyntaxRootAsync().ConfigureAwait(false);
-			var node = root.FindNode(context.Span);
-			string title = nameof(Resources.PX1001Fix).GetLocalized().ToString();
+			if (!context.Diagnostics.Any(diagnostic => FixableDiagnosticIds.Contains(diagnostic.Id)))
+				return Task.CompletedTask;
 
-			if (node != null)
-			{
-				var semanticModel = await context.Document.GetSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
-				context.RegisterCodeFix(CodeAction.Create(title, c =>
-					{
-						var pxContext = new PXContext(semanticModel.Compilation, codeAnalysisSettings: null);
-						var rewriter = new Rewriter(pxContext, context.Document, semanticModel);
-						var newNode = rewriter.Visit(node);
-						return Task.FromResult(context.Document.WithSyntaxRoot(root.ReplaceNode(node, newNode)));
-					}, title),
-					context.Diagnostics);
-			}
+			string title = nameof(Resources.PX1001Fix).GetLocalized().ToString();
+			var codeAction = CodeAction.Create(title, cancellation => RewriteGraphConstructionAsync(context.Document, context.Span, cancellation),
+											   equivalenceKey: title);
+
+			context.RegisterCodeFix(codeAction, context.Diagnostics);
+			return Task.CompletedTask;
+		}
+
+		private static async Task<Document> RewriteGraphConstructionAsync(Document document, TextSpan span, CancellationToken cancellation)
+		{
+			var root = await document.GetSyntaxRootAsync(cancellation).ConfigureAwait(false);
+			var nodeWithDiagnostic = root?.FindNode(span);
+
+			if (nodeWithDiagnostic == null)
+				return document;
+
+			var semanticModel = await document.GetSemanticModelAsync(cancellation).ConfigureAwait(false);
+
+			if (semanticModel == null)
+				return document;
+
+			var pxContext = new PXContext(semanticModel.Compilation, codeAnalysisSettings: null);
+			var rewriter = new Rewriter(pxContext, document, semanticModel, cancellation);
+			var newNode = rewriter.Visit(nodeWithDiagnostic);
+			var newRoot = root.ReplaceNode(nodeWithDiagnostic, newNode);
+
+			return document.WithSyntaxRoot(newRoot);
 		}
 	}
 }
