c0512ac69e9705644e937160b35bbf123f22108e
Sergey Nikomarov <snikomarov@acumatica.com>
2022-11-03 16:35:46 +0300

    ATR-557: added unit tests for exception serialization

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ExceptionSerialization/ExceptionSerializationTests.cs
@@ -0,0 +1,27 @@
+﻿using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.ExceptionSerialization;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.ExceptionSerialization
+{
+	public class ExceptionSerializationTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new ExceptionSerializationAnalyzer(
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled());
+
+		[Theory]
+		[EmbeddedFileData("PXSignatureRequiredException.cs")]
+		public void Exception_WithNewSerializableData_AndNoSerializationConstructor_AndNoGetObjectDataOverride(string actual) =>
+			VerifyCSharpDiagnostic(actual,
+				Descriptors.PX1063_NoSerializationConstructorInException.CreateFor(10, 22),
+				Descriptors.PX1064_NoGetObjectDataOverrideInExceptionWithNewFields.CreateFor(10, 22));
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ExceptionSerialization/MissingSerializationConstructorFixTests.cs
@@ -0,0 +1,48 @@
+﻿using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.ExceptionSerialization;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.ExceptionSerialization
+{
+	public class MissingSerializationConstructorTests : CodeFixVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new ExceptionSerializationAnalyzer(
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled());
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new MissingSerializationConstructorFix();
+
+		[Theory]
+		[EmbeddedFileData("LegacyBqlFieldGood.cs")]
+		public void TestDiagnostic_Good(string actual) => VerifyCSharpDiagnostic(actual);
+
+		[Theory]
+		[EmbeddedFileData("LegacyBqlFieldBad.cs")]
+		public void TestDiagnostic_Bad(string actual) => VerifyCSharpDiagnostic(actual,
+			Descriptors.PX1060_LegacyBqlField.CreateFor(12, 25, "legacyBoolField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(16, 25, "legacyByteField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(20, 25, "legacyShortField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(24, 25, "legacyIntField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(28, 25, "legacyLongField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(32, 25, "legacyFloatField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(36, 25, "legacyDoubleField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(40, 25, "legacyDecimalField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(44, 25, "legacyStringField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(48, 25, "legacyDateField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(52, 25, "legacyGuidField"),
+			Descriptors.PX1060_LegacyBqlField.CreateFor(56, 25, "legacyBinaryField"));
+
+		[Theory]
+		[EmbeddedFileData("LegacyBqlFieldBad.cs", "LegacyBqlFieldBad_Expected.cs")]
+		public void TestCodeFix(string actual, string expected) => VerifyCSharpFix(actual, expected);
+
+		
+	}
+}
