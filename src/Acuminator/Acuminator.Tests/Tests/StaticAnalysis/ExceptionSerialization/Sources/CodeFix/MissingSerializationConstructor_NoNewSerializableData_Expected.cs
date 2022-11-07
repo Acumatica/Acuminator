fd07a1eb1faf1a5d6cc57a8cffa7a81a8dbc0ae1
Sergey Nikomarov <snikomarov@acumatica.com>
2022-11-07 22:22:16 +0300

    ATR-557: changed test sources to test missing namespace addition by codefixes

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ExceptionSerialization/Sources/CodeFix/MissingSerializationConstructor_NoNewSerializableData_Expected.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ExceptionSerialization/Sources/CodeFix/MissingSerializationConstructor_NoNewSerializableData_Expected.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 
 using PX.Data;
+using System.Runtime.Serialization;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.ExceptionSerialization.Sources
 {
