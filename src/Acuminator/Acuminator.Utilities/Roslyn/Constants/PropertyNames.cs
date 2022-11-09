e76b796a86910081be28d3a5fa12b658d5322e04
Sergey Nikomarov <snikomarov@acumatica.com>
2022-11-09 03:05:59 +0300

    ATR-685: added symbol for PXException.Message

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Constants/PropertyNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acuminator.Utilities.Roslyn.Constants
+{
+	public static class PropertyNames
+	{
+		public static class Exception
+		{
+			public const string Message = nameof(System.Exception.Message);
+		}
+	}
+}
