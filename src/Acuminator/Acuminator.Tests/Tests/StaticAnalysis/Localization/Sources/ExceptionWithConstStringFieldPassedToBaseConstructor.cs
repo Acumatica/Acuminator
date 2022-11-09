a3e43d0183460ab6850b104d747785fc4cb828ab
Sergey Nikomarov <snikomarov@acumatica.com>
2022-11-09 04:11:46 +0100

    ATR-684: added unit tests for the type members passed to the exception base constructor call

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/Localization/Sources/ExceptionWithConstStringFieldPassedToBaseConstructor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using PX.Data;
+using System.Runtime.Serialization;
+using PX.Common;
+
+namespace Acuminator.Tests.Sources
+{
+	public class DetailNonLocalizableBypassedException : PXException
+	{
+		private const string MessageConst = "Non Localizable String, Ups!";
+
+		private static string MessageField = "Non Localizable String, Ups!";
+
+		private static string MessageProperty { get; } = "Non Localizable String, Ups!";
+
+		private static string MessageMethod() => "Non Localizable String, Ups!";
+
+		public object ItemToBypass { get; }
+
+		public DetailNonLocalizableBypassedException(object itemToBypass)
+			: base(MessageConst)
+		{
+			ItemToBypass = itemToBypass;
+		}
+
+		public DetailNonLocalizableBypassedException(Guid itemToBypass)
+			: base(MessageField)
+		{
+			ItemToBypass = itemToBypass;
+		}
+
+		public DetailNonLocalizableBypassedException(string itemToBypass)
+			: base(MessageProperty)
+		{
+			ItemToBypass = itemToBypass;
+		}
+
+		public DetailNonLocalizableBypassedException(int itemToBypass)
+			: base(MessageMethod())
+		{
+			ItemToBypass = itemToBypass;
+		}
+
+		public DetailNonLocalizableBypassedException(PXException exception)
+			: base(exception.Message)												//no alert
+		{
+			ItemToBypass = exception;
+		}
+
+		protected DetailNonLocalizableBypassedException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+			ReflectionSerializer.RestoreObjectProps(this, info);
+		}
+
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			ReflectionSerializer.GetObjectData(this, info);
+			base.GetObjectData(info, context);
+		}
+	}
+}
