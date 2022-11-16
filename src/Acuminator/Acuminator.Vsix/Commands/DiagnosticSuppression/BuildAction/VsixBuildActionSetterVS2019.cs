f90d52240b300c92663c3d74bea120e4e250d2bb
Sergey Nikomarov <snikomarov@acumatica.com>
2022-11-17 02:52:23 +0300

    ATR-690: added nullable analysis

--- src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/BuildAction/VsixBuildActionSetterVS2019.cs
+++ src/Acuminator/Acuminator.Vsix/Commands/DiagnosticSuppression/BuildAction/VsixBuildActionSetterVS2019.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿#nullable enable
+
+using System;
 using System.Threading.Tasks;
-using System.Windows;
 using Acuminator.Utilities.Common;
 using Acuminator.Vsix.Utilities;
 using Microsoft.VisualStudio.Threading;
@@ -47,13 +47,13 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
 #pragma warning disable VSTHRD010 // Invoke single-threaded types on Main thread
-			EnvDTE.DTE dte = await AcuminatorVSPackage.Instance.GetServiceAsync<EnvDTE.DTE>();
+			EnvDTE.DTE? dte = await AcuminatorVSPackage.Instance.GetServiceAsync<EnvDTE.DTE>();
 
 			if (dte == null)
 				return false;
 #pragma warning restore VSTHRD010 // Invoke single-threaded types on Main thread
 
-			EnvDTE.ProjectItem suppressionFileDteItem = dte.Solution.FindProjectItem(roslynSuppressionFilePath);
+			EnvDTE.ProjectItem? suppressionFileDteItem = dte.Solution.FindProjectItem(roslynSuppressionFilePath);
 
 			if (!TrySetBuildActionWithProjectItem(suppressionFileDteItem, buildActionToSet))
 				return false;
@@ -63,14 +63,14 @@ namespace Acuminator.Vsix.DiagnosticSuppression
 		}
 
 		#pragma warning disable VSTHRD010 // Invoke single-threaded types on Main thread
-		private bool TrySetBuildActionWithProjectItem(EnvDTE.ProjectItem suppressionFileDteItem, string buildActionToSet)
+		private bool TrySetBuildActionWithProjectItem(EnvDTE.ProjectItem? suppressionFileDteItem, string buildActionToSet)
 		{
 			if (suppressionFileDteItem == null)
 				return false;
 
 			try
 			{
-				EnvDTE.Property buildActionProperty = suppressionFileDteItem.Properties.Item("ItemType");
+				EnvDTE.Property? buildActionProperty = suppressionFileDteItem.Properties.Item("ItemType");
 
 				if (buildActionProperty == null)
 					return false;
