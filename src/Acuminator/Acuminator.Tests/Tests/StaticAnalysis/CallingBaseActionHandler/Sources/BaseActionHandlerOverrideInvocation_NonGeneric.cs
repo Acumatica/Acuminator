db55a254d322ebf8c1063e0f8528a64c5c0e6c58
Sergey Nikomarov <snikomarov@acumatica.com>
2022-12-28 22:30:10 +0300

    ATR-716: updated test source

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/CallingBaseActionHandler/Sources/BaseActionHandlerOverrideInvocation_NonGeneric.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/CallingBaseActionHandler/Sources/BaseActionHandlerOverrideInvocation_NonGeneric.cs
@@ -20,8 +20,6 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.CallingBaseActionHandler.Sources
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
 	public abstract class PaymentTransactionGraphtExtBase : PXGraphExtension<ARPaymentGraph>
 	{
-		public PXAction<ARPayment> captureOnlyCCPayment;
-
 		[PXUIField(DisplayName = "Record and Capture Preauthorization", MapEnableRights = PXCacheRights.Update, MapViewRights = PXCacheRights.Update)]
 		[PXProcessButton]
 		public virtual IEnumerable CaptureOnlyCCPayment(PXAdapter adapter)
