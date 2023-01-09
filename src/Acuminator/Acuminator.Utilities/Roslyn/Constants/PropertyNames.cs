2d57332ea1cd44e09b1d4d0e4aed972234938eb7
Sergey Nikomarov <snikomarov@acumatica.com>
2023-01-10 02:43:16 +0300

    ATR-705: added pxexceptioninfo symbols

--- src/Acuminator/Acuminator.Utilities/Roslyn/Constants/PropertyNames.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Constants/PropertyNames.cs
@@ -7,6 +7,8 @@ namespace Acuminator.Utilities.Roslyn.Constants
 		public static class Exception
 		{
 			public const string Message = nameof(System.Exception.Message);
+			public const string MessageFormat = "MessageFormat";
+			public const string MessageArguments = "MessageArguments";
 		}
 	}
 }
