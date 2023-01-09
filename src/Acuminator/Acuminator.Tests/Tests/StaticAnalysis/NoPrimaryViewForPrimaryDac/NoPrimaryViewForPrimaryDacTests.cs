d8755dbbb280a48136ce4387bb47d1969272dd85
Sergey Nikomarov <snikomarov@acumatica.com>
2023-01-09 19:54:40 +0300

    ATR-708: added nullable ref types annotation and made tests asynchronous

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoPrimaryViewForPrimaryDac/NoPrimaryViewForPrimaryDacTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoPrimaryViewForPrimaryDac/NoPrimaryViewForPrimaryDacTests.cs
@@ -1,11 +1,18 @@
-﻿using Acuminator.Analyzers.StaticAnalysis;
+﻿#nullable enable
+
+using System;
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.NoPrimaryViewForPrimaryDac;
 using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
 using Acuminator.Utilities;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+
 using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.NoPrimaryViewForPrimaryDac
@@ -20,23 +27,23 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.NoPrimaryViewForPrimaryDac
 			
 		[Theory]
 		[EmbeddedFileData("NoPrimaryViewForPrimaryDac.cs")]
-		public virtual void WithoutPrimaryView(string source) =>
-			VerifyCSharpDiagnostic(source,
+		public virtual Task WithoutPrimaryView(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
 				Descriptors.PX1018_NoPrimaryViewForPrimaryDac.CreateFor(line: 17, column: 56),
 				Descriptors.PX1018_NoPrimaryViewForPrimaryDac.CreateFor(line: 23, column: 15));
 
 		[Theory]
 		[EmbeddedFileData("HasPrimaryViewForPrimaryDac.cs")]
-		public virtual void WithPrimaryView_NoDiagnostics(string source) => VerifyCSharpDiagnostic(source);
+		public virtual Task WithPrimaryView_NoDiagnostics(string source) => VerifyCSharpDiagnosticAsync(source);
 
 		[Theory]
 		[EmbeddedFileData("NoPrimaryViewForPrimaryDacFluentBQL.cs")]
-		public virtual void WithoutPrimaryView_FluentBql(string source) =>
-			VerifyCSharpDiagnostic(source,
+		public virtual Task WithoutPrimaryView_FluentBql(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
 				Descriptors.PX1018_NoPrimaryViewForPrimaryDac.CreateFor(line: 18, column: 68));
 
 		[Theory]
 		[EmbeddedFileData("HasPrimaryViewForPrimaryDacFluentBQL.cs")]
-		public virtual void WithPrimaryView_FluentBql_NoDiagnostics(string source) => VerifyCSharpDiagnostic(source);
+		public virtual Task WithPrimaryView_FluentBql_NoDiagnostics(string source) => VerifyCSharpDiagnosticAsync(source);
 	}
 }
