119e19fb60caa172809bda2a3680067c3962ce0c
Sergey Nikomarov <snikomarov@acumatica.com>
2023-01-11 21:30:31 +0300

    ATR-718: added DTO for mixed boundness type attributes

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/FieldTypeAttributeInfo.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/FieldTypeAttributeInfo.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.CodeAnalysis;
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
 
 namespace Acuminator.Utilities.Roslyn.PXFieldAttributes
 {
 	/// <summary>
-	/// Information about the Acumatica field type attributes.
+	/// Information about the Acumatica field type attribute.
 	/// </summary>
-	public readonly struct FieldTypeAttributeInfo
+	public class FieldTypeAttributeInfo : IEquatable<FieldTypeAttributeInfo>
 	{
-		public ITypeSymbol FieldType { get; }
+		public ITypeSymbol? FieldType { get; }
 
 		public FieldTypeAttributeKind Kind { get; }
 
@@ -15,12 +20,38 @@ namespace Acuminator.Utilities.Roslyn.PXFieldAttributes
 			Kind == FieldTypeAttributeKind.PXDBCalcedAttribute || Kind == FieldTypeAttributeKind.PXDBScalarAttribute;
 
 		public bool IsFieldAttribute =>
-			Kind == FieldTypeAttributeKind.BoundTypeAttribute || Kind == FieldTypeAttributeKind.UnboundTypeAttribute;
+			Kind == FieldTypeAttributeKind.BoundTypeAttribute || 
+			Kind == FieldTypeAttributeKind.UnboundTypeAttribute || 
+			Kind == FieldTypeAttributeKind.MixedDbBoundnessTypeAttribute;
 
-		public FieldTypeAttributeInfo(FieldTypeAttributeKind attributeKind, ITypeSymbol fieldType)
+		public FieldTypeAttributeInfo(FieldTypeAttributeKind attributeKind, ITypeSymbol? fieldType)
 		{
 			FieldType = fieldType;
 			Kind = attributeKind;
+		}
+
+		public override bool Equals(object obj) => Equals(obj as FieldTypeAttributeInfo);
+
+		public virtual bool Equals(FieldTypeAttributeInfo? other)
+		{
+			if (ReferenceEquals(this, other))
+				return true;
+
+			return Kind == other?.Kind && object.Equals(FieldType, other.FieldType) &&
+				   GetType() == other.GetType();
+		}
+
+		public override int GetHashCode()
+		{
+			int hash = 17;
+
+			unchecked
+			{
+				hash = 23 * hash + Kind.GetHashCode();
+				hash = 23 * hash + (FieldType?.GetHashCode() ?? 0);
+			}
+
+			return hash;
 		}
 	}
 }
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/MixedDbBoundnessAttributeInfo.cs
@@ -0,0 +1,54 @@
+﻿#nullable enable
+
+using System;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.PXFieldAttributes
+{
+	/// <summary>
+	/// Information about the Acumatica field type attributes with mixed DB boundness.
+	/// </summary>
+	public class MixedDbBoundnessAttributeInfo : FieldTypeAttributeInfo, IEquatable<MixedDbBoundnessAttributeInfo>
+	{
+		/// <summary>
+		/// The type of the attribute.
+		/// </summary>
+		public INamedTypeSymbol AttributeType { get; }
+
+		/// <summary>
+		/// The value indicating whether the field type attribute is database bound by default. If null then the attribute does not have a default DB boundness.
+		/// </summary>
+		public bool? IsDbBoundByDefault { get; }
+
+		public MixedDbBoundnessAttributeInfo(INamedTypeSymbol attributeType, ITypeSymbol? fieldType, bool? isDbBoundByDefault) : 
+										base(FieldTypeAttributeKind.MixedDbBoundnessTypeAttribute, fieldType)
+		{
+			AttributeType = attributeType.CheckIfNull(nameof(attributeType));
+			IsDbBoundByDefault = isDbBoundByDefault;
+		}
+
+		public static MixedDbBoundnessAttributeInfo? Create(INamedTypeSymbol? attributeType, ITypeSymbol? fieldType, bool? isDbBoundByDefault) =>
+			attributeType != null
+				? new MixedDbBoundnessAttributeInfo(attributeType, fieldType, isDbBoundByDefault)
+				: null;
+
+		public override bool Equals(object obj) => Equals(obj as MixedDbBoundnessAttributeInfo);
+
+		public override bool Equals(FieldTypeAttributeInfo? other) => Equals(other as MixedDbBoundnessAttributeInfo);
+
+		public bool Equals(MixedDbBoundnessAttributeInfo? other) =>
+			base.Equals(other) && IsDbBoundByDefault == other.IsDbBoundByDefault;
+
+		public override int GetHashCode()
+		{
+			int hash = base.GetHashCode();
+			hash = unchecked(23 * hash + IsDbBoundByDefault.GetValueOrDefault().GetHashCode());
+
+			return hash;
+		}
+	}
+}
