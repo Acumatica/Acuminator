ed0aa67ac05fc38989a1e3919ace21b59abe884b
Sergey Nikomarov <snikomarov@acumatica.com>
2023-01-20 19:04:55 +0300

    ATR-718: added PXDBScalar and PXDBCalced boundness types and reworked DbBoundness combination rules

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Enum/DbBoundnessType.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Enum/DbBoundnessType.cs
@@ -27,14 +27,68 @@ namespace Acuminator.Utilities.Roslyn.PXFieldAttributes
 		DbBound = 2,
 
 		/// <summary>
+		/// The attribute application is PXDBScalar and is a formula calced on DB Side.
+		/// </summary>
+		PXDBScalar = 4,
+
+		/// <summary>
+		/// The attribute application is PXDBCalced and is a sub-query calced on DB Side.
+		/// </summary>
+		PXDBCalced = 8,
+
+		/// <summary>
 		/// The attribute application classification is unknown and cannot be deduced by means of static analysis.
 		/// </summary>
-		Unknown = 4
+		Unknown = 16
 	}
 
 
 	public static class DbBoundnessTypeCombiner
 	{
-		public static DbBoundnessType Combine(this DbBoundnessType x, DbBoundnessType y) => x >= y ? x : y;
+		public static DbBoundnessType Combine(this DbBoundnessType x, DbBoundnessType y)
+		{
+			if (x == y)
+				return x;
+			else if (y == DbBoundnessType.Unknown)
+				return y;
+
+			switch (x) 
+			{
+				case DbBoundnessType.Unknown:
+					return x;
+
+				case DbBoundnessType.NotDefined:
+				case DbBoundnessType.Unbound:
+					return x >= y ? x : y;
+
+				case DbBoundnessType.DbBound:
+					return x >= y ? x : DbBoundnessType.Unknown;
+
+				case DbBoundnessType.PXDBScalar:
+					switch (y)
+					{
+						case DbBoundnessType.DbBound:
+						case DbBoundnessType.PXDBCalced:
+						case DbBoundnessType.Unknown:
+							return DbBoundnessType.Unknown;
+						default:
+							return x;
+					}
+
+				case DbBoundnessType.PXDBCalced:
+					switch (y)
+					{
+						case DbBoundnessType.DbBound:
+						case DbBoundnessType.PXDBScalar:
+						case DbBoundnessType.Unknown:
+							return DbBoundnessType.Unknown;
+						default:
+							return x;
+					}
+
+				default:
+					return DbBoundnessType.Unknown;
+			}
+		}			
 	}
 }
