a3283bc9101810358f39b293fd51e2cb26374959
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-19 14:30:43 +0300

    ATR-745: added test for generic DAC extensions.

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoIsActiveMethodForExtension/Sources/GenericDacExtension_WithoutIsActive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.NoIsActiveMethodForExtension.Sources
+{
+	public sealed class Ext<TDac> : PXCacheExtension<TDac>
+	where TDac : IBqlTable, new()
+	{
+
+	}
+}
