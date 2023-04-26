a0f9fcd59d326d61c64fdbacdf8c9efe09ab285d
Sergey Nikomarov <snikomarov@acumatica.com>
2023-04-26 17:31:05 +0300

    ATR-752: added unit test for INUnit attribute

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacPropertyAttributes/Sources/DacFieldWithINUnitAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Data.BQL;
+using PX.Objects.IN;
+using PX.Objects.PO;
+
+namespace PX.Objects.HackathonDemo
+{
+	[PXHidden]
+	public class DacFieldWithINUnitAttribute : IBqlTable
+	{
+		#region UOM
+		public abstract class uOM : BqlString.Field<uOM> { }
+
+		[INUnit(typeof(POLineR.inventoryID), DisplayName = "UOM", BqlField = typeof(POLine.uOM))]	//Should not show
+		public virtual string UOM { get; set; }
+		#endregion
+	}
+}
