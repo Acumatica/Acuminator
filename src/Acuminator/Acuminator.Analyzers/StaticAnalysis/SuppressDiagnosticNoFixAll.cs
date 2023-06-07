8d550aefb7a58e79b5e1b4e963fc485e1e5882a0
Sergey Nikomarov <snikomarov@acumatica.com>
2023-06-07 23:06:03 +0300

    ATR-761: reworked the diagnostic suppression code fix to allow Fix All providers for PX1007

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/SuppressDiagnosticNoFixAll.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace Acuminator.Analyzers.StaticAnalysis
+{
+	[Shared]
+	[ExportCodeFixProvider(LanguageNames.CSharp)]
+	public class SuppressDiagnosticNoFixAll : SuppressDiagnosticFixBase
+	{
+		public override ImmutableArray<string> FixableDiagnosticIds { get; }
+
+		public SuppressDiagnosticNoFixAll()
+		{
+			FixableDiagnosticIds = AllCollectedFixableDiagnosticIds.Where(id => !DiagnosticIdsWithFixAllEnabled.Contains(id))
+																   .ToImmutableArray();
+		}
+
+		/// <summary>
+		/// Explicitly disable Fix All for diagnostics.
+		/// </summary>
+		/// <returns/>
+		public override FixAllProvider? GetFixAllProvider() => null;
+	}
+}
