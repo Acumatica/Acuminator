75d1d9594afd4b47d665ab287c4f374138a1a29c
Sergey Nikomarov <snikomarov@acumatica.com>
2023-07-20 20:37:25 +0200

    ATR-767: added PX1041 diagnostic tests

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NameConventionEventsInGraphsAndGraphExtensions/NameConventionEventsInGraphsAndGraphExtensionsTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NameConventionEventsInGraphsAndGraphExtensions/NameConventionEventsInGraphsAndGraphExtensionsTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.NameConventionEventsInGraphsAndGraphExtensions;
 using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Tests.Helpers;
@@ -25,36 +26,77 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.NameConventionEventsInGraphsAndG
 
 		protected override CodeFixProvider GetCSharpCodeFixProvider() =>
 			new NameConventionEventsInGraphsAndGraphExtensionsFix();
-		
+
+		#region Code Fix Tests
 		[Theory]
 		[EmbeddedFileData("CacheAttached.cs", "CacheAttached_Expected.cs")]
-		public Task CacheAttached(string actual, string expected) =>
+		public Task CacheAttached_Fix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
 		
 
 		[Theory]
 		[EmbeddedFileData("RowEventHandler.cs", "RowEventHandler_Expected.cs")]
-		public Task RowEventHandler(string actual, string expected) =>
+		public Task RowEventHandler_Fix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
 		
 
 		[Theory]
 		[EmbeddedFileData("FieldEventHandler.cs", "FieldEventHandler_Expected.cs")]
-		public Task FieldEventHandler(string actual, string expected) =>
+		public Task FieldEventHandler_Fix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
 		
 
 		[Theory]
 		[EmbeddedFileData("CacheAttachedWithArgUsages.cs", "CacheAttachedWithArgUsages_Expected.cs")]
-		public Task CacheAttachedWithArgUsages(string actual, string expected) =>
+		public Task CacheAttachedWithArgUsages_Fix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
 		
 
 		[Theory]
 		[EmbeddedFileData("EventHandlerWithArgsUsages.cs", "EventHandlerWithArgsUsages_Expected.cs")]
-		public Task EventHandlerWithArgsUsages(string actual, string expected) =>
+		public Task EventHandlerWithArgsUsages_Fix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
-		
+		#endregion
+
+		#region Diagnostic Tests
+		[Theory]
+		[EmbeddedFileData("CacheAttached.cs")]
+		public Task CacheAttached(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions.CreateFor(12, 26));
+
+		[Theory]
+		[EmbeddedFileData("CacheAttached_Expected.cs")]
+		public Task CacheAttached_AfterFix(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual);
+
+		[Theory]
+		[EmbeddedFileData("RowEventHandler.cs")]
+		public Task RowEventHandler(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions.CreateFor(12, 26));
+
+
+		[Theory]
+		[EmbeddedFileData("FieldEventHandler.cs")]
+		public Task FieldEventHandler(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions.CreateFor(12, 26));
+
+
+		[Theory]
+		[EmbeddedFileData("CacheAttachedWithArgUsages.cs")]
+		public Task CacheAttachedWithArgUsages(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions.CreateFor(12, 26));
+
+
+		[Theory]
+		[EmbeddedFileData("EventHandlerWithArgsUsages.cs")]
+		public Task EventHandlerWithArgsUsages(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions.CreateFor(12, 26));
+
 		[Theory]
 		[EmbeddedFileData("AdditionalParameters.cs")]
 		public Task AdditionalParameters_ShouldNotSuggestRefactoring(string actual) =>
@@ -68,6 +110,7 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.NameConventionEventsInGraphsAndG
 		[Theory]
 		[EmbeddedFileData("PXOverride.cs", "Override_DacAndBaseGraph.cs")]
 		public Task PXOverride_ShouldNotSuggestRefactoring(string actual, string dacAndBaseGraphSourceFile) =>
-			VerifyCSharpDiagnosticAsync(actual, dacAndBaseGraphSourceFile);	
+			VerifyCSharpDiagnosticAsync(actual, dacAndBaseGraphSourceFile);
+		#endregion
 	}
 }
