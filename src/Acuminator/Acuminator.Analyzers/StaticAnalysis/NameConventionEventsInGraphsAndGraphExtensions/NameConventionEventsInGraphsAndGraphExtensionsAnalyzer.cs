888fee8a6d5b8814bc9151b082d1d90884c16f28
Sergey Nikomarov <snikomarov@acumatica.com>
2023-07-20 21:30:11 +0200

    ATR-767: implemented the analyzer from the refactoring for name-convention events

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NameConventionEventsInGraphsAndGraphExtensions/NameConventionEventsInGraphsAndGraphExtensionsAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NameConventionEventsInGraphsAndGraphExtensions/NameConventionEventsInGraphsAndGraphExtensionsAnalyzer.cs
@@ -1,185 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Composition;
+using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
 using Acuminator.Analyzers.StaticAnalysis.PXGraph;
-using Acuminator.Utilities;
 using Acuminator.Utilities.Common;
-using Acuminator.Utilities.Roslyn;
+using Acuminator.Utilities.DiagnosticSuppression;
 using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
 using Acuminator.Utilities.Roslyn.Semantic.Symbols;
-using Acuminator.Utilities.Roslyn.Syntax;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeActions;
-using Microsoft.CodeAnalysis.CodeRefactorings;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Editing;
-
-using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Acuminator.Analyzers.StaticAnalysis.NameConventionEventsInGraphsAndGraphExtensions
 {
-	[ExportCodeRefactoringProvider(LanguageNames.CSharp), Shared]
 	public class NameConventionEventsInGraphsAndGraphExtensionsAnalyzer : PXGraphAggregatedAnalyzerBase
 	{
-		private const string ArgsParameterName = "e";
-		private const string EventHandlerMethodName = "_";
-		private const string EventArgsCachePropertyName = "Cache"; // Events.[EventType]<T>.Cache
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+			ImmutableArray.Create(Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions);
 
-		public NameConventionEventsInGraphsAndGraphExtensionsAnalyzer() : this(null)
-		{ }
+		public override bool ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel graph) => 
+			base.ShouldAnalyze(pxContext, graph) && graph.Type != GraphType.None;
 
-		public NameConventionEventsInGraphsAndGraphExtensionsAnalyzer(CodeAnalysisSettings codeAnalysisSettings) : base(codeAnalysisSettings)
-		{ }
-
-		protected override async Task ComputeRefactoringsAsync(CodeRefactoringContext context, SemanticModel semanticModel, PXContext pxContext)
+		public override void Analyze(SymbolAnalysisContext symbolContext, PXContext pxContext, PXGraphSemanticModel pxGraphOrExtension)
 		{
-			context.CancellationToken.ThrowIfCancellationRequested();
+			symbolContext.CancellationToken.ThrowIfCancellationRequested();
 
-			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-			var methodNode = root?.FindNode(context.Span)?.GetDeclaringMethodNode();
+			var graphOrExtensionWithEvents = PXGraphEventSemanticModel.EnrichGraphModelWithEvents(pxGraphOrExtension, symbolContext.CancellationToken);
+			var allDeclaredNamingConventionEvents = from @event in graphOrExtensionWithEvents.GetAllEvents()
+													where @event.SignatureType == EventHandlerSignatureType.Default &&
+														  @event.Symbol.IsDeclaredInType(pxGraphOrExtension.Symbol)
+													select @event;
 
-			if (methodNode == null)
-				return;
+			INamedTypeSymbol pxOverrideAttribute = pxContext.AttributeTypes.PXOverrideAttribute;
 
-			IMethodSymbol methodSymbol = semanticModel.GetDeclaredSymbol(methodNode, context.CancellationToken);
-
-			if (IsSuitableForRefactoring(methodSymbol, pxContext))
+			foreach (GraphEventInfoBase eventInfo in allDeclaredNamingConventionEvents)
 			{
-				var eventHandlerInfo = methodSymbol.GetEventHandlerInfo(pxContext);
+				symbolContext.CancellationToken.ThrowIfCancellationRequested();
 
-				if (eventHandlerInfo.SignatureType == EventHandlerSignatureType.Default
-					&& methodSymbol.Name.EndsWith("_" + eventHandlerInfo.Type, StringComparison.Ordinal)
-					&& pxContext.Events.EventHandlerSignatureTypeMap.TryGetValue( // check that there is a corresponding generic event args symbol
-						new EventInfo(eventHandlerInfo.Type, EventHandlerSignatureType.Generic), out var genericArgsSymbol))
+				if (IsSuitableForConversionToGenericSignature(eventInfo, pxContext, pxOverrideAttribute))
 				{
-					(string dacName, string fieldName) = ParseMethodName(methodSymbol.Name, eventHandlerInfo.Type);
+					var graphEventLocation = eventInfo.Node.Identifier.GetLocation();
 
-					string title = nameof(Resources.EventHandlerSignatureCodeActionTitle).GetLocalized().ToString();
-					CodeAction codeAction = 
-						CodeAction.Create(title, ct => ChangeSignatureAsync(context.Document, root, semanticModel, methodNode, methodSymbol,
-																			eventHandlerInfo.Type, genericArgsSymbol, dacName, fieldName, ct),
-										  title);
-					context.RegisterRefactoring(codeAction);
+					symbolContext.ReportDiagnosticWithSuppressionCheck(
+							Diagnostic.Create(Descriptors.PX1041_NameConventionEventsInGraphsAndGraphExtensions, graphEventLocation),
+							pxContext.CodeAnalysisSettings);
 				}
 			}
 		}
 
-		private bool IsSuitableForRefactoring(IMethodSymbol symbol, PXContext pxContext)
+		private bool IsSuitableForConversionToGenericSignature(GraphEventInfoBase eventInfo, PXContext pxContext, INamedTypeSymbol pxOverrideAttribute)
 		{
-			return symbol?.ContainingType?.OriginalDefinition != null
-			       && symbol.ContainingType.OriginalDefinition.IsPXGraphOrExtension(pxContext)
-			       && symbol.Parameters.Length <= 2
-			       && !symbol.IsOverride
-				   && !symbol.GetAttributes().Any(
-					   attr => attr.AttributeClass != null && attr.AttributeClass.Equals(pxContext.AttributeTypes.PXOverrideAttribute));
-		}
+			// event handlers with more than 2 parameters should be overrides which shouldn't be converted to generic events
+			// as well as C# overrides of base events
+			if (eventInfo.Symbol.Parameters.Length > 2 || eventInfo.Symbol.IsOverride)
+				return false;
 
-		private Task<Document> ChangeSignatureAsync(Document document, 
-			SyntaxNode root, SemanticModel semanticModel,
-			MethodDeclarationSyntax methodDeclaration, IMethodSymbol methodSymbol,
-			EventType eventType, INamedTypeSymbol genericArgsSymbol,
-			string dacName, string fieldName,
-			CancellationToken cancellationToken)
-		{
-			cancellationToken.ThrowIfCancellationRequested();
+			var eventAttributes	= eventInfo.Symbol.GetAttributes();
 
-			var newParameters = methodDeclaration.ParameterList.Parameters;
-			var cacheParameterSymbol = methodSymbol.Parameters[0];
-			ParameterSyntax argsParameter;
-			SyntaxToken parameterName;
+			// PXOverridden events can't be converted either
+			if (!eventAttributes.IsDefaultOrEmpty && eventAttributes.Any(a => pxOverrideAttribute.Equals(a.AttributeClass)))
+				return false;
 
-			if (eventType == EventType.CacheAttached)
-			{
-				parameterName = Identifier(ArgsParameterName);
-				argsParameter = newParameters[0];
-			}
-			else
-			{
-				newParameters = newParameters.RemoveAt(0); // "PXCache sender" parameter
-				argsParameter = newParameters[0];
-				parameterName = argsParameter.Identifier;
-			}
-
-			newParameters = newParameters.Replace(argsParameter, 
-				CreateArgsParameter(genericArgsSymbol, parameterName, dacName, fieldName));
-
-			var newMethodDeclaration = 
-				ReplaceParameterUsages(methodDeclaration, cacheParameterSymbol,
-					MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, 
-						IdentifierName(parameterName), IdentifierName(EventArgsCachePropertyName)), semanticModel)
-				.WithIdentifier(Identifier(EventHandlerMethodName))
-				.WithParameterList(methodDeclaration.ParameterList.WithParameters(newParameters));
-
-			root = root.ReplaceNode(methodDeclaration, newMethodDeclaration);
-
-			return Task.FromResult(document.WithSyntaxRoot(root));
-		}
-
-		private ParameterSyntax CreateArgsParameter(INamedTypeSymbol genericArgsSymbol, SyntaxToken parameterName, 
-			string dacName, string fieldName)
-		{
-			SeparatedSyntaxList<TypeSyntax> syntaxList;
-
-			if (String.IsNullOrEmpty(fieldName))
-			{
-				syntaxList = SingletonSeparatedList<TypeSyntax>(IdentifierName(dacName));
-			}
-			else
-			{
-				if (genericArgsSymbol.TypeParameters.Length == 2)
-				{
-					syntaxList = SeparatedList<TypeSyntax>(new TypeSyntax[] { IdentifierName(dacName),
-						QualifiedName(IdentifierName(dacName), IdentifierName(fieldName)) });
-				}
-				else
-				{
-					syntaxList = SingletonSeparatedList<TypeSyntax>(
-						QualifiedName(IdentifierName(dacName), IdentifierName(fieldName)));
-				}
-			}
-
-			return Parameter(parameterName)
-				.WithType(
-					QualifiedName(
-						IdentifierName(genericArgsSymbol.ContainingType.Name),
-						GenericName(
-								Identifier(genericArgsSymbol.Name))
-							.WithTypeArgumentList(
-								TypeArgumentList(syntaxList))));
-		}
-
-		private (string DacName, string FieldName) ParseMethodName(string methodName, EventType eventType)
-		{
-			var match = Regex.Match(methodName, @"([\w-[_]]+)_(\w+_){0,1}" + eventType,
-				RegexOptions.CultureInvariant | RegexOptions.Singleline);
-
-			if (match.Success)
-			{
-				string dacName = match.Groups[1].Value;
-				string fieldName = match.Groups[2].Success ? match.Groups[2].Value.TrimEnd('_') : null;
-
-				if (!String.IsNullOrEmpty(fieldName))
-					fieldName = Char.ToLowerInvariant(fieldName[0]) + fieldName.Substring(1, fieldName.Length - 1);
-
-				return (dacName, fieldName);
-			}
-
-			return (null, null);
-		}
-
-		private MethodDeclarationSyntax ReplaceParameterUsages(MethodDeclarationSyntax methodDeclaration, 
-			IParameterSymbol parameterSymbol, SyntaxNode replaceWith, SemanticModel semanticModel)
-		{
-			var rewriter = new ParameterUsagesRewriter(parameterSymbol, replaceWith, semanticModel);
-			return (MethodDeclarationSyntax) methodDeclaration.Accept(rewriter);
+			// check that there is a corresponding generic event args symbol
+			var eventTypeInfoForGenericSignature = new EventInfo(eventInfo.EventType, EventHandlerSignatureType.Generic);
+			return pxContext.Events.EventHandlerSignatureTypeMap.ContainsKey(eventTypeInfoForGenericSignature);
 		}
 	}
 }
