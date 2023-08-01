645758ea8056e7cc54673e454234b683e9598522
Sergey Nikomarov <snikomarov@acumatica.com>
2023-08-01 23:06:07 +0200

    ATR-767: refactoring + added comments

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/IPXGraphAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/IPXGraphAnalyzer.cs
@@ -1,19 +1,28 @@
-﻿using Acuminator.Analyzers.StaticAnalysis.AnalyzersAggregator;
+﻿#nullable enable
+
+using Acuminator.Analyzers.StaticAnalysis.AnalyzersAggregator;
 using Acuminator.Utilities;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Acuminator.Analyzers.StaticAnalysis.PXGraph
 {
     public interface IPXGraphAnalyzer : ISymbolAnalyzer
     {
+        /// <summary>
+        /// Analyzes.
+        /// </summary>
+        /// <param name="context">Symbol analysis context.</param>
+        /// <param name="pxContext">Acumatica context.</param>
+        /// <param name="pxGraph">The graph semantic model.</param>
         void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphSemanticModel pxGraph);
 
 		/// <summary>
 		/// Determine if the analyzer should run on graph.
 		/// </summary>
-		/// <param name="pxContext">Context.</param>
+		/// <param name="pxContext">Acumatica context.</param>
 		/// <param name="pxGraph">The graph semantic model.</param>
 		/// <returns/>
 		bool ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel graph);
--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/PXGraphAggregatedAnalyzerBase.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/PXGraphAggregatedAnalyzerBase.cs
@@ -1,9 +1,13 @@
-﻿using Acuminator.Utilities;
+﻿#nullable enable
+
+using System.Collections.Immutable;
+
+using Acuminator.Utilities;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System.Collections.Immutable;
 
 namespace Acuminator.Analyzers.StaticAnalysis.PXGraph
 {
