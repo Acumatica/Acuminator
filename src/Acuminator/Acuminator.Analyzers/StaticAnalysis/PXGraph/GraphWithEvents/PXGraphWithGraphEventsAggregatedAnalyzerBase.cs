c16561762fb2c30343beadda6ac28261917995bd
Sergey Nikomarov <snikomarov@acumatica.com>
2023-08-02 02:12:04 +0200

    ATR-767: added base class for graph with events aggregated analyzers

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/GraphWithEvents/PXGraphWithGraphEventsAggregatedAnalyzerBase.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+
+using System.Collections.Immutable;
+
+using Acuminator.Analyzers.StaticAnalysis.AnalyzersAggregator;
+using Acuminator.Utilities;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Acuminator.Analyzers.StaticAnalysis.PXGraph
+{
+	/// <summary>
+	/// Base class for aggregated graph analyzers with events.
+	/// </summary>
+	public abstract class PXGraphWithGraphEventsAggregatedAnalyzerBase : IPXGraphWithGraphEventsAnalyzer
+	{
+		/// <inheritdoc cref="ISymbolAnalyzer.SupportedDiagnostics"/>
+		public abstract ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+
+		/// <inheritdoc cref="IPXGraphWithGraphEventsAnalyzer.ShouldAnalyze(PXContext, PXGraphSemanticModel)"/>
+		public bool ShouldAnalyze(PXContext pxContext, PXGraphSemanticModel pxGraph) =>
+			pxGraph != null;
+
+		/// <inheritdoc cref="IPXGraphWithGraphEventsAnalyzer.ShouldAnalyze(PXContext, PXGraphEventSemanticModel)"/>
+		public virtual bool ShouldAnalyze(PXContext pxContext, PXGraphEventSemanticModel pxGraphWithEvents) => 
+			pxGraphWithEvents != null;
+
+		public abstract void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphEventSemanticModel pxGraphWithEvents);
+	}
+}
