0948d39e94ddfce044fc644ec9e3dad829b5076e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-02-16 11:39:45 +0100

    ATR-786: added nullable annotation + sorted usings

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Utils/DacSymbolsHierarchyUtils.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Utils/DacSymbolsHierarchyUtils.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using Microsoft.CodeAnalysis;
+
 using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn.Constants;
+
+using Microsoft.CodeAnalysis;
 
 namespace Acuminator.Utilities.Roslyn.Semantic.Dac
 {
