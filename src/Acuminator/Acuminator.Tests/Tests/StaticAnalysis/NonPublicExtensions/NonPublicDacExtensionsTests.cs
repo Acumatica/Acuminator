54a4fcc10fbde60897cb60cbda3823ac3a76df12
Sergey Nikomarov <snikomarov@acumatica.com>
2024-04-08 06:39:31 +0200

    ATR-801: added tests for non public DACs

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicExtensions/NonPublicDacExtensionsTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicExtensions/NonPublicDacExtensionsTests.cs
@@ -1,19 +1,24 @@
-﻿using System.Threading.Tasks;
+﻿#nullable enable
+
+using System.Threading.Tasks;
+
 using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.Dac;
 using Acuminator.Analyzers.StaticAnalysis.NonPublicGraphsDacsAndExtensions;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
 using Acuminator.Utilities;
+
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
+
 using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.NonPublicExtensions
 {
     public class NonPublicDacExtensionsTests : CodeFixVerifier
     {
-		protected override CodeFixProvider GetCSharpCodeFixProvider() => new NonPublicDAcGraphAndExtensionFix();
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new NonPublicDacGraphAndExtensionFix();
 
 		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
 			new DacAnalyzersAggregator(
@@ -31,14 +36,34 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.NonPublicExtensions
 				 Descriptors.PX1022_NonPublicDacExtension.CreateFor(47, 4));
 
 		[Theory]
+		[EmbeddedFileData("NonPublicDac.cs")]
+		public async Task NonPublicDacs(string actual) =>
+			 await VerifyCSharpDiagnosticAsync(actual,
+				 Descriptors.PX1022_NonPublicDac.CreateFor(6, 2),
+				 Descriptors.PX1022_NonPublicDac.CreateFor(16, 15),
+				 Descriptors.PX1022_NonPublicDac.CreateFor(31, 17),
+				 Descriptors.PX1022_NonPublicDac.CreateFor(43, 4));
+
+		[Theory]
         [EmbeddedFileData("NonPublicDacExtension_Expected.cs")]
         public async Task NonPublicDacExtensions_ShouldNotShowDiagnostic(string actual) =>
+			await VerifyCSharpDiagnosticAsync(actual);
+
+		[Theory]
+		[EmbeddedFileData("NonPublicDac_Expected.cs")]
+		public async Task NonPublicDac_ShouldNotShowDiagnostic(string actual) =>
 			await VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
 	    [EmbeddedFileData("NonPublicDacExtension.cs",
 						  "NonPublicDacExtension_Expected.cs")]
 	    public async Task NonPublicDacExtensions_CodeFix(string actual, string expected) =>
+			await VerifyCSharpFixAsync(actual, expected);
+
+		[Theory]
+		[EmbeddedFileData("NonPublicDac.cs",
+						  "NonPublicDac_Expected.cs")]
+		public async Task NonPublicDacs_CodeFix(string actual, string expected) =>
 			await VerifyCSharpFixAsync(actual, expected);
 	}
 }
