447e0c8d4738ca17e136f5521f10321ffdda0fe5
Sergey Nikomarov <snikomarov@acumatica.com>
2024-04-08 07:26:01 +0200

    ATR-801: added tests for non public graphs for PX1022

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicGraphsDacsAndExtensions/NonPublicGraphExtensionsTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicGraphsDacsAndExtensions/NonPublicGraphExtensionsTests.cs
@@ -1,12 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿#nullable enable
+
+using System.Threading.Tasks;
+
 using Acuminator.Analyzers.StaticAnalysis;
-using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Analyzers.StaticAnalysis.NonPublicGraphsDacsAndExtensions;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
 using Acuminator.Utilities;
+
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
+
 using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.NonPublicGraphsDacsAndExtensions
@@ -31,14 +36,34 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.NonPublicGraphsDacsAndExtensions
 				 Descriptors.PX1022_NonPublicGraphExtension.CreateFor(38, 4));
 
 		[Theory]
+		[EmbeddedFileData("NonPublicGraph.cs")]
+		public async Task NonPublicGraphs(string actual) =>
+			 await VerifyCSharpDiagnosticAsync(actual,
+				 Descriptors.PX1022_NonPublicGraph.CreateFor(10, 2),
+				 Descriptors.PX1022_NonPublicGraph.CreateFor(18, 8),
+				 Descriptors.PX1022_NonPublicGraph.CreateFor(30, 24),
+				 Descriptors.PX1022_NonPublicGraph.CreateFor(38, 4));
+
+		[Theory]
         [EmbeddedFileData("NonPublicGraphExtension_Expected.cs")]
         public async Task NonPublicGraphExtensions_ShouldNotShowDiagnostic(string actual) =>
+			await VerifyCSharpDiagnosticAsync(actual);
+
+		[Theory]
+		[EmbeddedFileData("NonPublicGraph_Expected.cs")]
+		public async Task NonPublicGraphs_ShouldNotShowDiagnostic(string actual) =>
 			await VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
 	    [EmbeddedFileData("NonPublicGraphExtension.cs",
 						  "NonPublicGraphExtension_Expected.cs")]
 	    public async Task NonPublicGraphExtensions_CodeFix(string actual, string expected) =>
+			await VerifyCSharpFixAsync(actual, expected);
+
+		[Theory]
+		[EmbeddedFileData("NonPublicGraph.cs",
+						  "NonPublicGraph_Expected.cs")]
+		public async Task NonPublicGraphs_CodeFix(string actual, string expected) =>
 			await VerifyCSharpFixAsync(actual, expected);
 	}
 }
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NonPublicGraphsDacsAndExtensions/Sources/NonPublicGraph_Expected.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.NonPublicGraphsDacsAndExtensions.Sources
+{
+	public class SOOrderGraph : PXGraph<SOOrderGraph>
+	{
+		public virtual void _(Events.RowUpdating<SOOrder> e)
+		{
+
+		}
+	}
+
+	public class SOOrderGraph2 : PXGraph<SOOrderGraph2>
+	{
+		public virtual void _(Events.RowUpdated<SOOrder> e)
+		{
+
+		}
+	}
+
+	public static class PurchaseGlobalState
+	{
+		public class PurchaseEngine
+		{
+			public sealed class SOOrderPurchaseEntry : PXGraph<SOOrderPurchaseEntry>
+			{
+				public virtual void _(Events.RowSelected<SOOrder> e)
+				{
+
+				}
+			}
+
+			public sealed class SOOrderPurchaseEntry2 : PXGraph<SOOrderPurchaseEntry2>
+			{
+				public virtual void _(Events.RowSelected<SOOrder> e)
+				{
+
+				}
+			}
+		}
+	}
+
+	[PXCacheName("SO Order")]
+	public class SOOrder : IBqlTable
+	{
+
+	}
+}
