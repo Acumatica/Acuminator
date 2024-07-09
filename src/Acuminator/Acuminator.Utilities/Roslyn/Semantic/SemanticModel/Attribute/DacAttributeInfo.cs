e31c0d48b92289472b70acd3a699ea3823e9ec5f
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-09 03:03:48 +0200

    ATR-446: added DAC attribute info

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Attribute/DacAttributeInfo.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Attribute
+{
+	/// <summary>
+	/// DAC or DAC extension attribute info.
+	/// </summary>
+	public class DacAttributeInfo : AttributeInfoBase
+	{
+		/// <summary>
+		/// Indicates if the attribute configures the default navigation.
+		/// </summary>
+		public bool IsDefaultNavigation { get; }
+
+		/// <summary>
+		/// Indicates if the attribute configures a projection DAC.
+		/// </summary>
+		public bool IsProjectionAttribute { get; }
+
+		public DacAttributeInfo(PXContext pxContext, AttributeData attributeData, int declarationOrder) : base(attributeData, declarationOrder)
+		{
+			IsDefaultNavigation   = AttributeType.IsDefaultNavigation(pxContext);
+			IsProjectionAttribute = AttributeType.InheritsFromOrEquals(pxContext.AttributeTypes.PXProjectionAttribute);
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Attribute/Utils/AttributeSymbolUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Acuminator.Utilities.Common;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Attribute
+{
+	public static class AttributeSymbolUtils
+	{
+		public static bool IsDefaultNavigation(this INamedTypeSymbol attributeType, PXContext pxContext)
+		{
+			attributeType.ThrowOnNull();
+
+			if (pxContext.CheckIfNull().AttributeTypes.PXPrimaryGraphBaseAttribute is INamedTypeSymbol pxPrimaryGraphBaseAttribute)
+				return attributeType.InheritsFromOrEquals(pxPrimaryGraphBaseAttribute);
+			else
+				return attributeType.InheritsFromOrEquals(pxContext.AttributeTypes.PXPrimaryGraphAttribute);
+		}
+	}
+}
