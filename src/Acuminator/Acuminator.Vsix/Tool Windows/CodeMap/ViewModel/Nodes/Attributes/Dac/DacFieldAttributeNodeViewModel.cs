17967ec2eba1e5ea04ac1d0bff9873a0b899471e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-10 01:19:43 +0200

    ATR-813: added separate nodes for DAC field and cache attached attributes

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Attributes/Dac/DacFieldAttributeNodeViewModel.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+using Acuminator.Utilities.Roslyn.Semantic.Attribute;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class DacFieldAttributeNodeViewModel : AttributeNodeViewModel<DacFieldAttributeInfo>
+	{
+		public DacFieldAttributeNodeViewModel(TreeNodeViewModel nodeVM, DacFieldAttributeInfo attributeInfo, bool isExpanded = false) :
+										 base(nodeVM, attributeInfo, isExpanded)
+		{
+			
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => 
+			treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
