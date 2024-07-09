943f29aee785955d4400d648704b702eae702602
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-09 03:37:28 +0200

    ATR-446: added graph attribute info

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Attribute/GraphAttributeInfo.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Attribute
+{
+	/// <summary>
+	/// Graph or graph extension attribute info.
+	/// </summary>
+	public class GraphAttributeInfo : AttributeInfoBase
+	{
+		/// <summary>
+		/// Indicates if the attribute configures the default navigation.
+		/// </summary>
+		public bool IsDefaultNavigation { get; }
+
+		/// <summary>
+		/// Indicates if the attribute configures access to the protected members of a base graph or base graph extension.
+		/// </summary>
+		public bool IsProtectedAccess { get; }
+
+		public GraphAttributeInfo(PXContext pxContext, AttributeData attributeData, int declarationOrder) : base(attributeData, declarationOrder)
+		{
+			IsDefaultNavigation = AttributeType.IsDefaultNavigation(pxContext);
+			IsProtectedAccess   = pxContext.AttributeTypes.PXProtectedAccessAttribute is INamedTypeSymbol protectedAccessAttribute && 
+								  AttributeType.InheritsFromOrEquals(protectedAccessAttribute);
+		}
+	}
+}
