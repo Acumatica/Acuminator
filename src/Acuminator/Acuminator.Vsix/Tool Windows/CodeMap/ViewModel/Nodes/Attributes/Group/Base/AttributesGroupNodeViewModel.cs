e5f7729000814c55f58de41b0bb3bfcf22254f09
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-10 02:01:47 +0200

    ATR-813: added base class for attribute group view model

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Attributes/Group/Base/AttributesGroupNodeViewModel.cs
@@ -0,0 +1,60 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Acuminator.Utilities.Roslyn.Semantic.Attribute;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public abstract class AttributesGroupNodeViewModel : TreeNodeViewModel, IGroupNodeWithCyclingNavigation
+	{
+		public override bool DisplayNodeWithoutChildren => false;
+
+		public override Icon NodeIcon => Icon.Category;
+
+		public abstract AttributePlacement Placement { get; }
+
+		protected abstract string AttributesGroupDescription { get; }
+
+		public override string Name => $"{AttributesGroupDescription}({Children.Count})";
+
+		protected abstract bool AllowNavigation { get; }
+
+		bool IGroupNodeWithCyclingNavigation.AllowNavigation => AllowNavigation;
+
+		int IGroupNodeWithCyclingNavigation.CurrentNavigationIndex
+		{
+			get;
+			set;
+		}
+
+		IList<TreeNodeViewModel> IGroupNodeWithCyclingNavigation.Children => Children;
+
+		protected AttributesGroupNodeViewModel(TreeNodeViewModel nodeVM, bool isExpanded = false) :
+										  base(nodeVM?.Tree!, nodeVM, isExpanded)
+		{
+			
+		}
+
+		public abstract IEnumerable<AttributeInfoBase> GetAttributeInfos();
+
+		public async override Task NavigateToItemAsync()
+		{
+			var childToNavigateTo = this.GetChildToNavigateTo();
+
+			if (childToNavigateTo != null)
+			{
+				await childToNavigateTo.NavigateToItemAsync();
+				IsExpanded = true;
+				Tree.SelectedItem = childToNavigateTo;
+			}
+		}
+
+		bool IGroupNodeWithCyclingNavigation.CanNavigateToChild(TreeNodeViewModel child) => CanNavigateToChild(child);
+
+		protected virtual bool CanNavigateToChild(TreeNodeViewModel child) => true;
+	}
+}
