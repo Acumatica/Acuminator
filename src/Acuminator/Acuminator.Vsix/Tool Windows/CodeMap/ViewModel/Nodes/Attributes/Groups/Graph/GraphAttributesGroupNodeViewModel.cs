1a2f8c821797f8575880f22f90a8f687718857a8
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-10 03:19:22 +0200

    ATR-813: added graph attributes group

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Attributes/Groups/Graph/GraphAttributesGroupNodeViewModel.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.Attribute;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class GraphAttributesGroupNodeViewModel : AttributesGroupNodeViewModel<GraphAttributeInfo>
+	{
+		public PXGraphEventSemanticModel GraphModel { get; }
+
+		public override AttributePlacement Placement => AttributePlacement.Graph;
+
+		protected override string AttributesGroupDescription => VSIXResource.CodeMap_Node_GraphAttributes;
+
+		protected override bool AllowNavigation => true;
+
+		protected GraphAttributesGroupNodeViewModel(PXGraphEventSemanticModel graphSemanticModel, TreeNodeViewModel nodeVM, bool isExpanded = false) :
+											   base(nodeVM, isExpanded)
+		{
+			GraphModel = graphSemanticModel.CheckIfNull();
+		}
+
+		public override IEnumerable<GraphAttributeInfo> AttributeInfos() => GraphModel.Attributes;
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => 
+			treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => 
+			treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
