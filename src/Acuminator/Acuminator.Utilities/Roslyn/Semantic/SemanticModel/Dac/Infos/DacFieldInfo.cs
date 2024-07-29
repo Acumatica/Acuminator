7e0f8bbf7a0d885484c5440563df9f3827776296
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-29 17:54:12 +0200

    ATR-815: added semantic info for DAC field as a pair of BQL field and DAC field property

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Infos/DacFieldInfo.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+using System;
+
+using Acuminator.Utilities.Common;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Dac
+{
+	/// <summary>
+	/// Information about a DAC field - a pair consisting of a DAC field property and a DAC BQL field.
+	/// </summary>
+	public class DacFieldInfo
+	{
+		public string Name { get; }
+
+		public ITypeSymbol? DacType { get; }
+
+		public DacPropertyInfo? PropertyInfo { get; }
+
+		public DacBqlFieldInfo? FieldInfo { get; }
+
+		public DacFieldInfo(DacPropertyInfo? dacPropertyInfo, DacBqlFieldInfo? dacFieldInfo)
+		{
+			if (dacPropertyInfo == null && dacFieldInfo == null)
+				throw new ArgumentNullException($"Both {nameof(dacPropertyInfo)} and {nameof(dacFieldInfo)} parameters cannot be null.");
+
+			PropertyInfo = dacPropertyInfo;
+			FieldInfo 	 = dacFieldInfo;
+			Name 		 = PropertyInfo?.Name ?? FieldInfo!.Name.ToPascalCase();
+			DacType 	 = PropertyInfo?.Symbol.ContainingType ?? FieldInfo!.Symbol.ContainingType;
+		}
+	}
+}
