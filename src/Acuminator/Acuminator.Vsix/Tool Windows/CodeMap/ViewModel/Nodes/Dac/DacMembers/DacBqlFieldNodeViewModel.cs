ff9a14b7f24e2dbd726d2bed980d1309669cc962
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-29 16:43:45 +0200

    ATR-815: added DAC bql field node

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacMembers/DacBqlFieldNodeViewModel.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class DacBqlFieldNodeViewModel : DacMemberNodeViewModel
+	{
+		public override Icon NodeIcon => Icon.DacBqlField;
+
+		public DacFieldInfo FieldInfo => (MemberInfo as DacFieldInfo)!;
+
+		public DacBqlFieldNodeViewModel(DacMemberCategoryNodeViewModel dacMemberCategoryVM, TreeNodeViewModel parent,
+										DacFieldInfo fieldInfo, bool isExpanded = false) :
+										base(dacMemberCategoryVM, parent, fieldInfo, isExpanded)
+		{
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
