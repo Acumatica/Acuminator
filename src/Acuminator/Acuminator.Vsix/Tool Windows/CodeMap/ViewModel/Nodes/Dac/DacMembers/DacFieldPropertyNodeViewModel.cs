12b1c57c447087f4d0758e7bc78ceb8f11d6b9b9
Sergey Nikomarov <snikomarov@acumatica.com>
2024-07-29 16:37:24 +0200

    ATR-815: added DAC field property node

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacMembers/DacFieldPropertyNodeViewModel.cs
@@ -0,0 +1,51 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Utilities.Roslyn.PXFieldAttributes;
+using Acuminator.Vsix.ToolWindows.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class DacFieldPropertyNodeViewModel : DacMemberNodeViewModel, IElementWithTooltip
+	{
+		public override Icon NodeIcon => Icon.DacFieldProperty;
+
+		public DacPropertyInfo PropertyInfo => (MemberInfo as DacPropertyInfo)!;
+
+		public bool IsDacProperty => PropertyInfo.IsDacProperty;
+
+		public bool IsKey => PropertyInfo.IsKey;
+
+		public bool IsIdentity => PropertyInfo.IsIdentity;
+
+		public DbBoundnessType EffectiveDbBoundness => PropertyInfo.EffectiveDbBoundness;
+
+		public DacFieldPropertyNodeViewModel(DacMemberCategoryNodeViewModel dacMemberCategoryVM, TreeNodeViewModel parent, 
+											 DacPropertyInfo propertyInfo, bool isExpanded = false) :
+										base(dacMemberCategoryVM, parent, propertyInfo, isExpanded)
+		{
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+
+		TooltipInfo? IElementWithTooltip.CalculateTooltip()
+		{
+			var attributeStrings = Children.OfType<AttributeNodeViewModel>()
+										   .Select(attribute => attribute.CalculateTooltip().Tooltip);
+			string aggregatedTooltip = string.Join(Environment.NewLine, attributeStrings);
+			return aggregatedTooltip.IsNullOrWhiteSpace()
+				? null
+				: new TooltipInfo(aggregatedTooltip);
+		}
+	}
+}
