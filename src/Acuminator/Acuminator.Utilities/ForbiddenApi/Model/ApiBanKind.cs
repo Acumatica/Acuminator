3207b7b1152fe2a8c9254fc6fdc27b654f3f57a4
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-11 01:25:57 +0200

    ATR-819: added API ban kind

---
+++ src/Acuminator/Acuminator.Utilities/ForbiddenApi/Model/ApiBanKind.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+using System;
+
+namespace Acuminator.Utilities.ForbiddenApi.Model
+{
+	[Flags]
+	public enum ApiBanKind : byte
+	{
+		None	  = 0b0000,
+		Acumatica = 0b0001,
+		ISV		  = 0b0010,
+		All		  = Acumatica | ISV,
+	}
+
+	public static class ApiBanKindUtils
+	{
+		public static bool IsAcumatica(this ApiBanKind kind) => kind.IsKind(ApiBanKind.Acumatica);
+
+		public static bool IsISV(this ApiBanKind kind) => kind.IsKind(ApiBanKind.ISV);
+	
+		public static bool IsKind(this ApiBanKind kind, ApiBanKind kindToCheck) => (kind & kindToCheck) == kindToCheck;
+	}
+}
