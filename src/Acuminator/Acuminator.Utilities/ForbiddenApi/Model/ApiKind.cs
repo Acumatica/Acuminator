4740f0bd44806b6c580c2e3cf93992fd01869f5d
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-11 01:28:38 +0200

    ATR-819: added ApiKind from CoreCompatibilyzer

---
+++ src/Acuminator/Acuminator.Utilities/ForbiddenApi/Model/ApiKind.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+namespace Acuminator.Utilities.ForbiddenApi.Model
+{
+	public enum ApiKind : byte
+	{
+		Undefined,
+		Namespace,
+		Type,
+		Field,
+		Property,
+		Event,
+		Method
+	}
+}
