f1db7356190a31344d1c00c32f4c032b9458d956
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-10 22:32:29 +0200

    ATR-819: added fordbidden analyzers draft

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ForbiddenAPI/ForbiddenApiAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Acuminator.Utilities;
+using Acuminator.Utilities.DiagnosticSuppression;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Acuminator.Analyzers.StaticAnalysis.ForbiddenApi
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class ForbiddenApiAnalyzer : PXDiagnosticAnalyzer
+	{
+		public const string CorrespondingType = "CorrespondingType";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptors.PX1061_LegacyBqlConstant);
+
+		protected override bool ShouldAnalyze(PXContext pxContext) =>
+			base.ShouldAnalyze(pxContext) && pxContext.IsAcumatica2019R1_OrGreater && pxContext.BqlConstantType != null;
+
+		public ForbiddenApiAnalyzer() : this(null)
+		{ }
+
+		public ForbiddenApiAnalyzer(CodeAnalysisSettings codeAnalysisSettings) : base(codeAnalysisSettings)
+		{ }
+
+		internal override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
+		{
+			compilationStartContext.RegisterSymbolAction(
+				c => Analyze(c, pxContext),
+				SymbolKind.NamedType);
+		}
+
+		private void Analyze(SymbolAnalysisContext context, PXContext pxContext)
+		{
+			context.CancellationToken.ThrowIfCancellationRequested();
+
+			
+		}
+	}
+}
