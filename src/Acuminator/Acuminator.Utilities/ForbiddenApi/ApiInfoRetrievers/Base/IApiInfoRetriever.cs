97370e2567737fdd1a86e7c8978505c2a696c410
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-11 01:04:01 +0200

    ATR-819: integrated parts of CoreCompatibility analyzer - exceptions and API retriever interface

---
+++ src/Acuminator/Acuminator.Utilities/ForbiddenApi/ApiInfoRetrievers/Base/IApiInfoRetriever.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.ForbiddenApi.ApiInfoRetrievers
+{
+	/// <summary>
+	/// Interface for a retriever of info for APIs.
+	/// </summary>
+	public interface IApiInfoRetriever
+	{
+		/// <summary>
+		/// Gets the information for API. Returns <c>null</c> if the API is not found.
+		/// </summary>
+		/// <param name="apiSymbol">The API symbol to check.</param>
+		/// <returns>
+		/// Returns the information about banned API or <c>null</c> if the API is not found.
+		/// </returns>
+		ApiSearchResult? GetInfoForApi(ISymbol apiSymbol);
+	}
+}
---
+++ src/Acuminator/Acuminator.Utilities/ForbiddenApi/Errors/ApiReaderException.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using System;
+using System.Runtime.Serialization;
+
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Utilities.ForbiddenApi.Errors
+{
+	/// <summary>
+	/// A special exception class for reading api.
+	/// </summary>
+	[Serializable]
+	public sealed class ApiReaderException : Exception
+	{
+		private const string DefaultError = "An error happened during the reading of the API list.";
+
+		public ApiReaderException() : base(DefaultError)
+		{
+		}
+
+		public ApiReaderException(string? message) : base(message.NullIfWhiteSpace() ?? DefaultError)
+		{
+		}
+
+		public ApiReaderException(string? message, Exception innerException) : base(message.NullIfWhiteSpace() ?? DefaultError, innerException)
+		{
+		}
+
+		private ApiReaderException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
