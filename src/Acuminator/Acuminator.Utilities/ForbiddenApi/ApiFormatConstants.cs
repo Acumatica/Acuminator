6880edfd1574175db81412be784da2dd0523692d
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-11 20:20:46 +0200

    ATR-819: integrated Api data structure from CoreCompatibilyzer

---
+++ src/Acuminator/Acuminator.Utilities/ForbiddenApi/ApiFormatConstants.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+namespace Acuminator.Utilities.ForbiddenApi
+{
+	public static class ApiFormatConstants
+	{
+		public static class Chars
+		{
+			public const char NamespaceSeparator = '-';
+			public const char NestedTypesSeparator = '+';
+		}
+
+		public static class Strings
+		{
+			public const string NamespaceSeparator = "-";
+			public const string NestedTypesSeparator = "+";
+		}
+	}
+}
