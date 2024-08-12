dbd0441caaec0dab789c31560729e3452cfe314e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-12 04:50:29 +0200

    ATR-819: added api storages to the forbidden API analyzer

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ForbiddenAPI/ForbiddenApiAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/ForbiddenAPI/ForbiddenApiAnalyzer.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Immutable;
 using System.Linq;
 
 using Acuminator.Utilities;
-using Acuminator.Utilities.DiagnosticSuppression;
 using Acuminator.Utilities.Roslyn.Semantic;
+using Acuminator.Utilities.ForbiddenApi.Storage;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -14,9 +16,22 @@ namespace Acuminator.Analyzers.StaticAnalysis.ForbiddenApi
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
 	public class ForbiddenApiAnalyzer : PXDiagnosticAnalyzer
 	{
-		public const string CorrespondingType = "CorrespondingType";
+		private const string _bannedApiFileRelativePath = @"StaticAnalysis\ForbiddenAPI\Data\BannedApis.txt";
+		private const string _bannedApiAssemblyResourceName = @"StaticAnalysis.ForbiddenAPI.Data.BannedApis.txt";
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptors.PX1061_LegacyBqlConstant);
+		private const string _whiteListFileRelativePath = @"StaticAnalysis\ForbiddenAPI\Data\WhiteList.txt";
+		private const string _whiteListAssemblyResourceName = @"StaticAnalysis.ForbiddenAPI.Data.WhiteList.txt";
+
+		public static ApiStorage BannedApi { get; } = new ApiStorage(_bannedApiFileRelativePath, _bannedApiAssemblyResourceName);
+
+		public static ApiStorage WhiteList { get; } = new ApiStorage(_whiteListFileRelativePath, _whiteListAssemblyResourceName);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = 
+			ImmutableArray.Create
+			(
+				Descriptors.PX1065_ForbiddenApiUsage_NoDetails,
+				Descriptors.PX1065_ForbiddenApiUsage_WithDetails
+			);
 
 		protected override bool ShouldAnalyze(PXContext pxContext) =>
 			base.ShouldAnalyze(pxContext) && pxContext.IsAcumatica2019R1_OrGreater && pxContext.BqlConstantType != null;
@@ -24,7 +39,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.ForbiddenApi
 		public ForbiddenApiAnalyzer() : this(null)
 		{ }
 
-		public ForbiddenApiAnalyzer(CodeAnalysisSettings codeAnalysisSettings) : base(codeAnalysisSettings)
+		public ForbiddenApiAnalyzer(CodeAnalysisSettings? codeAnalysisSettings) : base(codeAnalysisSettings)
 		{ }
 
 		internal override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
