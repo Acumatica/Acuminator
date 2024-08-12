58fc581c3c0e3cbe61fe3e0c86ce39472e4384f0
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-13 01:57:37 +0200

    ATR-819: added forbidden API settings

---
+++ src/Acuminator/Acuminator.Utilities/Settings/ForbiddenApiSettings.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+
+using System;
+using System.Composition;
+
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Utilities
+{
+	[Export]
+	public class ForbiddenApiSettings : IEquatable<ForbiddenApiSettings>
+	{
+		public static ForbiddenApiSettings Default { get; } = new ForbiddenApiSettings(bannedApiFilePath: null, whiteListApiFilePath: null);
+
+		public string? BannedApiFilePath { get; }
+
+		public string? WhiteListApiFilePath { get; }
+
+		public ForbiddenApiSettings(string? bannedApiFilePath, string? whiteListApiFilePath)
+		{
+			BannedApiFilePath = bannedApiFilePath.NullIfWhiteSpace()?.Trim();
+			WhiteListApiFilePath = whiteListApiFilePath.NullIfWhiteSpace()?.Trim();
+		}
+
+		public ForbiddenApiSettings WithBannedApiFilePath(string? bannedApiFilePath) =>
+			new(bannedApiFilePath, WhiteListApiFilePath);
+
+		public ForbiddenApiSettings WithWhiteListApiFilePath(string? whiteListApiFilePath) =>
+			new(BannedApiFilePath, whiteListApiFilePath);
+
+		public override bool Equals(object obj) => Equals(obj as ForbiddenApiSettings);
+
+		public bool Equals(ForbiddenApiSettings? other) =>
+			BannedApiFilePath == other?.BannedApiFilePath && WhiteListApiFilePath == other?.WhiteListApiFilePath;
+
+		public override int GetHashCode()
+		{
+			int hash = 17;
+
+			unchecked
+			{
+				hash = 23 * hash + (BannedApiFilePath?.GetHashCode() ?? 0);
+				hash = 23 * hash + (WhiteListApiFilePath?.GetHashCode() ?? 0);
+			}
+
+			return hash;
+		}
+	}
+}
