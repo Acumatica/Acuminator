9311bc42934242b7167f6fe6ec136a749cd13af2
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-15 13:53:53 +0200

    ATR-821: added tests for DAC semantic model

---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Dac/DacSemanticModelTests.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Xunit;
+using FluentAssertions;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Dac
+{
+	public class DacSemanticModelTests : SemanticModelTestsBase<DacSemanticModel>
+	{
+		[Theory]
+		[EmbeddedFileData("DacFields_OnlyBqlFields.cs")]
+		public async Task Test_DacFields_Recognition(string text)
+		{
+			var dacSemanticModel = await PrepareSemanticModelAsync(text).ConfigureAwait(false);
+
+			dacSemanticModel.DacFieldsByNames.Should().HaveCountGreaterThan(3);		// Check that fields from base types from PX.Objects were collected
+			dacSemanticModel.DacFieldsByNames.Should().ContainKey("docType");
+			dacSemanticModel.DacFieldsByNames.Should().ContainKey("refNbr");
+			dacSemanticModel.DacFieldsByNames.Should().ContainKey("noteID");
+		}
+
+		protected override Task<DacSemanticModel> PrepareSemanticModelAsync(RoslynTestContext context, CancellationToken cancellation = default)
+		{
+			var dacOrDacExtDeclaration = context.Root.DescendantNodes()
+													 .OfType<ClassDeclarationSyntax>()
+													 .FirstOrDefault();
+			dacOrDacExtDeclaration.Should().NotBeNull();
+
+			INamedTypeSymbol dacOrDacExtSymbol = context.SemanticModel.GetDeclaredSymbol(dacOrDacExtDeclaration);
+			dacOrDacExtDeclaration.Should().NotBeNull();
+
+			var dacModel = DacSemanticModel.InferModel(context.PXContext, dacOrDacExtSymbol, cancellation);
+			dacModel.Should().NotBeNull();
+
+			return Task.FromResult(dacModel!);
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Dac/Sources/DacFields_OnlyBqlFields.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PX.Data;
+using PX.Objects.AP;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Dac.Sources
+{
+	[PXHidden]
+	public class APInvoice2 : APInvoice
+	{
+		public new abstract class docType : PX.Data.BQL.BqlString.Field<docType> { }
+		public new abstract class refNbr : PX.Data.BQL.BqlString.Field<refNbr> { }
+		public new abstract class noteID : PX.Data.BQL.BqlGuid.Field<noteID> { }
+	}
+}
