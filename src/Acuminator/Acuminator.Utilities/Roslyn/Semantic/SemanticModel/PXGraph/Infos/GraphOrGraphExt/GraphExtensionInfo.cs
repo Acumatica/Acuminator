ad86692c80d70d06a87969ce8fc13ac64a39add8
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-16 01:51:19 +0200

    ATR-821: added graph infos

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Infos/GraphOrGraphExt/GraphExtensionInfo.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
+{
+	public class GraphExtensionInfo : GraphOrGraphExtInfoBase
+	{ 
+		public GraphExtensionInfo(ClassDeclarationSyntax? node, INamedTypeSymbol dac, int declarationOrder) :
+							 base(node, dac, declarationOrder)
+		{
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Infos/GraphOrGraphExt/GraphInfo.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
+{
+	public class GraphInfo : GraphOrGraphExtInfoBase
+	{ 
+		public GraphInfo(ClassDeclarationSyntax? node, INamedTypeSymbol dac, int declarationOrder) :
+					base(node, dac, declarationOrder)
+		{
+		}
+	}
+}
