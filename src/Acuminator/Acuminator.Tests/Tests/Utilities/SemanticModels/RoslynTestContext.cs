dbec86433059b058bfb4a5a49d06df487abc1e31
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-15 10:58:24 +0200

    ATR-821: added new base class for semantic model tests

---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/RoslynTestContext.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels
+{
+	public readonly record struct RoslynTestContext(Document Document, SemanticModel SemanticModel, SyntaxNode Root, PXContext PXContext){ }
+}
