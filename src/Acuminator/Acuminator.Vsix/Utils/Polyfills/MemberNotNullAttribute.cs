b9e02e2daf48f96db1a9c46117e2b44a989de0ce
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-20 21:44:09 +0200

    ATR-821: added MemberNotNullWhenAttribute and NotNullIfNotNullAttribute polyfills to VSIX project, added license to the MemberNotNullAttribute polyfill

--- src/Acuminator/Acuminator.Vsix/Utils/Polyfills/MemberNotNullAttribute.cs
+++ src/Acuminator/Acuminator.Vsix/Utils/Polyfills/MemberNotNullAttribute.cs
@@ -1,4 +1,9 @@
-﻿namespace System.Diagnostics.CodeAnalysis
+﻿#nullable enable
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Diagnostics.CodeAnalysis
 {
 	/// <summary>
 	/// Specifies that the method or property will ensure that the listed field and property members have not-null values.
