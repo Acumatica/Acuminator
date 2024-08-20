9447c88b310ef5eda63fce5a0c2c6f1fc81a185e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-20 23:31:10 +0200

    ATR-821: added new tests for DAC model

--- src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Dac/DacSemanticModelTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Dac/DacSemanticModelTests.cs
@@ -8,29 +8,50 @@ using System.Threading.Tasks;
 
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 using Xunit;
+
 using FluentAssertions;
-using Acuminator.Utilities.Roslyn.Semantic.Dac;
 
 namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Dac
 {
 	public class DacSemanticModelTests : SemanticModelTestsBase<DacSemanticModel>
 	{
 		[Theory]
-		[EmbeddedFileData("DacFields_OnlyBqlFields.cs")]
-		public async Task Test_DacFields_Recognition(string text)
+		[EmbeddedFileData("RegularDac.cs")]
+		public async Task RegularDac__DacFields_Recognition(string text)
 		{
 			var dacSemanticModel = await PrepareSemanticModelAsync(text).ConfigureAwait(false);
 
-			dacSemanticModel.DacFieldsByNames.Should().HaveCountGreaterThan(3);		// Check that fields from base types from PX.Objects were collected
-			dacSemanticModel.DacFieldsByNames.Should().ContainKey("docType");
-			dacSemanticModel.DacFieldsByNames.Should().ContainKey("refNbr");
-			dacSemanticModel.DacFieldsByNames.Should().ContainKey("noteID");
+			// Check that fields from base types from PX.Objects were collected
+			TestDacFields(dacSemanticModel, ["OrderType", "OrderNbr", "Status", "Tstamp"], fieldsCount: 4);
+
+			foreach (var dacField in dacSemanticModel.DacFields)
+			{
+				dacField.BqlFieldInfo.Should().NotBeNull();
+				dacField.PropertyInfo.Should().NotBeNull();
+			}
+		}
+
+		[Theory]
+		[EmbeddedFileData("DacFields_OnlyBqlFields.cs")]
+		public async Task Dac_DerivedFromExternalDac_DacFields_Recognition(string text)
+		{
+			var dacSemanticModel = await PrepareSemanticModelAsync(text).ConfigureAwait(false);
+
+			// Check that fields from base types from PX.Objects were collected
+			TestDacFields(dacSemanticModel, ["docType", "refNbr", "noteID"], minFieldsCount: 4);
+
+			foreach (var dacField in dacSemanticModel.DeclaredDacFields)
+			{
+				dacField.BqlFieldInfo.Should().NotBeNull();
+				dacField.PropertyInfo.Should().BeNull();
+			}
 		}
 
 		protected override Task<DacSemanticModel> PrepareSemanticModelAsync(RoslynTestContext context, CancellationToken cancellation = default)
@@ -47,6 +68,25 @@ namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Dac
 			dacModel.Should().NotBeNull();
 
 			return Task.FromResult(dacModel!);
+		}
+
+		private void TestDacFields(DacSemanticModel dacModel, string[]? requiredDacFields = null, int? minFieldsCount = null,
+									int? maxFieldsCount = null, int? fieldsCount = null)
+		{
+			if (minFieldsCount.HasValue)
+				dacModel.DacFieldsByNames.Should().HaveCountGreaterOrEqualTo(minFieldsCount.Value);
+
+			if (maxFieldsCount.HasValue)
+				dacModel.DacFieldsByNames.Should().HaveCountLessOrEqualTo(maxFieldsCount.Value);
+
+			if (fieldsCount.HasValue)
+				dacModel.DacFieldsByNames.Should().HaveCount(fieldsCount.Value);
+
+			if (requiredDacFields != null)
+			{
+				foreach (var requiredDacField in requiredDacFields)
+					dacModel.DacFieldsByNames.Should().ContainKey(requiredDacField);
+			}
 		}
 	}
 }
