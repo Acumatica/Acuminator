78d35e6ab8ae20ec7752521878a85e688f69b388
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-23 08:42:00 +0200

    ATR-8: added source for code fix test for PX1065

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoBqlFieldForDacFieldProperty/Sources/DacWithoutBqlFields_Expected.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoBqlFieldForDacFieldProperty/Sources/DacWithoutBqlFields_Expected.cs
@@ -2,59 +2,121 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using PX.Data;
 
 namespace PX.Analyzers.Test.Sources
 {
-	public class BadDac : IBqlTable
+	[PXHidden]
+	public class DacWithoutBqlFields : IBqlTable
 	{
-		public abstract class legacyBoolField : PX.Data.BQL.BqlBool.Field<legacyBoolField> { }
-		[PXBool]
-		public bool? LegacyBoolField { get; set; }
+		public abstract class boolField : PX.Data.BQL.BqlBool.Field<boolField> { }
 
-		public abstract class legacyByteField : PX.Data.BQL.BqlByte.Field<legacyByteField> { }
-		[PXByte]
-		public Byte? LegacyByteField { get; set; }
+		[PXDBBool]
+		public bool? BoolField { get; set; }
 
-		public abstract class legacyShortField : PX.Data.BQL.BqlShort.Field<legacyShortField> { }
+		public abstract class boolField2 : PX.Data.BQL.BqlBool.Field<boolField2> { }
+
+		[PXDBBool]
+		public Boolean? BoolField2 { get; set; }
+
+		public abstract class byteField : PX.Data.BQL.BqlByte.Field<byteField> { }
+
+		[PXDBByte]
+		public Byte? ByteField { get; set; }
+
+		public abstract class byteField2 : PX.Data.BQL.BqlByte.Field<byteField2> { }
+
+		[PXDBByte]
+		public byte? ByteField2 { get; set; }
+
+		public abstract class shortField : PX.Data.BQL.BqlShort.Field<shortField> { }
+
 		[PXShort]
-		public short? LegacyShortField { get; set; }
+		public short? ShortField { get; set; }
 
-		public abstract class legacyIntField : PX.Data.BQL.BqlInt.Field<legacyIntField> { }
+		public abstract class shortField2 : PX.Data.BQL.BqlShort.Field<shortField2> { }
+
+		[PXShort]
+		public Int16? ShortField2 { get; set; }
+
+		public abstract class intField : PX.Data.BQL.BqlInt.Field<intField> { }
+
 		[PXInt]
-		public Int32? LegacyIntField { get; set; }
+		public Int32? IntField { get; set; }
 
-		public abstract class legacyLongField : PX.Data.BQL.BqlLong.Field<legacyLongField> { }
+		public abstract class intField2 : PX.Data.BQL.BqlInt.Field<intField2> { }
+
+		[PXInt]
+		public int? IntField2 { get; set; }
+
+		public abstract class longField : PX.Data.BQL.BqlLong.Field<longField> { }
+
 		[PXLong]
-		public long? LegacyLongField { get; set; }
+		public long? LongField { get; set; }
 
-		public abstract class legacyFloatField : PX.Data.BQL.BqlFloat.Field<legacyFloatField> { }
+		public abstract class longField2 : PX.Data.BQL.BqlLong.Field<longField2> { }
+
+		[PXLong]
+		public Int64? LongField2 { get; set; }
+
+		public abstract class floatField : PX.Data.BQL.BqlFloat.Field<floatField> { }
+
 		[PXFloat]
-		public Single? LegacyFloatField { get; set; }
+		public Single? FloatField { get; set; }
 
-		public abstract class legacyDoubleField : PX.Data.BQL.BqlDouble.Field<legacyDoubleField> { }
+		public abstract class floatField2 : PX.Data.BQL.BqlFloat.Field<floatField2> { }
+
+		[PXFloat]
+		public float? FloatField2 { get; set; }
+
+		public abstract class doubleField : PX.Data.BQL.BqlDouble.Field<doubleField> { }
+
 		[PXDouble]
-		public double? LegacyDoubleField { get; set; }
+		public double? DoubleField { get; set; }
 
-		public abstract class legacyDecimalField : PX.Data.BQL.BqlDecimal.Field<legacyDecimalField> { }
+		public abstract class doubleField2 : PX.Data.BQL.BqlDouble.Field<doubleField2> { }
+
+		[PXDouble]
+		public Double? DoubleField2 { get; set; }
+
+		public abstract class decimalField : PX.Data.BQL.BqlDecimal.Field<decimalField> { }
+
 		[PXDecimal]
-		public Decimal? LegacyDecimalField { get; set; }
+		public Decimal? DecimalField { get; set; }
+		#region DecimalField2
+		public abstract class decimalField2 : PX.Data.BQL.BqlDecimal.Field<decimalField2> { }
 
-		public abstract class legacyStringField : PX.Data.BQL.BqlString.Field<legacyStringField> { }
+		[PXDecimal]
+		public decimal? DecimalField2 { get; set; }
+		#endregion
+		public abstract class stringField : PX.Data.BQL.BqlString.Field<stringField> { }
 		[PXString]
-		public string LegacyStringField { get; set; }
+		public string StringField { get; set; }
+		#region StringField2
+		public abstract class stringField2 : PX.Data.BQL.BqlString.Field<stringField2> { }
 
-		public abstract class legacyDateField : PX.Data.BQL.BqlDateTime.Field<legacyDateField> { }
+		[PXString]
+		public String StringField2 { get; set; }
+		#endregion
+		#region DateField
+		public abstract class dateField : PX.Data.BQL.BqlDateTime.Field<dateField> { }
 		[PXDate]
-		public DateTime? LegacyDateField { get; set; }
+		public DateTime? DateField { get; set; }
+		#endregion
+		public abstract class guidField : PX.Data.BQL.BqlGuid.Field<guidField> { }
 
-		public abstract class legacyGuidField : PX.Data.BQL.BqlGuid.Field<legacyGuidField> { }
 		[PXGuid]
-		public Guid? LegacyGuidField { get; set; }
+		public Guid? GuidField { get; set; }
 
-		public abstract class legacyBinaryField : PX.Data.BQL.BqlByteArray.Field<legacyBinaryField> { }
+		public abstract class binaryField : PX.Data.BQL.BqlByteArray.Field<binaryField> { }
+
 		[PXDBBinary]
-		public byte[] LegacyBinaryField { get; set; }
+		public byte[] BinaryField { get; set; }
+		#region BinaryField2
+		public abstract class binaryField2 : PX.Data.BQL.BqlByteArray.Field<binaryField2> { }
+
+		[PXDBBinary]
+		public Byte[] BinaryField2 { get; set; }
+		#endregion
 	}
 }
