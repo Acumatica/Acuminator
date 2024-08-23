f3fa1b9550175cd49a32a7266db6990ec836d42c
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-23 03:17:47 +0200

    ATR-8: refactoring - renamed BQL fields tests

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LegacyBqlField/LegacyBqlFieldTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LegacyBqlField/LegacyBqlFieldTests.cs
@@ -20,27 +20,29 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.LegacyBqlField
 	{
 		[Theory]
 		[EmbeddedFileData("LegacyBqlFieldGood.cs")]
-		public async Task TestDiagnostic_Good(string actual) => await VerifyCSharpDiagnosticAsync(actual);
+		public async Task Dac_WithTypedBqlFields_ShouldNotShowDiagnostic(string actual) => await VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
 		[EmbeddedFileData("LegacyBqlFieldBad.cs")]
-		public async Task TestDiagnostic_Bad(string actual) => await VerifyCSharpDiagnosticAsync(actual,
-			Descriptors.PX1060_LegacyBqlField.CreateFor(12, 25, "legacyBoolField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(16, 25, "legacyByteField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(20, 25, "legacyShortField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(24, 25, "legacyIntField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(28, 25, "legacyLongField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(32, 25, "legacyFloatField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(36, 25, "legacyDoubleField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(40, 25, "legacyDecimalField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(44, 25, "legacyStringField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(48, 25, "legacyDateField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(52, 25, "legacyGuidField"),
-			Descriptors.PX1060_LegacyBqlField.CreateFor(56, 25, "legacyBinaryField"));
+		public async Task Dac_WithLegacyBqlFields(string actual) => 
+			await VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1060_LegacyBqlField.CreateFor(12, 25, "legacyBoolField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(16, 25, "legacyByteField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(20, 25, "legacyShortField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(24, 25, "legacyIntField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(28, 25, "legacyLongField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(32, 25, "legacyFloatField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(36, 25, "legacyDoubleField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(40, 25, "legacyDecimalField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(44, 25, "legacyStringField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(48, 25, "legacyDateField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(52, 25, "legacyGuidField"),
+				Descriptors.PX1060_LegacyBqlField.CreateFor(56, 25, "legacyBinaryField"));
 
 		[Theory]
 		[EmbeddedFileData("LegacyBqlFieldBad.cs", "LegacyBqlFieldBad_Expected.cs")]
-		public async Task TestCodeFix(string actual, string expected) => await VerifyCSharpFixAsync(actual, expected);
+		public async Task Test_UpgradeBqlFields_CodeFix(string actual, string expected) => 
+			await VerifyCSharpFixAsync(actual, expected);
 
 		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => 
 			new DacAnalyzersAggregator(
