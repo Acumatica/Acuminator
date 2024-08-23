a5d47752fb1b17c8f1ee92c245a28eed579c50b4
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-23 04:10:31 +0200

    ATR-8: added unit tests for the diagnostic for missing BQL fields

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/NoBqlFieldForDacFieldProperty/NoBqlFieldForDacFieldPropertyTests.cs
@@ -0,0 +1,65 @@
+﻿
+#nullable enable
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.NoBqlFieldForDacFieldProperty;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.NoBqlFieldForDacFieldProperty
+{
+	public class NoBqlFieldForDacFieldPropertyTests : CodeFixVerifier
+	{
+		[Theory]
+		[EmbeddedFileData("DacWithoutBqlFields_Expected.cs")]
+		public async Task RegularDac_WithBqlFields_ShouldNotShowDiagnostic(string actual) => 
+			await VerifyCSharpDiagnosticAsync(actual);
+
+		[Theory]
+		[EmbeddedFileData("DacWithoutBqlFields.cs")]
+		public async Task RegularDac_WithoutBqlFields(string actual) => await VerifyCSharpDiagnosticAsync(actual,
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(13, 16, "BoolField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(16, 19, "BoolField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(19, 16, "ByteField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(22, 16, "ByteField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(25, 17, "ShortField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(28, 17, "ShortField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(31, 17, "IntField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(34, 15, "IntField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(37, 16, "LongField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(40, 17, "LongField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(43, 18, "FloatField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(46, 17, "FloatField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(49, 18, "DoubleField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(52, 18, "DoubleField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(55, 19, "DecimalField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(59, 19, "DecimalField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(62, 17, "StringField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(66, 17, "StringField2"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(70, 20, "DateField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(74, 16, "GuidField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(77, 17, "BinaryField"),
+			Descriptors.PX1065_NoBqlFieldForDacFieldProperty.CreateFor(81, 17, "BinaryField2"));
+
+		[Theory]
+		[EmbeddedFileData("DacWithoutBqlFields.cs", "DacWithoutBqlFields_Expected.cs")]
+		public async Task TestCodeFix(string actual, string expected) => 
+			await VerifyCSharpFixAsync(actual, expected);
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => 
+			new DacAnalyzersAggregator(
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled(),
+				new NoBqlFieldForDacFieldAnalyzer());
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new NoBqlFieldForDacFieldAnalyzerFix();
+	}
+}
