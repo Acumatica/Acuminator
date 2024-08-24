4519d51324a9f24e6fed30f8ff2b0f11a90467cc
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-24 15:06:24 +0200

    ATR-8: added base DAC to external dependency

--- src/Acuminator/Acuminator.Tests/ExternalDependency/src/NoBqlFieldForDacFieldProperty/BaseDacWithoutBqlField.cs
+++ src/Acuminator/Acuminator.Tests/ExternalDependency/src/NoBqlFieldForDacFieldProperty/BaseDacWithoutBqlField.cs
@@ -4,9 +4,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using PX.Data;
+
 namespace ExternalDependency.NoBqlFieldForDacFieldProperty
 {
-    public class BaseDacWithoutBqlField
+	[PXHidden]
+	public class BaseDacWithoutBqlField : IBqlTable
     {
-    }
+		[PXHidden]
+		public class BaseDac : IBqlTable
+		{
+			#region Status
+			public abstract class status : PX.Data.BQL.BqlString.Field<status> { }
+			#endregion
+
+			[PXInt]
+			public virtual int? ShipmentNbr { get; set; }
+
+			public virtual string? ExtraData { get; set; }
+		}
+	}
 }
