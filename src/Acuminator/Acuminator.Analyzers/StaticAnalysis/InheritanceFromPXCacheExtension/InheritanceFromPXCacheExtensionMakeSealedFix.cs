f194769a4a127a150ef3eebd889244f1384bb288
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-25 00:25:54 +0200

    ATR-823: reworked PX1011 code fix + fixed nullable warnings

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/InheritanceFromPXCacheExtension/InheritanceFromPXCacheExtensionMakeSealedFix.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/InheritanceFromPXCacheExtension/InheritanceFromPXCacheExtensionMakeSealedFix.cs
@@ -1,6 +1,6 @@
-﻿
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Composition;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
@@ -21,21 +21,35 @@ namespace Acuminator.Analyzers.StaticAnalysis.InheritanceFromPXCacheExtension
 
 		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
 		{
+			context.CancellationToken.ThrowIfCancellationRequested();
+
 			var root = await context.Document.GetSyntaxRootAsync().ConfigureAwait(false);
-			var node = root.FindNode(context.Span).FirstAncestorOrSelf<ClassDeclarationSyntax>();
+			var dacExtNode = root?.FindNode(context.Span).FirstAncestorOrSelf<ClassDeclarationSyntax>();
 			
-			if (node != null)
+			if (dacExtNode != null)
 			{	
 				string title = nameof(Resources.PX1011Fix).GetLocalized().ToString();
-				context.RegisterCodeFix(CodeAction.Create(title, async c =>
-					{
-						var newRoot = await context.Document.GetSyntaxRootAsync(c);
-						var newNode = node.AddModifiers(SyntaxFactory.Token(SyntaxKind.SealedKeyword));
-						newRoot = newRoot.ReplaceNode(node, newNode);
-						return context.Document.WithSyntaxRoot(newRoot);
-					}, title),
-					context.Diagnostics);
+				var document = context.Document;
+				var codeAction = CodeAction.Create(title,
+												cancellation => MakeDacExtensionSealedAsync(document, root!, dacExtNode, cancellation),
+												equivalenceKey: title);
+
+				context.RegisterCodeFix(codeAction, context.Diagnostics);
 			}
+		}
+
+		private static Task<Document> MakeDacExtensionSealedAsync(Document document, SyntaxNode root, ClassDeclarationSyntax dacExtNode, 
+																  CancellationToken cancellation)
+		{
+			cancellation.ThrowIfCancellationRequested();
+
+			var newNode = dacExtNode.AddModifiers(SyntaxFactory.Token(SyntaxKind.SealedKeyword));
+			var newRoot = root.ReplaceNode(dacExtNode, newNode);
+			var modifiedDocument = document.WithSyntaxRoot(newRoot);
+
+			cancellation.ThrowIfCancellationRequested();
+
+			return Task.FromResult(modifiedDocument);
 		}
 	}
 }
