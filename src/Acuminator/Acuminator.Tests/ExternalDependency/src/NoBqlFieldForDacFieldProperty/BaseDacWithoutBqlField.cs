faf74af020b4f777ca610616bb9711b8304597a3
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-24 15:00:38 +0200

    ATR-8: added new internal solution for external dependencies to tests.
    This solution should be used to test Acuminator data collection and analysis from the external dll

---
+++ src/Acuminator/Acuminator.Tests/ExternalDependency/src/NoBqlFieldForDacFieldProperty/BaseDacWithoutBqlField.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExternalDependency.NoBqlFieldForDacFieldProperty
+{
+    public class BaseDacWithoutBqlField
+    {
+    }
+}
