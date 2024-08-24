95b42dcd3e90aedf16dc47c4017ebebc2ad6a68e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-24 23:55:51 +0200

    ATR-823: reworked PX1014 code fix + fixed nulalble warnings

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NonNullableTypeForBqlField/NonNullableTypeForBqlFieldFix.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NonNullableTypeForBqlField/NonNullableTypeForBqlFieldFix.cs
@@ -1,53 +1,20 @@
-﻿
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-
-using Acuminator.Utilities.Roslyn;
-using Acuminator.Utilities.Roslyn.Constants;
-using Acuminator.Utilities.Roslyn.Semantic;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Editing;
 
 namespace Acuminator.Analyzers.StaticAnalysis.NonNullableTypeForBqlField
 {
 	[ExportCodeFixProvider(LanguageNames.CSharp), Shared]
 	public class NonNullableTypeForBqlFieldFix : CodeFixProvider
 	{
-		private class Rewriter : CSharpSyntaxRewriter
-		{
-			private readonly PXContext _pxContext;
-			private readonly Document _document;
-			private readonly SemanticModel _semanticModel;
-
-			public Rewriter(PXContext pxContext, Document document, SemanticModel semanticModel)
-			{
-				_pxContext = pxContext;
-				_document = document;
-				_semanticModel = semanticModel;
-			}
-
-			public override SyntaxNode VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
-			{
-				var generator = SyntaxGenerator.GetGenerator(_document);
-				var typeSymbol = _semanticModel.GetSymbolInfo(node.Type).Symbol as ITypeSymbol;
-				if (typeSymbol != null)
-				{
-					return generator.InvocationExpression(
-						generator.MemberAccessExpression(
-							generator.TypeExpression(_pxContext.PXGraph.Type),
-							generator.GenericName(DelegateNames.CreateInstance, typeSymbol)));
-				}
-
-				return base.VisitObjectCreationExpression(node);
-			}
-		}
-
 		public override ImmutableArray<string> FixableDiagnosticIds { get; } =
 			ImmutableArray.Create(Descriptors.PX1014_NonNullableTypeForBqlField.Id);
 
@@ -55,16 +22,42 @@ namespace Acuminator.Analyzers.StaticAnalysis.NonNullableTypeForBqlField
 
 		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
 		{
-			var root = await context.Document.GetSyntaxRootAsync().ConfigureAwait(false);
-			var node = root.FindNode(context.Span) as PropertyDeclarationSyntax;
+			context.CancellationToken.ThrowIfCancellationRequested();
 
-			if (node != null)
-			{
-				string title = nameof(Resources.PX1014Fix).GetLocalized().ToString();
-				context.RegisterCodeFix(CodeAction.Create(title, 
-					c => Task.FromResult(context.Document.WithSyntaxRoot(root.ReplaceNode(node.Type, SyntaxFactory.NullableType(node.Type)))), title),
-					context.Diagnostics);
-			}
+			var diagnostic = context.Diagnostics.FirstOrDefault(d => FixableDiagnosticIds.Contains(d.Id));
+
+			if (diagnostic == null)
+				return;
+
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+			if (root?.FindNode(context.Span) is not PropertyDeclarationSyntax propertyNode)
+				return;
+
+			string title = nameof(Resources.PX1014Fix).GetLocalized().ToString();
+			Document document = context.Document;
+			var codeAction = CodeAction.Create(title,
+											   cancellation => MakePropertyTypeNullable(document, root, propertyNode, cancellation),
+											   equivalenceKey: title);
+
+			context.RegisterCodeFix(codeAction, context.Diagnostics);
+		}
+
+		private static Task<Document> MakePropertyTypeNullable(Document document, SyntaxNode root, PropertyDeclarationSyntax propertyNode,
+																CancellationToken cancellation)
+		{
+			cancellation.ThrowIfCancellationRequested();
+
+			var nullableTypeNode = SyntaxFactory.NullableType(propertyNode.Type);
+			var modifiedRoot = root.ReplaceNode(propertyNode.Type, nullableTypeNode);
+
+			if (modifiedRoot == null)
+				return Task.FromResult(document);
+
+			cancellation.ThrowIfCancellationRequested();
+
+			var modifiedDocument = document.WithSyntaxRoot(modifiedRoot);
+			return Task.FromResult(modifiedDocument);
 		}
 	}
 }
