4322a51259da4eb3f08fd32656038aa9879ad675
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-25 06:15:51 +0200

    ATR-8: fixed DAC in the external dependency

--- src/Acuminator/Acuminator.Tests/ExternalDependency/src/NoBqlFieldForDacFieldProperty/BaseDacWithoutBqlField.cs
+++ src/Acuminator/Acuminator.Tests/ExternalDependency/src/NoBqlFieldForDacFieldProperty/BaseDacWithoutBqlField.cs
@@ -11,17 +11,13 @@ namespace ExternalDependency.NoBqlFieldForDacFieldProperty
 	[PXHidden]
 	public class BaseDacWithoutBqlField : IBqlTable
     {
-		[PXHidden]
-		public class BaseDac : IBqlTable
-		{
-			#region Status
-			public abstract class status : PX.Data.BQL.BqlString.Field<status> { }
-			#endregion
+		#region Status
+		public abstract class status : PX.Data.BQL.BqlString.Field<status> { }
+		#endregion
 
-			[PXInt]
-			public virtual int? ShipmentNbr { get; set; }
+		[PXInt]
+		public virtual int? ShipmentNbr { get; set; }
 
-			public virtual string? ExtraData { get; set; }
-		}
+		public virtual string? ExtraData { get; set; }
 	}
 }
