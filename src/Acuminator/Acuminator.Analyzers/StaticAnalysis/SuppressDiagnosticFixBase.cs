7ff78bc87a30ac9090e2023f934daa48dac78dbd
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-25 17:30:15 +0200

    ATR-823: returned grouping code action factory since it allows to set custom priority to code action

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/SuppressDiagnosticFixBase.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/SuppressDiagnosticFixBase.cs
@@ -102,7 +102,13 @@ namespace Acuminator.Analyzers.StaticAnalysis
 				suppressionCodeActions.Add(suppressWithSuppressionFileCodeAction);
 			}
 
-			return CodeAction.Create(groupCodeActionName, suppressionCodeActions.ToImmutable(), isInlinable: false);
+			// Use reflection based factory to create group code action with nested actions and custom priority.
+			// Setting code action priority is impossible via Roslyn public API.
+			var groupCodeAction = 
+				CodeActionWithNestedActionsFabric.CreateCodeActionWithNestedActions(groupCodeActionName, suppressionCodeActions.ToImmutable()) ??
+				CodeAction.Create(groupCodeActionName, suppressionCodeActions.ToImmutable(), isInlinable: false);
+
+			return groupCodeAction;
 		}
 
 		protected virtual CodeAction GetSuppressWithCommentCodeAction(Diagnostic diagnostic, CodeFixContext context, bool isNested)
