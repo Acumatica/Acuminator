052eac361dd30e3d4bf7a306e67deedb45d91ee4
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-27 00:32:40 +0200

    ATR-11: changed PX1066 severity to warning

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/Descriptors.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/Descriptors.cs
@@ -358,7 +358,7 @@ namespace Acuminator.Analyzers.StaticAnalysis
 			Rule("PX1065", nameof(Resources.PX1065TitleFormat).GetLocalized(), Category.Acuminator, DiagnosticSeverity.Error, DiagnosticsShortName.PX1065);
 
 		public static DiagnosticDescriptor PX1066_TypoInBqlFieldName { get; } =
-			Rule("PX1066", nameof(Resources.PX1066TitleFormat).GetLocalized(), Category.Acuminator, DiagnosticSeverity.Error, DiagnosticsShortName.PX1066);
+			Rule("PX1066", nameof(Resources.PX1066TitleFormat).GetLocalized(), Category.Acuminator, DiagnosticSeverity.Warning, DiagnosticsShortName.PX1066);
 
 		public static DiagnosticDescriptor PX1070_UiPresentationLogicInEventHandlers { get; } =
 			Rule("PX1070", nameof(Resources.PX1070Title).GetLocalized(), Category.Acuminator, DiagnosticSeverity.Error, DiagnosticsShortName.PX1070);
