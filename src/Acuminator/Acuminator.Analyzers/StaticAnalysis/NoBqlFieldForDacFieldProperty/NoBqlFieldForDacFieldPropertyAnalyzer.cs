836941336e8ee48e19c7a256fe0d63c2ac00ab07
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-27 01:08:33 +0200

    ATR-11:  reworked PX1065 and PX1066 analyzers

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NoBqlFieldForDacFieldProperty/NoBqlFieldForDacFieldPropertyAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/NoBqlFieldForDacFieldProperty/NoBqlFieldForDacFieldPropertyAnalyzer.cs
@@ -36,48 +36,110 @@ namespace Acuminator.Analyzers.StaticAnalysis.NoBqlFieldForDacFieldProperty
 				dacOrDacExt.DeclaredDacFields.Where(dacField => !dacField.HasFieldPropertyEffective)
 											 .Select(dacField => dacField.BqlFieldInfo!)
 											 .ToList();
-
+			
+			// Check every DAC field with a property to report DAC fields with missing BQL fields,
+			// and at the same time match declared BQL fields with properties against each DAC field with a property for possible typos
 			foreach (var dacField in dacOrDacExt.DacFields)
 			{
 				symbolContext.CancellationToken.ThrowIfCancellationRequested();
 
-				if (!dacField.HasBqlFieldEffective && dacField.HasAcumaticaAttributes)
-					AnalyzeDacPropertyWithoutBqlField(symbolContext, pxContext, dacOrDacExt, dacField, declaredBqlFieldsWithWithoutProperty);
-			}
-		}
+				//Skip DAC fields without properties or without Acumatica attributes
+				if (!dacField.HasFieldPropertyEffective || !dacField.HasAcumaticaAttributes)
+					continue;
 
-		private void AnalyzeDacPropertyWithoutBqlField(SymbolAnalysisContext symbolContext, PXContext pxContext, DacSemanticModel dacOrDacExt,
-													   DacFieldInfo dacFieldWithoutBqlField, List<DacBqlFieldInfo> declaredBqlFieldsWithWithoutProperty)
-		{
-			var bqlFieldsWithTypo = FindBqlFieldInfosWithinTypoDistance(dacFieldWithoutBqlField, dacOrDacExt, declaredBqlFieldsWithWithoutProperty, 
-																		symbolContext.CancellationToken);
-			if (bqlFieldsWithTypo.Count == 0)
-			{
-				ReportDacPropertyWithoutBqlField(symbolContext, pxContext, dacOrDacExt, dacFieldWithoutBqlField);
-				return;
-			}
-			else
-			{
-				foreach (var bqlFieldWithTypo in bqlFieldsWithTypo)
+				if (dacField.IsDeclaredInType(dacOrDacExt.Symbol))
 				{
-					symbolContext.CancellationToken.ThrowIfCancellationRequested();
-					ReportBqlFieldWithTypo(symbolContext, pxContext, bqlFieldWithTypo, dacFieldWithoutBqlField.Name);
+					AnalyzeDacPropertyDeclaredInThisType(symbolContext, pxContext, dacOrDacExt, dacField, declaredBqlFieldsWithWithoutProperty);
+				}
+				else
+				{
+					AnalyzeDacPropertyDeclaredInBaseTypes(symbolContext, pxContext, dacOrDacExt, dacField, declaredBqlFieldsWithWithoutProperty);
 				}
 			}
 		}
 
-		private List<DacBqlFieldInfo> FindBqlFieldInfosWithinTypoDistance(DacFieldInfo dacFieldWithoutBqlField, DacSemanticModel dacOrDacExt,
-																		  List<DacBqlFieldInfo> declaredBqlFieldsWithWithoutProperty,
+		private void AnalyzeDacPropertyDeclaredInThisType(SymbolAnalysisContext symbolContext, PXContext pxContext, DacSemanticModel dacOrDacExt,
+														  DacFieldInfo declaredDacField, List<DacBqlFieldInfo> declaredBqlFieldsWithWithoutProperty)
+		{
+			// if the declared DAC field has a BQL field declared in the same type then skip it
+			if (declaredDacField.HasBqlFieldDeclared)
+				return;
+
+			// if the declared DAC field does not have a BQL field then report it with PX1065 as missing BQL field
+			if (!declaredDacField.HasBqlFieldEffective)
+				ReportDacPropertyWithoutBqlField(symbolContext, pxContext, dacOrDacExt, declaredDacField);
+
+			// Now check declared BQL fields without corresponding properties for ones with names close to the "declaredDacField" name.
+			// 
+			// Since the DAC field "declaredDacField" is declared in "dacOrDacExt" type we need to support two scenarios:
+			// 1. BQL field with a typo is declared in the dacOrDacExt DAC and there is a DAC field property with correct name 
+			// also declared in the dacOrDacExt type that does not have a corresponding BQL field.
+			// 
+			// 2. BQL field with a typo is declared in the dacOrDacExt DAC and there is a DAC field property with correct name
+			// also declared in the dacOrDacExt type that has a corresponding BQL field in dacOrDacExt base types.
+			// It's OK to have a BQL field in this case, since it is in base types, and checked bql fields are in derived types, 
+			// so they would be hiding base BQL field anyway.
+			//
+			// The second scenario only makes sense for DACs, because DAC extensions can't have derived types, 
+			// and it makes little sense to redeclare existing BQL field in the chained DAC extension.
+			if (declaredDacField.HasBqlFieldEffective && dacOrDacExt.DacType != DacType.Dac)
+				return;
+
+			var declaredBqlFieldsWithTypo = FindBqlFieldInfosWithinTypoDistance(declaredDacField.Name, dacOrDacExt, declaredBqlFieldsWithWithoutProperty,
+																		symbolContext.CancellationToken);
+			if (declaredBqlFieldsWithTypo.Count > 0)
+			{
+				foreach (var bqlFieldWithTypo in declaredBqlFieldsWithTypo)
+				{
+					symbolContext.CancellationToken.ThrowIfCancellationRequested();
+					ReportBqlFieldWithTypo(symbolContext, pxContext, bqlFieldWithTypo, declaredDacField.Name);
+				}
+			}
+		}
+
+		private void AnalyzeDacPropertyDeclaredInBaseTypes(SymbolAnalysisContext symbolContext, PXContext pxContext, DacSemanticModel dacOrDacExt,
+														   DacFieldInfo dacPropertyInBaseTypes, List<DacBqlFieldInfo> declaredBqlFieldsWithWithoutProperty)
+		{
+			// If the property is declared in based types and does not have a BQL field then report DAC field with PX1065 as missing BQL field
+			if (!dacPropertyInBaseTypes.HasBqlFieldEffective)
+				ReportDacPropertyWithoutBqlField(symbolContext, pxContext, dacOrDacExt, dacPropertyInBaseTypes);
+
+			// Now check declared BQL fields without corresponding properties for ones with names close to the "dacPropertyInBaseTypes" name.
+			// 
+			// Since the DAC field "dacPropertyInBaseTypes" is declared in dacOrDacExt base types we need to support a scenario 
+			// when BQL field with a typo is declared in the dacOrDacExt DAC and there is a DAC field with correct name in the base types.
+			// It does not matter if that field has a BQL field or not - it is in base types, and checked bql fields are in derived types, 
+			// so they would be hiding base BQL field anyway.
+			//
+			// This scenario only makes sense for DACs, because DAC extensions can't have derived types, 
+			// and it makes little sense to redeclare existing BQL field in the chained DAC extension
+			if (dacOrDacExt.DacType != DacType.Dac)
+				return;
+			
+			var declaredBqlFieldsWithTypo = FindBqlFieldInfosWithinTypoDistance(dacPropertyInBaseTypes.Name, dacOrDacExt, declaredBqlFieldsWithWithoutProperty,
+																				symbolContext.CancellationToken);
+			if (declaredBqlFieldsWithTypo.Count > 0)
+			{
+				foreach (var bqlFieldWithTypo in declaredBqlFieldsWithTypo)
+				{
+					symbolContext.CancellationToken.ThrowIfCancellationRequested();
+					ReportBqlFieldWithTypo(symbolContext, pxContext, bqlFieldWithTypo, dacPropertyInBaseTypes.Name);
+				}
+			}
+		}
+
+		private List<DacBqlFieldInfo> FindBqlFieldInfosWithinTypoDistance(string dacFieldName, DacSemanticModel dacOrDacExt,
+																		  List<DacBqlFieldInfo> declaredBqlFieldsCandidates,
 																		  CancellationToken cancellation)
 		{
-			if (declaredBqlFieldsWithWithoutProperty.Count == 0 || !dacFieldWithoutBqlField.IsDeclaredInType(dacOrDacExt.Symbol))
+			if (declaredBqlFieldsCandidates.Count == 0)
 				return [];
 
-			string propertyName 	 = dacFieldWithoutBqlField.Name.ToLowerInvariant();
+			string propertyName 	 = dacFieldName.ToLowerInvariant();
 			int minDistance			 = int.MaxValue;
 			var nearestBqlFieldInfos = new List<DacBqlFieldInfo>(capacity: 4);
 
-			foreach (var bqlField in declaredBqlFieldsWithWithoutProperty)
+			foreach (var bqlField in declaredBqlFieldsCandidates)
 			{
 				cancellation.ThrowIfCancellationRequested();
 
