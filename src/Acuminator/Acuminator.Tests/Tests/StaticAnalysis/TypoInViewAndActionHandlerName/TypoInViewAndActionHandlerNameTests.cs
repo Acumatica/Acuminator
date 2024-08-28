0474352e4036097b637fe6c2e296f5063bf89e94
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-28 04:26:30 +0200

    ATR-824: refactoring - renamed PX1005 tests and made them async

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/TypoInViewAndActionHandlerName/TypoInViewAndActionHandlerNameTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/TypoInViewAndActionHandlerName/TypoInViewAndActionHandlerNameTests.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable
 
 using System;
+using System.Threading.Tasks;
 
 using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.PXGraph;
@@ -12,6 +13,7 @@ using Acuminator.Utilities;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
+
 using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.TypoInViewAndActionDelegateName
@@ -26,56 +28,48 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.TypoInViewAndActionDelegateName
 
 		[Theory]
         [EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_Good_SameName.cs")] 
-		public void TestDiagnostic_ShouldNotShowDiagnostic_SameName(string actual)
-        {
-            VerifyCSharpDiagnostic(actual);
-        }
+		public Task RegularGraph_ViewAndViewDelegate_HaveSameName_ShouldNotShowDiagnostic(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual);
 
-	    [Theory]
+		[Theory]
 	    [EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_Good_DifferentNames.cs")]
-	    public void TestDiagnostic_ShouldNotShowDiagnostic_DifferentNames(string actual)
-	    {
-		    VerifyCSharpDiagnostic(actual);
-	    }
+	    public Task RegularGraph_ViewAndViewDelegate_HaveTooDifferentNames_ShouldNotShowDiagnostic(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual);
 
-	    [Theory]
+		[Theory]
 	    [EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_Good_Override.cs")]
-	    public void TestDiagnostic_ShouldNotShowDiagnostic_Override(string actual)
-	    {
-		    VerifyCSharpDiagnostic(actual);
-	    }
+	    public Task RegularGraph_ViewDelegateWithOverride_ShouldNotShowDiagnostic(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
         [EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_Bad.cs")]
-        public void TestDiagnostic(string actual)
-        {
-            VerifyCSharpDiagnostic(actual,
+        public Task RegularGraph_TyposInViewDelegate(string actual) =>
+			VerifyCSharpDiagnosticAsync(actual,
 				Descriptors.PX1005_TypoInViewDelegateName.CreateFor(line: 16, column: 22, messageArgs: "Documents"));
-        }
 
-	    [Theory]
+		[Theory]
 	    [EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_Bad.cs",
 						  @"ViewDelegate\TypoInViewDelegateName_Bad_Expected.cs")]
-		public void TestCodeFix(string actual, string expected)
-	    {
-		    VerifyCSharpFix(actual, expected);
-	    }
+		public Task RegularGraph_CodeFix(string actual, string expected) =>
+			VerifyCSharpFixAsync(actual, expected);
 
 		[Theory]
 		[EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_GraphExtension_Bad.cs")]
-		public void TestDiagnostic_GraphExtension(string actual)
-		{
-			VerifyCSharpDiagnostic(actual, 
-				Descriptors.PX1005_TypoInViewDelegateName.CreateFor(line: 25, column: 22, messageArgs: "Documents"));
-		}
+		public Task TestDiagnostic_GraphExtension(string actual) =>
+			 VerifyCSharpDiagnosticAsync(actual,
+				Descriptors.PX1005_TypoInViewDelegateName.CreateFor(line: 23, column: 22, messageArgs: "ViewInBaseGraph"),
+				Descriptors.PX1005_TypoInViewDelegateName.CreateFor(line: 35, column: 22, messageArgs: "Documents"),
+				Descriptors.PX1005_TypoInViewDelegateName.CreateFor(line: 45, column: 22, messageArgs: "ViewInBaseGraph"));
 
 		[Theory]
 		[EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_GraphExtension_Bad_Expected.cs")]
-		public void TestDiagnostic_GraphExtension_ShouldNotShowDiagnostic(string actual) => VerifyCSharpDiagnostic(actual);
+		public Task GraphExtension_ViewDelegateTypos_ShouldNotShowDiagnostic(string actual) => 
+			 VerifyCSharpDiagnosticAsync(actual);
 
 		[Theory]
 		[EmbeddedFileData(@"ViewDelegate\TypoInViewDelegateName_GraphExtension_Bad.cs",
 						  @"ViewDelegate\TypoInViewDelegateName_GraphExtension_Bad_Expected.cs")]
-		public void TestCodeFix_GraphExtension(string actual, string expected) => VerifyCSharpFix(actual, expected);
+		public Task GraphExtension_ViewDelegateTypos_CodeFix_Rename(string actual, string expected) => 
+			VerifyCSharpFixAsync(actual, expected);
 	}
 }
