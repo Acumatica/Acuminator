e84bdde921b31df6788f635d00d5a6c6c7538079
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-31 20:19:40 +0200

    ATR-819: fixed and extended serialization tests

--- src/Acuminator/Acuminator.Tests/Tests/Utilities/CodeAnalysisSettingsSerialization/CodeAnalysisSettingsSerializationTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/CodeAnalysisSettingsSerialization/CodeAnalysisSettingsSerializationTests.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
 
 using Acuminator.Utilities;
 using Acuminator.Utilities.Settings.OutOfProcess;
 using Xunit;
-using FluentAssertions;
-
 
 namespace Acuminator.Tests.Tests.Utilities.CodeAnalysisSettingsSerialization
 {
@@ -20,23 +18,54 @@ namespace Acuminator.Tests.Tests.Utilities.CodeAnalysisSettingsSerialization
 		[InlineData(false, true, false, false, true)]
 		[InlineData(true, false, false, true, true)]
 		public void CheckCodeAnalysisSettingsSerialization(bool recursiveAnalysisEnabled, bool isvSpecificAnalyzersEnabled, bool staticAnalysisEnabled,
-															 bool suppressionMechanismEnabled, bool px1007DocumentationDiagnosticEnabled)
+														   bool suppressionMechanismEnabled, bool px1007DocumentationDiagnosticEnabled)
 		{
-			var expectedSettings = new CodeAnalysisSettings(recursiveAnalysisEnabled, isvSpecificAnalyzersEnabled,
-															staticAnalysisEnabled, suppressionMechanismEnabled, 
-															px1007DocumentationDiagnosticEnabled);
+			var expectedAnalysisSettings = new CodeAnalysisSettings(recursiveAnalysisEnabled, isvSpecificAnalyzersEnabled,
+																	staticAnalysisEnabled, suppressionMechanismEnabled, 
+																	px1007DocumentationDiagnosticEnabled);
+			var bannedApiSettings = BannedApiSettings.Default;
 
 			using var stream = new MemoryStream(capacity: sizeof(bool) * 5 + 20);
 			using var writer = new CodeAnalysisSettingsBinaryWriter(stream);
 
-			writer.WriteCodeAnalysisSettings(expectedSettings);
+			writer.WriteCodeAnalysisSettings(expectedAnalysisSettings, bannedApiSettings);
 
 			stream.Position = 0;
 
 			using var reader = new CodeAnalysisSettingsBinaryReader(stream);
 			var deserializedSettings = reader.ReadCodeAnalysisSettings();
 
-			Assert.Equal(deserializedSettings, expectedSettings);
+			Assert.Equal(deserializedSettings, expectedAnalysisSettings);
+		}
+
+		[Theory]
+		[InlineData(true, true, true, true, true, null, null)]
+		[InlineData(false, false, false, false, false, "", "    ")]
+		[InlineData(false, true, false, false, true, @"C:\bannedApiPath.txt", @"C:\whiteListPath.txt")]
+		[InlineData(true, false, false, true, true, null, @"C:\whiteListPath.txt")]
+		[InlineData(true, false, false, true, true, @"C:\bannedApiPath.txt", null)]
+		public void CheckCodeAnalysisAndBannedApiSettingsSerialization(bool recursiveAnalysisEnabled, bool isvSpecificAnalyzersEnabled,
+																	   bool staticAnalysisEnabled, bool suppressionMechanismEnabled, 
+																	   bool px1007DocumentationDiagnosticEnabled,
+																	   string? bannedApiFilePath, string? whiteListFilePath)
+		{
+			var expectedAnalysisSettings = new CodeAnalysisSettings(recursiveAnalysisEnabled, isvSpecificAnalyzersEnabled,
+																	staticAnalysisEnabled, suppressionMechanismEnabled,
+																	px1007DocumentationDiagnosticEnabled);
+			var expectedBannedApiSettings = new BannedApiSettings(bannedApiFilePath, whiteListFilePath);
+
+			using var stream = new MemoryStream(capacity: sizeof(bool) * 5 + 20);
+			using var writer = new CodeAnalysisSettingsBinaryWriter(stream);
+
+			writer.WriteCodeAnalysisSettings(expectedAnalysisSettings, expectedBannedApiSettings);
+
+			stream.Position = 0;
+
+			using var reader = new CodeAnalysisSettingsBinaryReader(stream);
+			var (deserializedAnalysisSettings, deserializedBannedApiSettings) = reader.ReadAllAnalysisSettings();
+
+			Assert.Equal(deserializedAnalysisSettings, expectedAnalysisSettings);
+			Assert.Equal(deserializedBannedApiSettings, expectedBannedApiSettings);
 		}
 	}
 }
