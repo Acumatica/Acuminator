bac10169391e3c2a744726782befb063450e94dc
Sergey Nikomarov <snikomarov@acumatica.com>
2024-08-31 03:08:08 +0200

    ATR-819: updated code analysis binary writer and reader to support banned API settings

--- src/Acuminator/Acuminator.Utilities/Settings/OutOfProcess/CodeAnalysisSettingsBinaryReader.cs
+++ src/Acuminator/Acuminator.Utilities/Settings/OutOfProcess/CodeAnalysisSettingsBinaryReader.cs
@@ -15,19 +15,36 @@ namespace Acuminator.Utilities.Settings.OutOfProcess
 			_reader = new BinaryReader(stream);
 		}
 
-		public CodeAnalysisSettings ReadCodeAnalysisSettings()
+		public (CodeAnalysisSettings AnalysisSettings, BannedApiSettings BannedApiOptions) ReadAnalysisSettings()
 		{
 			if (_isDisposed)
 				throw new ObjectDisposedException(objectName: nameof(CodeAnalysisSettingsBinaryReader));
 
-			bool recursiveAnalysisEnabled = _reader.ReadBoolean();
-			bool isvSpecificAnalyzersEnabled = _reader.ReadBoolean();
-			bool staticAnalysisEnabled = _reader.ReadBoolean();
-			bool suppressionMechanismEnabled = _reader.ReadBoolean();
+			var codeAnalysisSettings = ReadCodeAnalysisSettings();
+			var bannedApiSetings = ReadBannedApiSettings();
+
+			return (codeAnalysisSettings, bannedApiSetings);
+		}
+
+		private CodeAnalysisSettings ReadCodeAnalysisSettings()
+		{
+			bool recursiveAnalysisEnabled 			  = _reader.ReadBoolean();
+			bool isvSpecificAnalyzersEnabled 		  = _reader.ReadBoolean();
+			bool staticAnalysisEnabled 				  = _reader.ReadBoolean();
+			bool suppressionMechanismEnabled 		  = _reader.ReadBoolean();
 			bool px1007DocumentationDiagnosticEnabled = _reader.ReadBoolean();
 
 			return new CodeAnalysisSettings(recursiveAnalysisEnabled, isvSpecificAnalyzersEnabled,
-											staticAnalysisEnabled, suppressionMechanismEnabled, px1007DocumentationDiagnosticEnabled);
+											staticAnalysisEnabled, suppressionMechanismEnabled,
+											px1007DocumentationDiagnosticEnabled);
+		}
+
+		private BannedApiSettings ReadBannedApiSettings()
+		{
+			string bannedApiFilePath = _reader.ReadString();
+			string whiteListApiFilePath = _reader.ReadString();
+
+			return new BannedApiSettings(bannedApiFilePath, whiteListApiFilePath);
 		}
 
 		public void Dispose()
--- src/Acuminator/Acuminator.Utilities/Settings/OutOfProcess/CodeAnalysisSettingsBinaryWriter.cs
+++ src/Acuminator/Acuminator.Utilities/Settings/OutOfProcess/CodeAnalysisSettingsBinaryWriter.cs
@@ -17,18 +17,34 @@ namespace Acuminator.Utilities.Settings.OutOfProcess
 			_writer = new BinaryWriter(stream);
 		}
 
-		public void WriteCodeAnalysisSettings(CodeAnalysisSettings codeAnalysisSettings)
+		public void WriteCodeAnalysisSettings(CodeAnalysisSettings codeAnalysisSettings, BannedApiSettings bannedApiSettings)
 		{
 			codeAnalysisSettings.ThrowOnNull();
+			bannedApiSettings.ThrowOnNull();
 
 			if (_isDisposed)
 				throw new ObjectDisposedException(objectName: nameof(CodeAnalysisSettingsBinaryWriter));
 
+			WriteAnalysisSettings(codeAnalysisSettings);
+			WriteBannedApiSettings(bannedApiSettings);
+		}
+
+		private void WriteAnalysisSettings(CodeAnalysisSettings codeAnalysisSettings)
+		{
 			_writer.Write(codeAnalysisSettings.RecursiveAnalysisEnabled);
 			_writer.Write(codeAnalysisSettings.IsvSpecificAnalyzersEnabled);
 			_writer.Write(codeAnalysisSettings.StaticAnalysisEnabled);
 			_writer.Write(codeAnalysisSettings.SuppressionMechanismEnabled);
 			_writer.Write(codeAnalysisSettings.PX1007DocumentationDiagnosticEnabled);
+		}
+
+		private void WriteBannedApiSettings(BannedApiSettings bannedApiSettings)
+		{
+			string bannedApiFilePath = bannedApiSettings.BannedApiFilePath ?? string.Empty;
+			_writer.Write(bannedApiFilePath);
+
+			string whiteListApiFilePath = bannedApiSettings.WhiteListApiFilePath ?? string.Empty;
+			_writer.Write(whiteListApiFilePath);
 		}
 
 		public void Dispose()
