1431ebaee1b3d4c084bb1dca9a24a6c79f40be1d
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-01 20:05:32 +0200

    ATR-819: reworked API storage provider to update the cached API storage instance if the underlying source has changed.

--- src/Acuminator/Acuminator.Utilities/BannedApi/Storage/ApiStorageProvider.cs
+++ src/Acuminator/Acuminator.Utilities/BannedApi/Storage/ApiStorageProvider.cs
@@ -1,9 +1,6 @@
-﻿#nullable enable
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,79 +10,92 @@ using Acuminator.Utilities.BannedApi.Providers;
 
 namespace Acuminator.Utilities.BannedApi.Storage
 {
-    /// <summary>
-    /// An API storage helper that keeps and retrieves the API storage.
-    /// </summary>
-    public partial class ApiStorageProvider
-    {
+	/// <summary>
+	/// An API storage helper that keeps and retrieves the API storage.
+	/// </summary>
+	/// <remarks>
+	/// The API storage provider caches an instance of the API storage created by the provider.<br/>
+	/// For the cases when the underlying API source is modified, the provider will create and change a new instance of the API storage.<br/>
+	/// The update of the instance is based on the pull basis, i.e. the instance is updated only when it is requested by the consumer.<br/><br/>
+	/// 
+	/// This is OK for analyzers that request the API data at the beginning of the analysis, and then use it for the entire analysis iteration.<br/>
+	/// They will get the updated API data only on the next analysis iteration.<br/><br/>
+	/// 
+	/// If a push model with a file watcher was used instead (which would require more system resources), then the analyzers would get the updated API data immediately after the change.<br/>
+	/// This would lead to an inconsistent state of the analysis results, as the analyzers would report some results based on the old API data and some based on the new API data.<br/>
+	/// </remarks>
+	public partial class ApiStorageProvider
+	{
 		private readonly object _initializationRegularLock = new object();
 		private readonly SemaphoreSlim _initializationSemaphoreLock = new SemaphoreSlim(initialCount: 1, maxCount: 1);
-        private volatile IApiStorage? _instance;
 
-		private readonly string? _dataFileRelativePath;
-		private readonly string? _dataAssemblyResourceName;
+		private volatile IApiStorage? _instance;
+		private readonly IApiDataProvider _apiDataProvider;
 
-		public ApiStorageProvider(string? dataFileRelativePath, string? dataAssemblyResourceName)
+		public ApiStorageProvider(string? dataFilePath, Assembly? assemblyWithResource, string? dataAssemblyResourceName)
 		{
-			_dataFileRelativePath = dataFileRelativePath.NullIfWhiteSpace();
-			_dataAssemblyResourceName = dataAssemblyResourceName.NullIfWhiteSpace();
+			_apiDataProvider = CreateApiDataProvider(dataFilePath, assemblyWithResource, dataAssemblyResourceName);
 		}
 
-		public IApiStorage GetStorage(CancellationToken cancellation, IApiDataProvider? customApiDataProvider = null)
-        {
+		public ApiStorageProvider(IApiDataProvider apiDataProvider)
+		{
+			_apiDataProvider = apiDataProvider.CheckIfNull();
+		}
+
+		public IApiStorage GetStorage(CancellationToken cancellation)
+		{
 			cancellation.ThrowIfCancellationRequested();
 
-			if (_instance != null)
+			if (_instance != null && !_apiDataProvider.HasApiDataSourceChanged())
 				return _instance;
 
 			lock (_initializationRegularLock)
 			{
-				if (_instance == null)
-					_instance = GetStorageWithoutLocking(cancellation, customApiDataProvider);
+				if (_instance == null || _apiDataProvider.HasApiDataSourceChanged())
+				{
+					_instance = GetStorageWithoutLocking(cancellation);
+				}
 
 				return _instance;
 			}
 		}
 
-		private IApiStorage GetStorageWithoutLocking(CancellationToken cancellation, IApiDataProvider? customBannedApiDataProvider)
+		private IApiStorage GetStorageWithoutLocking(CancellationToken cancellation)
 		{
-			var bannedApiDataProvider = customBannedApiDataProvider ?? GetDefaultDataProvider();
-			var bannedApis = bannedApiDataProvider.GetApiData(cancellation);
+			var apiData = _apiDataProvider.GetApiData(cancellation);
 
 			cancellation.ThrowIfCancellationRequested();
 
-			return bannedApis == null
+			return apiData == null
 				? new DefaultApiStorage()
-				: new DefaultApiStorage(bannedApis);
+				: new DefaultApiStorage(apiData);
 		}
 
-		public async Task<IApiStorage> GetStorageAsync(CancellationToken cancellation, IApiDataProvider? customBannedApiDataProvider = null)
-        {
+		public async Task<IApiStorage> GetStorageAsync(CancellationToken cancellation)
+		{
 			cancellation.ThrowIfCancellationRequested();
 
-            if (_instance != null)
-                return _instance;
+			if (_instance != null && !_apiDataProvider.HasApiDataSourceChanged())
+				return _instance;
 
 			await _initializationSemaphoreLock.WaitAsync(cancellation).ConfigureAwait(false);
 
 			try
 			{
-				if (_instance == null)
-					_instance = await GetStorageAsyncWithoutLockingAsync(cancellation, customBannedApiDataProvider).ConfigureAwait(false);
+				if (_instance == null || _apiDataProvider.HasApiDataSourceChanged())
+					_instance = await GetStorageAsyncWithoutLockingAsync(cancellation).ConfigureAwait(false);
 
 				return _instance;
 			}
 			finally
 			{
 				_initializationSemaphoreLock.Release();
-			}		
+			}
 		}
 
-        private async Task<IApiStorage> GetStorageAsyncWithoutLockingAsync(CancellationToken cancellation, IApiDataProvider? customBannedApiDataProvider)
-        {
-			var bannedApiDataProvider = customBannedApiDataProvider ?? GetDefaultDataProvider();
-
-			var bannedApis = await bannedApiDataProvider.GetApiDataAsync(cancellation).ConfigureAwait(false);
+		private async Task<IApiStorage> GetStorageAsyncWithoutLockingAsync(CancellationToken cancellation)
+		{
+			var bannedApis = await _apiDataProvider.GetApiDataAsync(cancellation).ConfigureAwait(false);
 			cancellation.ThrowIfCancellationRequested();
 
 			return bannedApis == null
@@ -93,45 +103,29 @@ namespace Acuminator.Utilities.BannedApi.Storage
 				: new DefaultApiStorage(bannedApis);
 		}
 
-		private IApiDataProvider GetDefaultDataProvider()
-        {
-			if (_dataFileRelativePath == null && _dataAssemblyResourceName == null)
+		private static IApiDataProvider CreateApiDataProvider(string? dataFilePath, Assembly? assemblyWithResource, string? dataAssemblyResourceName)
+		{
+			dataFilePath = dataFilePath.NullIfWhiteSpace();
+			dataAssemblyResourceName = dataAssemblyResourceName.NullIfWhiteSpace();
+			bool hasAssemblyResource = assemblyWithResource != null && dataAssemblyResourceName != null;
+
+			if (dataFilePath == null && !hasAssemblyResource)
 				return new EmptyProvider(considerDataAvailable: false);
 
-			Assembly assembly 		 = typeof(ApiStorageProvider).Assembly;
-			var fileDataProvider 	 = MakeFileDataProvider(assembly);
-			var assemblyDataProvider = MakeAssemblyDataProvider(assembly);
+			var fileDataProvider = dataFilePath != null
+				? new FileDataProvider(dataFilePath)
+				: null;
+			var assemblyDataProvider = hasAssemblyResource
+				? new AssemblyResourcesDataProvider(assemblyWithResource!, dataAssemblyResourceName!)
+				: null;
 
 			if (fileDataProvider == null)
 				return assemblyDataProvider ?? new EmptyProvider(considerDataAvailable: false) as IApiDataProvider;
 			else if (assemblyDataProvider == null)
 				return fileDataProvider;
 
-			var apiDataProviders = new IApiDataProvider[] { fileDataProvider, assemblyDataProvider };
-			var defaultProvider = new DataProvidersCoalesceCombinator(apiDataProviders);
+			var defaultProvider = new DataProvidersCoalesceCombinator([fileDataProvider, assemblyDataProvider]);
 			return defaultProvider;
-        }
-
-		private FileDataProvider? MakeFileDataProvider(Assembly currentAssembly)
-		{
-			if (_dataFileRelativePath == null || currentAssembly.Location.IsNullOrWhiteSpace())
-				return null;
-
-			string folderWithExtension = Path.GetDirectoryName(currentAssembly.Location);
-			string filePath = Path.Combine(folderWithExtension, _dataFileRelativePath);
-
-			return new FileDataProvider(filePath);
-		}
-
-		private AssemblyResourcesDataProvider? MakeAssemblyDataProvider(Assembly currentAssembly)
-		{
-			if (_dataAssemblyResourceName == null)
-				return null;
-
-			string assemblyName = currentAssembly.GetName().Name;
-			string fullResourceName = $"{assemblyName}.{_dataAssemblyResourceName}";
-
-			return new AssemblyResourcesDataProvider(currentAssembly, fullResourceName);
 		}
 	}
 }
