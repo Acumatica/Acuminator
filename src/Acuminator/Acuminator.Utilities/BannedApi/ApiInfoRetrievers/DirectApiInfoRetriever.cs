71ec5037edb46ff84b03603b13e8b78c874862d1
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-02 07:57:35 +0200

    ATR-819: changed search for banned APIs in the direct API info retreiver to take ISV mode into account

--- src/Acuminator/Acuminator.Utilities/BannedApi/ApiInfoRetrievers/DirectApiInfoRetriever.cs
+++ src/Acuminator/Acuminator.Utilities/BannedApi/ApiInfoRetrievers/DirectApiInfoRetriever.cs
@@ -1,6 +1,4 @@
-﻿#nullable enable
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 using Acuminator.Utilities.Common;
@@ -9,37 +7,46 @@ using Acuminator.Utilities.BannedApi.Storage;
 
 using Microsoft.CodeAnalysis;
 
-namespace Acuminator.Utilities.BannedApi.ApiInfoRetrievers
+namespace Acuminator.Utilities.BannedApi.ApiInfoRetrievers;
+
+/// <summary>
+/// A retriever of the API info that only searches for the information of the API itself, all containing APIs are not checked.
+/// </summary>
+public class DirectApiInfoRetriever(IApiStorage apiStorage, CodeAnalysisSettings codeAnalysisSettings) : IApiInfoRetriever
 {
-    /// <summary>
-    /// A retriever of the API info that only searches for the information of the API itself, all containing APIs are not checked.
-    /// </summary>
-    public class DirectApiInfoRetriever(IApiStorage apiStorage) : IApiInfoRetriever
+	protected CodeAnalysisSettings AnalysisSettings { get; } = codeAnalysisSettings.CheckIfNull();
+
+	protected IApiStorage Storage { get; } = apiStorage.CheckIfNull();
+
+	public ApiSearchResult? GetInfoForApi(ISymbol apiSymbol)
 	{
-		protected IApiStorage Storage { get; } = apiStorage.CheckIfNull();
+		ApiKind apiKind = apiSymbol.GetApiKind();
+		ApiSearchResult? directApiInfo = GetInfoForApiImpl(apiSymbol, apiKind);
 
-		public ApiSearchResult? GetInfoForApi(ISymbol apiSymbol)
+		return directApiInfo;
+	}
+
+	protected virtual ApiSearchResult? GetInfoForApiImpl(ISymbol apiSymbol, ApiKind apiKind)
+	{
+		var apiSymbolFoundInDb = GetInfoForSymbol(apiSymbol, apiKind);
+		return apiSymbolFoundInDb != null
+			? new ApiSearchResult(closestBannedApi: apiSymbolFoundInDb, apiSymbolFoundInDb)
+			: null;
+	}
+
+	protected Api? GetInfoForSymbol(ISymbol symbol, ApiKind symbolKind)
+	{
+		string? symbolDocID = symbol.GetDocumentationCommentId().NullIfWhiteSpace();
+
+		if (symbolDocID == null)
+			return null;
+
+		var symbolApiInfo = Storage.GetApi(symbolKind, symbolDocID);
+		return symbolApiInfo?.BanKind switch
 		{
-			ApiKind apiKind = apiSymbol.GetApiKind();
-			ApiSearchResult? directApiInfo = GetInfoForApiImpl(apiSymbol, apiKind);
-
-			return directApiInfo;
-		}
-
-		protected virtual ApiSearchResult? GetInfoForApiImpl(ISymbol apiSymbol, ApiKind apiKind)
-		{
-			var apiSymbolFoundInDb = GetInfoForSymbol(apiSymbol, apiKind);
-			return apiSymbolFoundInDb != null
-				? new ApiSearchResult(closestBannedApi: apiSymbolFoundInDb, apiSymbolFoundInDb)
-				: null;
-		}
-
-		protected Api? GetInfoForSymbol(ISymbol symbol, ApiKind symbolKind)
-		{
-			string? symbolDocID = symbol.GetDocumentationCommentId().NullIfWhiteSpace();
-			return symbolDocID.IsNullOrWhiteSpace()
-				? null
-				: Storage.GetApi(symbolKind, symbolDocID);
-		}
+			ApiBanKind.General => symbolApiInfo,
+			ApiBanKind.ISV 	   => AnalysisSettings.IsvSpecificAnalyzersEnabled ? symbolApiInfo : null,
+			_ 				   => null
+		};
 	}
 }
