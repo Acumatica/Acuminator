6a3d25ad760c0244652feca83cb9f45d4f16403b
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-02 06:47:20 +0200

    ATR-819: implemented bootstrap of the analysis for banned APIs in hte analyzer - retrieveal of API data, reading of settings from the shared memory, etc

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/BannedApi/BannedApiAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/BannedApi/BannedApiAnalyzer.cs
@@ -1,13 +1,10 @@
-﻿#nullable enable
-
-using System;
+﻿using System;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 
 using Acuminator.Analyzers.Settings.OutOfProcess;
-using Acuminator.Analyzers.Utils;
 using Acuminator.Utilities;
 using Acuminator.Utilities.BannedApi.ApiInfoRetrievers;
 using Acuminator.Utilities.BannedApi.Providers;
@@ -20,190 +17,132 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace Acuminator.Analyzers.StaticAnalysis.BannedApi
+namespace Acuminator.Analyzers.StaticAnalysis.BannedApi;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public partial class BannedApiAnalyzer : PXDiagnosticAnalyzer
 {
-	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	public class BannedApiAnalyzer : PXDiagnosticAnalyzer
+	[MemberNotNullWhen(returnValue: true, nameof(BannedApiSettings))]
+	private bool BannedApiSettingsProvidedExternally { get; }
+
+	private BannedApiSettings? BannedApiSettings { get; set; }
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+		ImmutableArray.Create
+		(
+			Descriptors.PX1099_ForbiddenApiUsage_NoDetails,
+			Descriptors.PX1099_ForbiddenApiUsage_WithDetails
+		);
+
+	private readonly IApiStorage? _customBannedApiStorage;
+	private readonly IApiDataProvider? _customBannedApiDataProvider;
+
+	private readonly IApiStorage? _customWhiteListStorage;
+	private readonly IApiDataProvider? _customWhiteListDataProvider;
+
+	private readonly IApiInfoRetriever? _customBanInfoRetriever;
+	private readonly IApiInfoRetriever? _customWhiteListInfoRetriever;
+
+	public BannedApiAnalyzer() : this(customBannedApiStorage: null, customBannedApiDataProvider: null,
+									  customWhiteListStorage: null, customWhiteListDataProvider: null,
+									  customBanInfoRetriever: null, customWhiteListInfoRetriever: null,
+									  codeAnalysisSettings: null, bannedApiSettings: null)
 	{
-		private readonly bool _bannedApiSettingsProvidedExternally;
+	}
 
-		private BannedApiSettings? BannedApiSettings { get; set; }
+	public BannedApiAnalyzer(IApiStorage? customBannedApiStorage, IApiDataProvider? customBannedApiDataProvider,
+							 IApiStorage? customWhiteListStorage, IApiDataProvider? customWhiteListDataProvider,
+							 IApiInfoRetriever? customBanInfoRetriever, IApiInfoRetriever? customWhiteListInfoRetriever,
+							 CodeAnalysisSettings? codeAnalysisSettings, BannedApiSettings? bannedApiSettings) :
+						base(codeAnalysisSettings)
+	{
+		BannedApiSettings = bannedApiSettings;
+		BannedApiSettingsProvidedExternally = BannedApiSettings != null;
 
-		//private static ApiStorageProvider BannedApiProvider { get; }
+		_customBannedApiStorage 	  = customBannedApiStorage;
+		_customBannedApiDataProvider  = customBannedApiDataProvider;
+		_customWhiteListStorage 	  = customWhiteListStorage;
+		_customWhiteListDataProvider  = customWhiteListDataProvider;
+		_customBanInfoRetriever 	  = customBanInfoRetriever;
+		_customWhiteListInfoRetriever = customWhiteListInfoRetriever;
+	}
 
-		//private static ApiStorageProvider WhiteListProvider { get; }
-
-		//private static IApiDataProvider DefaultBannedApiDataProvider { get; }
-
-		//private static IApiDataProvider DefaultWhiteListDataProvider { get; }
-
-		private static readonly bool _isSuccessfullyInitialized;
-
-		static BannedApiAnalyzer()
+	protected override void ReadAcuminatorSettingsFromSharedMemory()
+	{
+		if (!SettingsProvidedExternally || !BannedApiSettingsProvidedExternally)
 		{
-			//try
-			//{
-			//	BannedApiProvider = new(_bannedApiFileRelativePath, _bannedApiAssemblyResourceName);
-			//	WhiteListProvider = new(_whiteListFileRelativePath, _whiteListAssemblyResourceName);
+			var (externalCodeAnalysisSettings, externalBannedApiSettings) =
+			   AnalyzersOutOfProcessSettingsProvider.GetCodeAnalysisAndBannedApiSettings();
 
-			//	BannedApiProvider.GetStorage(CancellationToken.None);
+			if (!SettingsProvidedExternally)
+				CodeAnalysisSettings = externalCodeAnalysisSettings;
 
-			//	DefaultBannedApiDataProvider = GetApiBanInfoRetriever(Banned)
+			if (!BannedApiSettingsProvidedExternally)
+				BannedApiSettings = externalBannedApiSettings;
+		}
+	}
 
-			//	_isSuccessfullyInitialized = true;
-			//}
-			//catch (Exception)
-			//{
-			//	_isSuccessfullyInitialized = false;
-			//}
+	protected override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
+	{
+		compilationStartContext.CancellationToken.ThrowIfCancellationRequested();
+
+		var banInfoRetriever = GetApiInfoRetriever(_customBanInfoRetriever, _customBannedApiStorage, _customBannedApiDataProvider,
+													BannedApiSettings?.BannedApiFilePath, compilationStartContext.CancellationToken);
+		if (banInfoRetriever == null)
+			return;
+
+		var whiteListInfoRetriever = GetApiInfoRetriever(_customWhiteListInfoRetriever, _customWhiteListStorage, _customWhiteListDataProvider,
+														 BannedApiSettings?.WhiteListApiFilePath, compilationStartContext.CancellationToken);
+
+		compilationStartContext.RegisterSyntaxNodeAction(context => AnalyzeSyntaxTree(context, banInfoRetriever, whiteListInfoRetriever),
+														 SyntaxKind.CompilationUnit);
+	}
+
+	private static IApiInfoRetriever? GetApiInfoRetriever(IApiInfoRetriever? customApiInfoRetriever, IApiStorage? customApiStorage,
+														  IApiDataProvider? customApiDataProvider, string? apiFilePath, 
+														  CancellationToken cancellation)
+	{
+		if (customApiInfoRetriever != null)
+			return customApiInfoRetriever;
+
+		if (customApiStorage != null)
+			return GetHierarchicalApiInfoRetrieverWithCache(customApiStorage);
+
+		if (customApiDataProvider != null)
+		{
+			var customStorageProvider = new ApiStorageProvider(customApiDataProvider);
+			var storageFromCustomApiDataProvider = customStorageProvider.GetStorage(cancellation);
+
+			return GetHierarchicalApiInfoRetrieverWithCache(storageFromCustomApiDataProvider);
 		}
 
-		protected static IApiInfoRetriever GetApiBanInfoRetriever(IApiStorage bannedApiStorage) =>
-			GetHierarchicalApiInfoRetrieverWithCache(bannedApiStorage);
-
-		protected static IApiInfoRetriever? GetWhiteListInfoRetriever(IApiStorage whiteListStorage) =>
-			whiteListStorage.ApiKindsCount > 0
-				? GetHierarchicalApiInfoRetrieverWithCache(whiteListStorage)
-				: null;
-
-		
-
-		protected static IApiInfoRetriever GetHierarchicalApiInfoRetrieverWithCache(IApiStorage storage) =>
-			new ApiInfoRetrieverWithWeakCache(
-				new HierarchicalApiBanInfoRetriever(storage));
-
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
-			ImmutableArray.Create
-			(
-				Descriptors.PX1099_ForbiddenApiUsage_NoDetails,
-				Descriptors.PX1099_ForbiddenApiUsage_WithDetails
-			);
-
-		private readonly IApiStorage? _customBannedApi;
-		private readonly IApiDataProvider? _customBannedApiDataProvider;
-
-		private readonly IApiStorage? _customWhiteList;
-		private readonly IApiDataProvider? _customWhiteListDataProvider;
-
-		private readonly IApiInfoRetriever? _customBanInfoRetriever;
-		private readonly IApiInfoRetriever? _customWhiteListInfoRetriever;
-
-		public BannedApiAnalyzer() : this(customBannedApi: null, customBannedApiDataProvider: null,
-										  customWhiteList: null, customWhiteListDataProvider: null,
-										  customBanInfoRetriever: null, customWhiteListInfoRetriever: null,
-										  codeAnalysisSettings: null, bannedApiSettings: null)
+		if (!apiFilePath.IsNullOrWhiteSpace())
 		{
+			var bannedApiDataStorageFrom = GlobalApiData.GetBannedApiData(apiFilePath, cancellation);
+			return GetHierarchicalApiInfoRetrieverWithCache(bannedApiDataStorageFrom);
 		}
 
-		public BannedApiAnalyzer(IApiStorage? customBannedApi, IApiDataProvider? customBannedApiDataProvider,
-								 IApiStorage? customWhiteList, IApiDataProvider? customWhiteListDataProvider,
-								 IApiInfoRetriever? customBanInfoRetriever, IApiInfoRetriever? customWhiteListInfoRetriever,
-								 CodeAnalysisSettings? codeAnalysisSettings, BannedApiSettings? bannedApiSettings) : 
-							base(codeAnalysisSettings)
+		return null;
+	}
+
+	protected static IApiInfoRetriever? GetHierarchicalApiInfoRetrieverWithCache(IApiStorage storage)
+	{
+		if (storage.ApiKindsCount == 0)
+			return null;
+
+		return new ApiInfoRetrieverWithWeakCache(
+					new HierarchicalApiBanInfoRetriever(storage));
+	}
+
+	private void AnalyzeSyntaxTree(in SyntaxNodeAnalysisContext syntaxContext, IApiInfoRetriever apiBanInfoRetriever,
+								   IApiInfoRetriever? whiteListInfoRetriever)
+	{
+		syntaxContext.CancellationToken.ThrowIfCancellationRequested();
+
+		if (syntaxContext.Node is CompilationUnitSyntax compilationUnitSyntax)
 		{
-			BannedApiSettings = bannedApiSettings;
-			_bannedApiSettingsProvidedExternally = BannedApiSettings != null;
-
-			_customBannedApi 			  = customBannedApi;
-			_customBannedApiDataProvider  = customBannedApiDataProvider;
-			_customWhiteList 			  = customWhiteList;
-			_customWhiteListDataProvider  = customWhiteListDataProvider;
-			_customBanInfoRetriever 	  = customBanInfoRetriever;
-			_customWhiteListInfoRetriever = customWhiteListInfoRetriever;
-		}
-
-		[SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1025:Configure generated code analysis",
-						 Justification = $"Configured in the {nameof(ConfigureAnalysisContext)} method")]
-		[SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1026:Enable concurrent execution",
-						 Justification = $"Configured in the {nameof(ConfigureAnalysisContext)} method")]
-		public override void Initialize(AnalysisContext context)
-		{
-			AcuminatorVsixPackageLoader.EnsurePackageLoaded();
-
-			if (!SettingsProvidedExternally || !_bannedApiSettingsProvidedExternally)
-			{
-				//Initialize settings from global values after potential package load
-				 var (externalCodeAnalysisSettings, externalBannedApiSettings) = 
-					AnalyzersOutOfProcessSettingsProvider.GetCodeAnalysisAndBannedApiSettings();
-
-				if (!SettingsProvidedExternally)
-					CodeAnalysisSettings = externalCodeAnalysisSettings;
-
-				if (!_bannedApiSettingsProvidedExternally)
-					BannedApiSettings = externalBannedApiSettings;
-			}
-
-			if (!CodeAnalysisSettings!.StaticAnalysisEnabled)
-				return;
-
-			ConfigureAnalysisContext(context);
-
-			context.RegisterCompilationStartAction(compilationStartContext =>
-			{
-				var pxContext = new PXContext(compilationStartContext.Compilation, CodeAnalysisSettings);
-
-				if (ShouldAnalyze(pxContext))
-				{
-					AnalyzeCompilation(compilationStartContext, pxContext);
-				}
-			});
-		}
-
-		protected override bool ShouldAnalyze(PXContext pxContext) =>
-			base.ShouldAnalyze(pxContext) && _isSuccessfullyInitialized;
-
-
-		private void AnalyzeCompilationForForbiddenApi(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
-		{
-			compilationStartContext.CancellationToken.ThrowIfCancellationRequested();
-
-			if (!_isSuccessfullyInitialized)
-				return;
-
-			var bannedApiStorage = _customBannedApi ?? GetBannedApiStorage(compilationStartContext.CancellationToken);
-
-			if (bannedApiStorage.ApiKindsCount == 0)
-				return;
-
-			compilationStartContext.CancellationToken.ThrowIfCancellationRequested();
-
-			var apiBanInfoRetriever = _customBanInfoRetriever ?? GetApiBanInfoRetriever(bannedApiStorage);
-
-			if (apiBanInfoRetriever == null)
-				return;
-
-			var whiteListStorage = _customWhiteList ?? GetWhiteListStorage(compilationStartContext.CancellationToken);
-			var whiteListInfoRetriever = _customWhiteListInfoRetriever ?? GetWhiteListInfoRetriever(whiteListStorage);
-
-			compilationStartContext.RegisterSyntaxNodeAction(context => AnalyzeSyntaxTree(context, apiBanInfoRetriever, whiteListInfoRetriever),
-															 SyntaxKind.CompilationUnit);
-		}
-
-		private IApiStorage GetCustomBannedApiStorage(CancellationToken cancellation) =>
-			BannedApiProvider.GetStorage(cancellation, _customBannedApiDataProvider);
-
-		private IApiStorage GetCustomWhiteListStorage(CancellationToken cancellation) =>
-			WhiteListProvider.GetStorage(cancellation, _customWhiteListDataProvider);
-
-
-		private void AnalyzeSyntaxTree(in SyntaxNodeAnalysisContext syntaxContext, IApiInfoRetriever apiBanInfoRetriever,
-									   IApiInfoRetriever? whiteListInfoRetriever)
-		{
-			syntaxContext.CancellationToken.ThrowIfCancellationRequested();
-
-			if (syntaxContext.Node is CompilationUnitSyntax compilationUnitSyntax)
-			{
-				var apiNodesWalker = new ApiNodesWalker(syntaxContext, apiBanInfoRetriever, whiteListInfoRetriever, checkInterfaces: false);
-				apiNodesWalker.CheckSyntaxTree(compilationUnitSyntax);
-			}
-		}
-
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("MicrosoftCodeAnalysisPerformance", "RS1012:Start action has no registered actions", 
-														 Justification = "Not supported override that exists only to implement abstract class")]
-		protected override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
-		{
-			throw new NotSupportedException($"The {nameof(BannedApiAnalyzer)} implementation does not support calls to this method" +
-											 " due to a custom implementation of analysis actions registration.");
+			var apiNodesWalker = new ApiNodesWalker(syntaxContext, apiBanInfoRetriever, whiteListInfoRetriever, checkInterfaces: false);
+			apiNodesWalker.CheckSyntaxTree(compilationUnitSyntax);
 		}
 	}
 }
