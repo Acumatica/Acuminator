e70a041e822eacdc0ee4188c711ad0b879b714a8
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-03 15:31:19 +0200

    ATR-819: added unit test for analysis when banned analysis is disabled

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BannedApi/BannedApiAnalysisDisabledTests.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis.BannedApi;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.BannedApi
+{
+	public class BannedApiAnalysisDisabledTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => 
+			new BannedApiAnalyzer(customBannedApiStorage: null, customBannedApiDataProvider: null, 
+								  customWhiteListStorage: null, customWhiteListDataProvider: null,
+								  customBanInfoRetriever: null, customWhiteListInfoRetriever: null,
+				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+											.WithSuppressionMechanismDisabled(),
+				BannedApiSettings.Default.WithBannedApiAnalysisDisabled());
+
+		[Theory]
+		[EmbeddedFileData("CallsToApisForbiddenToIsv.cs")]
+		public virtual async Task Calls_To_API_ForbiddenForISV(string source) =>
+			await VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData("CallsToMathRoundAPIs.cs")]
+		public virtual async Task Calls_To_MathRound_API(string source) =>
+			await VerifyCSharpDiagnosticAsync(source);
+	}
+}
