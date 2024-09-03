24c166ab3a13dd00162c9ac2a51be2e29673c695
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-03 15:25:35 +0200

    ATR-819: added tests for predefined banned APIs for non ISV context

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BannedApi/Sources/CallsToApisForbiddenToIsv.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace PX.Objects.HackathonDemo
+{
+	public class InfoService
+	{
+		public string PrintMethods(Type type)
+		{
+			var methodNames = type.GetMethods().Select(method => method.Name);
+			string methodsString = string.Join(Environment.NewLine, methodNames);
+			return methodsString;
+		}
+
+		public MethodInfo? GetMethod(Type type, string name)
+		{
+			var method = type.GetMethod(name);
+			return method;
+		}
+
+		public string PrintSystemInfo()
+		{
+			return $"OS: {Environment.OSVersion.VersionString},{Environment.NewLine}Processor count: {Environment.ProcessorCount}";
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BannedApi/Sources/CallsToMathRoundAPIs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using PX.Objects.AP;
+
+namespace PX.Objects.HackathonDemo
+{
+	public class Service
+	{
+		public decimal Convert(decimal amount, decimal rate, int? precision = null, MidpointRounding? midpointRounding = null)
+		{
+			if (precision.HasValue)
+			{
+				return midpointRounding.HasValue
+					? Math.Round(amount * rate, precision.Value, midpointRounding.Value)
+					: Math.Round(amount * rate, precision.Value);
+			}
+			else
+			{
+				if (midpointRounding.HasValue)
+				{
+					return Math.Round(amount * rate, midpointRounding.Value);
+				}
+				else
+					return Math.Round(amount * rate);
+			}
+		}
+
+		public double Convert(double amount, double rate, int? precision = null, MidpointRounding? midpointRounding = null)
+		{
+			if (precision.HasValue)
+			{
+				return midpointRounding.HasValue
+					? Math.Round(amount * rate, precision.Value, midpointRounding.Value)
+					: Math.Round(amount * rate, precision.Value);
+			}
+			else
+			{
+				if (midpointRounding.HasValue)
+				{
+					return Math.Round(amount * rate, midpointRounding.Value);
+				}
+				else
+					return Math.Round(amount * rate);
+			}
+		}
+	}
+}
