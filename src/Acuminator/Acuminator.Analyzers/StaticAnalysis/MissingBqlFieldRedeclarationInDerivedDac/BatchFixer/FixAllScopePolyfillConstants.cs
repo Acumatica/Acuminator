64bbad6c0d0ebc8a1ed28be2786175b5f32665b8
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-08 10:58:39 +0200

    ATR-180: added polyfill constants for Fix All scopes

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/MissingBqlFieldRedeclarationInDerivedDac/BatchFixer/FixAllScopePolyfillConstants.cs
@@ -0,0 +1,22 @@
+﻿using FixAllScope = Microsoft.CodeAnalysis.CodeFixes.FixAllScope;
+
+namespace Acuminator.Analyzers.StaticAnalysis.MissingBqlFieldRedeclarationInDerived;
+
+/// <summary>
+/// Class with integer constants for <see cref="FixAllScope"/> that provides polyfill constant values for <see cref="FixAllScope"/>.ContainingMember<br/>
+/// and <see cref="FixAllScope"/>.ContainingType. These constants are not available in earlier versions of Roslyn such as Roslyn 3.11.0.
+/// </summary>
+internal static class FixAllScopePolyfillConstants
+{
+	public const int Document = (int)FixAllScope.Document;
+
+	public const int Project = (int)FixAllScope.Project;
+
+	public const int Solution = (int)FixAllScope.Solution;
+
+	public const int Custom = (int)FixAllScope.Custom;
+
+	public const int ContainingMember = 4;
+
+	public const int ContainingType = 5;
+}
