9f44a5d2d274f4fcc1900ae0ab87b0ed21a00f80
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-08 21:45:28 +0200

    ATR-651: fixed Name calculation for explicit IGraphWithInitialization.Initialize method implementation

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphMembers/InitializationAndActivation/GraphInitializeMethodNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphMembers/InitializationAndActivation/GraphInitializeMethodNodeViewModel.cs
@@ -3,13 +3,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
+using Acuminator.Utilities.Roslyn.Constants;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
+using Microsoft.CodeAnalysis;
 
 namespace Acuminator.Vsix.ToolWindows.CodeMap
 {
 	public class GraphInitializeMethodNodeViewModel : GraphMemberNodeViewModel
 	{
+		protected readonly string _name;
+
+		public override string Name
+		{
+			get => _name;
+			protected set { }
+		}
+
 		public override Icon NodeIcon => Icon.InitializeMethodGraph;
 
 		public InitializeMethodInfo InitializeMethod => (InitializeMethodInfo)MemberInfo;
@@ -19,6 +31,20 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 											 base(graphInitializationAndActivationCategoryVM, graphInitializationAndActivationCategoryVM,
 												  initializeMethodInfo, isExpanded)
 		{
+			_name = GetName(initializeMethodInfo.Symbol);
+		}
+
+		private string GetName(IMethodSymbol methodSymbol)
+		{
+			if (methodSymbol.MethodKind != MethodKind.ExplicitInterfaceImplementation)
+				return methodSymbol.Name;
+
+			int lastDotIndex = methodSymbol.Name.LastIndexOf('.');
+
+			if (lastDotIndex < 0)
+				return methodSymbol.Name;
+
+			return methodSymbol.Name[(lastDotIndex + 1)..];
 		}
 
 		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => 
