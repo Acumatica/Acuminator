9f54db0781bf8d16f9336b83c19d4c0e5410266e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-08 21:08:32 +0200

    ATR-651: added Code Map node for Initialize method

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Graph/GraphMembers/InitializationAndActivation/GraphInitializeMethodNodeViewModel.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class GraphInitializeMethodNodeViewModel : GraphMemberNodeViewModel
+	{
+		public override Icon NodeIcon => Icon.InitializeMethodGraph;
+
+		public InitializeMethodInfo InitializeMethod => (InitializeMethodInfo)MemberInfo;
+
+		public GraphInitializeMethodNodeViewModel(GraphInitializationAndActivationCategoryNodeViewModel graphInitializationAndActivationCategoryVM,
+												  InitializeMethodInfo initializeMethodInfo, bool isExpanded = false) :
+											 base(graphInitializationAndActivationCategoryVM, graphInitializationAndActivationCategoryVM,
+												  initializeMethodInfo, isExpanded)
+		{
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => 
+			treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => 
+			treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
