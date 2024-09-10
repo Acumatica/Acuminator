a9a590c30ae13f211a30a8132647ebf972cc259e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-10 17:45:32 +0200

    ATR-448: added basic implementation for Code Map search box control and integrated it into the Code Map window

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/UI/CodeMapSearchBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap;
+
+/// <summary>
+/// Interaction logic for CodeMapSearchBox.xaml
+/// </summary>
+public partial class CodeMapSearchBox : UserControl
+{
+	public CodeMapSearchBox()
+	{
+		InitializeComponent();
+	}
+}
