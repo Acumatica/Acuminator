4348c084d9dd685ba987d6c6535cf833587141a1
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-11 20:20:01 +0200

    ATR-448: refactoring - updated converter's code with modern C# + enabled nullable analysis

--- src/Acuminator/Acuminator.Vsix/Tool Windows/Converters/BoolToVisibilityConverter.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/Converters/BoolToVisibilityConverter.cs
@@ -1,40 +1,35 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows;
 
+namespace Acuminator.Vsix.ToolWindows.Converters;
 
-namespace Acuminator.Vsix.ToolWindows.Converters
+/// <summary>
+/// Converter which converts <see cref="bool"/> bool values to the <see cref="Visibility"/> values.
+/// If converter parameter's value is "!" then the value is inverted.
+/// </summary>
+[ValueConversion(typeof(bool), typeof(Visibility), ParameterType = typeof(string))]
+public class BoolToVisibilityConverter : IValueConverter
 {
-	/// <summary>
-	/// Converter which converts <see cref="bool"/> bool values to the <see cref="Visibility"/> values.
-	/// If converter parameter's value is "!" then the value is inverted.
-	/// </summary>
-	[ValueConversion(typeof(bool), typeof(Visibility), ParameterType = typeof(string))]
-	public class BoolToVisibilityConverter : IValueConverter
+	private const string InverseVisibilityValue = "!";
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 	{
-		private const string InverseVisibilityValue = "!";
+		if (value is not bool visible)
+			return Visibility.Collapsed;
 
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		if (InverseVisibilityValue.Equals(parameter as string, StringComparison.Ordinal))
 		{
-			if (!(value is bool visible))
-				return Visibility.Collapsed;
-
-			if (InverseVisibilityValue.Equals(parameter as string, StringComparison.Ordinal))
-			{
-				visible = !visible;
-			}
-
-			return visible ? Visibility.Visible : Visibility.Collapsed;
+			visible = !visible;
 		}
 
-		public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
+		return visible ? Visibility.Visible : Visibility.Collapsed;
 	}
+
+	public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture) => 
+		throw new NotSupportedException();
 }
