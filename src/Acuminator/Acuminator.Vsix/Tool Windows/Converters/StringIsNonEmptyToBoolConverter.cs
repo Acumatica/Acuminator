aa3f224d1365eeee6ee622a51e459e8350e66b5b
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-11 20:20:36 +0200

    ATR-448: added string is not empty to bool converter

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/Converters/StringIsNonEmptyToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.ToolWindows.Converters;
+
+/// <summary>
+/// A one way converter which converts <see cref="string"/> values to the <see cref="bool"/> values.
+/// Returns <see langword="true"/> if the string is not null or empty. Otherwise, returns <see langword="false"/>.
+/// </summary>
+[ValueConversion(typeof(string), typeof(bool))]
+public class StringIsNonEmptyToBoolConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value == null)
+			return false;
+
+		string valueStr = (value as string) ?? value.ToString();
+		return !valueStr.IsNullOrEmpty();
+	}
+
+	public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture) =>
+		throw new NotSupportedException();
+}
