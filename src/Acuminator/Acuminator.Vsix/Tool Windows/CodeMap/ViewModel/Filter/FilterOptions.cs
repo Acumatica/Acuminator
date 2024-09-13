7f3404187be5348a6bb93de52c1baee090faf570
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-13 17:21:36 +0200

    ATR-448: added filter options

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Filter/FilterOptions.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap.Filter;
+
+public record FilterOptions(string? FilterPattern)
+{
+	public static readonly FilterOptions NoFilter = new FilterOptions((string?)null); 
+
+	[MemberNotNullWhen(returnValue: true, nameof(FilterPattern))]
+	public bool HasFilter => FilterPattern != null;
+
+	public string? FilterPattern { get; } = FilterPattern.IsNullOrEmpty() ? null : FilterPattern;
+}
