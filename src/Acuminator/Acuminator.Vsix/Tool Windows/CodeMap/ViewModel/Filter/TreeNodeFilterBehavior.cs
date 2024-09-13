de15ed2d44a55f87d4ae9ff5392860ed0e71ab0f
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-13 15:49:53 +0200

    ATR-448: added tree node filter behaviors

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Filter/TreeNodeFilterBehavior.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap.Filter;
+
+/// <summary>
+/// Values that represent different tree node behavior on filtering.
+/// </summary>
+public enum TreeNodeFilterBehavior
+{
+	DisplayedIfFilterMet,
+	DisplayedIfFilterNotMet,
+	AlwaysDisplayed,
+	AlwaysHidden
+}
