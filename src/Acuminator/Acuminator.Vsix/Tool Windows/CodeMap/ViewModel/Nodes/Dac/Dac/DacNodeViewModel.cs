d18d715bd79fc6e80ad14555cd297ccb835dc709
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-15 19:39:01 +0200

    ATR-828: added base type for DAC nodes

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/Dac/DacNodeViewModel.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Vsix.ToolWindows.CodeMap.Dac;
+using Acuminator.Vsix.ToolWindows.Common;
+using Acuminator.Vsix.Utilities;
+using Acuminator.Vsix.Utilities.Navigation;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class DacNodeViewModel : DacNodeViewModelBase, IElementWithTooltip
+	{
+		public DacSemanticModelForCodeMap DacModelForCodeMap { get; }
+
+		public override string Name
+		{
+			get => DacModelForCodeMap.Name;
+			protected set { }
+		}
+
+		public override Icon NodeIcon => DacModelForCodeMap.DacType == DacType.Dac
+			? Icon.Dac
+			: Icon.DacExtension;
+
+		public override ExtendedObservableCollection<ExtraInfoViewModel> ExtraInfos { get; }
+
+		public DacNodeViewModel(DacSemanticModelForCodeMap dacModel, TreeViewModel tree, bool isExpanded) : 
+						   base(tree, parent: null, isExpanded)
+		{
+			DacModelForCodeMap = dacModel.CheckIfNull();
+			ExtraInfos		   = new ExtendedObservableCollection<ExtraInfoViewModel>(GetDacExtraInfos(DacModelForCodeMap));
+		}
+
+		public override Task NavigateToItemAsync() => DacModelForCodeMap.Symbol.NavigateToAsync();
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => 
+			treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
