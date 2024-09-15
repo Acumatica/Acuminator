2400d1c2b91b02237ee3cd9f705d24ae1a8625fe
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-15 19:22:18 +0200

    ATR-828: extended base types category node

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacCategories/DacBaseTypesCategoryNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacCategories/DacBaseTypesCategoryNodeViewModel.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Acuminator.Utilities.Common;
-using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Vsix.ToolWindows.CodeMap.Dac;
 
 namespace Acuminator.Vsix.ToolWindows.CodeMap
 {
@@ -16,13 +16,14 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 
 		public override Icon NodeIcon => Icon.Category;
 
+		public DacInfo? BaseDacInfo => DacViewModel.DacModelForCodeMap.BaseDacInfo;
+
+		public DacExtensionInfo? BaseDacExtensionInfo => DacViewModel.DacModelForCodeMap.BaseDacExtensionInfo;
+
 		public DacBaseTypesCategoryNodeViewModel(DacNodeViewModel dacViewModel, TreeNodeViewModel parent, bool isExpanded) : 
 											base(dacViewModel, parent, DacMemberCategory.BaseTypes, isExpanded)
 		{		
 		}
-
-		public override IEnumerable<DacFieldInfo> GetCategoryDacFields() => 
-			DacModel.DeclaredDacFields.Where(field => field.IsKey);
 
 		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
 
