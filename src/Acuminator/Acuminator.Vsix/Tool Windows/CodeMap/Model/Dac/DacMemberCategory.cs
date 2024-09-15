9eb0ce0e5a5c5ad35f16e9c184a9ab6a07ad0451
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-15 14:19:20 +0200

    ATR-828: created separate subfolder for DAC semantic model for code map

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Model/Dac/DacMemberCategory.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/Model/Dac/DacMemberCategory.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Acuminator.Vsix.ToolWindows.CodeMap
+namespace Acuminator.Vsix.ToolWindows.CodeMap.Dac
 {
 	public enum DacMemberCategory
 	{
