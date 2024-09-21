d00632297bc43a463bb9030f1bf5d227aaa09e0f
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-21 21:19:59 +0200

    ATR-829: added separate properties and constant classes for PX1067

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/MissingBqlFieldRedeclarationInDerivedDac/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Acuminator.Analyzers.StaticAnalysis.MissingBqlFieldRedeclarationInDerived;
+
+internal static class Separators
+{
+	public const string TypePartSeparator = ":";
+	public const string FieldsSeparator = "|";
+}
---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/MissingBqlFieldRedeclarationInDerivedDac/PX1067DiagnosticProperty.cs
@@ -0,0 +1,6 @@
+﻿namespace Acuminator.Analyzers.StaticAnalysis.MissingBqlFieldRedeclarationInDerived;
+
+internal static class PX1067DiagnosticProperty
+{
+	public const string BqlFieldsWithBqlTypesData = nameof(BqlFieldsWithBqlTypesData);
+}
