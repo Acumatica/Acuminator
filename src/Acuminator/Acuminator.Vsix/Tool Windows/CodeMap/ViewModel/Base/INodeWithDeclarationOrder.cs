848c0ff9b93e1b1a866ed110c858b3d94eee606e
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-23 14:50:10 +0200

    ATR-834: changed interface interface from INodeWithSymbol to INodeWithDeclarationOrder

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Base/INodeWithDeclarationOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Interface for Code Map node with a declaration order.
+	/// </summary>
+	public interface INodeWithDeclarationOrder
+	{
+		/// <summary>
+		/// The declaration order.
+		/// </summary>
+		int DeclarationOrder { get; }
+	}
+}
