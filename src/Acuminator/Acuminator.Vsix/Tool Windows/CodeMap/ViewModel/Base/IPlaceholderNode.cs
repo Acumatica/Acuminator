140234cdd6c910a8a4411a2801729fed9f3b573d
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-28 21:56:11 +0200

    ATR-830: added interface for placeholder nodes and utils to replace them with a subtree

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Base/IPlaceholderNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	/// <summary>
+	/// Interface for a Code Map placeholder node that represents symbol which data is not collected yet.
+	/// </summary>
+	public interface IPlaceholderNode
+	{
+		/// <summary>
+		/// The placeholder symbol represented by the node.
+		/// </summary>
+		INamedTypeSymbol PlaceholderSymbol { get; }
+
+		/// <summary>
+		/// The declaration order of the placeholder symbol.
+		/// </summary>
+		int PlaceholderSymbolDeclarationOrder { get; }
+	}
+}
