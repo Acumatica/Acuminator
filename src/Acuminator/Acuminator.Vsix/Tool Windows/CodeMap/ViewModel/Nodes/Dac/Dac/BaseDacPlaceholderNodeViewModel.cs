e35caa5627e8f51ef7a39e8036bb55140528c7c6
Sergey Nikomarov <snikomarov@acumatica.com>
2024-09-28 22:01:32 +0200

    ATR-830: integrated new placeholder interface and utils into the base DAC placeholder node

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/Dac/BaseDacPlaceholderNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/Dac/BaseDacPlaceholderNodeViewModel.cs
@@ -5,14 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Acuminator.Utilities.Common;
-using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.Dac;
 using Acuminator.Vsix.ToolWindows.CodeMap.Dac;
 using Acuminator.Vsix.Utilities;
 
+using Microsoft.CodeAnalysis;
+
 namespace Acuminator.Vsix.ToolWindows.CodeMap
 {
-	public class BaseDacPlaceholderNodeViewModel : DacNodeViewModelBase
+	public class BaseDacPlaceholderNodeViewModel : DacNodeViewModelBase, IPlaceholderNode
 	{
 		public DacNodeViewModel ContainingDacNode { get; }
 
@@ -23,6 +24,12 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 		public override bool IsExpanderAlwaysVisible => true;
 
 		public override ExtendedObservableCollection<ExtraInfoViewModel> ExtraInfos { get; }
+
+		#region IPlaceholderNode implementation
+		INamedTypeSymbol IPlaceholderNode.PlaceholderSymbol => DacOrDacExtInfo.Symbol;
+
+		int IPlaceholderNode.PlaceholderSymbolDeclarationOrder => DacOrDacExtInfo.DeclarationOrder;
+		#endregion
 
 		public BaseDacPlaceholderNodeViewModel(DacOrDacExtInfoBase dacOrDacExtInfo, DacNodeViewModel containingDacNode, 
 											   TreeNodeViewModel parent, bool isExpanded) : 
@@ -46,66 +53,7 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 		{
 			base.BeforeNodeExpansionChanged(oldValue, newValue);
 
-			bool isExpanding = newValue;
-
-			if (!isExpanding || Tree.CodeMapViewModel.IsCalculating)
-				return false;
-
-			bool oldIsCalculating = Tree.CodeMapViewModel.IsCalculating;
-
-			try
-			{
-				Tree.CodeMapViewModel.IsCalculating = true;
-				BuildBaseDacNodes();
-
-				// Never expand substitute node
-				return false;
-			}
-			finally
-			{
-				Tree.CodeMapViewModel.IsCalculating = oldIsCalculating;
-			}
-		}
-
-		private void BuildBaseDacNodes()
-		{
-			var pxContext = ContainingDacNode.DacModel.PXContext;
-			var semanticModelFactory = Tree.CodeMapViewModel.SemanticModelFactory;
-
-			if (!semanticModelFactory.TryToInferSemanticModel(DacOrDacExtInfo.Symbol, pxContext, out ISemanticModel? semanticModel, 
-															 DacOrDacExtInfo.DeclarationOrder) ||
-				semanticModel is not DacSemanticModelForCodeMap baseDacSemanticModel)
-			{
-				return;
-			}
-
-			var treeBuilder   = Tree.CodeMapViewModel.TreeBuilder;
-			var filterOptions = Tree.CodeMapViewModel.FilterVM.CreateFilterOptionsFromCurrentFilter();
-			var rootParent	  = Parent;
-
-			if (rootParent == null)
-				return;
-
-			var baseDacNode = treeBuilder.CreateAttachedRootWithSubTree(baseDacSemanticModel, Tree, rootParent, filterOptions, 
-																		expandRoots: true, expandChildren: false,
-																		Tree.CodeMapViewModel.CancellationToken ?? default) as DacNodeViewModel;
-			if (baseDacNode == null)
-				return;
-
-			int nodeIndex = rootParent.AllChildren.IndexOf(this);
-
-			if (nodeIndex >= 0)
-			{
-				rootParent.AllChildren.RemoveAt(nodeIndex);
-				rootParent.AllChildren.Insert(nodeIndex, baseDacNode);
-			}
-			else
-			{
-				rootParent.AllChildren.Remove(this);
-				rootParent.AllChildren.Add(baseDacNode);
-			}
-
-			Tree.RefreshFlattenedNodesList();
+			return this.ReplacePlaceholderWithSubTreeOnExpansion(ContainingDacNode.DacModel.PXContext, isExpanding: newValue);
 		}
 	}
 }
