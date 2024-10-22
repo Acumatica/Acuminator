5546c1fc2729aa33ec249a870f02a3c93e03c539
Sergey Nikomarov <snikomarov@acumatica.com>
2024-10-22 03:20:27 +0200

    ATR-852: added unit test for PX1099 for async APIs calls

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BannedApi/BannedApiTestsNonIsv.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BannedApi/BannedApiTestsNonIsv.cs
@@ -37,12 +37,71 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.BannedApi
 		public virtual async Task Calls_To_MathRound_API(string source) =>
 			await VerifyCSharpDiagnosticAsync(source,
 				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(15, 13, Resources.PX1099Title_MethodFormatArg, "System.Math.Round(System.Decimal,System.Int32)",
-					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number. Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."),
+					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number." + 
+					" Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."),
 				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(24, 18, Resources.PX1099Title_MethodFormatArg, "System.Math.Round(System.Decimal)",
-					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number. Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."),
+					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number." + 
+					" Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."),
 				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(34, 13, Resources.PX1099Title_MethodFormatArg, "System.Math.Round(System.Double,System.Int32)",
-					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number. Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."),
+					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number." + 
+					" Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."),
 				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(43, 18, Resources.PX1099Title_MethodFormatArg, "System.Math.Round(System.Double)",
-					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number. Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."));
+					"Math.Round uses Banker's Rounding by default, which rounds to the closest even number." + 
+					" Usually, this is not the desired rounding behavior. Use the Math.Round overload with the MidpointRounding parameter to explicitly specify the desired rounding behavior."));
+
+		[Theory]
+		[EmbeddedFileData("CallsToAsyncAPIs.cs")]
+		public async virtual Task Calls_To_Async_APIs(string source) =>
+			await VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(12, 41, Resources.PX1099Title_MethodFormatArg, 
+					"System.Linq.ParallelEnumerable.AsParallel``1(System.Collections.Generic.IEnumerable{``0})",
+					"Usage of Parallel LINQ is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." + 
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(14, 13, Resources.PX1099Title_TypeFormatArg,
+					"System.Threading.Tasks.Parallel",
+					"Usage of the Parallel type is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." + 
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(16, 30, Resources.PX1099Title_MethodFormatArg, 
+					"System.Threading.Tasks.Task.Run``1(System.Func{``0})",
+					"Usage of the Task.Run method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." +
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(17, 35, Resources.PX1099Title_MethodFormatArg, 
+					"System.Threading.Tasks.Task`1.ConfigureAwait(System.Boolean)",
+					"Usage of the Task.ConfigureAwait method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." + 
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(19, 33, Resources.PX1099Title_MethodFormatArg, 
+					"System.Threading.Tasks.Task.Run(System.Action)",
+					"Usage of the Task.Run method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." +
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(20, 28, Resources.PX1099Title_MethodFormatArg,
+					"System.Threading.Tasks.Task.ConfigureAwait(System.Boolean)",
+					"Usage of the Task.ConfigureAwait method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." +
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(23, 20, Resources.PX1099Title_MethodFormatArg,
+					"System.Threading.Tasks.Task.Start",
+					"Usage of the Task.Start method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." + 
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(25, 26, Resources.PX1099Title_MethodFormatArg,
+					"System.Threading.Tasks.Task`1.ConfigureAwait(System.Boolean)",
+					"Usage of the Task.ConfigureAwait method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." +
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(28, 23, Resources.PX1099Title_MethodFormatArg,
+					"System.Threading.Tasks-Task.Start(System.Threading.Tasks.TaskScheduler)",
+					"Usage of the Task.Start method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." +
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources."),
+
+				Descriptors.PX1099_ForbiddenApiUsage_WithReason.CreateFor(30, 29, Resources.PX1099Title_MethodFormatArg,
+					"System.Threading.Tasks.Task.ConfigureAwait(System.Boolean)",
+					"Usage of the Task.ConfigureAwait method is forbidden in Acumatica because it may lead to deadlocks and a loss of Acumatica synchronization context." +
+					" It also prevents Acumatica request profiler and resource governor from correctly managing system resources.")
+				);
 	}
 }
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BannedApi/Sources/CallsToAsyncAPIs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PX.Objects.HackathonDemo
+{
+	public class Service
+	{
+		public static async Task<bool> RunParallelAsync()
+		{
+			var plinq = Enumerable.Range(1, 100).AsParallel().Sum();
+
+			Parallel.ForEach(Enumerable.Range(1, 100), i => Calculation(i));
+
+			var hotTaskGeneric = Task.Run(() => 100*100);
+			var sum = await hotTaskGeneric.ConfigureAwait(false);
+
+			var hotTaskNonGeneric = Task.Run(() => Calculation(100));
+			await hotTaskNonGeneric.ConfigureAwait(false);
+
+			var coldTaskGeneric = new Task<int>(() => 100 * 100);
+			coldTaskGeneric.Start();
+
+			await coldTaskGeneric.ConfigureAwait(false);
+
+			var coldTaskNonGeneric = new Task(() => Calculation(100));
+			coldTaskNonGeneric.Start(TaskScheduler.Current);
+
+			await coldTaskNonGeneric.ConfigureAwait(false);
+
+			return true;
+		}
+
+		private static void Calculation(int i)
+		{ }
+	}
+}
