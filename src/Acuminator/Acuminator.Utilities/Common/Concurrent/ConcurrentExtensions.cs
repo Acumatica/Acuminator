cb503770e3eab4cde2a0a2c72104a72d46e86142
Sergey Nikomarov <snikomarov@acumatica.com>
2026-05-08 17:06:47 +0200

    ATR-966: reworked and extended concurrent utils - added extensions for value tasks, added catching of all exceptions, not just operation cancelled exceptions

--- src/Acuminator/Acuminator.Utilities/Common/Concurrent/ConcurrentExtensions.cs
+++ src/Acuminator/Acuminator.Utilities/Common/Concurrent/ConcurrentExtensions.cs
@@ -35,18 +35,41 @@ namespace Acuminator.Utilities.Common
 		/// A Task extension method that attempts to await task which could be cancelled.
 		/// </summary>
 		/// <param name="task">The task to act on.</param>
+		/// <param name="continueOnCapturedContext">(Optional) True to continue on captured context.</param>
 		/// <returns/>      
-		public async static Task<bool> TryAwait(this Task? task)
+		public async static Task<bool> TryAwait(this Task? task, bool continueOnCapturedContext = false)
 		{
 			if (task == null || task.IsCanceled || task.IsFaulted)
 				return false;
 
 			try
 			{
-				await task.ConfigureAwait(false);
+				await task.ConfigureAwait(continueOnCapturedContext);
 				return true;
 			}
-			catch (OperationCanceledException)
+			catch (Exception exception)
+			{
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// A <see cref="ValueTask"/> extension method that attempts to await task which could be cancelled.
+		/// </summary>
+		/// <param name="task">The task to act on.</param>
+		/// <param name="continueOnCapturedContext">(Optional) True to continue on captured context.</param>
+		/// <returns/>
+		public async static ValueTask<bool> TryAwait(this ValueTask? task, bool continueOnCapturedContext = false)
+		{
+			if (task == null || task.Value.IsCanceled || task.Value.IsFaulted)
+				return false;
+
+			try
+			{
+				await task.Value.ConfigureAwait(continueOnCapturedContext);
+				return true;
+			}
+			catch (Exception exception)
 			{
 				return false;
 			}
@@ -57,18 +80,44 @@ namespace Acuminator.Utilities.Common
 		/// </summary>
 		/// <typeparam name="TResult">Type of the result.</typeparam>
 		/// <param name="task">The task to act on.</param>
+		/// <param name="continueOnCapturedContext">(Optional) True to continue on captured context.</param>
 		/// <returns/>      
-		public async static Task<TaskResult<TResult>> TryAwait<TResult>(this Task<TResult>? task)
+		public async static Task<TaskResult<TResult>> TryAwait<TResult>(this Task<TResult>? task,
+																		bool continueOnCapturedContext = false)
 		{
 			if (task == null || task.IsCanceled || task.IsFaulted)
 				return new TaskResult<TResult>(false, default);
 
 			try
 			{
-				TResult? result = await task.ConfigureAwait(false);
+				TResult? result = await task.ConfigureAwait(continueOnCapturedContext);
 				return new TaskResult<TResult>(true, result);
 			}
-			catch (OperationCanceledException)
+			catch (Exception exception)
+			{
+				return new TaskResult<TResult>(false, default);
+			}
+		}
+
+		/// <summary>
+		/// A <see cref="ValueTask{TResult}"/> extension method that attempts to await task which could be cancelled.
+		/// </summary>
+		/// <typeparam name="TResult">Type of the result.</typeparam>
+		/// <param name="task">The task to act on.</param>
+		/// <param name="continueOnCapturedContext">(Optional) True to continue on captured context.</param>
+		/// <returns/>
+		public async static ValueTask<TaskResult<TResult>> TryAwait<TResult>(this ValueTask<TResult>? task, 
+																			 bool continueOnCapturedContext = false)
+		{
+			if (task == null || task.Value.IsCanceled || task.Value.IsFaulted)
+				return new TaskResult<TResult>(false, default);
+
+			try
+			{
+				TResult? result = await task.Value.ConfigureAwait(continueOnCapturedContext);
+				return new TaskResult<TResult>(true, result);
+			}
+			catch (Exception exception)
 			{
 				return new TaskResult<TResult>(false, default);
 			}
