21221cb58130ea5c1d034c4e5f77453be071167f
Sergey Nikomarov <snikomarov@acumatica.com>
2026-05-12 08:22:53 +0200

    ATR-966: refactored tagger type enum into a separate file

--- src/Acuminator/Acuminator.Vsix/Coloriser/Base/PXTaggerBase.cs
+++ src/Acuminator/Acuminator.Vsix/Coloriser/Base/PXTaggerBase.cs
@@ -15,32 +15,6 @@ using Shell = Microsoft.VisualStudio.Shell;
 
 namespace Acuminator.Vsix.Coloriser
 {
-	/// <summary>
-	/// Values that represent tagger types.
-	/// </summary>
-	public enum TaggerType
-	{
-		/// <summary>
-		/// The general tagger which chooses other taggers according to the settings.
-		/// </summary>
-		General,
-
-		/// <summary>
-		/// The tagger based on Roslyn 
-		/// </summary>
-		Roslyn,
-
-		/// <summary>
-		/// The tagger based on regular expressions
-		/// </summary>
-		RegEx,
-
-		/// <summary>
-		/// The tagger used for outlining
-		/// </summary>
-		Outlining
-	};
-
 	public abstract class PXTaggerBase : IDisposable
 	{
 #pragma warning disable CS0067
---
+++ src/Acuminator/Acuminator.Vsix/Coloriser/Base/TaggerType.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+
+namespace Acuminator.Vsix.Coloriser
+{
+	/// <summary>
+	/// Values that represent tagger types.
+	/// </summary>
+	public enum TaggerType
+	{
+		/// <summary>
+		/// The general tagger which chooses other taggers according to the settings.
+		/// </summary>
+		General,
+
+		/// <summary>
+		/// The tagger based on Roslyn 
+		/// </summary>
+		Roslyn,
+
+		/// <summary>
+		/// The tagger based on regular expressions
+		/// </summary>
+		RegEx,
+
+		/// <summary>
+		/// The tagger used for outlining
+		/// </summary>
+		Outlining
+	};
+}
