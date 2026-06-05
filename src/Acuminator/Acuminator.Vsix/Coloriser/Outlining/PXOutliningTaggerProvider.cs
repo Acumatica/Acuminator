5d49238699d9b50213942b7c1f7dd7f038d040c0
Sergey Nikomarov <snikomarov@acumatica.com>
2026-06-05 16:33:34 +0200

    ATR-972: integrated new notification mechanism into the outlining tagger

--- src/Acuminator/Acuminator.Vsix/Coloriser/Outlining/PXOutliningTaggerProvider.cs
+++ src/Acuminator/Acuminator.Vsix/Coloriser/Outlining/PXOutliningTaggerProvider.cs
@@ -1,5 +1,4 @@
 ﻿#nullable enable
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
@@ -7,30 +6,36 @@ using System.Linq;
 
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
 
-namespace Acuminator.Vsix.Coloriser
+namespace Acuminator.Vsix.Coloriser;
+
+[ContentType("CSharp")]
+[TagType(typeof(IOutliningRegionTag))]
+[TextViewRole(PredefinedTextViewRoles.Document)]
+[Export(typeof(ITaggerProvider))]
+public class PXOutliningTaggerProvider : ITaggerProvider
 {
-	[ContentType("CSharp")]
-	[TagType(typeof(IOutliningRegionTag))]
-	[TextViewRole(PredefinedTextViewRoles.Document)]
-	[Export(typeof(ITaggerProvider))]
-	public class PXOutliningTaggerProvider : ITaggerProvider
+	private readonly ITextDocumentFactoryService _textDocumentFactory;
+
+	[ImportingConstructor]
+	public PXOutliningTaggerProvider(ITextDocumentFactoryService textDocumentFactory)
 	{
-		public ITagger<T>? CreateTagger<T>(ITextBuffer buffer) where T : ITag
+		_textDocumentFactory = textDocumentFactory;
+	}
+
+	public ITagger<T>? CreateTagger<T>(ITextBuffer buffer) where T : ITag
+	{
+		if (buffer == null || !ThreadHelper.CheckAccess())
+			return null;
+
+		PXOutliningTagger outliningTagger = buffer.Properties.GetOrCreateSingletonProperty(typeof(PXOutliningTagger), () =>
 		{
-			if (buffer == null || !ThreadHelper.CheckAccess())
-				return null;
+			return new PXOutliningTagger(buffer, _textDocumentFactory, subscribeToSettingsChanges: true, useCacheChecking: true);
+		});
 
-			PXOutliningTagger outliningTagger = buffer.Properties.GetOrCreateSingletonProperty(() =>
-			{
-				return new PXOutliningTagger(buffer, subscribeToSettingsChanges: true, useCacheChecking: true);
-			});
-
-			return outliningTagger as ITagger<T>;
-		}
+		return outliningTagger as ITagger<T>;
 	}
 }
